3c0ae27dc3df0ef600c2dac8522de51f8d9e0c0b
Jari Petays <jari.petays@iki.fi>
2021-12-09 13:50:00 +0200

    add LoggerConfigEditor to create default config

---
+++ Assets/Editor/Prg/Editors/LoggerConfigEditor.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using System.Text;
+using Prg.Scripts.Common.Util;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Prg.Editors
+{
+    [CustomEditor(typeof(LoggerConfig))]
+    public class LoggerConfigEditor : UnityEditor.Editor
+    {
+        private static readonly string[] ExcludedFolders =
+        {
+            "Assets/Photon",
+            "Assets/TextMesh Pro",
+        };
+
+        public override void OnInspectorGUI()
+        {
+            if (serializedObject.isEditingMultipleObjects)
+            {
+                DrawDefaultInspector();
+                return;
+            }
+            if (GUILayout.Button("Create Default Config"))
+            {
+                serializedObject.Update();
+                UpdateState(serializedObject);
+                serializedObject.ApplyModifiedProperties();
+            }
+            GUILayout.Space(20);
+            DrawDefaultInspector();
+        }
+
+        private static void UpdateState(SerializedObject serializedObject)
+        {
+            var loggerRules = serializedObject.FindProperty("_loggerRules");
+            var curValue = loggerRules.stringValue;
+            var newValue = LoadAssetFolders();
+            loggerRules.stringValue = newValue;
+        }
+
+        private static string LoadAssetFolders()
+        {
+            var folders = AssetDatabase.GetSubFolders("Assets");
+            var builder = new StringBuilder();
+            builder.Append(".*PhotonListener.*=1");
+            foreach (var folder in folders)
+            {
+                if (ExcludedFolders.Contains(folder))
+                {
+                    continue;
+                }
+                var line = folder.Replace("Assets/", "^");
+                line += ".*=0";
+                builder.AppendLine().Append(line);
+            }
+            return builder.ToString();
+        }
+    }
+}
