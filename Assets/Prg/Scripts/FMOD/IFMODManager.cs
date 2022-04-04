5c12ad3221faece04aed4609304ea2a08aa9943a
Miska <miskaharkonen@gmail.com>
2022-04-04 11:30:37 +0300

    Fmod from examples to main
    Moved Fmod from examples to main.
    Installed fmod plugin to main.

---
+++ Assets/Prg/Scripts/FMOD/IFMODManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prg.Scripts.Service.FMOD
+{
+    public interface IFMODManager
+    {
+
+        /// <summary>
+        /// Get instance of FMOD studiolistener. Create one if one doesn't exist
+        /// </summary>
+        void GetStudioListener();
+
+        void PlaySound(string name);
+
+        void StartBGM();
+
+        void StopBGM();
+
+        void SetBGMIntensity(int intensity);
+    }
+}
+
