66d597e3242a570d1ce5ee5d992b302649dadfd2
Miska <miskaharkonen@gmail.com>
2022-04-11 11:48:53 +0300

    Updated to FMODManager
    reworked FMODmanager script. Changed FMODManager script to use FMOD events to access all FMOD functionality. Added function to change audio based on shield level.
    Changed location of FMODManager from "Prg/Scripts/FMOD" to "Prg/Scripts/FMODManager".
    Added .gitignore for fmod_editor.log

---
+++ Assets/Prg/Scripts/FMODManager/FMODManager.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+namespace Prg.Scripts.FMODManager.FMODManager
+{
+    /// <summary>
+    /// AudioManager using FMOD
+    /// </summary>
+    public class FMODManager : MonoBehaviour, IFMODManager
+
+    {
+        public StudioListener studioListener = new StudioListener();
+        public StudioEventEmitter eventEmitter;
+        [EventRef]
+        public string ShieldStateEvent = "";
+        EventInstance shieldState;
+        [EventRef]
+        public string MusicStateEvent = "";
+        EventInstance musicState;
+
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void BeforeSceneLoad()
+        {
+
+            var fMODManager = FindObjectOfType<FMODManager>();
+            if (fMODManager == null)
+            {
+
+                UnityExtensions.CreateGameObjectAndComponent<FMODManager>(nameof(FMODManager), true);
+
+            }
+
+
+
+        }
+
+        public void Start()
+        {
+            //gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
+        }
+
+        public void GetStudioListener()
+        {
+            studioListener = FindObjectOfType<StudioListener>();
+            if (studioListener == null)
+            {
+
+                UnityExtensions.CreateGameObjectAndComponent<StudioListener>(nameof(StudioListener), true);
+
+            }
+
+
+        }
+
+
+        public void GetEventEmitter()
+        {
+            eventEmitter = GetComponent<FMODUnity.StudioEventEmitter>();
+            if (eventEmitter == null)
+            {
+
+                UnityExtensions.CreateGameObjectAndComponent<FMODUnity.StudioEventEmitter>(nameof(FMODUnity.StudioEventEmitter), true);
+
+            }
+
+        }
+
+        public void PlaySound(string name)
+        {
+            RuntimeManager.PlayOneShot(name);
+        }
+
+        public void StartBGM()
+        {
+            musicState = RuntimeManager.CreateInstance(MusicStateEvent);
+            musicState.start();
+        }
+
+        public void StopBGM()
+        {
+            musicState.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            musicState.release();
+        }
+
+        public void SetBGMIntensity(PARAMETER_ID id,float intensity, bool ignoreseekspeed)
+        {
+            musicState.setParameterByID(id, intensity);
+        }
+
+        public void SetShieldLevel(PARAMETER_ID id, float intensity, bool ignoreseekspeed)
+        {
+            shieldState.setParameterByID(id, intensity);
+        }
+    }
+
+}
---
+++ Assets/Prg/Scripts/FMODManager/IFMODManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prg.Scripts.FMODManager.FMODManager
+{
+    public interface IFMODManager
+    {
+
+        /// <summary>
+        /// Get instance of FMOD studiolistener. Create one if one doesn't exist.
+        /// </summary>
+        void GetStudioListener();
+
+        /// <summary>
+        /// Get instance of FMOD event emitter. Create one if one doesn't exist.
+        /// </summary>
+        void GetEventEmitter();
+
+        /// <summary>
+        /// Play sound once. Takes clip name as string "name".
+        /// </summary>
+        /// <param name="name"></param>
+        void PlaySound(string name);
+
+        /// <summary>
+        /// Start playing background music set to MusicEvent.
+        /// </summary>
+        void StartBGM();
+
+        /// <summary>
+        /// Stop playing background music set to MusicEvent and release instance from memory.
+        /// </summary>
+        void StopBGM();
+
+        /// <summary>
+        /// Change background music by intensity level. Takes FMOD PARAMETER_ID for intensity, float value for amount of intensity and
+        /// bool to ignore seek speed, bool is set to false by deafault.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="intensity"></param>
+        /// <param name="ignoreseekspeed"></param>
+        void SetBGMIntensity(FMOD.Studio.PARAMETER_ID id, float intensity, bool ignoreseekspeed);
+
+        void SetShieldLevel(FMOD.Studio.PARAMETER_ID id, float intensity, bool ignoreseekspeed);
+    }
+}
+
