69fc6005fc7a0657e95738f2da96a475a3b23849
Miska <miskaharkonen@gmail.com>
2022-05-28 05:03:52 +0300

    AudioTesting with FMOD 2.02
    Added audiotest component to 85-room-test-2 with background music
    Added FMOD event emitter to Ball in 85-room-test-2 to play collision effects
    Added "JazzBGM" event to FMOD.
    JazzBGM has MusicEvent parameter to control music modulation.
    Added "Transition" event to FMOD. It plays different collision effects.

---
+++ Assets/Prg/Scripts/FMOD/AudioTestScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prg.Scripts.FMODManager.AudioTestScript
+{
+    public class AudioTestScript : MonoBehaviour
+    {
+        //FMODManager.FMODManager _fMOD;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //_fMOD.StartBGM();
+
+
+        }
+    }
+
+}
+
