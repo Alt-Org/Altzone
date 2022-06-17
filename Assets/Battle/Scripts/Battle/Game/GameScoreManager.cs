5909d4d0568352b827b733794860b152a30ca0d6
Jari Petays <jari.petays@iki.fi>
2022-06-17 09:43:54 +0300

    add GameScoreManager stub implementation

---
+++ Assets/Battle/Scripts/Battle/Game/GameScoreManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using Altzone.Scripts.Battle;
+using Photon.Realtime;
+using Prg.Scripts.Common.Unity.Window;
+using Prg.Scripts.Common.Unity.Window.ScriptableObjects;
+using UnityEngine;
+
+namespace Battle.Scripts.Battle.Game
+{
+    /// <summary>
+    /// Collects local scores (from master client) and synchronizes them over network.
+    /// </summary>
+    public class GameScoreManager : MonoBehaviour
+    {
+        [Header("Settings"), SerializeField] private WindowDef _gameOverWindow;
+
+        public void ShowGameOverWindow(Room room, int winType, int winningTeam, int blueScore, int redScore)
+        {
+            PhotonBattle.SetRoomScores(room, winType, winningTeam, blueScore, redScore);
+            StartCoroutine(LoadGameOverWindow());
+        }
+        
+        private IEnumerator LoadGameOverWindow()
+        {
+            yield return null;
+            Debug.Log($"LoadGameOverWindow {_gameOverWindow}");
+            WindowManager.Get().ShowWindow(_gameOverWindow);
+        }
+    }
+}
