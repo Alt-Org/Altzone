a419b247eccf0a244fc3a49bcd844a6015e33478
Jari Petays <jari.petays@iki.fi>
2022-06-18 13:36:15 +0300

    add simple head and wall collision handling (just calculate scores)

--- Assets/Battle/Scripts/Battle/Game/GameScoreManager.cs
+++ Assets/Battle/Scripts/Battle/Game/GameScoreManager.cs
@@ -3,6 +3,7 @@ using Altzone.Scripts.Battle;
 using Photon.Realtime;
 using Prg.Scripts.Common.Unity.Window;
 using Prg.Scripts.Common.Unity.Window.ScriptableObjects;
+using UnityConstants;
 using UnityEngine;
 
 namespace Battle.Scripts.Battle.Game
@@ -14,12 +15,57 @@ namespace Battle.Scripts.Battle.Game
     {
         [Header("Settings"), SerializeField] private WindowDef _gameOverWindow;
 
+        [Header("Live Data"), SerializeField] private int _blueHeadScore;
+        [SerializeField] private int _blueWallScore;
+        [SerializeField] private int _redHeadScore;
+        [SerializeField] private int _redWallScore;
+
+        public void Reset()
+        {
+            _blueHeadScore = 0;
+            _blueWallScore = 0;
+            _redHeadScore = 0;
+            _redWallScore = 0;
+        }
+
+        public void OnHeadCollision(Collision2D collision)
+        {
+            var otherGameObject = collision.gameObject;
+            if (otherGameObject.CompareTag(Tags.RedTeam))
+            {
+                _blueHeadScore += 1;
+                return;
+            }
+            if (otherGameObject.CompareTag(Tags.BlueTeam))
+            {
+                _redHeadScore += 1;
+                return;
+            }
+            throw new UnityException($"invalid collision with {otherGameObject.name} {otherGameObject.tag}");
+        }
+
+        public void OnWallCollision(Collision2D collision)
+        {
+            var otherGameObject = collision.gameObject;
+            if (otherGameObject.CompareTag(Tags.RedTeam))
+            {
+                _blueWallScore += 1;
+                return;
+            }
+            if (otherGameObject.CompareTag(Tags.BlueTeam))
+            {
+                _redWallScore += 1;
+                return;
+            }
+            throw new UnityException($"invalid collision with {otherGameObject.name} {otherGameObject.tag}");
+        }
+
         public void ShowGameOverWindow(Room room, int winType, int winningTeam, int blueScore, int redScore)
         {
             PhotonBattle.SetRoomScores(room, winType, winningTeam, blueScore, redScore);
             StartCoroutine(LoadGameOverWindow());
         }
-        
+
         private IEnumerator LoadGameOverWindow()
         {
             yield return null;
--- Assets/Battle/Scripts/Battle/IGameScoreManager.cs
+++ Assets/Battle/Scripts/Battle/IGameScoreManager.cs
@@ -1,9 +1,16 @@
 using Photon.Realtime;
+using UnityEngine;
 
 namespace Battle.Scripts.Battle
 {
     internal interface IGameScoreManager
     {
+        void Reset();
+        
+        void OnHeadCollision(Collision2D collision);
+
+        void OnWallCollision(Collision2D collision);
+
         void ShowGameOverWindow(Room room, int winType, int winningTeam, int blueScore, int redScore);
     }
 }
