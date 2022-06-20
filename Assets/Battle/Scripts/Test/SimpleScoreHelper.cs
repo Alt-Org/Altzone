ac92cb549a715fcb158c4ad3824caa098b0dc12c
Jari Petays <jari.petays@iki.fi>
2022-06-20 07:41:48 +0300

    add SimpleScoreHelper to listen score changes and show them on the UI

---
+++ Assets/Battle/Scripts/Test/SimpleScoreHelper.cs
@@ -0,0 +1,34 @@
+using Altzone.Scripts.Battle;
+using Battle.Scripts.Battle;
+using TMPro;
+using UnityEngine;
+
+namespace Battle.Scripts.Test
+{
+    public class SimpleScoreHelper : MonoBehaviour
+    {
+        [Range(1, 2), SerializeField] private int _teamNumber;
+        [SerializeField] private TMP_Text _scoreText;
+
+        private IGameScoreManager _scoreManager;
+        
+        private void OnEnable()
+        {
+            _scoreManager = Context.GetGameScoreManager;
+        }
+
+        private void Update()
+        {
+            if (_teamNumber == PhotonBattle.TeamBlueValue)
+            {
+                var score = _scoreManager.BlueScore;
+                _scoreText.text = $"{score.x:0}-{score.y:0}";
+            }
+            else if (_teamNumber == PhotonBattle.TeamRedValue)
+            {
+                var score = _scoreManager.RedScore;
+                _scoreText.text = $"{score.x:0}-{score.y:0}";
+            }
+        }
+    }
+}
