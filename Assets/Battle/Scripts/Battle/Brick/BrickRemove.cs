0a4a1498743f2ee2ec904eb321179e34d613211f
Jari Petays <jari.petays@iki.fi>
2022-06-26 08:54:03 +0300

    add some san ity checks and remove debug logging

--- Assets/Battle/Scripts/Battle/Brick/BrickRemove.cs
+++ Assets/Battle/Scripts/Battle/Brick/BrickRemove.cs
@@ -7,25 +7,21 @@ namespace Battle.Scripts.Battle.Brick
     [RequireComponent(typeof(PhotonView))]
     public class BrickRemove : MonoBehaviour
     {
-        private int _viewId;
-
-        private void Awake()
-        {
-            _viewId = PhotonView.Get(this).ViewID;
-        }
-
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            if (!enabled)
+            {
+                return; // Collision events will be sent to disabled MonoBehaviours, to allow enabling Behaviours in response to collisions.
+            }
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                return;
+            }
             var otherGameObject = collision.gameObject;
             if (otherGameObject.CompareTag(Tags.Ball))
             {
                 PhotonNetwork.Destroy(gameObject);
             }
-        }
-
-        private void OnDestroy()
-        {
-            Debug.Log($"{name} view {_viewId}");
         }
     }
 }
