eda28421dfccca40340019768157889b909ef631
Jari Petays <jari.petays@iki.fi>
2022-06-26 10:13:54 +0300

    simplify SceneLoader and check always that scene exists

--- Assets/Prg/Scripts/Common/Unity/Window/SceneLoader.cs
+++ Assets/Prg/Scripts/Common/Unity/Window/SceneLoader.cs
@@ -1,4 +1,5 @@
 using Prg.Scripts.Common.Unity.Window.ScriptableObjects;
+using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 #if PHOTON_UNITY_NETWORKING
@@ -14,12 +15,28 @@ namespace Prg.Scripts.Common.Unity.Window
     {
         public static void LoadScene(WindowDef windowDef)
         {
+            int FindFirstSceneIndex(string sceneName)
+            {
+                var sceneCount = SceneManager.sceneCountInBuildSettings;
+                // scenePath = Assets/UiProto/Scenes/10-uiProto.unity
+                var unitySceneName = $"/{sceneName}.unity";
+                for (var index = 0; index < sceneCount; ++index)
+                {
+                    var scenePath = SceneUtility.GetScenePathByBuildIndex(index);
+                    if (scenePath.EndsWith(unitySceneName))
+                    {
+                        return index;
+                    }
+                }
+                throw new UnityException($"scene not found: {sceneName}");
+            }
             var scene = windowDef.Scene;
             Assert.IsNotNull(scene, "scene != null");
             Assert.IsFalse(string.IsNullOrEmpty(scene.SceneName), "string.IsNullOrEmpty(scene.SceneName)");
+            var sceneIndex = FindFirstSceneIndex(scene.SceneName);
             if (scene.IsNetworkScene)
             {
-                Debug.Log($"LoadScene NETWORK {scene.SceneName}", windowDef);
+                Debug.Log($"NETWORK {scene.SceneName} ({sceneIndex})", windowDef);
 #if PHOTON_UNITY_NETWORKING
                 PhotonNetwork.LoadLevel(scene.SceneName);
                 return;
@@ -27,23 +44,8 @@ namespace Prg.Scripts.Common.Unity.Window
                 throw new UnityException("PHOTON_UNITY_NETWORKING not available");
 #endif
             }
-
-            var sceneCount = SceneManager.sceneCountInBuildSettings;
-            // scenePath = Assets/UiProto/Scenes/10-uiProto.unity
-            var unitySceneNameMatcher = $"/{scene.SceneName}.unity";
-            for (var index = 0; index < sceneCount; ++index)
-            {
-                var scenePath = SceneUtility.GetScenePathByBuildIndex(index);
-                if (scenePath.EndsWith(unitySceneNameMatcher))
-                {
-                    Debug.Log($"LoadScene LOCAL {scene.SceneName} ({index})", windowDef);
-                    SceneManager.LoadScene(index);
-                    return;
-                }
-            }
-            var firstScenePath = SceneUtility.GetScenePathByBuildIndex(0);
-            Debug.LogWarning($"LoadScene LOCAL FALLBACK {firstScenePath} (0)");
-            SceneManager.LoadScene(0);
+            Debug.Log($"LOCAL {scene.SceneName} ({sceneIndex})", windowDef);
+            SceneManager.LoadScene(sceneIndex);
         }
     }
 }
