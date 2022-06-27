b33bf213db71b6be10d5961f6b7a1df760d4128d
Jari Petays <jari.petays@iki.fi>
2022-06-27 17:50:28 +0300

    hide scores if not master client

--- Assets/Battle/Scripts/Test/SimpleScoreHelper.cs
+++ Assets/Battle/Scripts/Test/SimpleScoreHelper.cs
@@ -1,5 +1,6 @@
 using Altzone.Scripts.Battle;
 using Battle.Scripts.Battle;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
@@ -11,14 +12,19 @@ namespace Battle.Scripts.Test
         [SerializeField] private TMP_Text _scoreText;
 
         private IGameScoreManager _scoreManager;
-        
+
         private void OnEnable()
         {
             _scoreManager = Context.GetGameScoreManager;
+            _scoreText.text = string.Empty;
         }
 
         private void Update()
         {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                return;
+            }
             if (_teamNumber == PhotonBattle.TeamBlueValue)
             {
                 var score = _scoreManager.BlueScore;
