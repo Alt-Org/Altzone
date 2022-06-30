1ed7fa09372d78bc74f9c748973c8eaea26e9fd8
Jari Petays <jari.petays@iki.fi>
2022-06-30 15:12:29 +0300

    add positions for primary (blue) and alternate (red) "Raid UI"

--- Assets/Raid/Scripts/RaidPosition.cs
+++ Assets/Raid/Scripts/RaidPosition.cs
@@ -6,8 +6,14 @@ namespace Raid.Scripts
     public class RaidPosition : MonoBehaviour
     {
         [Header("Settings"), SerializeField] private Collider2D _areaCollider;
+        [SerializeField] private Transform _primaryRaidPosition;
+        [SerializeField] private Transform _alternateRaidPosition;
 
         [Header("Live Data"), SerializeField] private Bounds _areaBounds;
+
+        public Transform PrimaryRaidPosition => _primaryRaidPosition;
+
+        public Transform AlternateRaidPosition => _alternateRaidPosition;
 
         public Bounds GetRaidArea => _areaBounds;
 
