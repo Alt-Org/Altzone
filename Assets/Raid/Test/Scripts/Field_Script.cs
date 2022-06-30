ae18d7bd827f0a3df526c1fbaf4afcb7ca656a26
Pugnantiz <62379790+Pugnantiz@users.noreply.github.com>
2022-06-30 23:11:22 +0300

    2D Raid Testi + 2D Tilemap editor package + Tilemap
    Lisätty 2D Tilemap editor package, 2D raid testi sceneen grid ja Tilemap, sekä tile palette, ja tile paletteen Unityn omasta hexagon spritesta kopiot eri väreillä kuvaamaan pommien numeroita (sin-1, vihr-2, pun-3, turk-4, tum.pun-5, viol-6). Ikonit lisätään tarvittaessa myöhemmin.

--- Assets/Raid/Test/Scripts/Field_Script.cs
+++ Assets/Raid/Test/Scripts/Field_Script.cs
@@ -5,7 +5,26 @@ using UnityEngine.Tilemaps;
 
 public class Field_Script : MonoBehaviour
 {
-    //public Tilemap tilemap;
+    public Tilemap tilemap { get; private set; }
+
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    public void Draw(Hexa_struct[,] state)
+    {
+        int width = state.GetLength(0);
+        int height = state.GetLength(1);
+
+        for(int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Hexa_struct hexa = state[x, y];
+            }
+        }      
+    }
 
     public Tile tileUnrevealed;
     public Tile tileBomb;
@@ -16,6 +35,4 @@ public class Field_Script : MonoBehaviour
     public Tile tileNumber4;
     public Tile tileNumber5;
     public Tile tileNumber6;
-
-
 }
