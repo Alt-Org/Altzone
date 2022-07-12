c8690c0a64f719bc21bca6059653a63fa9e10eb4
Jari Petays <jari.petays@iki.fi>
2022-07-12 19:31:43 +0300

    add some stats for character models table

--- Assets/Editor/Altzone/MenuModels.cs
+++ Assets/Editor/Altzone/MenuModels.cs
@@ -12,6 +12,10 @@ namespace Editor.Altzone
             ModelLoader.LoadAndClearModels();
             var models = Models.GetAll<CharacterModel>();
             var builder = new StringBuilder();
+            var speedStats = new Stats();
+            var resistanceStats = new Stats();
+            var attackStats = new Stats();
+            var defenceStats = new Stats();
             builder
                 .Append("| Nimi | Defenssi | Speed | Resistance | Attack | Defence |").AppendLine()
                 .Append("|:---- |:-------- |:-----:|:----------:|:------:|:-------:|").AppendLine();
@@ -19,8 +23,37 @@ namespace Editor.Altzone
             {
                 builder
                     .Append($"| {m.Name} | {m.MainDefence} | {m.Speed} | {m.Resistance} | {m.Attack} | {m.Defence} |").AppendLine();
+                speedStats.Add(m.Speed);
+                resistanceStats.Add(m.Resistance);
+                attackStats.Add(m.Attack);
+                defenceStats.Add(m.Defence);
             }
+            builder
+                .Append($"| Min | &nbsp; | {speedStats.Min} | {resistanceStats.Min} | {attackStats.Min} | {defenceStats.Min} |").AppendLine();
+            builder
+                .Append($"| Max | &nbsp; | {speedStats.Max} | {resistanceStats.Max} | {attackStats.Max} | {defenceStats.Max} |").AppendLine();
+            builder
+                .Append($"| Delta | &nbsp; | {speedStats.Delta} | {resistanceStats.Delta} | {attackStats.Delta} | {defenceStats.Delta} |").AppendLine();
             Debug.Log($"(needs to scroll down to see the table)\r\nModels\r\n{builder}");
+        }
+
+        private class Stats
+        {
+            public int Min { get; private set; } = int.MaxValue;
+            public int Max { get; private set; } = int.MinValue;
+            public int Delta => Max - Min;
+
+            public void Add(int value)
+            {
+                if (value < Min)
+                {
+                    Min = value;
+                }
+                if (value > Max)
+                {
+                    Max = value;
+                }
+            }
         }
     }
 }
