6c255e0a305c6de1b5dc749027e9d08cfe0fdb6a
Jari Petays <jari.petays@iki.fi>
2022-07-14 17:15:56 +0300

    add extension method IBattleTeam.GetBallDropPositionAndDirection

--- Assets/Battle/Scripts/Battle/Game/IBattleTeam.cs
+++ Assets/Battle/Scripts/Battle/Game/IBattleTeam.cs
@@ -16,4 +16,45 @@ namespace Battle.Scripts.Battle.Game
         
         void SetPlayMode(BattlePlayMode playMode);
     }
+    
+    internal static class BattleTeamExtensions
+    {
+        public static void GetBallDropPositionAndDirection(this IBattleTeam startTeam, out Vector2 ballDropPosition, out Vector2 direction)
+        {
+            if (startTeam == null)
+            {
+                do
+                {
+                    direction = Random.insideUnitCircle;
+                } while (direction.x == 0 || direction.y == 0);
+
+                ballDropPosition = Vector2.zero;
+                return;
+            }
+            var center = Vector3.zero;
+
+            var transform1 = startTeam.FirstPlayer.PlayerTransform;
+            var pos1 = transform1.position;
+            if (startTeam.PlayerCount == 1)
+            {
+                direction = center - pos1;
+                ballDropPosition = pos1;
+                return;
+            }
+            var transform2 = startTeam.SecondPlayer.PlayerTransform;
+            var pos2 = transform2.position;
+            var dist1 = Mathf.Abs((pos1 - center).sqrMagnitude);
+            var dist2 = Mathf.Abs((pos2 - center).sqrMagnitude);
+            if (dist1 < dist2)
+            {
+                direction = pos1 - pos2;
+                ballDropPosition = pos1;
+            }
+            else
+            {
+                direction = pos2 - pos1;
+                ballDropPosition = pos2;
+            }
+        }
+    }
 }
--- Assets/Battle/Scripts/Test/StartTheBallTest.cs
+++ Assets/Battle/Scripts/Test/StartTheBallTest.cs
@@ -236,7 +236,7 @@ namespace Battle.Scripts.Test
                           $"TEAM {startingTeam} players {startTeam.PlayerCount} speed {speed:0.00}");
             }
 
-            GetDirectionAndPosition(startTeam, out var direction, out var ballDropPosition);
+            startTeam.GetBallDropPositionAndDirection(out var ballDropPosition, out var direction);
 
             Debug.Log($"{name} speed {speed:0.00} direction {direction.normalized} position {ballDropPosition}");
             if (_forceSpeedOverride > 0)
@@ -268,44 +268,6 @@ namespace Battle.Scripts.Test
             startDistance = blueDistance;
             otherDistance = redDistance;
             return PhotonBattle.TeamBlueValue;
-        }
-
-        private static void GetDirectionAndPosition(IBattleTeam startTeam, out Vector2 direction, out Vector2 ballDropPosition)
-        {
-            if (startTeam == null)
-            {
-                do
-                {
-                    direction = Random.insideUnitCircle;
-                } while (direction.x == 0 || direction.y == 0);
-
-                ballDropPosition = Vector2.zero;
-                return;
-            }
-            var center = Vector3.zero;
-
-            var transform1 = startTeam.FirstPlayer.PlayerTransform;
-            var pos1 = transform1.position;
-            if (startTeam.PlayerCount == 1)
-            {
-                direction = center - pos1;
-                ballDropPosition = pos1;
-                return;
-            }
-            var transform2 = startTeam.SecondPlayer.PlayerTransform;
-            var pos2 = transform2.position;
-            var dist1 = Mathf.Abs((pos1 - center).sqrMagnitude);
-            var dist2 = Mathf.Abs((pos2 - center).sqrMagnitude);
-            if (dist1 < dist2)
-            {
-                direction = pos1 - pos2;
-                ballDropPosition = pos1;
-            }
-            else
-            {
-                direction = pos2 - pos1;
-                ballDropPosition = pos2;
-            }
         }
     }
 }
