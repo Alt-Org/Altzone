c6582fa16f3abbd3e9bfe908069b53d29c993ed6
WilllRU <willy.rueter@gmail.com>
2022-07-19 12:51:51 +0300

    Replace LateUpdate with coroutine

--- Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
+++ Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Prg.Scripts.Common.PubSub;
 using UnityEngine;
 
@@ -30,28 +31,33 @@ namespace Battle.Scripts.Ui
             this.Unsubscribe();
         }
 
-        private void LateUpdate()
+        private IEnumerator UpdateSling()
         {
-            // This'll need to be improved upon, and might be replaced with a coroutine
-            if(_playerIsOn)
+            _playerIsOn = true;
+            _lineObject.SetActive(true);
+
+            while (_playerIsOn)
             {
                 _line.SetPosition(0, _playerTransform.position);
+                yield return null;
             }
+
+            _lineObject.SetActive(false);
         }
+            
 
         private void SlingStart(UiEvents.SlingshotStart start)
         {
             // This needs to be re-done, This only works if there is at least a player
             // that occupies the first position on the list
             _playerTransform = start.TeamTrackers[0].Player1Transform;
-            _playerIsOn = true;
-            _lineObject.SetActive(true);
+            StartCoroutine(UpdateSling());
         }
 
         private void SlingEnd(UiEvents.SlingshotEnd end)
         {
             _playerIsOn = false;
-            _lineObject.SetActive(false);
+            
         }
     }
 }
