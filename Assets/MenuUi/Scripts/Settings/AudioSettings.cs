492fdf647bbaa6b90023c298bb173a961f093b37
WilllRU <willy.rueter@gmail.com>
2022-08-03 10:08:05 +0300

    Add AudioMenuSettings

---
+++ Assets/MenuUi/Scripts/Settings/AudioSettings.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Altzone.Scripts.Service.Audio;
+
+namespace MenuUi.Scripts.Settings
+{
+    public class AudioMenuSettings : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+        public void SetMasterLevel(float sliderValue)
+        {
+
+        }
+
+
+
+    }
+
+}
