11258471e907e3fc0f86ea28421913b3da8c5874
Jari Petays <jari.petays@iki.fi>
2022-08-09 08:29:28 +0300

    clean up code and prefab, fix SlingStart event handler to find correct team for slingshot tracking

--- Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
+++ Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using Battle.Scripts.Battle;
 using Battle.Scripts.Battle.Game;
-
 using Prg.Scripts.Common.PubSub;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -10,19 +9,22 @@ namespace Battle.Scripts.Ui
 {
     internal class SlingshotTrajectory : MonoBehaviour
     {
+        [Header("Team Settings"), SerializeField] private int _teamNumber;
 
-        [Header("Team"), SerializeField] private int _teamNum;
-        [Header("Line"), SerializeField] private GameObject _lineObject;
+        [Header("Line Settings"), SerializeField] private GameObject _lineObject;
+        [SerializeField] private float _lineLengthMultiplier = 2.0f;
+        
         private LineRenderer _line;
-        private bool _playerIsOn = false;
+        private bool _playerIsOn;
         private IPlayerManager _playerManager;
-        [SerializeField] private float _lineSize = 2.0f;
-
 
         private void Awake()
         {
+            Debug.Log($"{name} team {_teamNumber}");
             _playerManager = FindObjectOfType<PlayerManager>();
+            Assert.IsNotNull(_playerManager, "_playerManager != null");
             _line = _lineObject.GetComponent<LineRenderer>();
+            Assert.IsNotNull(_line, "_line != null");
             _line.SetPosition(0, new Vector2(0f, 0f));
             _line.SetPosition(1, new Vector2(0f, 0f));
         }
@@ -38,11 +40,9 @@ namespace Battle.Scripts.Ui
             this.Unsubscribe();
         }
 
-        private IEnumerator UpdateSling(int num)
+        private IEnumerator UpdateSling(IBattleTeam battleTeam)
         {
-            IBattleTeam startTeam = _playerManager.GetBattleTeam(num);
             _playerIsOn = true;
-            
             _lineObject.SetActive(true);
 
             while (_playerIsOn)
@@ -50,30 +50,30 @@ namespace Battle.Scripts.Ui
                 // Because of how this is setup, the line of that shows where the ball will be shot,
                 // changes according to which player will shoot the ball according to the conditions
                 // set within the GetBallDropPositionAndDirection()
-                startTeam.GetBallDropPositionAndDirection(out var ball, out var dir);
-                _line.SetPosition(0, ball);
-                _line.SetPosition(1, ball + (dir * _lineSize));
+                battleTeam.GetBallDropPositionAndDirection(out var dropPosition, out var direction);
+                _line.SetPosition(0, dropPosition);
+                _line.SetPosition(1, dropPosition + (direction * _lineLengthMultiplier));
                 yield return null;
             }
 
             _lineObject.SetActive(false);
         }
-            
 
         private void SlingStart(UiEvents.SlingshotStart start)
         {
-            // Prevents an index error and limits the amount of lines rendered when the
-            // ball is shot by one team (When the ball hits a player and not their shield)
-            if (start.TeamTrackers.Count > _teamNum)
+            var team = start.TeamTrackers.Find(x => x.TeamNumber == _teamNumber);
+            Debug.Log($"{name} team {_teamNumber} found {team}");
+            if (team == null || team.PlayerCount == 0)
             {
-                StartCoroutine(UpdateSling(start.TeamTrackers[_teamNum].TeamNumber));
+                return;
             }
+            var battleTeam = _playerManager.GetBattleTeam(_teamNumber);
+            StartCoroutine(UpdateSling(battleTeam));
         }
 
         private void SlingEnd(UiEvents.SlingshotEnd end)
         {
             _playerIsOn = false;
-            
         }
     }
 }
