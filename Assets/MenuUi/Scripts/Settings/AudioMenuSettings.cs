44adfadc6c74634f35198b857b78f595d96bd06a
WilllRU <willy.rueter@gmail.com>
2022-08-10 14:44:32 +0300

    Change AudioMenuSettings.cs

--- Assets/MenuUi/Scripts/Settings/AudioMenuSettings.cs
+++ Assets/MenuUi/Scripts/Settings/AudioMenuSettings.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Assertions;
 using Altzone.Scripts.Service.Audio;
 
 namespace MenuUi.Scripts.Settings
@@ -13,24 +14,41 @@ namespace MenuUi.Scripts.Settings
 
         void Awake()
         {
-
+            if (!PlayerPrefs.HasKey("PlayerMasterVolume"))
+            {
+                MakeVolumePrefs();
+            }
+            _master.value = PlayerPrefs.GetFloat("PlayerMasterVolume");
+            _menuSFX.value = PlayerPrefs.GetFloat("PlayerMenuSFXVolume");
+            _gameMusic.value = PlayerPrefs.GetFloat("PlayerGameMusicVolume");
+            _gameSFX.value = PlayerPrefs.GetFloat("PlayerGameSFXVolume");
         }
         public void SetMasterLevel(float sliderValue)
         {
-
+            PlayerPrefs.SetFloat("PlayerMasterVolume", sliderValue);
         }
         public void SetMenuSFXLevel(float sliderValue)
         {
-
+            PlayerPrefs.SetFloat("PlayerMenuSFXVolume", sliderValue);
         }
         public void SetMusicLevel(float sliderValue)
         {
-
+            PlayerPrefs.SetFloat("PlayerGameMusicVolume", sliderValue);
         }
         public void SetGameSFXLevel(float sliderValue)
         {
-
+            PlayerPrefs.SetFloat("PlayerGameSFXVolume", sliderValue);
         }
+
+        private void MakeVolumePrefs()
+        {
+            float defaultVolume = 1.0f;
+            PlayerPrefs.SetFloat("PlayerMasterVolume", defaultVolume);
+            PlayerPrefs.SetFloat("PlayerMenuSFXVolume", defaultVolume);
+            PlayerPrefs.SetFloat("PlayerGameMusicVolume", defaultVolume);
+            PlayerPrefs.SetFloat("PlayerGameSFXVolume", defaultVolume);
+        }
+
     }
 
 }
