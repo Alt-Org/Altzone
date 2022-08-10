78f1e6b3446de73fadd0563441053add6139373a
Jari Petays <jari.petays@iki.fi>
2022-08-10 11:11:55 +0300

    add PhotonView to SlingshotTrajectory for network RPC

--- Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
+++ Assets/Battle/Scripts/Ui/SlingshotTrajectory.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using Battle.Scripts.Battle;
 using Battle.Scripts.Battle.Game;
+using Photon.Pun;
 using Prg.Scripts.Common.PubSub;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace Battle.Scripts.Ui
 {
+    /// <summary>
+    /// Utility class to visualize "slingshot" between one or two team member(s).
+    /// </summary>
+    [RequireComponent(typeof(PhotonView))]
     internal class SlingshotTrajectory : MonoBehaviour
     {
         [Header("Team Settings"), SerializeField] private int _teamNumber;
 
         [Header("Line Settings"), SerializeField] private GameObject _lineObject;
         [SerializeField] private float _lineLengthMultiplier = 2.0f;
-        
+
+        private PhotonView _photonView;
         private LineRenderer _line;
         private bool _playerIsOn;
         private IPlayerManager _playerManager;
@@ -21,12 +27,12 @@ namespace Battle.Scripts.Ui
         private void Awake()
         {
             Debug.Log($"{name} team {_teamNumber}");
+            _photonView = PhotonView.Get(this);
             _playerManager = FindObjectOfType<PlayerManager>();
             Assert.IsNotNull(_playerManager, "_playerManager != null");
             _line = _lineObject.GetComponent<LineRenderer>();
             Assert.IsNotNull(_line, "_line != null");
-            _line.SetPosition(0, new Vector2(0f, 0f));
-            _line.SetPosition(1, new Vector2(0f, 0f));
+            HideSling();
         }
 
         private void OnEnable()
@@ -40,35 +46,21 @@ namespace Battle.Scripts.Ui
             this.Unsubscribe();
         }
 
-        private IEnumerator UpdateSling(IBattleTeam battleTeam)
-        {
-            _playerIsOn = true;
-            _lineObject.SetActive(true);
-
-            while (_playerIsOn)
-            {
-                // Because of how this is setup, the line of that shows where the ball will be shot,
-                // changes according to which player will shoot the ball according to the conditions
-                // set within the GetBallDropPositionAndDirection()
-                battleTeam.GetBallDropPositionAndDirection(out var dropPosition, out var direction);
-                _line.SetPosition(0, dropPosition);
-                _line.SetPosition(1, dropPosition + (direction * _lineLengthMultiplier));
-                yield return null;
-            }
-
-            _lineObject.SetActive(false);
-        }
+        #region Event handlers
 
         private void SlingStart(UiEvents.SlingshotStart start)
         {
             var team = start.TeamTrackers.Find(x => x.TeamNumber == _teamNumber);
-            Debug.Log($"{name} team {_teamNumber} found {team}");
             if (team == null || team.PlayerCount == 0)
             {
                 return;
             }
-            var battleTeam = _playerManager.GetBattleTeam(_teamNumber);
-            StartCoroutine(UpdateSling(battleTeam));
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                return;
+            }
+            Debug.Log($"{name} team {_teamNumber} found {team}");
+            _photonView.RPC(nameof(SlingStartRpc), RpcTarget.All, team.TeamNumber);
         }
 
         private void SlingEnd(UiEvents.SlingshotEnd end)
@@ -82,7 +74,71 @@ namespace Battle.Scripts.Ui
             {
                 return;
             }
-            _playerIsOn = false;
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                return;
+            }
+            Debug.Log($"{name} team {_teamNumber} found {team}");
+            _photonView.RPC(nameof(SlingEndRpc), RpcTarget.All, team.TeamNumber);
         }
+
+        #endregion
+
+        #region Sling implementation
+
+        private void ShowSling()
+        {
+            var battleTeam = _playerManager.GetBattleTeam(_teamNumber);
+            if (battleTeam == null)
+            {
+                // No can do if team has exited.
+                return;
+            }
+            _playerIsOn = true;
+            _lineObject.SetActive(true);
+            StartCoroutine(UpdateSling(battleTeam));
+        }
+
+        private void HideSling()
+        {
+            _playerIsOn = false;
+            _line.SetPosition(0, Vector2.zero);
+            _line.SetPosition(1, Vector2.zero);
+            _lineObject.SetActive(false);
+        }
+
+        private IEnumerator UpdateSling(IBattleTeam battleTeam)
+        {
+            while (_playerIsOn)
+            {
+                // Because of how this is setup, the line of that shows where the ball will be shot,
+                // changes according to which player will shoot the ball according to the conditions
+                // set within the GetBallDropPositionAndDirection()
+                battleTeam.GetBallDropPositionAndDirection(out var dropPosition, out var direction);
+                _line.SetPosition(0, dropPosition);
+                _line.SetPosition(1, dropPosition + (direction * _lineLengthMultiplier));
+                yield return null;
+            }
+        }
+
+        #endregion
+
+        #region Photon RPC
+
+        [PunRPC]
+        private void SlingStartRpc(int teamNumber)
+        {
+            Assert.AreEqual(_teamNumber, teamNumber);
+            ShowSling();
+        }
+
+        [PunRPC]
+        private void SlingEndRpc(int teamNumber)
+        {
+            Assert.AreEqual(_teamNumber, teamNumber);
+            HideSling();
+        }
+
+        #endregion
     }
 }
