2c01e1e6b44b3f3df76c71ba576e0107cbf97039
Jari Petays <jari.petays@iki.fi>
2022-08-18 09:46:10 +0300

    add AudioManager service with simple and most stupid (KISS) UNITY implementation

--- Assets/Altzone/Scripts/Service/Audio/IAudioManager.cs
+++ Assets/Altzone/Scripts/Service/Audio/IAudioManager.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Altzone.Scripts.Service.Audio
 {
     /// <summary>
@@ -9,5 +11,17 @@ namespace Altzone.Scripts.Service.Audio
         float MenuEffectsVolume { get; set; }
         float GameEffectsVolume { get; set; }
         float GameMusicVolume { get; set; }
+
+        void RegisterAudioClip(string audioClipName, AudioClip audioClip);
+        
+        void PlayMenuEffect(string audioClipName);
+        void PlayGameEffect(string audioClipName);
+        void PlayGameEffect(string audioClipName, Vector3 worldPosition);
+        void PlayGameMusic(string audioClipName);
+
+        void PlayMenuEffect(AudioClip audioClip);
+        void PlayGameEffect(AudioClip audioClip);
+        void PlayGameEffect(AudioClip audioClip, Vector3 worldPosition);
+        void PlayGameMusic(AudioClip audioClip);
     }
 }
--- Assets/Altzone/Scripts/Service/Audio/UnityAudioManager.cs
+++ Assets/Altzone/Scripts/Service/Audio/UnityAudioManager.cs
@@ -1,31 +1,125 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Altzone.Scripts.Config;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Altzone.Scripts.Service.Audio
 {
+    /// <summary>
+    /// AudioManager service with simple and most stupid (KISS) UNITY implementation.
+    /// </summary>
     public class UnityAudioManager : MonoBehaviour, IAudioManager
     {
+        private const float DefaultVolume = 1.0f;
+
+        [Header("Live Data"), SerializeField] private float _masterVolume;
+        [SerializeField] private float _menuEffectsVolume;
+        [SerializeField] private float _gameEffectsVolume;
+        [SerializeField] private float _gameMusicVolume;
+
+        private readonly Dictionary<string, AudioClip> _audioClips = new();
+
         float IAudioManager.MasterVolume
         {
-            get => throw new System.NotImplementedException();
-            set => throw new System.NotImplementedException();
+            get => _masterVolume;
+            set => _masterVolume = value;
         }
 
         float IAudioManager.MenuEffectsVolume
         {
-            get => throw new System.NotImplementedException();
-            set => throw new System.NotImplementedException();
+            get => _menuEffectsVolume;
+            set => _menuEffectsVolume = value;
         }
 
         float IAudioManager.GameEffectsVolume
         {
-            get => throw new System.NotImplementedException();
-            set => throw new System.NotImplementedException();
+            get => _gameEffectsVolume;
+            set => _gameEffectsVolume = value;
         }
 
         float IAudioManager.GameMusicVolume
         {
-            get => throw new System.NotImplementedException();
-            set => throw new System.NotImplementedException();
+            get => _gameMusicVolume;
+            set => _gameMusicVolume = value;
+        }
+
+        private void Awake()
+        {
+            _masterVolume = PlayerPrefs.GetFloat(PlayerPrefKeys.MasterVolume, DefaultVolume);
+            _menuEffectsVolume = PlayerPrefs.GetFloat(PlayerPrefKeys.MenuSfxVolume, DefaultVolume);
+            _gameEffectsVolume = PlayerPrefs.GetFloat(PlayerPrefKeys.GameSfxVolume, DefaultVolume);
+            _gameMusicVolume = PlayerPrefs.GetFloat(PlayerPrefKeys.GameMusicVolume, DefaultVolume);
+        }
+
+        void IAudioManager.RegisterAudioClip(string audioClipName, AudioClip audioClip)
+        {
+            Assert.IsFalse(_audioClips.ContainsKey(audioClipName), "_audioClips.ContainsKey(audioClipName)");
+            _audioClips.Add(audioClipName, audioClip);
+        }
+
+        void IAudioManager.PlayMenuEffect(string audioClipName)
+        {
+            Assert.IsTrue(_audioClips.ContainsKey(audioClipName), "_audioClips.ContainsKey(audioClipName)");
+            ((IAudioManager)this).PlayMenuEffect(_audioClips[audioClipName]);
+        }
+
+        void IAudioManager.PlayGameEffect(string audioClipName)
+        {
+            Assert.IsTrue(_audioClips.ContainsKey(audioClipName), "_audioClips.ContainsKey(audioClipName)");
+            ((IAudioManager)this).PlayGameEffect(_audioClips[audioClipName]);
+        }
+
+        void IAudioManager.PlayGameEffect(string audioClipName, Vector3 worldPosition)
+        {
+            Assert.IsTrue(_audioClips.ContainsKey(audioClipName), "_audioClips.ContainsKey(audioClipName)");
+            ((IAudioManager)this).PlayGameEffect(_audioClips[audioClipName], worldPosition);
+        }
+
+        void IAudioManager.PlayGameMusic(string audioClipName)
+        {
+            Assert.IsTrue(_audioClips.ContainsKey(audioClipName), "_audioClips.ContainsKey(audioClipName)");
+            ((IAudioManager)this).PlayGameMusic(_audioClips[audioClipName]);
+        }
+
+        void IAudioManager.PlayMenuEffect(AudioClip audioClip)
+        {
+            PlayAudio(audioClip, _menuEffectsVolume * _masterVolume);
+        }
+
+        void IAudioManager.PlayGameEffect(AudioClip audioClip)
+        {
+            PlayAudio(audioClip, _gameEffectsVolume * _masterVolume);
+        }
+
+        void IAudioManager.PlayGameEffect(AudioClip audioClip, Vector3 worldPosition)
+        {
+            PlayAudio(audioClip, _gameEffectsVolume * _masterVolume, worldPosition);
+        }
+
+        void IAudioManager.PlayGameMusic(AudioClip audioClip)
+        {
+            PlayAudio(audioClip, _gameMusicVolume * _masterVolume);
+        }
+
+        private static void PlayAudio(AudioClip clip, float volume, [CallerMemberName] string memberName = null)
+        {
+            if (!(volume > Mathf.Epsilon))
+            {
+                return;
+            }
+            Debug.Log($"{memberName} {clip.name} {volume}");
+            AudioSource.PlayClipAtPoint(clip, Vector3.zero, volume);
+        }
+
+        private static void PlayAudio(AudioClip clip, float volume, Vector3 position, [CallerMemberName] string memberName = null)
+        {
+            if (!(volume > 0))
+            {
+                return;
+            }
+            Debug.Log($"{memberName} {clip.name} {volume} {position}");
+            AudioSource.PlayClipAtPoint(clip, position, volume);
         }
     }
 }
