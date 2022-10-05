d4ab02a07d59533e5949c452323f583a82647c16
Rikuliikane <114590114+Rikuliikane@users.noreply.github.com>
2022-10-05 13:12:41 +0300

    DefenssiMuokkaus

---
+++ Assets/MenuUi/Scripts/Pie.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pie : MonoBehaviour
+{
+   public Image[] imagesPieChart;
+   public float[] values;
+    void Start()
+    {
+        SetValues(values);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void SetValues(float[] valuesToSet)
+    {
+        float totalValues = 0;
+        for(int i = 0; i < imagesPieChart.Length; i++)
+        {
+            totalValues += FindPercentage(valuesToSet, i);
+            imagesPieChart[i].fillAmount = totalValues;
+        }
+    }
+
+    private float FindPercentage(float[] valueToSet, int index)
+    {
+        float totalAmount = 0;
+        for(int i = 0; i < valueToSet.Length; i++)
+        {
+            totalAmount += valueToSet[i];
+        }
+        return valueToSet[index] / totalAmount;
+    }
+}
