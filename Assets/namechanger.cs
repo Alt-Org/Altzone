7d836ffcf032716c9cd552e03d238deb1b2da8b0
Samuel <lindstromsamppa2@gmail.com>
2022-10-11 15:45:22 +0300

    Updating character gallery.

---
+++ Assets/namechanger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class namechanger : MonoBehaviour
+{
+   public string theName;
+   public GameObject inputField;
+   public GameObject textDisplay;
+
+}
