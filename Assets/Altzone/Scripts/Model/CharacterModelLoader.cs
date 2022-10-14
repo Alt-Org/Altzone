409caf55f34fed416a7c3cb42d6066cab7287cf9
Jari Petays <jari.petays@iki.fi>
2022-10-14 08:50:14 +0300

    simplify CharacterModelLoader code

--- Assets/Altzone/Scripts/Model/CharacterModelLoader.cs
+++ Assets/Altzone/Scripts/Model/CharacterModelLoader.cs
@@ -11,35 +11,18 @@ namespace Altzone.Scripts.Model
     /// </remarks>
     public static class CharacterModelLoader
     {
-        public static void UpdateListsInfo()
-        {
-            Characters LoadCharacterModel(string _name, Defence _mainDefence, int _speed, int _resistance,int _attack,int _defence)
-            {
-            return LoadCharacterModel(_name, _mainDefence, _speed, _resistance, _attack, _defence);
-            }
-        }
         public static List<CharacterModel> LoadModels()
         {
-            // HAHMOT ja niiden kuvaukset (+ värit)
-            // https://docs.google.com/spreadsheets/d/1GBlkKJia89lFvEspTzrq_IJ3XXfCTRDQmB4NrZs-Npo/edit#gid=0
-            
-             UpdateListsInfo();
             return new List<CharacterModel>()
             {
-                LoadCharacterModel("Koulukiusaaja", Defence.Desensitisation, 3, 9, 7, 3),
-                LoadCharacterModel("Vitsiniekka", Defence.Deflection, 9, 3, 3, 4),
-                LoadCharacterModel("Pappi", Defence.Introjection, 5, 5, 4, 4),
-                LoadCharacterModel("Taiteilija", Defence.Projection, 4, 2, 9, 5),
-                LoadCharacterModel("Hodariläski", Defence.Retroflection, 3, 7, 2, 9),
-                LoadCharacterModel("Älykkö", Defence.Egotism, 6, 2, 6, 5),
-                LoadCharacterModel("Tytöt", Defence.Confluence, 5, 6, 2, 6)
+                new((int)Defence.Desensitisation, "Koulukiusaaja", Defence.Desensitisation, 3, 9, 7, 3),
+                new((int)Defence.Deflection, "Vitsiniekka", Defence.Deflection, 9, 3, 3, 4),
+                new((int)Defence.Introjection, "Pappi", Defence.Introjection, 5, 5, 4, 4),
+                new((int)Defence.Projection, "Taiteilija", Defence.Projection, 4, 2, 9, 5),
+                new((int)Defence.Retroflection, "Hodariläski", Defence.Retroflection, 3, 7, 2, 9),
+                new((int)Defence.Egotism, "Älykkö", Defence.Egotism, 6, 2, 6, 5),
+                new((int)Defence.Confluence, "Tytöt", Defence.Confluence, 5, 6, 2, 6)
             };
-
-            CharacterModel LoadCharacterModel(string name, Defence mainDefence, int speed, int resistance, int attack, int defence)
-            {
-                var id = (int)mainDefence;
-                return new CharacterModel(id, name, mainDefence, speed, resistance, attack, defence);
-            }
         }
     }
 }
