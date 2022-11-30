0bb58338c1919098b4c642bad7ba069cf7c1e83c
Pasi <pasi_ylarinne@hotmail.com>
2022-11-30 14:05:10 +0200

    Shield Collider functionality

--- Assets/Battle/Scripts/Test/BallHandlerTest.cs
+++ Assets/Battle/Scripts/Test/BallHandlerTest.cs
@@ -2,16 +2,13 @@ using UnityEngine;
 
 public class BallHandlerTest : MonoBehaviour
 {
-    [SerializeField] private Vector2 startPosition;
     [SerializeField] private Vector2 startingSpeed;
 
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.position = startPosition;
         LaunchBall();
     }
 
---
+++ Assets/Battle/Scripts/Test/GreenSquareTest.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GreenSquareTest : MonoBehaviour
+{
+    private Transform _transform;
+    private Camera _camera;
+    private Vector2 _lookTowards;
+    private Vector2 _targetPosition;
+    private Vector2 _tempPosition;
+    [SerializeField] private float _movementSpeed;
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        _lookTowards = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        _transform.up = _lookTowards;
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _targetPosition = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        }
+        var maxWorld = _camera.ViewportToWorldPoint(Vector2.one);
+        var minWorld = _camera.ViewportToWorldPoint(Vector2.zero);
+        _targetPosition.x = Mathf.Clamp(_targetPosition.x, -5, 5);
+        _targetPosition.y = Mathf.Clamp(_targetPosition.y, -9, 9);
+
+        var maxDistanceDelta = _movementSpeed * Time.deltaTime;
+        _tempPosition = Vector2.MoveTowards(_transform.position, _targetPosition, maxDistanceDelta);
+        _transform.position = _tempPosition;
+    }
+}
