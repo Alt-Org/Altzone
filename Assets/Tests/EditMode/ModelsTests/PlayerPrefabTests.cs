47505eefd73b97305eb88254599462f38609a909
Jari Petays <jari.petays@iki.fi>
2023-01-18 09:30:13 +0200

    catch exceptions from store.GetBattleCharacter (when no data or mis-configuration?)

--- Assets/Tests/EditMode/ModelsTests/PlayerPrefabTests.cs
+++ Assets/Tests/EditMode/ModelsTests/PlayerPrefabTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Altzone.Scripts.Config;
 using Altzone.Scripts.Model;
 using NUnit.Framework;
@@ -11,16 +12,26 @@ namespace Assets.Tests.EditMode.ModelsTests
         public void GetCurrentPlayerPrefabTest()
         {
             Debug.Log($"test");
+            var gameConfig = GameConfig.Get();
             var playerDataCache = GameConfig.Get().PlayerDataCache;
             var customCharacterModelId = playerDataCache.CustomCharacterModelId;
-            var battleCharacter = Storefront.Get().GetBattleCharacter(customCharacterModelId);
+            var store = Storefront.Get();
+            var prefabId = 0;
+            try
+            {
+                var battleCharacter = store.GetBattleCharacter(customCharacterModelId);
+                Debug.Log($"{battleCharacter}");
+                prefabId = battleCharacter.PlayerPrefabId;
+                Assert.IsTrue(prefabId >= 0);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"GetBattleCharacter failed {e.Message}");
+                Assert.Fail("Check that CustomCharacterModels exist or restart UNITY to reset Storefront");
+            }
 
-            var prefabId = battleCharacter.PlayerPrefabId;
-            Assert.IsTrue(prefabId >= 0);
-
-            var playerPrefabs = GameConfig.Get().PlayerPrefabs;
+            var playerPrefabs = gameConfig.PlayerPrefabs;
             var playerPrefab = playerPrefabs.GetPlayerPrefab(prefabId);
-
             Assert.IsNotNull(playerPrefab);
         }
 
