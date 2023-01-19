57e5b3ef1e75fc03009907ccff2b439517470512
Otto Ikonen <ottoikonen@kamk.fi>
2023-01-19 05:59:34 +0200

    Cluetiles for mine indication

--- Assets/Raid/Scripts/Raid_Grid.cs
+++ Assets/Raid/Scripts/Raid_Grid.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class Raid_Grid : MonoBehaviour
 {
-    public static int Gridrows;
-    public static int Gridcolumns;
+    public int AmountOfMines;
     public GameObject[,] grid = new GameObject[9,9];
 
     private void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < AmountOfMines; i++)
         {
             PlaceMines();
         }
+
+        PlaceNumberTiles();
     }
 
     void PlaceFurniture()
@@ -28,14 +29,101 @@ public class Raid_Grid : MonoBehaviour
 
         if(grid[x,y] == null)
         {
-            GameObject empty = new GameObject();
+            GameObject MineTile = Instantiate(Resources.Load("Prefabs/Mine", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
 
-            grid[x,y] = empty;
+            grid[x,y] = MineTile;
             Debug.Log("(" + x + ", " + y + ")");
         }
         else
         {
             PlaceMines();
+        }
+    }
+
+    void PlaceNumberTiles()
+    {
+        for (int y = 0; y < 9; y++)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                if (grid[x,y] == null)
+                {
+                    // Tile is empty and thus can't be a mine.
+
+                    // Check nearby cells for mines (up, right, down, left and corners)
+
+                    int NearbyMines = 0;
+
+                    if (y+1 < 9)
+                    {
+                        if (grid[x, y + 1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x+1 < 9)
+                    {
+                        if (grid[x+1, y] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (y-1 >= 0)
+                    {
+                        if (grid[x, y-1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x - 1 >= 0)
+                    {
+                        if (grid[x-1, y] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x+1 < 9 && y+1 < 9)
+                    {
+                        if (grid[x+1, y+1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x + 1 < 9 && y - 1 >= 0)
+                    {
+                        if (grid[x+1, y-1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x - 1 >= 0 && y - 1 >= 0)
+                    {
+                        if (grid[x-1, y-1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (x - 1 >= 0 && y + 1 < 9)
+                    {
+                        if (grid[x-1, y+1] != null)
+                        {
+                            NearbyMines++;
+                        }
+                    }
+
+                    if (NearbyMines > 0)
+                    {
+                        GameObject NumberTile = Instantiate(Resources.Load("Prefabs/" + NearbyMines, typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                    }
+                }
+            }
         }
     }
 
