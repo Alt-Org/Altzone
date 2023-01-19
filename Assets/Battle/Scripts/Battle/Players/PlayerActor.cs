784f2990ad6206dcfeb56b9cb90dd294cc6443e7
Pasi <pasi_ylarinne@hotmail.com>
2023-01-19 14:02:18 +0200

    PlayerActor movement happens with a coroutine

--- Assets/Battle/Scripts/Battle/Players/PlayerActor.cs
+++ Assets/Battle/Scripts/Battle/Players/PlayerActor.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Battle.Scripts.Battle.Players
@@ -22,12 +23,17 @@ namespace Battle.Scripts.Battle.Players
             _targetPosition = transform.position;
         }
 
-        private void Update()
+        private IEnumerator MoveCoroutine(Vector2 targetPosition)
         {
-            var maxDistanceDelta = _movementSpeed * _playerMoveSpeedMultiplier * Time.deltaTime;
-            _tempPosition = Vector2.MoveTowards(_transform.position, _targetPosition, maxDistanceDelta);
-            _transform.position = _tempPosition;
-            _hasTarget = !(Mathf.Approximately(_tempPosition.x, _targetPosition.x) && Mathf.Approximately(_tempPosition.y, _targetPosition.y));
+            _hasTarget = true;
+            while (_hasTarget)
+            {
+                yield return null;
+                var maxDistanceDelta = _movementSpeed * _playerMoveSpeedMultiplier * Time.deltaTime;
+                _tempPosition = Vector2.MoveTowards(_transform.position, _targetPosition, maxDistanceDelta);
+                _transform.position = _tempPosition;
+                _hasTarget = !(Mathf.Approximately(_tempPosition.x, _targetPosition.x) && Mathf.Approximately(_tempPosition.y, _targetPosition.y));
+            }
         }
 
         #region IPlayerActor
@@ -37,6 +43,7 @@ namespace Battle.Scripts.Battle.Players
         void IPlayerActor.MoveTo(Vector2 targetPosition)
         {
             _targetPosition = targetPosition;
+            StartCoroutine(MoveCoroutine(targetPosition));
         }
 
         void IPlayerActor.Rotate(float angle)
