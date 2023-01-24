a371514a6607c95c7c0f51e4b0a81956cb4993e4
Jari Petays <jari.petays@iki.fi>
2023-01-24 09:17:43 +0200

    RaidGameRoomModelTest first working version

--- Assets/Tests/PlayMode/InstantiateTests/RaidGameRoomModelTest.cs
+++ Assets/Tests/PlayMode/InstantiateTests/RaidGameRoomModelTest.cs
@@ -1,6 +1,10 @@
 using System.Collections;
+using System.Threading.Tasks;
+using Altzone.Scripts.Config;
+using Altzone.Scripts.Model;
+using Altzone.Scripts.Model.Dto;
+using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace Assets.Tests.PlayMode.InstantiateTests
@@ -10,24 +14,47 @@ namespace Assets.Tests.PlayMode.InstantiateTests
     /// </summary>
     public class RaidGameRoomModelTest : PlayModeTestSupport
     {
+        protected override void MyOneTimeSetUp()
+        {
+            // Load used dependencies here so they are ready when needed. 
+            GameConfig.Get();
+        }
+
         [UnityTest]
         public IEnumerator MainTestLoop()
         {
-            var scene = SceneManager.GetActiveScene();
-            Debug.Log($"test with scene {scene.buildIndex} {scene.name}");
+            Debug.Log($"test");
 
-            CreateRaidGameRoomModel();
+            yield return null;
+            GetFirstRaidGameRoomModel();
 
             // This test must be manually cancelled.
-            while (!IsTestDone)
-            {
-                yield return null;
-            }
+            yield return new WaitUntil(() => IsTestDone);
             Debug.Log($"done {Time.frameCount}");
         }
 
-        private void CreateRaidGameRoomModel()
+        private async Task GetFirstRaidGameRoomModel()
         {
+            Debug.Log($"start {Time.frameCount}");
+            var models = await Store.GetAllRaidGameRoomModels();
+            Assert.IsTrue(models.Count > 0);
+            MonoBehaviour.StartCoroutine(ShowRaidGameRoom(models[0]));
+            Debug.Log($"done {Time.frameCount}");
+        }
+
+        private IEnumerator ShowRaidGameRoom(IRaidGameRoomModel room)
+        {
+            Debug.Log($"room {room}");
+            foreach (var furnitureLocation in room.FurnitureLocations)
+            {
+                var model = Store.GetFurnitureModel(furnitureLocation.FurnitureId);
+                Assert.IsNotNull(model);
+                Debug.Log($"furniture {model.Id} {model.PrefabName} x,y={furnitureLocation.X},{furnitureLocation.Y}");
+                var position = new Vector3(furnitureLocation.X, furnitureLocation.Y, 0);
+                var gameObject = FurnitureModel.Instantiate(model, position, Quaternion.identity);
+                Assert.NotNull(gameObject);
+                yield return null;
+            }
         }
     }
 }
