70df32926df045c304d5be27ba96ca3b5f36a343
ScrubMans <113660852+ScrubMans@users.noreply.github.com>
2023-02-01 06:26:43 +0200

    betterTestControllPanel

---
+++ Assets/Battle/Scripts/Test/PlayerTestControlPanel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Battle.Scripts.Battle;
+using Battle.Scripts.Battle.Players;
+using UnityEngine;
+namespace Battle.Scripts.Battle.Test
+{
+    public class PlayerTestControlPanel : MonoBehaviour
+    {
+        [SerializeField] private bool _moveTo;
+        [SerializeField] private bool _startRotate;
+        [SerializeField] private float _rotate;
+        [SerializeField] private Vector2 _targetPosition;
+        private IPlayerInputTarget _IPlayerInputTarget;
+        private IPlayerDriver _IPlayerDriver;
+
+
+        void Update()
+        {
+            if (GameObject.Find("PlayerDriverPhoton(Clone)") != null && _moveTo)
+            {
+                _moveTo = false;
+                _IPlayerInputTarget = GameObject.Find("PlayerDriverPhoton(Clone)").GetComponent<PlayerDriverPhoton>();
+                _IPlayerInputTarget.MoveTo(_targetPosition);
+            }
+            if (GameObject.Find("PlayerDriverPhoton(Clone)") != null && _startRotate)
+            {
+                _startRotate = false;
+                _IPlayerDriver = GameObject.Find("PlayerDriverPhoton(Clone)").GetComponent<PlayerDriverPhoton>();
+                _IPlayerDriver.Rotate(_rotate);
+            }
+            
+        }
+    }
+}
