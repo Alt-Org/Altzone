d4a443a33115baf5a1c90936b9cffeb6291069e4
Jari Petays <jari.petays@iki.fi>
2023-02-08 17:56:05 +0200

    add Rotate(bool isUpsideDown) to IBattleCamera

--- Assets/Battle/Scripts/Battle/IBattleCamera.cs
+++ Assets/Battle/Scripts/Battle/IBattleCamera.cs
@@ -5,6 +5,7 @@ namespace Battle.Scripts.Battle
     internal interface IBattleCamera
     {
         Camera Camera { get; }
+        void Rotate(bool isUpsideDown);
         bool IsRotated { get; }
 
         void DisableAudio();
