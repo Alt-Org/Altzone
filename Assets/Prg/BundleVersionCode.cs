bd293d5f42fba06dc440918b705798ab0037c9ce
psykkis <psykkis@hotmail.com>
2023-03-01 16:17:19 +0200

    ALT Zone 48
    timantit tuotu kentälle kerättäväksi, sen enempää toiminnallisuutta niissä ei vielä ole

--- Assets/Prg/BundleVersionCode.cs
+++ Assets/Prg/BundleVersionCode.cs
@@ -6,7 +6,7 @@ namespace Prg
     /// </summary>
     public static class BundleVersionCode
     {
-        private const string BundleVersionCodeValue = ".47";
+        private const string BundleVersionCodeValue = ".48";
 
         public static string Get => BundleVersionCodeValue;
     }
