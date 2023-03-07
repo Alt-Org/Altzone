41e75ebf00e00f4333b0af485b5e07571df2a5df
Jari Petays <jari.petays@iki.fi>
2023-03-07 12:20:09 +0200

    move GameFiles from interface to a 'better place'

--- Assets/Altzone/Scripts/IStorefront.cs
+++ Assets/Altzone/Scripts/IStorefront.cs
@@ -6,17 +6,6 @@ using Altzone.Scripts.Model.Dto;
 namespace Altzone.Scripts
 {
     /// <summary>
-    /// Hardcoded files names for local storage.
-    /// </summary>
-    public static class GameFiles
-    {
-        public const string ClanGameRoomModelsFilename = "GameClanGameRoomModels.json";
-        public const string ClanInventoryItemsFilename = "GameClanInventoryItems.json";
-        public const string PlayerGameRoomModelsFilename = "GamePlayerClanGameRoomModels.json";
-        public const string PlayerCustomCharacterModelsFilename = "GamePlayerCustomCharacterModels.json";
-    }
-
-    /// <summary>
     /// Store CRUD operations for model and custom objects.
     /// </summary>
     /// <remarks>
--- Assets/Altzone/Scripts/Storefront.cs
+++ Assets/Altzone/Scripts/Storefront.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 namespace Altzone.Scripts
 {
     /// <summary>
+    /// Hardcoded files names for local storage.
+    /// </summary>
+    public static class GameFiles
+    {
+        public const string ClanGameRoomModelsFilename = "GameClanGameRoomModels.json";
+        public const string ClanInventoryItemsFilename = "GameClanInventoryItems.json";
+        public const string PlayerGameRoomModelsFilename = "GamePlayerClanGameRoomModels.json";
+        public const string PlayerCustomCharacterModelsFilename = "GamePlayerCustomCharacterModels.json";
+    }
+
+    /// <summary>
     /// Factory class for <c>IStorefront</c>.
     /// </summary>
     public static class Storefront
