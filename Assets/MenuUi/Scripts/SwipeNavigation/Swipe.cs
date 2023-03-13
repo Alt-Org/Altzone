2bb7382da085652a96c836a466e8e9f6eb91d4c7
ScrubMans <113660852+ScrubMans@users.noreply.github.com>
2023-03-13 15:59:57 +0200

    swipe matkaa suurennettu

--- Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
+++ Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
@@ -29,12 +29,12 @@ namespace MenuUi.Scripts.SwipeNavigation
             {
 
                 endPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
-                if (startPosition.x - 20f > endPosition.x && _nextNaviTarget != null)
+                if (startPosition.x - 50f > endPosition.x && _nextNaviTarget != null)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_nextNaviTarget);
                 }
-                if (startPosition.x + 20f < endPosition.x && _prevNaviTarget != null)
+                if (startPosition.x + 50f < endPosition.x && _prevNaviTarget != null)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_prevNaviTarget);
