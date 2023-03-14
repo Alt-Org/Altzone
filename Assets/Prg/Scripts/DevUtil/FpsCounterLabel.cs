ffa694eeb8bddd86b3b1f4910fe67806d6f218ba
Jari Petays <jari.petays@iki.fi>
2023-03-14 11:22:52 +0200

    minimize runtime memory allocation ja increase Android font size

--- Assets/Prg/Scripts/DevUtil/FpsCounterLabel.cs
+++ Assets/Prg/Scripts/DevUtil/FpsCounterLabel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Prg.Scripts.DevUtil
@@ -8,13 +10,14 @@ namespace Prg.Scripts.DevUtil
     /// </summary>
     public class FpsCounterLabel : MonoBehaviour
     {
-        [Header("This only runs in Editor or Development Build")] public int _note;
+        [Header("This only runs in Editor or Development Build")] public int _labelCount;
 
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
         private readonly GUIStyle _guiLabelStyle = new();
-        private readonly Rect _guiLabelPosition = new(5, 5, 100, 25);
+        private readonly Rect _guiLabelPosition = new(5, 5, 100, 50);
 
         private string _fpsLabel = string.Empty;
+        private readonly Dictionary<int, string> _labels = new();
 
         private IEnumerator Start()
         {
@@ -23,8 +26,9 @@ namespace Prg.Scripts.DevUtil
                 enabled = false;
                 yield break;
             }
-            _guiLabelStyle.fontStyle = FontStyle.Bold;
             _guiLabelStyle.normal.textColor = Color.white;
+            _guiLabelStyle.fontStyle = FontStyle.Bold;
+            _guiLabelStyle.fontSize = AppPlatform.IsMobile || AppPlatform.IsSimulator ? 24 : 12;
             StartCoroutine(FrameRateCalculator());
         }
 
@@ -38,7 +42,13 @@ namespace Prg.Scripts.DevUtil
                 var startFrames = Time.frameCount;
                 yield return delay;
                 var fps = (int)((Time.frameCount - startFrames) / (Time.time - startTime));
-                _fpsLabel = $"{fps} fps";
+                if (!_labels.TryGetValue(fps, out var label))
+                {
+                    label = $"{fps} fps";
+                    _labels.Add(fps, label);
+                    _labelCount = _labels.Count;
+                }
+                _fpsLabel = label;
             }
         }
 
