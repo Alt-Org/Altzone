64b5d42969e8f1922da9eb3174078eb4da4caaba
MrHexxar <122272662+MrHexxar@users.noreply.github.com>
2023-03-15 12:18:14 +0200

    Sorting for inventory
    Somewhat unsure about the resource efficiency of the code but it does function

--- Assets/MenuUi/Scripts/Varasto/InvFront.cs
+++ Assets/MenuUi/Scripts/Varasto/InvFront.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Altzone.Scripts.Model.Poco.Game;
 using Altzone.Scripts;
 using System.Collections;
+using UnityEditor;
 
 public class InvFront : MonoBehaviour
 {
@@ -48,28 +49,60 @@ public class InvFront : MonoBehaviour
             slotsList.Add(newObject);
             i++;
         }
+
+        //SortByString();
     }
 
     public void SortStored() // A very much hardcoded system for sorting 
     {
         if (sortingBy < maxSortingBy) { sortingBy++; }
-        else { sortingBy = 0;}
+        else { sortingBy = 0; }
+
+        List<GameObject> reSlots = slotsList;
+        int forVal = reSlots.Count;
 
         switch (sortingBy)
         {
             case 0:
                 sortText.text = "Sorted by: Alphabet";
-                slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Name);
+                //slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Name);
+                for (int a = 0; a < forVal; a++)
+                {
+                    reSlots.Sort((GameObject t1, GameObject t2) =>
+                    {
+                        return t1.GetComponent<InvSlot>().contains.Name.CompareTo(t2.GetComponent<InvSlot>().contains.Name);
+                    });
+                }
                 break;
             case 1:
                 sortText.text = "Sorted by: Weight";
-                slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Weight);
+                //slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Weight);
+                for (int a = 0; a < forVal; a++)
+                {
+                    reSlots.Sort((GameObject t1, GameObject t2) =>
+                    {
+                        return t1.GetComponent<InvSlot>().contains.Weight.CompareTo(t2.GetComponent<InvSlot>().contains.Weight);
+                    });
+                }
                 break;
             case 2:
                 sortText.text = "Sorted by: Material?";
-                slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Material);
+                //slotsList.OrderBy(x => x.GetComponent<InvSlot>().contains.Material);
+                for (int a = 0; a < forVal; a++)
+                {
+                    reSlots.Sort((GameObject t1, GameObject t2) =>
+                    {
+                        return t1.GetComponent<InvSlot>().contains.Material.CompareTo(t2.GetComponent<InvSlot>().contains.Material);
+                    });
+                }
                 break;
             default: sortText.text = "Something broke"; break; // Just as a safety measure
+        }
+
+        for (int i = 0; i < forVal; ++i)
+        {
+            Undo.SetTransformParent(reSlots[i].transform, reSlots[i].transform.parent, "Sort Children");
+            reSlots[i].transform.SetSiblingIndex(i);
         }
     }
 
@@ -81,6 +114,28 @@ public class InvFront : MonoBehaviour
         }
         catch { /* Slot is empty */ }
     }
+
+    //private void SortByString()
+    //{
+    //    List<Transform> reSlots = ToTransforms(slotsList);
+    //    int forVal = reSlots.Count;
+    //    for (int a = 0; a < forVal; a++) { reSlots.Sort((Transform t1, Transform t2) => { return t1.name.CompareTo(t2.name); }); }
+    //    for (int i = 0; i < forVal; ++i)
+    //    {
+    //        Undo.SetTransformParent(reSlots[i], reSlots[i].parent, "Sort Children");
+    //        reSlots[i].SetSiblingIndex(i);
+    //    }
+    //}
+
+    //private List<Transform> ToTransforms(List<GameObject> objects)
+    //{
+    //    List<Transform> result = new List<Transform>();
+    //    foreach(GameObject obj in objects)
+    //    {
+    //        result.Add(obj.transform);
+    //    }
+    //    return result;
+    //}
 
     //public void UnInform() { infoScreen.SetActive(false); invScreen.SetActive(true); }
 
