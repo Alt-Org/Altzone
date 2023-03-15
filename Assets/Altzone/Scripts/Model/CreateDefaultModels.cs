ae390669de11d521bf07de5d60d20e7bcaf431db
Jari Petays <jari.petays@iki.fi>
2023-03-15 10:43:42 +0200

    bump CustomCharactersVersion up by one to force changes in effect

--- Assets/Altzone/Scripts/Model/CreateDefaultModels.cs
+++ Assets/Altzone/Scripts/Model/CreateDefaultModels.cs
@@ -30,7 +30,7 @@ namespace Altzone.Scripts.Model
             };
         }
 
-        internal const int CustomCharactersVersion = 4;
+        internal const int CustomCharactersVersion = 5;
 
         /// <summary>
         /// Player custom character classes are created by the player itself (or given to the player by the game).<br />
