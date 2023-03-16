93787b33d8b273f7001632414c47118b188fd425
Pasi <pasi_ylarinne@hotmail.com>
2023-03-16 14:51:35 +0200

    New BoxCollider shields, Ball movement is limited to grid and angle to 45 degree increments

--- Assets/Battle/Scripts/Battle/Players/PlayerActor.cs
+++ Assets/Battle/Scripts/Battle/Players/PlayerActor.cs
@@ -23,6 +23,7 @@ namespace Battle.Scripts.Battle.Players
         private int _shieldResistance;
         private int _shieldHitPoints;
         private float _shieldDeformDelay;
+        private float _angleLimit;
 
         private void Awake()
         {
@@ -32,6 +33,7 @@ namespace Battle.Scripts.Battle.Players
             _shieldResistance = variables._shieldResistance;
             _shieldHitPoints = _shieldResistance;
             _shieldDeformDelay = variables._shieldDeformDelay;
+            _angleLimit = variables._angleLimit;
             _shieldPoseManager = GetComponentInChildren<ShieldPoseManager>();
         }
 
@@ -66,7 +68,9 @@ namespace Battle.Scripts.Battle.Players
 
         void IPlayerActor.SetRotation(float angle)
         {
-            _geometryRoot.eulerAngles = new Vector3(0, 0, angle);
+            var multiplier = Mathf.Round (angle / _angleLimit);
+            var newAngle = _angleLimit * multiplier;
+            _geometryRoot.eulerAngles = new Vector3(0, 0, newAngle);
         }
 
         void IPlayerActor.ShieldHit(int damage)
