ed73f244ac00ad5687653ba101e1696b6593a8a3
ScrubMans <113660852+ScrubMans@users.noreply.github.com>
2023-03-16 10:56:00 +0200

    Slide animation

--- Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
+++ Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
@@ -10,16 +10,41 @@ namespace MenuUi.Scripts.SwipeNavigation
         private PlayerInput _playerInput;
         private Vector2 startPosition;
         private Vector2 endPosition;
+        private Vector2 _currentPosition;
+        private bool _touching;
+        [SerializeField] private bool _canSlide;
 
+        [SerializeField] private GameObject _slidingUI;
+        
         [SerializeField] private WindowDef _prevNaviTarget;
         [SerializeField] private WindowDef _nextNaviTarget;
 
         [SerializeField] private float _distanceToSwitch;
         [SerializeField] private float _YdistanceToNotSwitch;
+        
 
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void slideAnimation()
+        {
+            if (_slidingUI == null)
+            {
+                return;
+            }
+            if(_touching == false)
+            {
+                _slidingUI.transform.position = Vector3.MoveTowards(_slidingUI.transform.position, new Vector3(0,0,0),2000 * Time.deltaTime);
+                return;
+            }
+            if(_canSlide && _currentPosition.x > startPosition.x + 50 ||
+            _canSlide && _currentPosition.x < startPosition.x - 50 )
+            {
+                _slidingUI.transform.position = Vector3.MoveTowards(_slidingUI.transform.position, new Vector3(_currentPosition.x - startPosition.x, 0, 0), 2000 * Time.deltaTime);
+                return;
+            }
         }
 
         public void IsTouching(InputAction.CallbackContext context)
@@ -27,23 +52,45 @@ namespace MenuUi.Scripts.SwipeNavigation
             if (context.performed)
             {
                 startPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
+                _touching = true;
             }
             if (context.canceled)
             {
-
+                
                 endPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
-                if (startPosition.x - _distanceToSwitch > endPosition.x && _nextNaviTarget != null && startPosition.y - _YdistanceToNotSwitch !< endPosition.y && startPosition.y + _YdistanceToNotSwitch !> endPosition.y)
+                if (startPosition.x - _distanceToSwitch > endPosition.x && _nextNaviTarget != null && _canSlide)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_nextNaviTarget);
                 }
-                if (startPosition.x + _distanceToSwitch < endPosition.x && _prevNaviTarget != null && startPosition.y - _YdistanceToNotSwitch !< endPosition.y && startPosition.y + _YdistanceToNotSwitch !> endPosition.y)
+                if (startPosition.x + _distanceToSwitch < endPosition.x && _prevNaviTarget != null && _canSlide)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_prevNaviTarget);
                 }
+                _touching = false;
+                _canSlide = true;
+            }
+        }
+
+        private void Update()
+        {
+
+            if (_touching == true)
+            {
+                _currentPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
+                if(startPosition.y - _YdistanceToNotSwitch !< _currentPosition.y && startPosition.y + _YdistanceToNotSwitch !> _currentPosition.y)
+                {
+                    
+                }
+                else
+                {
+                    _canSlide = false;
+                }
 
             }
+            
+            slideAnimation();
         }
     }
 }
