be9ee36baac8b142bd0b16beed6fb7800176ee33
ElkkuPRO045 <emppujatala@gmail.com>
2023-03-24 14:33:21 +0200

    Revert "Merge branch 'main' of https://github.com/Alt-Org/Altzone"
    This reverts commit 237ea0661dd948ccbb69744d984b92e6bb696641, reversing
    changes made to 2b5d92a6113e0858641c0b8a4bfc1af58e3bb6f0.

--- Assets/Prg/BundleVersionCode.cs
+++ Assets/Prg/BundleVersionCode.cs
@@ -6,7 +6,7 @@ namespace Prg
     /// </summary>
     public static class BundleVersionCode
     {
-        private const string BundleVersionCodeValue = ".53";
+        private const string BundleVersionCodeValue = ".52";
 
         public static string Get => BundleVersionCodeValue;
     }
