5d5ff9db8f554f740ecbf2e2b257027a33ba72a3
ScrubMans <113660852+ScrubMans@users.noreply.github.com>
2023-03-28 13:27:29 +0300

    More bot scripts

---
+++ Assets/Battle/Scripts/Battle/Bot/BotDriver.cs
@@ -0,0 +1,154 @@
+using System;
+using Altzone.Scripts.Battle;
+using Altzone.Scripts.Config;
+using Battle.Scripts.Battle.Players;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Battle.Scripts.Battle.Bot
+{
+    /// <summary>
+    /// Photon <c>PlayerDriver</c> implementation.
+    /// </summary>
+    internal class BotDriver : PlayerDriver, IPlayerDriver
+    {
+        [SerializeField] private PlayerActorBase _playerPrefab;
+
+        private IPlayerActor _playerActor;
+        private IGridManager _gridManager;
+        private IBattlePlayArea _battlePlayArea;
+        private IPlayerDriverState _state;
+        private PhotonView _photonView;
+        private int _playerPos;
+        private int _teamNumber;
+        private double _movementDelay;
+        private float _arenaScaleFactor;
+        private static bool IsNetworkSynchronize => PhotonNetwork.IsMasterClient;
+
+        public string PlayerName;
+
+        private bool _isLocal;
+
+        [Header ("Testing")]
+        [SerializeField] private bool _isTesting = false;
+        [SerializeField] private int _playerPrefabID;
+
+        private void Awake()
+        {
+            _battlePlayArea = Context.GetBattlePlayArea;
+            _photonView = PhotonView.Get(this);
+            _gridManager = Context.GetGridManager;
+            _playerPos = PhotonBattle.GetPlayerPos(_photonView.Owner);
+            _arenaScaleFactor = _battlePlayArea.ArenaScaleFactor;
+            _playerActor = InstantiatePlayerPrefab(_photonView.Owner);
+            _teamNumber = PhotonBattle.GetTeamNumber(_playerPos);
+            _movementDelay = GameConfig.Get().Variables._playerMovementNetworkDelay;
+            //_photonView.ObservedComponents.Add((PlayerActor)_playerActor);
+        }
+
+        private IPlayerActor InstantiatePlayerPrefab(Player player)
+        {
+            var playerTag = $"{_teamNumber}:{_playerPos}:{player.NickName}";
+            PlayerName = playerTag;
+            Debug.Log($"heoooo22{playerTag}");
+            name = name.Replace("Clone", playerTag);
+            if (_playerPrefab != null)
+            {
+                return PlayerActor.InstantiatePrefabFor(this, _playerPos, _playerPrefab, playerTag, _arenaScaleFactor);
+            }
+
+            var playerPrefabs = GameConfig.Get().PlayerPrefabs;
+            var playerPrefabId = PhotonBattle.GetPlayerPrefabId(player);
+            if (_isTesting)
+            {
+                playerPrefabId = _playerPrefabID;
+            }
+            var playerPrefab = playerPrefabs.GetPlayerPrefab(playerPrefabId);
+            var playerActor = PlayerActor.InstantiatePrefabFor(this, _playerPos, playerPrefab, playerTag, _arenaScaleFactor);
+            return playerActor;
+        }
+
+        private void OnEnable()
+        {
+            var player = _photonView.Owner;
+            _isLocal = player.IsLocal;
+            _state = GetPlayerDriverState(this);
+            _state.ResetState(_playerActor, _teamNumber);
+            if (_teamNumber == PhotonBattle.TeamBetaValue)
+            {
+                ((IPlayerDriver)this).Rotate(180f);
+            }
+            if (!_isLocal)
+            {
+                return;
+            }
+            var playerInputHandler = Context.GetPlayerInputHandler;
+            playerInputHandler.SetPlayerDriver(this);
+        }
+
+        #region IPlayerDriver
+
+        string IPlayerDriver.NickName => _photonView.Owner.NickName;
+
+        int IPlayerDriver.TeamNumber => _teamNumber;
+
+        int IPlayerDriver.ActorNumber => _photonView.Owner.ActorNumber;
+
+        bool IPlayerDriver.IsLocal => _photonView.Owner.IsLocal;
+
+        int IPlayerDriver.PlayerPos => _playerPos;
+
+        void IPlayerDriver.Rotate(float angle)
+        {
+            _playerActor.SetRotation(angle);
+        }
+
+        void IPlayerInputTarget.MoveTo(Vector2 targetPosition)
+        {
+            if (!_state.CanRequestMove)
+            {
+                return;
+            }
+            var gridPos = _gridManager.WorldPointToGridPosition(targetPosition);
+            var isSpaceFree = _gridManager.IsMovementGridSpaceFree(gridPos, _teamNumber);
+            if (!isSpaceFree)
+            {
+                return;
+            }
+            _state.IsWaitingToMove(true);
+            var movementStartTime = PhotonNetwork.Time + _movementDelay;
+            _photonView.RPC(nameof(MoveDelayedRpc), RpcTarget.All, gridPos.Row, gridPos.Col, movementStartTime);
+        }
+
+        void IPlayerDriver.SetCharacterPose(int poseIndex)
+        {
+            if (!IsNetworkSynchronize)
+            {
+                return;
+            }
+            _photonView.RPC(nameof(SetPlayerCharacterPoseRpc), RpcTarget.All, poseIndex);
+        }
+
+        #endregion
+
+        #region Photon RPC
+
+        [PunRPC]
+        private void MoveDelayedRpc(int row, int col, double movementStartTime)
+        {
+            var moveExecuteDelay = Math.Max(0, movementStartTime - PhotonNetwork.Time);
+            var gridPos = new GridPos(row, col);
+            _state.DelayedMove(gridPos, (float)moveExecuteDelay);
+        }
+
+        [PunRPC]
+        private void SetPlayerCharacterPoseRpc(int poseIndex)
+        {
+            _playerActor.SetCharacterPose(poseIndex);
+        }
+
+        #endregion
+    }
+}
+
