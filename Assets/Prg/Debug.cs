f18e87dad6a7b431aeed1dd1d36491990bc98dca
Jari Petays <jari.petays@iki.fi>
2023-03-30 17:56:18 +0300

    bypass UnityEngine.Debug.Log methods and use directly UnityEngine.Debug.unityLogger that does the actual work anyway

--- Assets/Prg/Debug.cs
+++ Assets/Prg/Debug.cs
@@ -121,13 +121,24 @@ public static class Debug
         return _currentFrameCount;
     }
 
-    #region Just for compability or notice to use fully qualified version in UnityEngine namespace
+    #region Just for compability in UnityEngine namespace
 
     public static void Break() => UnityEngine.Debug.Break();
     public static void DebugBreak() => UnityEngine.Debug.DebugBreak();
-    private const string NotHere = "Use fully qualified version in UnityEngine.Debug namespace";
-    public static void DrawLine() => throw new NotImplementedException(NotHere);
-    public static void DrawRay() => throw new NotImplementedException(NotHere);
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration, bool depthTest) =>
+        UnityEngine.Debug.DrawLine(start, end, color, duration, depthTest);
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration) => UnityEngine.Debug.DrawLine(start, end, color, duration);
+    public static void DrawLine(Vector3 start, Vector3 end, Color color) => UnityEngine.Debug.DrawLine(start, end, color);
+    public static void DrawLine(Vector3 start, Vector3 end) => UnityEngine.Debug.DrawLine(start, end);
+
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration, bool depthTest) =>
+        UnityEngine.Debug.DrawRay(start, dir, color, duration, depthTest);
+
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color, float duration) => UnityEngine.Debug.DrawRay(start, dir, color, duration);
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color) => UnityEngine.Debug.DrawRay(start, dir, color);
+    public static void DrawRay(Vector3 start, Vector3 dir) => UnityEngine.Debug.DrawRay(start, dir);
 
     #endregion
 
@@ -138,7 +149,7 @@ public static class Debug
         var method = frame.GetMethod();
         if (method == null || method.ReflectedType == null)
         {
-            UnityEngine.Debug.Log(message, context);
+            UnityEngine.Debug.unityLogger.Log(LogType.Log, (object)message, context);
         }
         else if (IsMethodAllowedForLog(method))
         {
@@ -146,11 +157,11 @@ public static class Debug
             if (AppPlatform.IsEditor)
             {
                 var contextTag = context != null ? _contextTag : string.Empty;
-                UnityEngine.Debug.Log($"{prefix}{message}{contextTag}", context);
+                UnityEngine.Debug.unityLogger.Log(LogType.Log, (object)$"{prefix}{message}{contextTag}", context);
             }
             else
             {
-                UnityEngine.Debug.Log($"{prefix}{message}");
+                UnityEngine.Debug.unityLogger.Log(LogType.Log, (object)$"{prefix}{message}", context);
             }
         }
     }
@@ -162,28 +173,28 @@ public static class Debug
         var method = frame.GetMethod();
         if (method == null || method.ReflectedType == null)
         {
-            UnityEngine.Debug.LogFormat(format, args);
+            UnityEngine.Debug.unityLogger.LogFormat(LogType.Log, format, args);
         }
         else if (IsMethodAllowedForLog(method))
         {
-            UnityEngine.Debug.LogFormat($"{GetPrefix(method)}{format}", args);
+            UnityEngine.Debug.unityLogger.LogFormat(LogType.Log, $"{GetPrefix(method)}{format}", args);
         }
     }
 
     [Conditional("UNITY_EDITOR"), Conditional("FORCE_LOG")]
     public static void LogWarning(string message, Object context = null)
     {
-        UnityEngine.Debug.LogWarning(message, context);
+        UnityEngine.Debug.unityLogger.Log(LogType.Warning, (object)message, context);
     }
 
     public static void LogError(string message, Object context = null)
     {
-        UnityEngine.Debug.LogError(message, context);
+        UnityEngine.Debug.unityLogger.Log(LogType.Error, (object)message, context);
     }
 
     public static void LogException(Exception exception)
     {
-        UnityEngine.Debug.LogException(exception);
+        UnityEngine.Debug.unityLogger.LogException(exception);
     }
 
     private static string GetPrefix(MemberInfo method, string memberName = null)
