54a6dc71c133cc204ad8439aaf7ba454eb6d51ca
Jari Petays <jari.petays@iki.fi>
2023-04-04 17:11:14 +0300

    use 'faster' FindAnyObjectByType instead of old and slow FindObjectOfType (aka FindFirstObjectByType)

--- Assets/Battle/Scripts/Battle/Context.cs
+++ Assets/Battle/Scripts/Battle/Context.cs
@@ -7,11 +7,16 @@ namespace Battle.Scripts.Battle
     /// <summary>
     /// Service locator pattern for important objects in <c>Battle</c> assembly.
     /// </summary>
+    /// <remarks>
+    /// See:  FindFirstObjectByType vs FindAnyObjectByType and FindObjectsByType vs FindObjectsOfType<br />
+    /// https://gamedev.stackexchange.com/questions/204610/findobjectoftype-vs-findfirstobjectbytype<br />
+    /// https://gamedev.stackexchange.com/questions/204741/whats-the-difference-between-findobjectsbytype-and-findobjectsoftype
+    /// </remarks>
     internal static class Context
     {
-        internal static PlayerInputHandler GetPlayerInputHandler => Object.FindObjectOfType<PlayerInputHandler>();
-        internal static Camera GetBattleCamera => Object.FindObjectOfType<GameCamera>().Camera;
-        internal static GridManager GetGridManager => Object.FindObjectOfType<GridManager>();
-        internal static PlayerPlayArea GetBattlePlayArea => Object.FindObjectOfType<PlayerPlayArea>();
+        internal static PlayerInputHandler GetPlayerInputHandler => Object.FindAnyObjectByType<PlayerInputHandler>();
+        internal static Camera GetBattleCamera => Object.FindAnyObjectByType<GameCamera>().Camera;
+        internal static GridManager GetGridManager => Object.FindAnyObjectByType<GridManager>();
+        internal static PlayerPlayArea GetBattlePlayArea => Object.FindAnyObjectByType<PlayerPlayArea>();
     }
 }
