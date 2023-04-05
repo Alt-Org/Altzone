2dfc3674f7bd66e78f0701ebafe32e98372cc015
Jari Petays <jari.petays@iki.fi>
2023-04-05 12:22:03 +0300

    revert commit https://github.com/Alt-Org/Altzone/commit/54a6dc71c133cc204ad8439aaf7ba454eb6d51ca it seems that newer versions of Find...() API is not supported in UNITY 2021.3.16

--- Assets/Battle/Scripts/Battle/Context.cs
+++ Assets/Battle/Scripts/Battle/Context.cs
@@ -8,15 +8,16 @@ namespace Battle.Scripts.Battle
     /// Service locator pattern for important objects in <c>Battle</c> assembly.
     /// </summary>
     /// <remarks>
+    /// After UNITY 2021.33.18 you can use faster FindObjectsByType API calls:
     /// See:  FindFirstObjectByType vs FindAnyObjectByType and FindObjectsByType vs FindObjectsOfType<br />
     /// https://gamedev.stackexchange.com/questions/204610/findobjectoftype-vs-findfirstobjectbytype<br />
     /// https://gamedev.stackexchange.com/questions/204741/whats-the-difference-between-findobjectsbytype-and-findobjectsoftype
     /// </remarks>
     internal static class Context
     {
-        internal static PlayerInputHandler GetPlayerInputHandler => Object.FindAnyObjectByType<PlayerInputHandler>();
-        internal static Camera GetBattleCamera => Object.FindAnyObjectByType<GameCamera>().Camera;
-        internal static GridManager GetGridManager => Object.FindAnyObjectByType<GridManager>();
-        internal static PlayerPlayArea GetBattlePlayArea => Object.FindAnyObjectByType<PlayerPlayArea>();
+        internal static PlayerInputHandler GetPlayerInputHandler => Object.FindObjectOfType<PlayerInputHandler>();
+        internal static Camera GetBattleCamera => Object.FindObjectOfType<GameCamera>().Camera;
+        internal static GridManager GetGridManager => Object.FindObjectOfType<GridManager>();
+        internal static PlayerPlayArea GetBattlePlayArea => Object.FindObjectOfType<PlayerPlayArea>();
     }
 }
