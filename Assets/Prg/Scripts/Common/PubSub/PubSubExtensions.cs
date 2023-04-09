8cd1646fdab5cfeeecd294acf009194e41c32071
Jari Petays <jari.petays@iki.fi>
2023-04-09 09:43:31 +0300

    remove unused code and fix doc comments

--- Assets/Prg/Scripts/Common/PubSub/PubSubExtensions.cs
+++ Assets/Prg/Scripts/Common/PubSub/PubSubExtensions.cs
@@ -6,12 +6,11 @@ using UnityEngine.SceneManagement;
 namespace Prg.Scripts.Common.PubSub
 {
     /// <summary>
-    /// Simple Publish Subscribe Pattern using Extension Methods.<br />
-    /// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+    /// Simple Publish Subscribe Pattern using Extension Methods to delegate work to actual implementation.
     /// </summary>
     public static class PubSubExtensions
     {
-        private static readonly Hub Hub = new Hub();
+        private static readonly Hub Hub = new ();
         private static bool _isApplicationQuitting;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -46,39 +45,57 @@ namespace Prg.Scripts.Common.PubSub
             SceneManager.sceneUnloaded += _ => CheckHandlerCount();
         }
 
-        public static bool Exists<T>(this object subscriber)
-        {
-            return Hub.Exists<T>(subscriber);
-        }
-
-        public static void Publish<T>(this object _)
-        {
-            Hub.Publish(default(T));
-        }
-
+        /// <summary>
+        /// Publish a message to all subscribers.
+        /// </summary>
+        /// <param name="_">Sending object is not used</param>
+        /// <param name="data">The message to send</param>
+        /// <typeparam name="T">Type of the message</typeparam>
         public static void Publish<T>(this object _, T data)
         {
             Hub.Publish(data);
         }
 
+        /// <summary>
+        /// Subscribes to a message.
+        /// </summary>
+        /// <param name="subscriber">The subscriber</param>
+        /// <param name="messageHandler">Callback to receive the message</param>
+        /// <param name="messageSelector">Predicate to filter messages</param>
+        /// <typeparam name="T">Type of the message</typeparam>
         public static void Subscribe<T>(this object subscriber, Action<T> messageHandler, Predicate<T> messageSelector = null)
         {
             Hub.Subscribe(subscriber, messageHandler, messageSelector);
         }
 
+        /// <summary>
+        /// Unsubscribes to all messages. 
+        /// </summary>
+        /// <param name="subscriber">The subscriber</param>
         public static void Unsubscribe(this object subscriber)
         {
             Hub.Unsubscribe(subscriber);
         }
 
+        /// <summary>
+        /// Unsubscribes to messages of type <c>T</c>. 
+        /// </summary>
+        /// <param name="subscriber">The subscriber</param>
+        /// <typeparam name="T">Type of the message</typeparam>
         public static void Unsubscribe<T>(this object subscriber)
         {
             Hub.Unsubscribe(subscriber, (Action<T>)null);
         }
 
-        public static void Unsubscribe<T>(this object subscriber, Action<T> handler)
+        /// <summary>
+        /// Unsubscribes to messages for given callback. 
+        /// </summary>
+        /// <param name="subscriber">The subscriber</param>
+        /// <param name="messageHandler">Callback subscribed to</param>
+        /// <typeparam name="T">Type of the message</typeparam>
+        public static void Unsubscribe<T>(this object subscriber, Action<T> messageHandler)
         {
-            Hub.Unsubscribe(subscriber, handler);
+            Hub.Unsubscribe(subscriber, messageHandler);
         }
     }
 }
