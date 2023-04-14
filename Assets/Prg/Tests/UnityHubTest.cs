201caa5bd3ee1ac0a146d5865f02960121eb3295
Jari Petays <jari.petays@iki.fi>
2023-04-14 12:56:07 +0300

    add UnsubscribeTest

--- Assets/Prg/Tests/UnityHubTest.cs
+++ Assets/Prg/Tests/UnityHubTest.cs
@@ -21,7 +21,7 @@ namespace Prg.Tests
             var hub = game1.GetHub();
             Debug.Log($"<b>test</b> {hub}");
             Assert.IsNotNull(hub);
-            
+
             var callbackCount = 0;
             game1.Subscribe<GameObject>(param =>
             {
@@ -31,7 +31,7 @@ namespace Prg.Tests
             var handlerCount = hub.CheckHandlerCount();
             Assert.AreEqual(1, handlerCount);
             yield return null;
-            
+
             game1.Publish(game1);
             Assert.AreEqual(1, callbackCount);
             yield return null;
@@ -39,7 +39,7 @@ namespace Prg.Tests
             game1.Publish(game1);
             Assert.AreEqual(2, callbackCount);
             yield return null;
-            
+
             // Destroy and invalidate our reference.
             // Subscription should be removed automatically.
             Object.DestroyImmediate(game1);
@@ -57,6 +57,60 @@ namespace Prg.Tests
             yield return null;
 
             // Subscription should be removed automatically.
+            handlerCount = hub.CheckHandlerCount();
+            Assert.AreEqual(0, handlerCount);
+        }
+
+        [UnityTest]
+        public IEnumerator UnsubscribeTest()
+        {
+            var game1 = new GameObject("game1");
+            yield return null;
+            Assert.IsNotNull(game1);
+            Debug.Log($"created {game1}");
+
+            var hub = game1.GetHub();
+            Debug.Log($"<b>test</b> {hub}");
+            Assert.IsNotNull(hub);
+            
+            var callbackCount = 0;
+            game1.Subscribe<GameObject>(param =>
+            {
+                callbackCount += 1;
+                Debug.Log($"callback-1 for {param.name} count {callbackCount}");
+            });
+            var handlerCount = hub.CheckHandlerCount();
+            Assert.AreEqual(1, handlerCount);
+            yield return null;
+            
+            game1.Subscribe<GameObject>(param =>
+            {
+                callbackCount += 1;
+                Debug.Log($"callback-2 for {param.name} count {callbackCount}");
+            });
+            // No callbacks, handlers created.
+            Assert.AreEqual(0, callbackCount);
+            handlerCount = hub.CheckHandlerCount();
+            Assert.AreEqual(2, handlerCount);
+            yield return null;
+            
+            game1.Publish(game1);
+            // Yes callbacks, handlers created.
+            Assert.AreEqual(2, callbackCount);
+            handlerCount = hub.CheckHandlerCount();
+            Assert.AreEqual(2, handlerCount);
+            yield return null;
+            
+            game1.Unsubscribe();
+            // No more callbacks, handlers removed.
+            Assert.AreEqual(2, callbackCount);
+            handlerCount = hub.CheckHandlerCount();
+            Assert.AreEqual(0, handlerCount);
+            yield return null;
+            
+            game1.Publish(game1);
+            // Same as before - no more callbacks, no handlers.
+            Assert.AreEqual(2, callbackCount);
             handlerCount = hub.CheckHandlerCount();
             Assert.AreEqual(0, handlerCount);
         }
