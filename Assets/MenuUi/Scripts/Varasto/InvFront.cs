cd60ac36f792d8daf385c881ee7920c10b5a6702
MrHexxar <122272662+MrHexxar@users.noreply.github.com>
2023-04-14 13:59:03 +0300

    InvFront gets images from a list if they exist
    None of them do exist but they would be got if they did

--- Assets/MenuUi/Scripts/Varasto/InvFront.cs
+++ Assets/MenuUi/Scripts/Varasto/InvFront.cs
@@ -18,10 +18,10 @@ public class InvFront : MonoBehaviour
 
     [Header("Placeholders")] // These should not remain to the finalized game
     [SerializeField] private Sprite _furnImagePlaceholder;
-    [SerializeField] private string _imagePathPlaceholder;
 
-    [Header("Prefabs")]
+    [Header("Non-scene objects")]
     [SerializeField] private GameObject _invSlot;
+    [SerializeField] private List<Sprite> _icons; // Place images in this list for use as icons, but also, the exact name of the image must be set in the GameFurniture string Filename
 
     private List<GameFurniture> _items;
     private List<GameObject> _slotsList = new List<GameObject>();
@@ -29,7 +29,7 @@ public class InvFront : MonoBehaviour
     bool startCompleted = false;
 
     private int _maxSortingBy = 2;
-    private int _sortingBy; // used as a carrier for info on how to sort
+    private int _sortingBy = -1; // used as a carrier for info on how to sort
 
     private void OnEnable()
     {
@@ -38,7 +38,6 @@ public class InvFront : MonoBehaviour
 
     private IEnumerator Begin()
     {
-        _sortingBy = -1; // So that the first sort style is Alphabet
         yield return Storefront.Get().GetAllGameFurnitureYield(result => _items = result.ToList());
 
         MakeSlots();
@@ -55,11 +54,8 @@ public class InvFront : MonoBehaviour
         for (int i = 0; i < _items.Count; i++)
         {
             GameObject newSlot = Instantiate(_invSlot, _content);
-
-            // Adds an event to the new slot that enables it to show information about the furniture it is showcasing
             var slotVal = i;
             newSlot.GetComponent<Button>().onClick.AddListener(() => OnShowInfo(slotVal));
-
             _slotsList.Add(newSlot);
         }
     }
@@ -67,21 +63,21 @@ public class InvFront : MonoBehaviour
     private void SetSlots()
     {
         int i = 0;
-        foreach (GameFurniture furn in _items)
+        foreach (GameFurniture _furn in _items)
         {
             Transform toSet = _slotsList[i].transform;
 
             // Icon - Placeholder
-            toSet.GetChild(0).GetComponent<Image>().sprite = GetImage("null");
+            toSet.GetChild(0).GetComponent<Image>().sprite = GetIcon(_furn.Filename);
 
             // Name
-            toSet.GetChild(1).GetComponent<TMP_Text>().text = furn.Name;
+            toSet.GetChild(1).GetComponent<TMP_Text>().text = _furn.Name;
 
             // Weight
-            toSet.GetChild(2).GetComponent<TMP_Text>().text = furn.Weight + " KG";
+            toSet.GetChild(2).GetComponent<TMP_Text>().text = _furn.Weight + " KG";
 
             // Shape - Placeholder
-            toSet.GetChild(3).GetComponent<Image>().sprite = GetImage("null");
+            toSet.GetChild(3).GetComponent<Image>().sprite = GetIcon(_furn.Shape);
 
             i++;
         }
@@ -113,38 +109,39 @@ public class InvFront : MonoBehaviour
     void OnShowInfo(int slotVal)
     {
         Transform parentSlot = _infoSlot.transform;
+        GameFurniture _furn = _items[slotVal];
 
         // Icon - Placeholder
-        parentSlot.GetChild(0).GetComponent<Image>().sprite = GetImage("null");
+        parentSlot.GetChild(0).GetComponent<Image>().sprite = GetIcon(_furn.Filename);
 
         // Name
-        parentSlot.GetChild(1).GetComponent<TMP_Text>().text = _items[slotVal].Name;
+        parentSlot.GetChild(1).GetComponent<TMP_Text>().text = _furn.Name;
 
         // Weight
-        parentSlot.GetChild(2).GetComponent<TMP_Text>().text = _items[slotVal].Weight + " KG";
+        parentSlot.GetChild(2).GetComponent<TMP_Text>().text = _furn.Weight + " KG";
 
         // Material text
-        parentSlot.GetChild(3).GetComponent<TMP_Text>().text = _items[slotVal].Material;
+        parentSlot.GetChild(3).GetComponent<TMP_Text>().text = _furn.Material;
 
         // Type - Placeholder
-        parentSlot.GetChild(4).GetComponent<Image>().sprite = GetImage("null");
+        parentSlot.GetChild(4).GetComponent<Image>().sprite = GetIcon(_furn.Shape);
 
         // Type Text
-        parentSlot.GetChild(5).GetComponent<TMP_Text>().text = _items[slotVal].Shape;
+        parentSlot.GetChild(5).GetComponent<TMP_Text>().text = _furn.Shape;
 
         _infoSlot.SetActive(true);
     }
 
-    private Sprite GetImage(string path)
-    { // Here will come the strange thingy that gets the images using https://docs.unity3d.com/ScriptReference/Resources.Load.html once i figure out how to handle the folder itself
-        return _furnImagePlaceholder;
-    }
+    private Sprite GetIcon(string name)
+    {
+        Sprite returned = _icons.Find(x => x.name == name);
 
-    private struct FurnSet
-    { // Contains the GameFurniture and some extra for the inventory front to look good
-        GameFurniture _furniture;
-        Sprite _sprite; // Furniture icon
-        Sprite _typeSprite; // Type icon
-    }
+        if (returned == null)
+        {
+            Debug.LogWarning($"No icon of name '{name}' was found");
+            return _furnImagePlaceholder;
+        }
 
+        return returned;
+    }
 }
