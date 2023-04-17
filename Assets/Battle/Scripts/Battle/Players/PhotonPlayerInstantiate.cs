8b0518975516588eca10a4a3244cffc80395d482
Jari Petays <jari.petays@iki.fi>
2023-04-17 13:58:18 +0300

    Connect local player input handler to photon player driver driver (so that PlayerDriverPhoton can be controlled remotely by anybody).

--- Assets/Battle/Scripts/Battle/Players/PhotonPlayerInstantiate.cs
+++ Assets/Battle/Scripts/Battle/Players/PhotonPlayerInstantiate.cs
@@ -130,7 +130,13 @@ namespace Battle.Scripts.Battle.Players
         {
             Assert.IsTrue(player.IsLocal, "player.IsLocal");
             Debug.Log($"{player.GetDebugLabel()} prefab {networkPrefabName}");
-            PhotonNetwork.Instantiate(networkPrefabName, Vector3.zero, Quaternion.identity);
+            var instance = PhotonNetwork.Instantiate(networkPrefabName, Vector3.zero, Quaternion.identity);
+            // Connect local player input handler to photon player driver driver. 
+            var playerDriver = instance.GetComponent<PlayerDriverPhoton>();
+            Assert.IsNotNull(playerDriver, $"top level 'PlayerDriverPhoton' is missing from prefab {networkPrefabName}");
+            var playerInputHandler = Context.GetPlayerInputHandler;
+            playerInputHandler.OnMoveTo = playerDriver.MoveTo;
+            
         }
     }
 }
