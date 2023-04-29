725a3d53e1ba7d285bd79c1464918af68a693025
Jari Petays <jari.petays@iki.fi>
2023-04-29 14:41:42 +0300

    add Button for Select/Change Photon Region (does not work yet)

--- Assets/MenuUi/Scripts/ChangeRegion/ChangeRegionView.cs
+++ Assets/MenuUi/Scripts/ChangeRegion/ChangeRegionView.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Prg.Scripts.Common.Photon;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MenuUi.Scripts.ChangeRegion
 {
@@ -13,6 +14,8 @@ namespace MenuUi.Scripts.ChangeRegion
         [SerializeField] private TextMeshProUGUI _titleText;
         [SerializeField] private GameObject _lineTemplate;
         [SerializeField] private Transform _contentRoot;
+
+        private List<LineData> _lineDataList = new();
 
         public string TitleText
         {
@@ -29,7 +32,7 @@ namespace MenuUi.Scripts.ChangeRegion
         public void ResetView()
         {
             TitleText = string.Empty;
-            DeleteExtraLines(_contentRoot, 0);
+            DeleteExtraLines(_contentRoot, 0, ref _lineDataList);
         }
 
         public void UpdateRegionList(IReadOnlyList<PhotonRegionList.PhotonRegion> regions)
@@ -37,46 +40,40 @@ namespace MenuUi.Scripts.ChangeRegion
             var regionsCount = regions.Count;
             if (_contentRoot.childCount > regionsCount)
             {
-                DeleteExtraLines(_contentRoot, regionsCount);
+                DeleteExtraLines(_contentRoot, regionsCount, ref _lineDataList);
             }
             else if (_contentRoot.childCount < regionsCount)
             {
-                AddMoreTextLines(_contentRoot, regionsCount, _lineTemplate);
+                AddMoreTextLines(_contentRoot, regionsCount, _lineTemplate, ref _lineDataList);
             }
             for (var i = 0; i < _contentRoot.childCount; ++i)
             {
-                var child = _contentRoot.GetChild(i);
-                UpdateTextLine(child, regions[i]);
+                _lineDataList[i].UpdateRegion(regions[i]);
             }
         }
 
-        private static void UpdateTextLine(Transform parent, PhotonRegionList.PhotonRegion region)
-        {
-            var regionCode = parent.GetChild(0).GetComponent<TextMeshProUGUI>();
-            var regionName = parent.GetChild(1).GetComponent<TextMeshProUGUI>();
-            var regionPing = parent.GetChild(2).GetComponent<TextMeshProUGUI>();
-
-            regionCode.text = region.Region;
-            regionName.text = GetRegionName(region.Region);
-            regionPing.text = region.Ping > 0 ? $"{region.Ping} ms" : "---   ";
-        }
-
-        private static void DeleteExtraLines(Transform parent, int minCount)
+        private static void DeleteExtraLines(Transform parent, int minCount, ref List<LineData> lineDataList)
         {
             var childCount = parent.childCount;
             for (var i = childCount - 1; i >= minCount; --i)
             {
                 var child = parent.GetChild(i).gameObject;
+                var index = lineDataList.FindIndex(x => x._parent.Equals(child));
+                if (index >= 0)
+                {
+                    lineDataList.RemoveAt(index);
+                }
                 Destroy(child);
             }
         }
 
-        private static void AddMoreTextLines(Transform parent, int maxCount, GameObject template)
+        private static void AddMoreTextLines(Transform parent, int maxCount, GameObject template, ref List<LineData> lineDataList)
         {
             while (parent.childCount < maxCount)
             {
                 var instance = Instantiate(template, parent);
                 instance.SetActive(true);
+                lineDataList.Add(new LineData(instance));
             }
         }
 
@@ -106,5 +103,42 @@ namespace MenuUi.Scripts.ChangeRegion
             { "us", "USA, East, Washington D.C." },
             { "usw", "USA, West, San José" },
         };
+
+        private class LineData
+        {
+            public readonly GameObject _parent;
+            private readonly TextMeshProUGUI _code;
+            private readonly TextMeshProUGUI _name;
+            private readonly TextMeshProUGUI _ping;
+
+            private PhotonRegionList.PhotonRegion _region;
+
+            public LineData(GameObject parent)
+            {
+                _parent = parent;
+                var textComponents = parent.GetComponentsInChildren<TextMeshProUGUI>();
+                _code = textComponents[0];
+                _name = textComponents[1];
+                _ping = textComponents[2];
+                var button = parent.GetComponentsInChildren<Button>()[0];
+                button.onClick.AddListener(() =>
+                {
+                    if (_parent == null)
+                    {
+                        // We have been destroyed!
+                        return;
+                    }
+                    Debug.Log($"click {_region?.Region}");
+                });
+            }
+
+            public void UpdateRegion(PhotonRegionList.PhotonRegion region)
+            {
+                _region = region;
+                _code.text = region.Region;
+                _name.text = GetRegionName(region.Region);
+                _ping.text = region.Ping > 0 ? $"{region.Ping} ms" : "---   ";
+            }
+        }
     }
 }
