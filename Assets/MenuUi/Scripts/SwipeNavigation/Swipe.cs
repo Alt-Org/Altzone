962173062e89421ac8c4e40382185010bbd97af9
Jari Petays <jari.petays@iki.fi>
2023-05-06 09:31:48 +0300

    koitin laittaa swipe navigoinnin koodin sillä tavalla että itse ymmärrän sen toiminnan paremmin - lähinnä nimesin asioita uusiksi

--- Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
+++ Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
@@ -1,37 +1,39 @@
+using System.Collections.Generic;
 using MenuUi.Scripts.Window;
 using MenuUi.Scripts.Window.ScriptableObjects;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections.Generic;
 
 namespace MenuUi.Scripts.SwipeNavigation
 {
     public class Swipe : MonoBehaviour
     {
+        private const float SlideTouchSensitivity = 5f;
+        private const float SlideMoveSpeedFactor = 2000f;
+
         private PlayerInput _playerInput;
-        private Vector2 startPosition;
-        private Vector2 endPosition;
+        private Vector2 _startPosition;
+        private Vector2 _endPosition;
         private Vector2 _currentPosition;
         private List<Vector2> _defaultPos;
-        private bool _touching;
+        private bool _isTouching;
         private bool _canCheck;
         private bool _canSlide;
 
         [SerializeField] private GameObject[] _slidingUI;
-        
+
         [SerializeField] private WindowDef _prevNaviTarget;
         [SerializeField] private WindowDef _nextNaviTarget;
 
         [SerializeField] private float _distanceToSwitch;
 
-
         private void Awake()
         {
             _defaultPos = new List<Vector2>();
             _playerInput = GetComponent<PlayerInput>();
-            foreach (GameObject sliding in _slidingUI)
+            foreach (var sliding in _slidingUI)
             {
-                _defaultPos.Add(new Vector2(sliding.transform.position.x, sliding.transform.position.y));
+                _defaultPos.Add(sliding.transform.position);
             }
         }
 
@@ -39,76 +41,79 @@ namespace MenuUi.Scripts.SwipeNavigation
         {
             if (context.performed)
             {
-                startPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
-                _touching = true;
+                _startPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
+                _isTouching = true;
                 _canCheck = true;
                 _canSlide = false;
+                return;
             }
             if (context.canceled)
             {
-                
-                endPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
-                if (startPosition.x - _distanceToSwitch > endPosition.x && _nextNaviTarget != null && _canSlide)
+                _endPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
+                if (_startPosition.x - _distanceToSwitch > _endPosition.x && _nextNaviTarget != null && _canSlide)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_nextNaviTarget);
                 }
-                if (startPosition.x + _distanceToSwitch < endPosition.x && _prevNaviTarget != null && _canSlide)
+                if (_startPosition.x + _distanceToSwitch < _endPosition.x && _prevNaviTarget != null && _canSlide)
                 {
                     var windowManager = WindowManager.Get();
                     windowManager.ShowWindow(_prevNaviTarget);
                 }
-                _touching = false;
+                _isTouching = false;
                 _canSlide = false;
             }
         }
 
         private void Update()
         {
-            if (_touching == true)
+            if (_isTouching)
             {
                 _currentPosition = _playerInput.actions["TouchPosition"].ReadValue<Vector2>();
-                swipeCheck();
+                UpdateSwipeState();
             }
-            if (_canSlide == true && _slidingUI[0] != null)
+            if (_canSlide && _slidingUI.Length > 0)
             {
-                foreach (Vector2 slidingPos in _defaultPos)
+                foreach (var slidingPos in _defaultPos)
                 {
-                    foreach (GameObject slidingObj in _slidingUI)
+                    foreach (var slidingObj in _slidingUI)
                     {
-                        slidingObj.transform.position = Vector3.MoveTowards(slidingObj.transform.position, new Vector3(_currentPosition.x - startPosition.x + slidingPos.x, slidingPos.y, 0), 2000 * Time.deltaTime);
+                        slidingObj.transform.position = Vector3.MoveTowards(
+                            slidingObj.transform.position,
+                            new Vector3(_currentPosition.x - _startPosition.x + slidingPos.x, slidingPos.y, 0),
+                            SlideMoveSpeedFactor * Time.deltaTime);
                     }
                 }
             }
             else
             {
-                foreach (Vector2 sliding in _defaultPos)
+                foreach (var sliding in _defaultPos)
                 {
-                    foreach (GameObject slidingObj in _slidingUI)
+                    foreach (var slidingObj in _slidingUI)
                     {
-                        slidingObj.transform.position = Vector3.MoveTowards(slidingObj.transform.position, new Vector3(sliding.x, sliding.y, 0), 2000 * Time.deltaTime);
+                        slidingObj.transform.position = Vector3.MoveTowards(slidingObj.transform.position,
+                            new Vector3(sliding.x, sliding.y, 0),
+                            SlideMoveSpeedFactor * Time.deltaTime);
                     }
                 }
             }
-            
         }
 
-        private void swipeCheck()
+        private void UpdateSwipeState()
         {
-            if(_canCheck == false)
+            if (!_canCheck)
             {
                 return;
             }
-            if(startPosition.y - 5 > _currentPosition.y || startPosition.y + 5 < _currentPosition.y)
+            if (_startPosition.y - SlideTouchSensitivity > _currentPosition.y || _startPosition.y + SlideTouchSensitivity < _currentPosition.y)
             {
                 _canCheck = false;
                 return;
             }
-            if(_currentPosition.x > startPosition.x + 5 || _currentPosition.x < startPosition.x - 5)
+            if (_currentPosition.x > _startPosition.x + SlideTouchSensitivity || _currentPosition.x < _startPosition.x - SlideTouchSensitivity)
             {
                 _canSlide = true;
                 _canCheck = false;
-                return;
             }
         }
     }
