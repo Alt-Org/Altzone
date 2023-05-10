b7269ba41a55b1701f98f98380ddbc39588c8154
Jari Petays <jari.petays@iki.fi>
2023-05-10 09:22:31 +0300

    rename stuff to make code more comprehensible(?)

--- Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
+++ Assets/MenuUi/Scripts/SwipeNavigation/Swipe.cs
@@ -11,7 +11,7 @@ namespace MenuUi.Scripts.SwipeNavigation
     /// </summary>
     public class Swipe : MonoBehaviour
     {
-        private const float SlideTouchSensitivity = 5f;
+        private const float SlideTouchThreshold = 5f;
         private const float SlideMoveSpeedFactor = 2000f;
 
         private PlayerInput _playerInput;
@@ -20,11 +20,14 @@ namespace MenuUi.Scripts.SwipeNavigation
         private Vector2 _endPosition;
         private Vector2 _currentPosition;
         private List<Vector2> _defaultPos;
+
         // This is only to reduce garbage collectors work avoiding Vector3/Vector2 conversions in Update()
         private Vector3 _tempMoveTargetPos = Vector3.zero;
+
+        // Swipe state control
         private bool _isTouching;
-        private bool _canCheck;
-        private bool _canSlide;
+        private bool _isSwipingStarted;
+        private bool _isSlidingHorizontally;
 
         [Header("UI components that should slide"), SerializeField] private GameObject[] _slidingUI;
 
@@ -59,25 +62,30 @@ namespace MenuUi.Scripts.SwipeNavigation
             {
                 _startPosition = _touchPositionInputAction.ReadValue<Vector2>();
                 _isTouching = true;
-                _canCheck = true;
-                _canSlide = false;
+                // Swipe to any direction not over threshold yet
+                _isSwipingStarted = false;
+                // Slide horizontally not started yet
+                _isSlidingHorizontally = false;
                 return;
             }
             if (context.canceled)
             {
                 _endPosition = _touchPositionInputAction.ReadValue<Vector2>();
-                if (_startPosition.x - _distanceToSwitch > _endPosition.x && _nextNaviTarget != null && _canSlide)
+                if (_isSlidingHorizontally)
                 {
-                    var windowManager = WindowManager.Get();
-                    windowManager.ShowWindow(_nextNaviTarget);
-                }
-                if (_startPosition.x + _distanceToSwitch < _endPosition.x && _prevNaviTarget != null && _canSlide)
-                {
-                    var windowManager = WindowManager.Get();
-                    windowManager.ShowWindow(_prevNaviTarget);
+                    if (_nextNaviTarget != null && _startPosition.x - _distanceToSwitch > _endPosition.x)
+                    {
+                        var windowManager = WindowManager.Get();
+                        windowManager.ShowWindow(_nextNaviTarget);
+                    }
+                    if (_prevNaviTarget != null && _startPosition.x + _distanceToSwitch < _endPosition.x)
+                    {
+                        var windowManager = WindowManager.Get();
+                        windowManager.ShowWindow(_prevNaviTarget);
+                    }
                 }
                 _isTouching = false;
-                _canSlide = false;
+                _isSlidingHorizontally = false;
             }
         }
 
@@ -86,14 +94,18 @@ namespace MenuUi.Scripts.SwipeNavigation
             if (_isTouching)
             {
                 _currentPosition = _touchPositionInputAction.ReadValue<Vector2>();
-                UpdateSwipeState();
-            }
-            if (_canSlide)
-            {
-                foreach (var slidingPos in _defaultPos)
+                if (!_isSwipingStarted)
                 {
-                    foreach (var slidingObj in _slidingUI)
+                    CheckIfSwipeDetected();
+                }
+            }
+            foreach (var slidingPos in _defaultPos)
+            {
+                foreach (var slidingObj in _slidingUI)
+                {
+                    if (_isSlidingHorizontally)
                     {
+                        // Move away
                         _tempMoveTargetPos.x = _currentPosition.x - _startPosition.x + slidingPos.x;
                         _tempMoveTargetPos.y = slidingPos.y;
                         slidingObj.transform.position = Vector3.MoveTowards(
@@ -101,37 +113,30 @@ namespace MenuUi.Scripts.SwipeNavigation
                             _tempMoveTargetPos,
                             SlideMoveSpeedFactor * Time.deltaTime);
                     }
-                }
-                return;
-            }
-            foreach (var sliding in _defaultPos)
-            {
-                foreach (var slidingObj in _slidingUI)
-                {
-                    _tempMoveTargetPos.x = sliding.x;
-                    _tempMoveTargetPos.y = sliding.y;
-                    slidingObj.transform.position = Vector3.MoveTowards(slidingObj.transform.position,
-                        _tempMoveTargetPos,
-                        SlideMoveSpeedFactor * Time.deltaTime);
+                    else
+                    {
+                        // This is called on every frame - unless sliding horizontally!
+                        _tempMoveTargetPos.x = slidingPos.x;
+                        _tempMoveTargetPos.y = slidingPos.y;
+                        slidingObj.transform.position = Vector3.MoveTowards(slidingObj.transform.position,
+                            _tempMoveTargetPos,
+                            SlideMoveSpeedFactor * Time.deltaTime);
+                    }
                 }
             }
         }
 
-        private void UpdateSwipeState()
+        private void CheckIfSwipeDetected()
         {
-            if (!_canCheck)
+            if (_startPosition.y - SlideTouchThreshold > _currentPosition.y || _startPosition.y + SlideTouchThreshold < _currentPosition.y)
             {
+                _isSwipingStarted = true;
                 return;
             }
-            if (_startPosition.y - SlideTouchSensitivity > _currentPosition.y || _startPosition.y + SlideTouchSensitivity < _currentPosition.y)
+            if (_currentPosition.x > _startPosition.x + SlideTouchThreshold || _currentPosition.x < _startPosition.x - SlideTouchThreshold)
             {
-                _canCheck = false;
-                return;
-            }
-            if (_currentPosition.x > _startPosition.x + SlideTouchSensitivity || _currentPosition.x < _startPosition.x - SlideTouchSensitivity)
-            {
-                _canSlide = true;
-                _canCheck = false;
+                _isSwipingStarted = true;
+                _isSlidingHorizontally = true;
             }
         }
     }
