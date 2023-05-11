089fe7ffe7bb114446f61ccfe35488fce4fd49cb
Jari Petays <jari.petays@iki.fi>
2023-05-11 07:09:27 +0300

    add PhotonRoomAutoCreate (helper)

--- Assets/Prg/Scripts/Common/Photon/PhotonPlayerAutoInstantiate.cs
+++ Assets/Prg/Scripts/Common/Photon/PhotonPlayerAutoInstantiate.cs
@@ -10,10 +10,17 @@ namespace Prg.Scripts.Common.Photon
     public class PhotonPlayerAutoInstantiate : MonoBehaviour
     {
         [Header("Settings"), SerializeField] private GameObject _networkPrefab;
+        [SerializeField] private bool _isAutoCreateRoom;
 
         private IEnumerator Start()
         {
             Debug.Log($"wait for room {PhotonNetwork.NetworkClientState}");
+            if (!PhotonNetwork.InRoom && _isAutoCreateRoom)
+            {
+                // Create a room for us
+                gameObject.AddComponent<PhotonRoomAutoCreate>();
+            }
+            // Wait for a room.
             while (enabled)
             {
                 if (PhotonNetwork.InRoom)
@@ -22,10 +29,12 @@ namespace Prg.Scripts.Common.Photon
                 }
                 yield return null;
             }
-            if (!PhotonNetwork.InRoom)
+            if (!enabled || !PhotonNetwork.InRoom)
             {
+                // Disabled or not in a room - no can do!
                 yield break;
             }
+            // Instantiate Photon player.
             Debug.Log($"PhotonNetwork.Instantiate {_networkPrefab.name} {PhotonNetwork.NetworkClientState}", _networkPrefab);
             PhotonNetwork.Instantiate(_networkPrefab.name, Vector3.zero, Quaternion.identity);
         }
