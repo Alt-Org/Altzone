2fb41bc70f01bc1b341b7c71a5295c940cd7d1a5
Jari Petays <jari.petays@iki.fi>
2023-05-16 08:34:35 +0300

    use same BundleVersionCodeValue in all builds so that they can connect with Photon

--- Assets/Prg/BundleVersionCode.cs
+++ Assets/Prg/BundleVersionCode.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 namespace Prg
 {
     /// <summary>
-    /// Machine generated code!<br />
+    /// Machine generated code below!<br />
     /// Last used Android BundleVersionCode and CompiledOnDate for any build. We go mobile first.
     /// </summary>
     public static class BundleVersionCode
@@ -12,11 +12,11 @@ namespace Prg
         private const string BundleVersionCodeValue = ".57";
         private const string CompiledOnDateValue = "2023-12-05 09:47";
 
+        public static string Get => BundleVersionCodeValue;
+
 #if UNITY_EDITOR
-        public static string Get => PlayerSettings.Android.bundleVersionCode.ToString();
         public static string CompiledOnDate => DateTime.Now.FormatMinutes();
 #else
-        public static string Get => BundleVersionCodeValue;
         public static string CompiledOnDate => CompiledOnDateValue;
 #endif
     }
