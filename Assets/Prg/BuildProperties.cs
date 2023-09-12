753ff4970d85593c420d0bc0ee06d859ee304619
psykkis <psykkis@hotmail.com>
2023-09-12 14:40:44 +0300

    ALT Zone 71 build
    uusi animaatio

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "70";
-        private const string CompiledOnDateValue = "2023-08-09 15:01";
+        private const string BundleVersionCodeValue = "71";
+        private const string CompiledOnDateValue = "2023-12-09 14:33";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
