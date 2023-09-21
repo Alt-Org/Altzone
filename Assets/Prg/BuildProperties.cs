98a73b795d0e2ac2751a44c72f4b53f32453bc6b
psykkis <psykkis@hotmail.com>
2023-09-21 10:43:14 +0300

    ALT Zone 75
    - grafiikka-animaatiotestaus päättyy: otettu ylimääräinen animaatio pois

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "74";
-        private const string CompiledOnDateValue = "2023-21-09 10:21";
+        private const string BundleVersionCodeValue = "75";
+        private const string CompiledOnDateValue = "2023-21-09 10:37";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
