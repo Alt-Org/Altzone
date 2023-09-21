8b56be546119a3a98604c7a7750e729cbf25f300
psykkis <psykkis@hotmail.com>
2023-09-21 09:12:40 +0300

    ALT Zone 73
    grafiikkatesti nro 2
    
    - tehty myös ruudun kääntyminen sen mukaan miten pelaaja pitää puhelinta kädessään
    - ryöstö tulee voittajille

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "72";
-        private const string CompiledOnDateValue = "2023-21-09 08:42";
+        private const string BundleVersionCodeValue = "73";
+        private const string CompiledOnDateValue = "2023-21-09 09:05";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
