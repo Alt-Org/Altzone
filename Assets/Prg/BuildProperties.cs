d06d9987b4459bf4a562706166a24d988a15d63c
psykkis <psykkis@hotmail.com>
2023-09-25 16:39:26 +0300

    ALT Zone 77
    - battlen musiikki vaihdettu oikearytmiseksi

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "76";
-        private const string CompiledOnDateValue = "2023-25-09 16:10";
+        private const string BundleVersionCodeValue = "77";
+        private const string CompiledOnDateValue = "2023-25-09 16:34";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
