36a4b81f949e0623b0517f5c6eb8e1a7157f0ae3
psykkis <psykkis@hotmail.com>
2023-11-27 15:05:12 +0200

    ALT Zone 83
    vaihdettu pelimekaniikka siten että aina kun pelihahmot ovat lähekkäin, kilpi ei ole aktiivinen ja kun ne ovat kauempana niin kilpi on aktiivinen > pelihahmo suojautuu siis vain ammusta päin, ei tiimikaveria päin

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "82";
-        private const string CompiledOnDateValue = "2023-24-11 15:49";
+        private const string BundleVersionCodeValue = "83";
+        private const string CompiledOnDateValue = "2023-27-11 15:01";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
