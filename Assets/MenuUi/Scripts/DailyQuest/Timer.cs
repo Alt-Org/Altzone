d71dc243bfd117d4d78b363edc3e783eb153c243
JuhoKosonen <s2100995@edu.bc.fi>
2024-01-19 17:47:16 +0200

    dailyquest

--- Assets/MenuUi/Scripts/DailyQuest/Timer.cs
+++ Assets/MenuUi/Scripts/DailyQuest/Timer.cs
@@ -11,21 +11,21 @@ public class Timer : MonoBehaviour
 
     void Start()
     {
-     
+
         DateTime now = DateTime.Now;
-       
+
         midnight = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0, 0).AddDays(1);
     }
 
     void Update()
     {
         Time.timeScale = 1f;
-      
+
         TimeSpan timeRemaining = midnight - DateTime.Now;
-        
+
         if (DateTime.Now >= midnight)
         {
-            
+
             midnight = midnight.AddDays(1);
         }
 
