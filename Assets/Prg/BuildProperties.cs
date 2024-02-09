2e659138ba2823504db950ed614baebf81de8b9c
psykkis <psykkis@hotmail.com>
2024-02-09 10:21:46 +0200

    ALT Zone 93
    - palautettu ammuksen tunnesanaäänet

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "92";
-        private const string CompiledOnDateValue = "2024-08-02 22:03";
+        private const string BundleVersionCodeValue = "93";
+        private const string CompiledOnDateValue = "2024-09-02 10:18";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
