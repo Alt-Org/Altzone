7aee9e2d6a9d8087a6574658f7133f45522a129f
psykkis <psykkis@hotmail.com>
2024-02-12 13:52:58 +0200

    webgl96

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -9,7 +9,7 @@ namespace Prg
     public static class BuildProperties
     {
         private const string BundleVersionCodeValue = "96";
-        private const string CompiledOnDateValue = "2024-12-02 12:32";
+        private const string CompiledOnDateValue = "2024-12-02 13:03";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
