1bc987e51a64da4de717cec4202f1b684e5d0d82
psykkis <psykkis@hotmail.com>
2024-02-12 12:54:55 +0200

    ALT Zone 96
    - Unity-versio päivitetty 2022.3.19f1
    - Emoji-tuki chattiin
    - WebGL näppäimistö toimii mobiililaitteilla

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "95";
-        private const string CompiledOnDateValue = "2024-09-02 11:23";
+        private const string BundleVersionCodeValue = "96";
+        private const string CompiledOnDateValue = "2024-12-02 12:32";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
