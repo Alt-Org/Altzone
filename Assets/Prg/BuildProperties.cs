45f816a1b494cb402bb83c1748ac2f266bec86db
psykkis <psykkis@hotmail.com>
2024-03-18 19:25:03 +0200

    ALT Zone 99
    - Sielunkodin päivitys: zoomausominaisuus, huoneista poistuminen korjattu, WebGL ei enää yritä asettaa ruudunkääntö asetuksia, torninäkymässä huonekaluja ei voi enää poimia ylös ilman että muokkausmoodi on päällä

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "98";
-        private const string CompiledOnDateValue = "2024-18-03 13:28";
+        private const string BundleVersionCodeValue = "99";
+        private const string CompiledOnDateValue = "2024-18-03 19:23";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
