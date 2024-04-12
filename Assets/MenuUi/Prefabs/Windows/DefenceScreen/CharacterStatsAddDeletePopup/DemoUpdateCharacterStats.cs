257261fd926722e53cec39ae3851abee6f7453bd
AMSemse <112924939+AMSemse@users.noreply.github.com>
2024-04-12 13:28:03 +0300

    Character stats window_WIP

--- Assets/MenuUi/Prefabs/Windows/DefenceScreen/CharacterStatsAddDeletePopup/DemoUpdateCharacterStats.cs
+++ Assets/MenuUi/Prefabs/Windows/DefenceScreen/CharacterStatsAddDeletePopup/DemoUpdateCharacterStats.cs
@@ -1,39 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DemoUpdateCharacterStats : MonoBehaviour
 {
+    public TMP_Text CharacterName;
     public TMP_Text SpeedNumber;
     public TMP_Text ResistanceNumber;
     public TMP_Text AttackNumber;
     public TMP_Text DefenceNumber;
 
     public string DemoCharacterName = "yeyeye";
-    public float Speed = 3f;
-    public float Resistance = 6f;
-    public float Attack = -2f;
-    public float Defence = 2f;
-    public float Hp;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {    
         SpeedNumber = GameObject.Find("Speed/StatNumber").GetComponent<TMP_Text>();
         ResistanceNumber = GameObject.Find("Resistance/StatNumber").GetComponent<TMP_Text>();
         AttackNumber = GameObject.Find("Attack/StatNumber").GetComponent<TMP_Text>();
         DefenceNumber = GameObject.Find("Defence/StatNumber").GetComponent<TMP_Text>();
+        CharacterName = GameObject.Find("CharacterImagePanel/CharacterName").GetComponent<TMP_Text>();
+
+        SetDefaultCharacterStats();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpeedNumber.text = Speed.ToString();
-        ResistanceNumber.text = Resistance.ToString();
-        AttackNumber.text = Attack.ToString();
-        DefenceNumber.text = Defence.ToString();
+        
     }
 
+
+    
+    public void UpdateCharacterSpeed()
+    {
+        Debug.Log("eeyeeyeeee");
+        //SpeedNumber.text = DemoCharacterStatsStatic.defaultCharacterStats[4].ToString() + 1;
+    }
+
+    public void SetDefaultCharacterStats()
+    {
+        SpeedNumber.text = DemoCharacterStatsStatic.defaultCharacterStats[4].ToString();
+        ResistanceNumber.text = DemoCharacterStatsStatic.defaultCharacterStats[3].ToString();
+        AttackNumber.text = DemoCharacterStatsStatic.defaultCharacterStats[2].ToString();
+        DefenceNumber.text = DemoCharacterStatsStatic.defaultCharacterStats[1].ToString();
+        CharacterName.text = DemoCharacterName;
+    }
 
 }
