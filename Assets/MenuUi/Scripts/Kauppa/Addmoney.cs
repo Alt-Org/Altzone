9269558f3f68bc0f8fed1db70ae2279886ad1364
Rasmuspa <rasmus.pasonen@gmail.com>
2024-04-25 16:34:55 +0300

    Kolikkokauppa muokkauksia sekä konfirmaatio ikkuna lisäys

--- Assets/MenuUi/Scripts/Kauppa/Addmoney.cs
+++ Assets/MenuUi/Scripts/Kauppa/Addmoney.cs
@@ -20,8 +20,48 @@ public class Addmoney : MonoBehaviour
         money.text = moneyAmount.ToString();
     }
 
-    public void Addcoins()
+    // Method for adding 10 coins
+    public void Add10Coins()
     {
-        moneyAmount += 10;
+        AddCoins(10);
+    }
+
+    // Method for adding 20 coins
+    public void Add20Coins()
+    {
+        AddCoins(20);
+    }
+
+    // Method for adding 30 coins
+    public void Add30Coins()
+    {
+        AddCoins(30);
+    }
+    // Method for adding 40 coins
+    public void Add40Coins()
+    {
+        AddCoins(40);
+    }
+    // Method for adding 50 coins
+    public void Add50Coins()
+    {
+        AddCoins(50);
+    }
+    // Method for adding 60 coins
+    public void Add60Coins()
+    {
+        AddCoins(60);
+    }
+
+    // Method for adding a custom amount of coins
+    public void AddCustomCoins(int amount)
+    {
+        AddCoins(amount);
+    }
+
+    // General method to add coins
+    private void AddCoins(int amount)
+    {
+        moneyAmount += amount;
     }
 }
