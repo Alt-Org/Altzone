84fa1f19cd93f75f3ed9f1e58ed259ad4d02f938
Bigshower <ferisbusiness1@gmail.com>
2024-05-02 12:19:31 +0300

    Lisätty votinglist ServerClan scriptiin

--- Assets/MenuUi/Scripts/ServerClasses/ServerClan.cs
+++ Assets/MenuUi/Scripts/ServerClasses/ServerClan.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary>
 /// Clan object received from the server
@@ -17,4 +18,73 @@ public class ServerClan
     public string id { get; set; }
     public int? furnitureCount { get; set; }
     public int? raidRoomCount { get; set; }
+
+    // Luokka, joka sis‰lt‰‰ yksitt‰isen ‰‰nestyksen tiedot
+    public class Voting
+    {
+        public string ItemName { get; set; }
+        public int Votes { get; set; }
+
+        public Voting(string itemName)
+        {
+            ItemName = itemName;
+            Votes = 0;
+        }
+    }
+
+    // Luokka, joka edustaa serverilt‰ saapuvaa Clan-oliota
+
+
+    // Lista Voting-objekteja ‰‰nestyst‰ varten
+    public List<Voting> VoteList { get; private set; }
+
+    void Awake()
+    {
+        // Alusta ‰‰nestyslista
+        VoteList = new List<Voting>();
+    }
+
+    // Metodi, jolla voi lis‰t‰ uuden ‰‰nestyksen listalle
+    public void AddVoting(string itemName)
+    {
+        VoteList.Add(new Voting(itemName));
+    }
+}
+
+// Luokka, joka hallinnoi serverin tietoja
+public class ServerManager : MonoBehaviour
+{
+    // Singleton-instanssi ServerManager-luokasta
+    public static ServerManager ManagerInstance { get; private set; }
+
+    // ServerClan-instanssi
+    public ServerClan Clan { get; private set; }
+
+    void Awake()
+    {
+        // Varmista, ett‰ on vain yksi instanssi olemassa
+        if (ManagerInstance == null)
+        {
+            ManagerInstance = this;
+            DontDestroyOnLoad(gameObject);
+            GetComponentInChildren<ServerClan>(); // Haetaan ServerClan-instanssi t‰m‰n GameObjectin alihierarkiasta
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+// Luokka, joka sis‰lt‰‰ yksitt‰isen ‰‰nestyksen tiedot
+public class Voting
+{
+    public string itemName { get; set; }
+    public int votes { get; set; }
+
+    public Voting(string name)
+    {
+        itemName = name;
+        votes = 0;
+    }
 }
