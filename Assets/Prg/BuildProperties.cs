0d7d86dfae93dfa3d1787503a86932d1ace9989b
psykkis <psykkis@hotmail.com>
2024-05-12 17:50:32 +0300

    ALT Zone 106
    - sielunkoti: äänimaisemaa lisätty, vaakatila ja zoom-toiminto lisätty
    - pääpainikkeiden korostus (entisen himmeyden tilalle)
    - pelaajaprofiilin muokkaus: peliaika ja hiilijalanjälki
    - tunnottomat-luokan pelihahmon erikoistaito
    - klaaniin liittyminen aloitettu
    - kolikkokauppa toimii joka ikkunassa
    - uusi varastopainike

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "105";
-        private const string CompiledOnDateValue = "2024-06-05 15:18";
+        private const string BundleVersionCodeValue = "106";
+        private const string CompiledOnDateValue = "2024-12-05 17:46";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
