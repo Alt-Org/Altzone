b33cbc4afbbe62a6158ac9549586ad8d601f17b5
psykkis <psykkis@hotmail.com>
2024-05-19 19:24:48 +0300

    ALT Zone 109 build
    - daily task siistitty
    - fontteja korjattu
    - leaderboard + pisteet
    - sielunkoti fixauksia

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "108";
-        private const string CompiledOnDateValue = "2024-17-05 14:42";
+        private const string BundleVersionCodeValue = "109";
+        private const string CompiledOnDateValue = "2024-19-05 19:18";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
