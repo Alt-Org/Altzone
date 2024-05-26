dcf6e6455581d42f873af19c6d2ee6b23f4de5f9
psykkis <psykkis@hotmail.com>
2024-05-26 17:59:58 +0300

    ALT Zone 110
    - Battlessa: uusi tausta, skaalattu ruudukko, desensitisaatiohahmon demografiikka, kilven maksimiosumien arvon muutos, hahmojen sijanti indikaattori loistepallolla, egotismi- ja desensitisaatiohahmojen skaalaus uuteen ruudukkoon
    - Sielunkoti: äänimanageri luotu, huonekalujen grafiikkaa fiksattu

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "109";
-        private const string CompiledOnDateValue = "2024-19-05 19:27";
+        private const string BundleVersionCodeValue = "110";
+        private const string CompiledOnDateValue = "2024-26-05 17:50";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
