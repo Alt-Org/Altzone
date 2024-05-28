ed4bd8e6d77db8e390b9743181943066d7ac83e4
psykkis <psykkis@hotmail.com>
2024-05-28 19:26:31 +0300

    ALT Zone 111
    - SDK korjattu
    - battlen kenttäkoon korjaus
    - daily taskin tehtävälehdyke
    - Alustava korjaus kameran sijainnin asettamiselle kun laitetta käännetään

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "110";
-        private const string CompiledOnDateValue = "2024-26-05 18:04";
+        private const string BundleVersionCodeValue = "111";
+        private const string CompiledOnDateValue = "2024-28-05 19:22";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
