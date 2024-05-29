f7979cb33b595031b58e81f883ab2ccd0bbfa71a
psykkis <psykkis@hotmail.com>
2024-05-29 10:47:25 +0300

    ALT Zone 112
    - pelin kuvake laitettu
    - sielunkoti: huoneen koko päivitetty, huonekalujen sijoittelua ja kokoa fixattu, alustava avatar laitettu huoneisiin liikkumaan

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "111";
-        private const string CompiledOnDateValue = "2024-28-05 19:22";
+        private const string BundleVersionCodeValue = "112";
+        private const string CompiledOnDateValue = "2024-29-05 10:40";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
