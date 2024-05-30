892c46b5c99a65379e77f00a79df68cef4b43f23
psykkis <psykkis@hotmail.com>
2024-05-30 07:34:19 +0300

    ALT Zone 113
    - avatarin liikkuminen sielunkodissa reaaliaikaisesti
    - hahmogallerian näkymä
    - kilven aktivointialuetta pienennetty
    - säätöä kohti uutta buildisysteemiä

--- Assets/Prg/BuildProperties.cs
+++ Assets/Prg/BuildProperties.cs
@@ -8,8 +8,8 @@ namespace Prg
     /// </summary>
     public static class BuildProperties
     {
-        private const string BundleVersionCodeValue = "112";
-        private const string CompiledOnDateValue = "2024-29-05 10:49";
+        private const string BundleVersionCodeValue = "113";
+        private const string CompiledOnDateValue = "2024-30-05 07:27";
 
         public static string BundleVersionCode => BundleVersionCodeValue;
 
