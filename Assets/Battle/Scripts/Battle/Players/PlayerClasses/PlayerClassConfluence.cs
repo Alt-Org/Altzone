67e7b9d2e9b325c817962d8d120e44a5ab178464
Hanska7 <henri.hertzberg@gmail.com>
2024-06-04 17:50:53 +0300

    Confluence hahmouokan alustava ShieldFlipperin määrittely ilman määrittelyjä kahdelle kilpiluokalle

--- Assets/Battle/Scripts/Battle/Players/PlayerClasses/PlayerClassConfluence.cs
+++ Assets/Battle/Scripts/Battle/Players/PlayerClasses/PlayerClassConfluence.cs
@@ -1,18 +1,55 @@
 using System;
 using UnityEngine;
 
+
+
+
 namespace Battle.Scripts.Battle.Players
 {
     internal class PlayerClassConfluence : MonoBehaviour, IPlayerClass
     {
+
+
+        // Variables
+
+        [SerializeField] GameObject _shieldGumball;
+        [SerializeField] GameObject _shieldPopped;
+
+        private int _shieldTurn = 0;
+
+
+
+
+
+
+
+
         [Obsolete("SpecialAbilityOverridesBallBounce is deprecated, please use return value of OnBallShieldCollision instead.")]
         public bool SpecialAbilityOverridesBallBounce => false;
 
         public bool OnBallShieldCollision()
         { return true; }
 
+
+
         public void OnBallShieldBounce()
-        { }
+        {
+            if (_shieldTurn == 0)
+            {
+                _shieldTurn++;
+                ShieldFlipper();
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "_shieldTurn set to 1", _syncedFixedUpdateClock.UpdateCount));
+            }
+            else
+            {
+                _shieldTurn = 0;
+                ShieldFlipper();
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "_shieldTurn set to 0", _syncedFixedUpdateClock.UpdateCount));
+            }
+
+
+        }
+
 
         [Obsolete("ActivateSpecialAbility is deprecated, please use OnBallShieldCollision and/or OnBallShieldBounce instead.")]
         public void ActivateSpecialAbility()
@@ -29,6 +66,21 @@ namespace Battle.Scripts.Battle.Players
         private void Start()
         {
             _syncedFixedUpdateClock = Context.GetSyncedFixedUpdateClock;
+        }
+
+
+        private void ShieldFlipper() {
+
+            if (_shieldTurn == 0)
+            {
+                _shieldGumball.SetActive(true);
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Shield is set to _shieldGumball", _syncedFixedUpdateClock.UpdateCount));
+            }
+            else
+            {
+                _shieldPopped.SetActive(true);
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Shield is set to _shieldPopped", _syncedFixedUpdateClock.UpdateCount));
+            }
         }
     }
 }
