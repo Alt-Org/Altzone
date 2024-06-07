b6e20aee731880b27f939f8502daaaff40ade32b
Niklas <niklas.tontti603619@gmail.com>
2024-06-07 19:38:34 +0300

    updated battle to work with previous commit
    changed photon battle to use photon battle room defined in the altzone assembly
    updated player driver photon

--- Assets/Battle/Scripts/Battle/Players/PlayerDriverPhoton.cs
+++ Assets/Battle/Scripts/Battle/Players/PlayerDriverPhoton.cs
@@ -172,7 +172,7 @@ namespace Battle.Scripts.Battle.Players
         #region Message Listeners
         private void OnTeamsReadyForGameplay(TeamsAreReadyForGameplay data)
         {
-            _playerActor.SetRotation(_teamNumber == PhotonBattle.TeamAlphaValue ?  0 : 180f);
+            _playerActor.SetRotation(_teamNumber == PhotonBattle.TeamAlphaValue ? 0 : 180f);
             _playerActor.SetSpriteVariant(data.LocalPlayer.TeamNumber == _teamNumber ? PlayerActor.SpriteVariant.A : PlayerActor.SpriteVariant.B);
             _playerActor.ResetSprite();
         }
