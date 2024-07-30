b52af1bdebec1ecbd70d11cb4bb4193dfda6fd1c
Hanska7 <henri.hertzberg@gmail.com>
2024-07-30 17:40:24 +0300

    Confluent classin toiminnallisuus toimimaan

--- Assets/Battle/Scripts/Battle/Players/PlayerClasses/PlayerClassConfluent.cs
+++ Assets/Battle/Scripts/Battle/Players/PlayerClasses/PlayerClassConfluent.cs
@@ -14,14 +14,20 @@ namespace Battle.Scripts.Battle.Players
         [SerializeField] private GameObject _shieldGumball;
         [SerializeField] private GameObject _shieldPopped;
         [SerializeField] private ShieldManager _shieldManager;
-        [SerializeField] private int _teammateVacuumDuration;
-        [SerializeField] private int _teammateVacuumStrength;
-        [SerializeField] private int _teammateVacuumStrengthIncrement;
+
+        [Header("Teammate Vacuum Strength")]
+        [SerializeField] private float _teammateVacuumStrength;
+        [SerializeField] private float _teammateVacuumStrengthIncrement;
+
+        [Header("Teammate Vacuum Timer")]
+        [SerializeField] private int _teammateVacuumDuration; 
+        [SerializeField] private int _teammateVacuumDurationIncrement;
 
 
 
 
-        private int _shieldTurn = 0;
+
+       
 
 
         public bool BounceOnBallShieldCollision => true;
@@ -31,11 +37,20 @@ namespace Battle.Scripts.Battle.Players
         {
             Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "OnBallShieldCollision called", _syncedFixedUpdateClock.UpdateCount));
 
+            //Timer on
+            //Increase vacuum strength
 
-            // Laita timer p‰‰lle
-            // Laita voimaa kovemmalle
-            _teammateVacuumTimer = _teammateVacuumDuration;
-            
+
+            _teammateVacuumState = true;
+            _teammateVacuumStrength += _teammateVacuumStrengthIncrement;
+
+            Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "TeammateVacuum on", _syncedFixedUpdateClock.UpdateCount));
+            Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "teammateVacuumStrength is "+ _teammateVacuumStrength, _syncedFixedUpdateClock.UpdateCount));
+
+
+            _teammateVacuumDuration += _teammateVacuumDurationIncrement;
+
+
         }
 
 
@@ -62,21 +77,22 @@ namespace Battle.Scripts.Battle.Players
         }
 
 
+        private IDriver _driver;
 
+        private int _shieldTurn = 0;
+
+        private Transform _teammateTransform;
+
+        private bool _teammateVacuumState = false;
+        private int _teammateVacuumTimer;
+
+        
 
 
         // Debug
         private const string DEBUG_LOG_NAME = "[BATTLE] [PLAYER CLASS CONFLUENT] ";
         private const string DEBUG_LOG_NAME_AND_TIME = "[{0:000000}] " + DEBUG_LOG_NAME;
         private SyncedFixedUpdateClock _syncedFixedUpdateClock; // only needed for logging time
-
-        private Transform _teammateTransform;
-
-        private int _teammateVacuumTimer;
-
-        
-        
-        
 
 
         // debug
@@ -85,6 +101,9 @@ namespace Battle.Scripts.Battle.Players
             _syncedFixedUpdateClock = Context.GetSyncedFixedUpdateClock;
 
             this.Subscribe<TeamsAreReadyForGameplay>(OnTeamsAreReadyForGameplay);
+
+            _teammateVacuumStrength -= _teammateVacuumStrengthIncrement;
+            _teammateVacuumDuration -= _teammateVacuumDurationIncrement;
 
         }
 
@@ -96,35 +115,90 @@ namespace Battle.Scripts.Battle.Players
 
             foreach (IDriver driver in data.AllDrivers)
             {
-                if (driver.PlayerActor == playerActor) teamnumber = driver.TeamNumber;
-            }
+                if (driver.PlayerActor == playerActor)
+                {
+                    teamnumber = driver.TeamNumber;
+                    _driver = driver;
 
+
+                }
+            }
 
 
             foreach (IDriver driver in data.AllDrivers)
             {
-                if (driver.TeamNumber == teamnumber && driver.PlayerActor != playerActor);
+                if (driver.TeamNumber == teamnumber && driver.PlayerActor != playerActor)
+                {
+
+
+                    _teammateTransform = driver.ActorShieldTransform;
+
+
+                    
+
+                }
             }
-
-
-
         }
+
+
+        
+
 
 
 
         private void FixedUpdate()
         {
 
+            if (_teammateVacuumState == true && _teammateVacuumDuration > _teammateVacuumTimer)
 
-            //Siirr‰ tiimikaveria l‰hemm‰s timerin ajan strengthin perusteella
+            {
+                _teammateVacuumTimer++;
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "TeammateVacuumTimer is " + _teammateVacuumTimer, _syncedFixedUpdateClock.UpdateCount));
 
 
-            //transform on oma sijainti
-            
+                //transform on oma sijainti
+                //_teammateTransform on kaverin sijainti
 
+
+
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Own position is " + _driver.ActorShieldTransform.position, _syncedFixedUpdateClock.UpdateCount));
+
+                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Teammate position is " + _teammateTransform.position, _syncedFixedUpdateClock.UpdateCount));
+
+
+                //Siirr‰ itse‰ l‰hemm‰s tiimikaveria timerin ajan strengthin perusteella
+
+
+
+                Vector3 newPosition = _driver.ActorShieldTransform.position + (_teammateTransform.position - _driver.ActorShieldTransform.position).normalized * (_teammateVacuumStrength/(float)SyncedFixedUpdateClock.UPDATES_PER_SECOND);
+                _driver.ActorCharacterTransform.position = newPosition;
+                _driver.ActorShieldTransform.position = newPosition;
+
+
+
+
+
+                
+
+
+
+                //Laske tiimikaveri suhteessa positioon eli vektori C eli B-A
+
+                //Normalisoi C vektori
+
+                //Kerro C vektori ajalla ja voimalla eli saat siirtym‰n
+
+
+
+
+            }
+            else {
+                _teammateVacuumState = false;
+                _teammateVacuumTimer = 0;
+            }
         }
 
-        
+
 
 
 
