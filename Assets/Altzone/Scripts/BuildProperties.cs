d11f62150c6a8f3019beab9f7e27760486bb553a
psykkis <psykkis@hotmail.com>
2024-08-15 21:41:34 +0300

    auto update version 15.8.2024

--- Assets/Altzone/Scripts/BuildProperties.cs
+++ Assets/Altzone/Scripts/BuildProperties.cs
@@ -2,8 +2,8 @@ namespace Altzone.Scripts
 {
     internal static class BuildProperties
     {
-        internal const string CompiledOnDate = "2024-08-14 19:07";
-        internal const int BundleVersionCode = 127;
+        internal const string CompiledOnDate = "2024-08-15 21:41";
+        internal const int BundleVersionCode = 128;
         internal const bool IsMuteOtherAudioSources = false;
     }
 }
