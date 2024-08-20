23d0ea4bb396f6874f51855ead98d661731c9ded
Retskia <reetta.karki00@gmail.com>
2024-08-20 16:10:45 +0300

    AvatarEditor muokkausta
    skaalauksta, sekä koodi hahmolle ja divaanille.

--- Assets/MenuUi/Scripts/HahmonValinta/CharacterLoader.cs
+++ Assets/MenuUi/Scripts/HahmonValinta/CharacterLoader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Altzone.Scripts.Model.Poco.Game;
 using Altzone.Scripts.Model.Poco.Player;
 using Altzone.Scripts;
-using MenuUi.Scripts.CharacterGallery;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Runtime.CompilerServices;
@@ -11,8 +10,12 @@ using System.Runtime.CompilerServices;
 
 public class CharacterLoader : MonoBehaviour
 {
-    [SerializeField] private List<GalleryCharacterClassInfo> _info;
-    [SerializeField] private Image _characterImage;  
+    [SerializeField] private List<AvatarClassInfo> _avatarClassInfoList;
+    private Image _characterImage;
+    private Image _divanImage;
+    private string _divanObjectName = "divaani";
+    PlayerData _playerData = null;
+
 
     private void OnEnable()
     {
@@ -21,25 +24,26 @@ public class CharacterLoader : MonoBehaviour
 
     public void RefreshPlayerCurrentCharacter()
     {
-        PlayerData _playerData = null;
+        _characterImage = GetComponent<Image>();
+        _divanImage = GameObject.Find(_divanObjectName).GetComponent<Image>();
         Storefront.Get().GetPlayerData(ServerManager.Instance.Player.uniqueIdentifier, p => _playerData = p);
 
         int selectedCharacterId = _playerData.SelectedCharacterId;
-        UpdateCharacterImage(selectedCharacterId);  
+        UpdateCharacterImage(selectedCharacterId);
     }
 
-    private GalleryCharacterInfo GetCharacterPrefabInfo(int prefabId)
+    private AvatarInfo GetCharacterPrefabInfo_bkp(int prefabId)
     {
         CharacterClassID characterClass = CustomCharacter.GetClassID((CharacterID)prefabId);
 
         int classValue = (int)characterClass >> 8;
         classValue--;
 
-        if (classValue < 0 || classValue >= _info.Count)
+        if (classValue < 0 || classValue >= _avatarClassInfoList.Count)
         {
             return null;
         }
-        GalleryCharacterClassInfo classObject = _info[classValue];
+        AvatarClassInfo classObject = _avatarClassInfoList[classValue];
 
         int characterValue = CustomCharacter.GetInsideCharacterID((CharacterID)prefabId);
         characterValue--;
@@ -48,16 +52,36 @@ public class CharacterLoader : MonoBehaviour
         {
             return null;
         }
-        GalleryCharacterInfo character = classObject.list[characterValue];
+        AvatarInfo character = classObject.list[characterValue];
         return character;
     }
 
     private void UpdateCharacterImage(int prefabId)
     {
-        GalleryCharacterInfo character = GetCharacterPrefabInfo(prefabId);
+        AvatarInfo character = GetCharacterPrefabInfo_bkp(prefabId);
         if (character != null && _characterImage != null)
         {
-            _characterImage.sprite = character.Image;
+            _characterImage.sprite = character.CharacterImage;
+            _divanImage.sprite = character.DivanImage;
         }
     }
 }
+
+    [Serializable]
+public class AvatarInfo
+{
+    public string Name;
+    public CharacterID id;
+    public Sprite CharacterImage;
+    public Sprite DivanImage;
+}
+
+
+[Serializable]
+public class AvatarClassInfo
+{
+    public string Name;
+    public CharacterClassID id;
+    public List<AvatarInfo> list;
+}
+
