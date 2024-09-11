5ce458fe89fdbeb4d3def209a729597a01888b67
Agripepsi <jarkko.kanth@protonmail.com>
2024-09-11 16:57:47 +0300

    Scripti Avatarin muokkausnappeille
    Scripti joka on kiinni Avatarin muokkauksessa käytettävissä napeissa, sisältää tiedon mitä Avatarin kohtaa muokataan ja viittauksen kuvatiedostoon joka tulee muokattuun kohtaan.

---
+++ Assets/MenuUi/Scripts/AvatarEditor/FeatureButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MenuUi.Scripts.AvatarEditor
+{
+    public class FeatureButton : MonoBehaviour
+    {
+        public FeatureSlot _slot;
+        public Sprite _sprite;
+
+    }
+}
