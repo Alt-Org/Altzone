9f16977fe3d40a8b1099f94c78270ab5c55cae3e
Niko Mattila <niko.mattila@pp.inet.fi>
2024-09-12 01:27:15 +0300

    Added a script that sends Section Enter and Close messages
    Added this to the SoulHome, Main Menu and Clan page.
    To use all that needs to be done is to add the script and then select the RoomType where this script is used to tell the GAManager that which room sends the message.

---
+++ Assets/Altzone/Scripts/DataAnalytics/GameAnalyticsSectionHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Altzone.Scripts.GA
+{
+    public enum SectionName
+    {
+        None,
+        MainMenu,
+        SoulHome,
+        DailyTask,
+        Leaderboard,
+        ClanPage,
+        ClanSelection,
+        CharacterScreen,
+        Settings,
+        Voting
+    }
+
+
+    public class GameAnalyticsSectionHandler : MonoBehaviour
+    {
+        [SerializeField] private SectionName _sectionName = SectionName.None;
+
+        private void OnEnable()
+        {
+            if (_sectionName == SectionName.None) return;
+            string name = GetSectionName();
+            if (GameAnalyticsManager.Instance != null) GameAnalyticsManager.Instance.EnterSection(name);
+        }
+
+        private void OnDisable()
+        {
+            if (_sectionName == SectionName.None) return;
+            string name = GetSectionName();
+            if (GameAnalyticsManager.Instance != null) GameAnalyticsManager.Instance.ExitSection(name);
+        }
+
+        private string GetSectionName()
+        {
+            switch (_sectionName)
+            {
+               case SectionName.MainMenu:
+                    return "MainMenu";
+                case SectionName.SoulHome:
+                    return "SoulHome";
+                case SectionName.DailyTask:
+                    return "DailyTask";
+                case SectionName.Leaderboard:
+                    return "Leaderboard";
+                case SectionName.ClanPage:
+                    return "ClanPage";
+                case SectionName.ClanSelection:
+                    return "ClanSelection";
+                case SectionName.CharacterScreen:
+                    return "CharacterScreen";
+                case SectionName.Settings:
+                    return "Settings";
+                case SectionName.Voting:
+                    return "Voting";
+                default:
+                    return "";
+            }
+        }
+    }
+}
