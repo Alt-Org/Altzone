75b937fa9eebd55fd2e72a686567246af47ed3d9
Niklas <niklas.tontti603619@gmail.com>
2024-09-23 16:54:43 +0300

    changed game controller to use battle debug logger

--- Assets/Battle/Scripts/Battle/Game/GameController.cs
+++ Assets/Battle/Scripts/Battle/Game/GameController.cs
@@ -45,10 +45,8 @@ namespace Battle.Scripts.Battle.Game
 
         #region DEBUG
         private BattleDebugLogger _battleDebugLogger;
-        private const string DEBUG_LOG_NAME = "[BATTLE] [GAME CONTROLLER] ";
-        private const string DEBUG_LOG_NAME_AND_TIME = "[{0:000000}] " + DEBUG_LOG_NAME;
-        private const string DEBUG_LOG_GAME_STARTUP = DEBUG_LOG_NAME + "GAME STARTUP: ";
-        private const string DEBUG_LOG_SLING_SEQUENCE = DEBUG_LOG_NAME_AND_TIME + "Sling sequence: ";
+        private const string DebugLogGameStartup = "GAME STARTUP: ";
+        private const string DebugLogSlingSequence = "Sling sequence: ";
         #endregion DEBUG
 
         #region Private Methods
@@ -85,21 +83,21 @@ namespace Battle.Scripts.Battle.Game
 
         private void OnSyncedFixedUpdateClockStarted(SyncedFixedUpdateClockStarted data)
         {
-            Debug.Log(DEBUG_LOG_GAME_STARTUP + "SYNCED FIXED UPDATE CLOCK STARTED");
+            _battleDebugLogger.LogInfo(DebugLogGameStartup + "SYNCED FIXED UPDATE CLOCK STARTED");
             _syncedFixedUpdateClockStarted = true;
             TryToStart();
         }
 
         private void OnTeamsReadyForGameplay(TeamsAreReadyForGameplay data)
         {
-            Debug.Log(DEBUG_LOG_GAME_STARTUP + "TEAMS ARE READY FOR GAMEPLAY");
+            _battleDebugLogger.LogInfo(DebugLogGameStartup + "TEAMS ARE READY FOR GAMEPLAY");
             _teamsAreReadyForGameplay = true;
             TryToStart();
         }
 
         private void OnSlingControllerReady(SlingControllerReady data)
         {
-            Debug.Log(DEBUG_LOG_GAME_STARTUP + "SLING CONTROLLER READY");
+            _battleDebugLogger.LogInfo(DebugLogGameStartup + "SLING CONTROLLER READY");
             _slingControllerReady = true;
             TryToStart();
         }
@@ -108,15 +106,15 @@ namespace Battle.Scripts.Battle.Game
 
         private void OnBallSlinged(BallSlinged data)
         {
-            Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Ball slinged", _syncedFixedUpdateClock.UpdateCount));
-            Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "enabling player movement", _syncedFixedUpdateClock.UpdateCount));
+            _battleDebugLogger.LogInfo("Ball slinged");
+            _battleDebugLogger.LogInfo(DebugLogSlingSequence + "enabling player movement");
             _playerManager.SetPlayerMovementEnabled(true);
-            Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "finished", _syncedFixedUpdateClock.UpdateCount));
+            _battleDebugLogger.LogInfo(DebugLogSlingSequence + "finished");
         }
 
         private void OnSoulWallSegmentRemoved(SoulWallSegmentRemoved data)
         {
-            Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Brick removed", _syncedFixedUpdateClock.UpdateCount));
+            _battleDebugLogger.LogInfo("Brick removed");
             if (PhotonNetwork.IsMasterClient)
             {
                 _photonView.RPC(nameof(SlingReactivate), RpcTarget.All, _syncedFixedUpdateClock.UpdateCount + _syncedFixedUpdateClock.ToUpdates(GameConfig.Get().Variables._networkDelay), data.Side);
@@ -138,22 +136,22 @@ namespace Battle.Scripts.Battle.Game
 
         private void Sling(BattleTeamNumber slingingTeam)
         {
-            Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "activating sling", _syncedFixedUpdateClock.UpdateCount));
+            _battleDebugLogger.LogInfo(DebugLogSlingSequence + "activating sling");
             _slingController.SlingActivate(slingingTeam);
             int slingModeEndUpdateNumber = _syncedFixedUpdateClock.UpdateCount + _syncedFixedUpdateClock.ToUpdates(GameConfig.Get().Variables._slingAimingTimeSec);
 
             _syncedFixedUpdateClock.ExecuteOnUpdate(slingModeEndUpdateNumber, 0, () =>
             {
-                Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "disabling player movement", _syncedFixedUpdateClock.UpdateCount));
+                _battleDebugLogger.LogInfo(DebugLogSlingSequence + "disabling player movement");
                 _playerManager.SetPlayerMovementEnabled(false);
             });
 
             _syncedFixedUpdateClock.ExecuteOnUpdate(slingModeEndUpdateNumber + _syncedFixedUpdateClock.ToUpdates(GameConfig.Get().Variables._networkDelay), 0, () =>
             {
-                Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "waiting for players to stop moving", _syncedFixedUpdateClock.UpdateCount));
+                _battleDebugLogger.LogInfo(DebugLogSlingSequence + "waiting for players to stop moving");
                 _syncedFixedUpdateClock.ExecuteOnUpdate(Mathf.Max(_playerManager.LastPlayerTeleportUpdateNumber, _syncedFixedUpdateClock.UpdateCount) + 1, -1, () =>
                 {
-                    Debug.Log(string.Format(DEBUG_LOG_SLING_SEQUENCE + "launching sling", _syncedFixedUpdateClock.UpdateCount));
+                    _battleDebugLogger.LogInfo(DebugLogSlingSequence + "launching sling");
                     _slingController.SlingLaunch();
                 });
             });
@@ -168,7 +166,7 @@ namespace Battle.Scripts.Battle.Game
         {
             _clientsReady++;
             int realPlayerCount = PhotonNetwork.CurrentRoom.Players.Values.Sum(x => PhotonBattle.IsRealPlayer(x) ? 1 : 0);
-            Debug.Log(string.Format(DEBUG_LOG_GAME_STARTUP + "{0}/{1} CLIENTS READY", _clientsReady, realPlayerCount));
+            _battleDebugLogger.LogInfo(DebugLogGameStartup + "{0}/{1} CLIENTS READY", _clientsReady, realPlayerCount);
             if (_clientsReady < realPlayerCount) return;
 
             if (PhotonNetwork.IsMasterClient)
@@ -184,13 +182,13 @@ namespace Battle.Scripts.Battle.Game
         {
             _syncedFixedUpdateClock.ExecuteOnUpdate(gameStartUpdateNumber, 0, () =>
             {
-                Debug.Log(DEBUG_LOG_GAME_STARTUP + "STARTING GAME");
-                Debug.Log(DEBUG_LOG_GAME_STARTUP + "ENABLING PLAYER MOVEMENT");
+                _battleDebugLogger.LogInfo(DebugLogGameStartup + "STARTING GAME");
+                _battleDebugLogger.LogInfo(DebugLogGameStartup + "ENABLING PLAYER MOVEMENT");
                 _playerManager.SetPlayerMovementEnabled(true);
-                Debug.Log(DEBUG_LOG_GAME_STARTUP + "STARTING SLING SEQUENCE");
+                _battleDebugLogger.LogInfo(DebugLogGameStartup + "STARTING SLING SEQUENCE");
                 Sling(slingingTeam);
                 _gameStarted = true;
-                Debug.Log(DEBUG_LOG_GAME_STARTUP + "GAME STARTED");
+                _battleDebugLogger.LogInfo(DebugLogGameStartup + "GAME STARTED");
                 this.Publish(new GameStarted());
             });
             _playerManager.AnalyticsReportPlayerCharacterSelection();
@@ -203,7 +201,7 @@ namespace Battle.Scripts.Battle.Game
         {
             _syncedFixedUpdateClock.ExecuteOnUpdate(activationUpdateNumber, 0, () =>
             {
-                Debug.Log(string.Format(DEBUG_LOG_NAME_AND_TIME + "Starting sling sequence", _syncedFixedUpdateClock.UpdateCount));
+                _battleDebugLogger.LogInfo("Starting sling sequence");
                 Sling(slingingTeam);
             });
         }
