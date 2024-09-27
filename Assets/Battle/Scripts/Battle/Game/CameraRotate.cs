d204e97c8d6541d0e0ea9afc3b5211c24097c6ac
jugi-dev <jukkar123@gmail.com>
2024-09-27 11:04:27 +0300

    Cleaned up CameraRotate
    Cleaned up CameraRotate and deleted the unused CameraRotateTest script

--- Assets/Battle/Scripts/Battle/Game/CameraRotate.cs
+++ Assets/Battle/Scripts/Battle/Game/CameraRotate.cs
@@ -1,42 +1,51 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 namespace Battle.Scripts.Battle.Game
 {
     internal class CameraRotate : MonoBehaviour
     {
-        //UiGridRenderer laittaa tämän koodin päälle kun grid on luotu, jotta sitä voi kääntää tämän koodin startissa
-        [SerializeField] Transform Camera;
-        [SerializeField] Transform Background;
-        [SerializeField] Transform GridOverlay;
-        [SerializeField] Transform DiamondCounters;
-        [SerializeField] RectTransform BetaDiamonds;
-        [SerializeField] RectTransform AlphaDiamonds;
-        [SerializeField] Transform DiamondCounters2;
-        [SerializeField] RectTransform BetaDiamonds2;
-        [SerializeField] RectTransform AlphaDiamonds2;
+        // UiGridRenderer laittaa tämän koodin päälle kun grid on luotu, jotta sitä voi kääntää tämän koodin startissa
 
-        private int TeamNumber;
-
+        //Private methods
         private void Start()
         {
-            if (PhotonNetwork.InRoom)
+            if (!PhotonNetwork.InRoom) return;
+
+            Player localPlayer = PhotonNetwork.LocalPlayer;
+            int localPlayerPos = PhotonBattle.GetPlayerPos(localPlayer);
+            _teamNumber = (int)PhotonBattle.GetTeamNumber(localPlayerPos);
+
+            Debug.Log($"TeamNumber {_teamNumber} pos {localPlayerPos} {localPlayer.GetDebugLabel()}");
+
+            if (_teamNumber == 2)
             {
-                var player = PhotonNetwork.LocalPlayer;
-                var playerPos = PhotonBattle.GetPlayerPos(player);
-                TeamNumber = (int)PhotonBattle.GetTeamNumber(playerPos);
-                Debug.Log($"TeamNumber {TeamNumber} pos {playerPos} {player.GetDebugLabel()}");
-                if (TeamNumber == 2)   //2
-                {
-                    Camera.eulerAngles = new Vector3(0, 0, 180);
-                    Background.eulerAngles = new Vector3(0, 0, 180);
-                    GridOverlay.eulerAngles = new Vector3(0, 0, 180);
-                    AlphaDiamonds.anchoredPosition = new Vector2(BetaDiamonds.anchoredPosition.x, BetaDiamonds.anchoredPosition.y);
-                    AlphaDiamonds2.anchoredPosition = new Vector2(BetaDiamonds2.anchoredPosition.x, BetaDiamonds2.anchoredPosition.y);
-                    BetaDiamonds.anchoredPosition = new Vector2(-BetaDiamonds2.anchoredPosition.x, -BetaDiamonds2.anchoredPosition.y);
-                    BetaDiamonds2.anchoredPosition = new Vector2(-AlphaDiamonds.anchoredPosition.x, -AlphaDiamonds.anchoredPosition.y);
-                }
+                RotateCamera();
             }
         }
+
+        private void RotateCamera()
+        {
+            _camera.eulerAngles = new Vector3(0, 0, 180);
+            _background.eulerAngles = new Vector3(0, 0, 180);
+            _gridOverlay.eulerAngles = new Vector3(0, 0, 180);
+            _alphaDiamonds.anchoredPosition = new Vector2(_betaDiamonds.anchoredPosition.x, _betaDiamonds.anchoredPosition.y);
+            _alphaDiamonds2.anchoredPosition = new Vector2(_betaDiamonds2.anchoredPosition.x, _betaDiamonds2.anchoredPosition.y);
+            _betaDiamonds.anchoredPosition = new Vector2(-_betaDiamonds2.anchoredPosition.x, -_betaDiamonds2.anchoredPosition.y);
+            _betaDiamonds2.anchoredPosition = new Vector2(-_alphaDiamonds.anchoredPosition.x, -_alphaDiamonds.anchoredPosition.y);
+        }
+
+        // Private fields
+        private int _teamNumber;
+        [SerializeField] private Transform _camera;
+        [SerializeField] private Transform _background;
+        [SerializeField] private Transform _gridOverlay;
+        [SerializeField] private Transform _diamondCounters;
+        [SerializeField] private RectTransform _betaDiamonds;
+        [SerializeField] private RectTransform _alphaDiamonds;
+        [SerializeField] private Transform _diamondCounters2;
+        [SerializeField] private RectTransform _betaDiamonds2;
+        [SerializeField] private RectTransform _alphaDiamonds2;
     }
 }
