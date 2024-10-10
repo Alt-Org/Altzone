04c60a1034afd4a6be7d7bc9cdad89a72e07dd8d
Niko Mattila <niko.mattila@pp.inet.fi>
2024-10-11 02:43:17 +0300

    Debug: Added random colourisation to the logbox message panel
    The msgstorage currently assigns random colours to the messages.
    Niklas will change these later.

--- Assets/MenuUi/Scripts/Debug/LogBoxMessageHandler.cs
+++ Assets/MenuUi/Scripts/Debug/LogBoxMessageHandler.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using TMPro;
 using DebugUi.Scripts.BattleAnalyzer;
+using UnityEngine.UI;
 
 namespace DebugUi.Scripts.BattleAnalyzer
 {
     public class LogBoxMessageHandler : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _textField;
+        [SerializeField] private Image _backgroundImage;
 
         private IReadOnlyMsgObject _msgObject;
 
@@ -54,6 +56,28 @@ namespace DebugUi.Scripts.BattleAnalyzer
                     break;
                 default:
                     _textField.color = Color.gray; // Default color
+                    break;
+            }
+
+            switch (msgObject.ColorGroup)
+            {
+                case 0:
+                    _backgroundImage.color = new(0.3f,0,0);
+                    break;
+                case 1:
+                    _backgroundImage.color = new(0.3f, 0, 0.3f);
+                    break;
+                case 2:
+                    _backgroundImage.color = new(0.2f, 0, 0.4f);
+                    break;
+                case 3:
+                    _backgroundImage.color = new(0.1f, 0, 0.5f);
+                    break;
+                case 4:
+                    _backgroundImage.color = new(0f, 0, 0.6f);
+                    break;
+                default:
+                    _backgroundImage.color = Color.gray; // Default color
                     break;
             }
         }
