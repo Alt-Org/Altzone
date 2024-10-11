1fe1104c84aaec5c84ec81d112313de196a939a8
Niko Mattila <niko.mattila@pp.inet.fi>
2024-10-11 12:24:37 +0300

    Added a POST method for multiforms.

--- Assets/Prg/WebRequests.cs
+++ Assets/Prg/WebRequests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using Prg;
 using UnityEngine.Networking;
@@ -60,6 +61,57 @@ public static class WebRequests
                     if (request.result != UnityWebRequest.Result.Success)
                     {
                         Debug.LogWarning("Error posting data to " + address + " - " + request.error + ": "+ request.downloadHandler.text);
+                    }
+
+                    if (callback != null)
+                    {
+                        callback(request);
+                    }
+                }
+            }
+        }
+    }
+
+    public static IEnumerator Post(string address, List<IMultipartFormSection> formData, string accessToken, string secretKey, string id, Action<UnityWebRequest> callback)
+    {
+        byte[] boundary = UnityWebRequest.GenerateBoundary();
+        byte[] formSections = UnityWebRequest.SerializeFormSections(formData, boundary);
+        byte[] terminate = System.Text.Encoding.UTF8.GetBytes(String.Concat("\r\n--", System.Text.Encoding.UTF8.GetString(boundary), "--"));
+        byte[] data = new byte[formSections.Length + terminate.Length];
+        Buffer.BlockCopy(formSections, 0, data, 0, formSections.Length);
+        Buffer.BlockCopy(terminate, 0, data, formSections.Length, terminate.Length);
+
+        using (UnityWebRequest request = UnityWebRequest.Post(address, formData))
+        {
+            using (UploadHandlerRaw uploadHandler = new UploadHandlerRaw(data))
+            {
+                using (DownloadHandlerBuffer downloadHandler = new DownloadHandlerBuffer())
+                {
+
+                    request.uploadHandler.Dispose();
+                    request.downloadHandler.Dispose();
+                    request.uploadHandler = uploadHandler;
+                    request.downloadHandler = downloadHandler;
+                    request.SetRequestHeader("Content-Type", "multipart/form-data");
+
+                    if (accessToken != null)
+                    {
+                        request.SetRequestHeader("authorization", "Bearer " + accessToken);
+                    }
+                    if (secretKey != null)
+                    {
+                        request.SetRequestHeader("Secret", secretKey);
+                    }
+                    if (id != null)
+                    {
+                        request.SetRequestHeader("BattleId", id);
+                    }
+
+                    yield return request.SendWebRequest();
+
+                    if (request.result != UnityWebRequest.Result.Success)
+                    {
+                        Debug.LogWarning("Error posting data to " + address + " - " + request.error + ": " + request.downloadHandler.text);
                     }
 
                     if (callback != null)
