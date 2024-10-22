df94875deb2ae425585128ba24cc9d337a53ace9
Jari Petays <jari.petays@iki.fi>
2024-10-22 15:52:47 +0300

    add new Battle folder

---
+++ Assets/Battle/Scripts/BattleTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Battle.Scripts
+{
+    public class BattleTest : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log($"{nameof(BattleTest)}");
+        }
+    }
+}
