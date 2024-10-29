5e88088f86d248cfba54f474f85ccc7dca77b95f
Milo Koskela <115208829+kosmilo@users.noreply.github.com>
2024-10-29 09:48:27 +0200

    Check and show warning for empty/none values in create clan
    Now checks that language, age and goal are selected, and that the password is set if the clan is private

--- Assets/MenuUi/Scripts/Clan/ClanCreateNew.cs
+++ Assets/MenuUi/Scripts/Clan/ClanCreateNew.cs
@@ -11,62 +11,69 @@ using Altzone.Scripts.Model.Poco.Clan;
 
 public class ClanCreateNew : MonoBehaviour
 {
-    //List
     [SerializeField] private List<string> _valuesList;
-
-    //GameObject
     [SerializeField] private GameObject _valuePrefab;
     [SerializeField] private GameObject _valuesContainer;
 
-    //InputFields Or TMP InputFields
+    [Header("Input fields")]
+    [SerializeField] private TMP_InputField _clanNameField;
+    [SerializeField] private TMP_InputField _clanPhraseField;
+    [SerializeField] private TMP_InputField _clanPasswordField;
 
-    [SerializeField] private TMP_InputField _clanNameInputField;
-    //[SerializeField] private TMP_InputField _clanTagInputField;
-    [SerializeField] private TMP_InputField _clanPhrase;
-    [SerializeField] private TMP_InputField _clanAge;
-    [SerializeField] private TMP_InputField _clanMembers;
-    [SerializeField] private TMP_InputField _clanPassword;
-
-    //Toggles
+    [Header("Toggles")]
     [SerializeField] private Toggle _AgeTeinit;
     [SerializeField] private Toggle _AgeTaaperot;
     [SerializeField] private Toggle _AgeAikuiset;
     [SerializeField] private Toggle _AgeKaikenIkaiset;
     [SerializeField] private Toggle _openClanButton;
 
-    //Warnings
+    [Header("Warnings")]
+    [SerializeField] private GameObject _clanNameWarningOutline;
+    [SerializeField] private GameObject _clanPasswordWarningOutline;
+    [SerializeField] private GameObject _clanLanguageWarningOutline;
+    [SerializeField] private GameObject _clanGoalWarningOutline;
+    [SerializeField] private GameObject _clanAgeWarningOutline;
+    [SerializeField] private PopupController _warningPopup;
 
-    [SerializeField] private Image _ClanNameWarningImage;
-    [SerializeField] private PopupController _NameTakenPopup;
-
-    //Buttons
-
+    [Header("Buttons")]
     [SerializeField] private Button _returnToMainClanViewButton;
     [SerializeField] private Button _clanValues;
     [SerializeField] private Button _buttonLogo;
 
-    // Dropdowns Or TMP Dropdowns
+    [Header("Dropdowns")]
+    [SerializeField] private TMP_Dropdown _clanLanguageDropdown;
+    [SerializeField] private TMP_Dropdown _clanGoalDropdown;
 
-    [SerializeField] private TMP_Dropdown _ClanLanguageDropdown;
-    [SerializeField] private TMP_Dropdown _ClanGoals;
+    [Header("Other Inputs")]
+    [SerializeField] private ClanRightsPanel _clanRightsPanel;
 
+    [Header("Navigation")]
     [SerializeField] protected WindowDef _naviTarget;
 
     private ClanAge _clanAgeEnum = ClanAge.None;
+    private ClanRoleRights[] _defaultRights = new ClanRoleRights[3]  {
+        ClanRoleRights.None,
+        ClanRoleRights.EditSoulHome,
+        ClanRoleRights.EditClanSettings | ClanRoleRights.EditSoulHome | ClanRoleRights.EditMemberRights
+    };
 
     private void Reset()
     {
         StopAllCoroutines();
-        _clanNameInputField.text = "";
-        //_clanTagInputField.text = "";
-        //_gameCoinsInputField.text = "";
+        _clanNameField.text = "";
         _openClanButton.isOn = false;
 
+        _clanNameWarningOutline.SetActive(false);
+        _clanPasswordWarningOutline.SetActive(false);
+        _clanLanguageWarningOutline.SetActive(false);
+        _clanGoalWarningOutline.SetActive(false);
+        _clanAgeWarningOutline.SetActive(false);
 
         SetLanguageDropdown();
         SetValuesPanel();
         SetGoalsDropDown();
         SetToggleListeners();
+        _clanRightsPanel.InitializeRightsToggles(_defaultRights);
     }
 
     private void OnEnable()
@@ -76,21 +83,21 @@ public class ClanCreateNew : MonoBehaviour
 
     private void SetLanguageDropdown()
     {
-        _ClanLanguageDropdown.options.Clear();
+        _clanLanguageDropdown.options.Clear();
         foreach (Language language in Enum.GetValues(typeof(Language)))
         {
             string text = ClanDataTypeConverter.GetLanguageText(language);
-            _ClanLanguageDropdown.options.Add(new TMP_Dropdown.OptionData(text));
+            _clanLanguageDropdown.options.Add(new TMP_Dropdown.OptionData(text));
         }
     }
 
     private void SetGoalsDropDown()
     {
-        _ClanGoals.options.Clear();
+        _clanGoalDropdown.options.Clear();
         foreach (Goals goal in Enum.GetValues(typeof(Goals)))
         {
             string text = ClanDataTypeConverter.GetGoalText(goal);
-            _ClanGoals.options.Add(new TMP_Dropdown.OptionData(text));
+            _clanGoalDropdown.options.Add(new TMP_Dropdown.OptionData(text));
         }
     }
 
@@ -166,17 +173,13 @@ public class ClanCreateNew : MonoBehaviour
             }
         });
     }
-
-    private void SetClanAge(ClanAge age)
-    {
-        _clanAgeEnum = age;
-    }
+    private void SetClanAge(ClanAge age) => _clanAgeEnum = age;
 
     private void SetValuesPanel()
     {
         for (int i = _valuesContainer.transform.childCount - 1; i >= 0; i--)
         {
-            Destroy(_valuesContainer.transform.GetChild(i));
+            Destroy(_valuesContainer.transform.GetChild(i).gameObject);
         }
         foreach (string Text in _valuesList)
         {
@@ -187,32 +190,21 @@ public class ClanCreateNew : MonoBehaviour
 
     public void PostClanToServer()
     {
-        string clanName = _clanNameInputField.text;
-        //string clanTag = _clanTagInputField.text;
-        //int gameCoins = int.Parse(_gameCoinsInputField.text);
+        string clanName = _clanNameField.text;
+        string phrase = _clanPhraseField.text;
         bool isOpen = !_openClanButton.isOn;
+        string password = _clanPasswordField.text;
+        Language language = (Language)_clanLanguageDropdown.value;
+        Goals goal = (Goals)_clanGoalDropdown.value;
+        ClanAge age = _clanAgeEnum;
+        ClanRoleRights[] clanRights = _clanRightsPanel.ClanRights;
 
-        Language language = (Language)_ClanLanguageDropdown.value;
-        Goals goals = (Goals)_ClanGoals.value;
-        string Phrase = _clanPhrase.text;
-        Debug.Log($"language: {language}, Phrase: {Phrase}, Goals: {goals} clanAgeEnum {_clanAgeEnum},");
-
-        if (clanName == string.Empty /*|| clanTag == string.Empty || _gameCoinsInputField.text == string.Empty*/)
+        if (!CheckClanValuesValidity(clanName, phrase, isOpen, password, language, goal, age))
         {
-            _ClanNameWarningImage.gameObject.SetActive(true);
-            _NameTakenPopup.ActivatePopUp("Lisää klaanin nimi");
             return;
         }
 
-        /*if (_clanPhrase.text== string.Empty)
-        {
-           _ClanNameWarningImage.gameObject.SetActive(true);
-            _NameTakenPopup.ActivatePopUp("Lis�� klaanin motto");
-            return;
-        }*/
-
-
-        StartCoroutine(ServerManager.Instance.PostClanToServer(clanName, clanName.Trim().Substring(0, 4), isOpen, null, _clanAgeEnum, goals, Phrase, language, clan =>
+        StartCoroutine(ServerManager.Instance.PostClanToServer(clanName, clanName.Trim().Substring(0, 4), isOpen, null, age, goal, phrase, language, clan =>
         {
             if (clan == null)
             {
@@ -222,6 +214,7 @@ public class ClanCreateNew : MonoBehaviour
             Debug.Log($"naviTarget {_naviTarget} isCurrentPopOutWindow {true}", _naviTarget);
             var windowManager = WindowManager.Get();
             //windowManager.PopCurrentWindow();
+
             // Check if navigation target window is already in window stack and we area actually going back to it via button.
             var windowCount = windowManager.WindowCount;
             if (windowCount > 1)
@@ -240,7 +233,53 @@ public class ClanCreateNew : MonoBehaviour
                 }
             }
             windowManager.ShowWindow(_naviTarget);
-            return;
         }));
+    }
+
+    private bool CheckClanValuesValidity(string clanName, string phrase, bool isOpen, string password, Language language, Goals goal, ClanAge age)
+    {
+        bool validInputs = true;
+
+        if (clanName == string.Empty)
+        {
+            _clanNameWarningOutline.SetActive(true);
+            _warningPopup.ActivatePopUp("Lisää klaanin nimi");
+            validInputs = false;
+        }
+        else _clanNameWarningOutline.SetActive(false);
+
+        if (!isOpen && password == string.Empty)
+        {
+            _clanPasswordWarningOutline.SetActive(true);
+            _warningPopup.ActivatePopUp("Lukituilla klaaneilla tulee olla salasana");
+            validInputs = false;
+        }
+        else _clanPasswordWarningOutline.SetActive(false);
+
+        if (language == Language.None)
+        {
+            _clanLanguageWarningOutline.SetActive(true);
+            validInputs = false;
+            _warningPopup.ActivatePopUp("Valitse klaanin kieli");
+        }
+        else _clanLanguageWarningOutline.SetActive(false);
+
+        if (goal == Goals.None)
+        {
+            _clanGoalWarningOutline.SetActive(true);
+            validInputs = false;
+            _warningPopup.ActivatePopUp("Valitse klaanin tavoite");
+        }
+        else _clanGoalWarningOutline.SetActive(false);
+
+        if (age == ClanAge.None)
+        {
+            _clanAgeWarningOutline.SetActive(true);
+            validInputs = false;
+            _warningPopup.ActivatePopUp("Valitse klaanin ikäryhmä");
+        }
+        else _clanAgeWarningOutline.SetActive(false);
+
+        return validInputs;
     }
 }
