e31660bf984511078c99c665925b143bb9415491
Niko Mattila <niko.mattila@pp.inet.fi>
2024-10-30 15:13:24 +0200

    Updated AudioManager to make adding new AudioSources easier
    One can now add new AudioSources by pressing a button on AudioManager script while in editor.
    You can and should include a name for these new objects directly form the inputfield on the script.
    You can also define the AudioType for the sound (this is still a little incomplete)
    Adding the Audiosource also automatically adds it into the audiolist.
    Also turned AudioManager into a prefab so it could be used outside of SoulHome.
    Sounds can now be searched with the audio name, type or id.
    Type is currently tied to enums and you can properly add more of them easily yet.
    ID is the position of the sound in the audiolist, with 0 being reverved to empty and 1 being reserved to Music.

--- Assets/MenuUi/Scripts/SoulHome/SoulHomeAudioManager.cs
+++ Assets/MenuUi/Scripts/SoulHome/SoulHomeAudioManager.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace MenuUI.Scripts.SoulHome
 {
     public enum AudioType
     {
+        None,
         Music,
         Click,
         PopupError,
@@ -19,13 +22,9 @@ namespace MenuUI.Scripts.SoulHome
     {
         [SerializeField] private AudioSource _musicAudio;
         private MusicList _musicList;
-        [SerializeField] private AudioSource _clickAudio;
-        [SerializeField] private AudioSource _popupErrorAudio;
-        [SerializeField] private AudioSource _revertAudio;
-        [SerializeField] private AudioSource _saveAudio;
-        [SerializeField] private AudioSource _rotateAudio;
-        [SerializeField] private AudioSource _setFurnitureAudio;
+        [SerializeField] private List<AudioBlock> _audioList;
 
+        [SerializeField] private GameObject _audioSourcePrefab;
         // Start is called before the first frame update
         void Start()
         {
@@ -37,29 +36,36 @@ namespace MenuUI.Scripts.SoulHome
 
         public void PlayAudio(AudioType type)
         {
-            switch (type)
+            if (type == AudioType.None) return;
+
+            if (type == AudioType.Music) PlayMusic();
+
+            foreach(AudioBlock block in _audioList)
             {
-                case AudioType.Music:
-                    _musicList.PlayMusic();
-                    break;
-                case AudioType.Click:
-                    _clickAudio.Play();
-                    break;
-                case AudioType.PopupError:
-                    _popupErrorAudio.Play();
-                    break;
-                case AudioType.Revert:
-                    _revertAudio.Play();
-                    break;
-                case AudioType.Save:
-                    _saveAudio.Play();
-                    break;
-                case AudioType.Rotate:
-                    _rotateAudio.Play();
-                    break;
-                case AudioType.SetFurniture:
-                    _setFurnitureAudio.Play();
-                    break;
+                if(block.type == type) block.audioSource.Play();
+            }
+        }
+
+        public void PlayAudio(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return;
+
+            if (string.Equals(name, "Music")) PlayMusic();
+
+            foreach (AudioBlock block in _audioList)
+            {
+                if (string.Equals(name, block.name)) block.audioSource.Play();
+            }
+        }
+        public void PlayAudio(int value)
+        {
+            if (value < 1) return;
+
+            if (value == 1) PlayMusic();
+
+            if (_audioList.Count < value-2)
+            {
+                _audioList[value-2].audioSource.Play();
             }
         }
 
@@ -77,7 +83,7 @@ namespace MenuUI.Scripts.SoulHome
 
         public string PrevMusicTrack()
         {
-            if(_musicList == null) return null;
+            if (_musicList == null) return null;
             return _musicList.PrevTrack();
         }
 
@@ -85,6 +91,76 @@ namespace MenuUI.Scripts.SoulHome
         {
             if (_musicList == null) return;
             _musicList.StopMusic();
+        }
+
+        public void AddAudio(string name, AudioType type)
+        {
+            GameObject gameObject = Instantiate(_audioSourcePrefab, transform);
+            if (!string.IsNullOrWhiteSpace(name))
+                gameObject.name = name;
+            AudioBlock audioBlock = new(gameObject.name);
+            audioBlock.type = type;
+            audioBlock.audioSource = gameObject.GetComponent<AudioSource>();
+            _audioList.Add(audioBlock);
+        }
+    }
+
+    [Serializable]
+    public class AudioBlock
+    {
+        [ReadOnly] public string name;
+        public AudioSource audioSource;
+        public AudioType type;
+
+        public AudioBlock(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    [CustomEditor(typeof(SoulHomeAudioManager))]
+    public class AudioManagerEditor : Editor
+    {
+        private AudioType _type = AudioType.Music;
+        private string _newName = "";
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            SoulHomeAudioManager script = (SoulHomeAudioManager)target;
+            EditorGUILayout.LabelField("");
+            EditorGUILayout.LabelField("New AudioSource", EditorStyles.boldLabel);
+            _newName = EditorGUILayout.TextField("Name", _newName);
+            _type = (AudioType)EditorGUILayout.EnumPopup("Audio Type",_type);
+            if (GUILayout.Button("Add AudioSource"))
+            {
+                script.AddAudio(_newName, _type);
+                _newName = "";
+            }
+        }
+    }
+
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+
+    }
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property,
+                                                GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+
+        public override void OnGUI(Rect position,
+                                   SerializedProperty property,
+                                   GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
         }
     }
 }
