68a5830bb2f4ecea3a7de6c9dae150e37b3dc6bf
Milo Koskela <115208829+kosmilo@users.noreply.github.com>
2024-10-31 09:30:29 +0200

    Clan join and create view layout update

---
+++ Assets/Prg/Scripts/Common/Unity/GridAutoScale.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Sets the GridLayoutGroup's cell width and height to fill the constrainted axis.
+/// </summary>
+// [ExecuteInEditMode]
+[RequireComponent(typeof(GridLayoutGroup))]
+public class GridAutoScale : MonoBehaviour
+{
+    [SerializeField] private float _aspectRatio = 1f;
+
+    private GridLayoutGroup _gridLayout;
+    private RectTransform _gridContainer;
+
+    private void OnEnable()
+    {
+        if (_gridLayout == null) _gridLayout = GetComponent<GridLayoutGroup>();
+        if (_gridContainer == null) _gridContainer = GetComponent<RectTransform>();
+
+        if (_gridContainer.rect.width == 0 || _gridContainer.rect.height == 0) return;
+        if (_aspectRatio <= 0) _aspectRatio = 0.000001f;
+
+        switch (_gridLayout.constraint)
+        {
+            case GridLayoutGroup.Constraint.FixedColumnCount:
+                float gridCellWidth = _gridContainer.rect.width / _gridLayout.constraintCount - _gridLayout.spacing.x / _gridLayout.constraintCount * (_gridLayout.constraintCount - 1);
+                if (gridCellWidth <= 0) gridCellWidth = 0.000001f;
+                _gridLayout.cellSize = new Vector2(gridCellWidth, gridCellWidth / _aspectRatio);
+                break;
+            case GridLayoutGroup.Constraint.FixedRowCount:
+                float gridCellHeight = _gridContainer.rect.width / _gridLayout.constraintCount - _gridLayout.spacing.y / _gridLayout.constraintCount * (_gridLayout.constraintCount - 1);
+                if (gridCellHeight <= 0) gridCellHeight = 0.000001f;
+                _gridLayout.cellSize = new Vector2(gridCellHeight * _aspectRatio, gridCellHeight);
+                break;
+        }
+    }
+}
