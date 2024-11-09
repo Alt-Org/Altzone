b5a5d989ac7ba5ef637027f214fabb0752b8e4f9
Ilona <venusflytrapd.d@gmail.com>
2024-11-09 13:44:54 +0300

    Basic logic bug fixed
    -Item width and height are calculated correctly: available width/height spacing calculation is now (spacing.x * (columns - 1)) instead of
    ((spacing.x / columns) * (columns - 1))

--- Assets/MenuUi/Scripts/FlexibleGridLayout/FlexibleHorizontalGrid.cs
+++ Assets/MenuUi/Scripts/FlexibleGridLayout/FlexibleHorizontalGrid.cs
@@ -5,13 +5,10 @@ public class FlexibleHorizontalGrid : LayoutGroup
 {
     [Header("Flexible Horizontal Grid")]
 
-    public int rows;
-    public int columns;
+    private int rows;
+    private int columns = 4;
     public Vector2 cellSize;
     public Vector2 spacing;
-
-    public bool fitX;
-    public bool fitY;
 
     public override void CalculateLayoutInputHorizontal()
     {
@@ -50,21 +47,24 @@ public class FlexibleHorizontalGrid : LayoutGroup
         //    columns = Mathf.CeilToInt(transform.childCount / (float)rows);
         //}
 
-        rows = Mathf.CeilToInt(transform.childCount / (float)columns);
-        fitX = true;
-        fitY = false;
+        rows = CalculateRowsAmount();
+        Debug.Log(rows);
+        //fitX = true;
+        //fitY = false;
 
         float parentWidth = rectTransform.rect.width;
         float parentHeight = rectTransform.rect.height;
 
-        float availableWidth = parentWidth - padding.left - padding.right - ((spacing.x / columns) * (columns - 1));
-        float availableHeight = parentHeight - padding.top - padding.bottom - ((spacing.y / rows) * (rows - 1));
+        float availableWidth = parentWidth - padding.left - padding.right - (spacing.x * (columns - 1));  //((spacing.x / columns) * (columns - 1));
+        float availableHeight = parentHeight - padding.top - padding.bottom - (spacing.y * (rows - 1));  //((spacing.y / rows) * (rows - 1));
 
         float cellWidth = availableWidth / columns;
         float cellHeight = availableHeight / rows;
 
-        cellSize.x = fitX ? cellWidth : cellSize.x;
-        cellSize.y = fitY ? cellHeight : cellSize.y;
+        //cellSize.x = fitX ? cellWidth : cellSize.x;
+        //cellSize.y = fitY ? cellHeight : cellSize.y;
+        cellSize.x = cellWidth;
+        cellSize.y = cellHeight;
 
         int columnCount = 0;
         int rowCount = 0;
@@ -83,6 +83,28 @@ public class FlexibleHorizontalGrid : LayoutGroup
             SetChildAlongAxis(item, 1, yPos, cellSize.y);
 
         }
+    }
+    private int CalculateRowsAmount()
+    {
+        if (rectChildren.Count <= 6)
+            return 1;
+
+        else if (rectChildren.Count > 6 && rectChildren.Count <= 12)
+            return 2;
+
+        else
+        {
+            Debug.LogWarning("Number of children cannot be greater than 12");
+            return Mathf.CeilToInt(rectChildren.Count / columns);
+        }
+    }
+    private int CalculateCellWidth()
+    {
+        return 0;
+    }
+    private int CalculateColumnsAmount()
+    {
+        return 0;
     }
 
     public override void CalculateLayoutInputVertical()
