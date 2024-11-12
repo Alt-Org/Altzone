bbce846cbf5f583edd73cf1025b662396a868816
Ilona <venusflytrapd.d@gmail.com>
2024-11-12 11:46:56 +0300

    Parent grows in size as new rows are added
    - Growth by the change of rectTransform.sizeDelta of a parent

--- Assets/MenuUi/Scripts/FlexibleGridLayout/FlexibleHorizontalGrid.cs
+++ Assets/MenuUi/Scripts/FlexibleGridLayout/FlexibleHorizontalGrid.cs
@@ -3,52 +3,42 @@ using UnityEngine.UI;
 
 public class FlexibleHorizontalGrid : LayoutGroup
 {
+    /// Examples of grid:
+    /// 
+    ///  6 items            [] [] [] []
+    ///                     [] []
+
+    ///  12 items           [] [] [] []
+    ///                     [] [] [] []
+    ///                     [] [] [] [] 
+
+
     [Header("Flexible Horizontal Grid")]
 
     private int rows;
-    private int columns = 4;
+    public int columns;
+
     public Vector2 cellSize;
     public Vector2 spacing;
+
+    public float oneRowHeight;
+    private int previousRowCount = -2;
 
     public override void CalculateLayoutInputHorizontal()
     {
         base.CalculateLayoutInputHorizontal();
 
-        //if (fitType == FitType.WIDTH || fitType == FitType.HEIGHT || fitType == FitType.UNIFORM)
-        //{
-        //    float squareRoot = Mathf.Sqrt(transform.childCount);
-        //    rows = columns = Mathf.CeilToInt(squareRoot);
-        //    switch (fitType)
-        //    {
-        //        case FitType.WIDTH:
-        //            fitX = true;
-        //            fitY = false;
-        //            break;
-        //        case FitType.HEIGHT:
-        //            fitX = false;
-        //            fitY = true;
-        //            break;
-        //        case FitType.UNIFORM:
-        //            fitX = fitY = true;
-        //            break;
-        //    }
-        //}
-
-        //if (fitType == FitType.WIDTH || fitType == FitType.FIXEDCOLUMNS)
-        //{
-        //    rows = Mathf.CeilToInt(transform.childCount / (float)columns);
-        //    if (fitType == FitType.FIXEDCOLUMNS)
-        //    {
-        //        fitX = true;
-        //    }
-        //}
-        //if (fitType == FitType.HEIGHT || fitType == FitType.FIXEDROWS)
-        //{
-        //    columns = Mathf.CeilToInt(transform.childCount / (float)rows);
-        //}
-
+        //Calculating current rows 
         rows = CalculateRowsAmount();
+        //Updating rectTransform(parent) size
+        if (rows != previousRowCount)
+        {
+            previousRowCount = rows;
+            float newHeight = oneRowHeight * rows;
+            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, newHeight);
+        }
         Debug.Log(rows);
+
         //fitX = true;
         //fitY = false;
 
@@ -61,8 +51,6 @@ public class FlexibleHorizontalGrid : LayoutGroup
         float cellWidth = availableWidth / columns;
         float cellHeight = availableHeight / rows;
 
-        //cellSize.x = fitX ? cellWidth : cellSize.x;
-        //cellSize.y = fitY ? cellHeight : cellSize.y;
         cellSize.x = cellWidth;
         cellSize.y = cellHeight;
 
@@ -86,18 +74,41 @@ public class FlexibleHorizontalGrid : LayoutGroup
     }
     private int CalculateRowsAmount()
     {
-        if (rectChildren.Count <= 6)
-            return 1;
-
-        else if (rectChildren.Count > 6 && rectChildren.Count <= 12)
-            return 2;
-
-        else
-        {
-            Debug.LogWarning("Number of children cannot be greater than 12");
-            return Mathf.CeilToInt(rectChildren.Count / columns);
-        }
+        return Mathf.CeilToInt((float)rectChildren.Count / columns);
     }
+
+    //if (fitType == FitType.WIDTH || fitType == FitType.HEIGHT || fitType == FitType.UNIFORM)
+    //{
+    //    float squareRoot = Mathf.Sqrt(transform.childCount);
+    //    rows = columns = Mathf.CeilToInt(squareRoot);
+    //    switch (fitType)
+    //    {
+    //        case FitType.WIDTH:
+    //            fitX = true;
+    //            fitY = false;
+    //            break;
+    //        case FitType.HEIGHT:
+    //            fitX = false;
+    //            fitY = true;
+    //            break;
+    //        case FitType.UNIFORM:
+    //            fitX = fitY = true;
+    //            break;
+    //    }
+    //}
+
+    //if (fitType == FitType.WIDTH || fitType == FitType.FIXEDCOLUMNS)
+    //{
+    //    rows = Mathf.CeilToInt(transform.childCount / (float)columns);
+    //    if (fitType == FitType.FIXEDCOLUMNS)
+    //    {
+    //        fitX = true;
+    //    }
+    //}
+    //if (fitType == FitType.HEIGHT || fitType == FitType.FIXEDROWS)
+    //{
+    //    columns = Mathf.CeilToInt(transform.childCount / (float)rows);
+    //}
     private int CalculateCellWidth()
     {
         return 0;
