92ac6d60b6076bc091a20c68465b3d2c819b848e
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2024-12-16 14:51:53 +0200

    Added OnTextSizeChange event to SettingsCarrier and refactoring code order
    Moved code blocks around to have a logical order: instance variable > enums > events > settings variables > functions. To make this code easier to understand.

--- Assets/MenuUi/Scripts/Settings/SettingsCarrier.cs
+++ Assets/MenuUi/Scripts/Settings/SettingsCarrier.cs
@@ -3,14 +3,75 @@ using System;
 using System.Collections.Generic;
 using Altzone.Scripts.Model.Poco.Game;
 
-public class SettingsCarrier : MonoBehaviour
+public class SettingsCarrier : MonoBehaviour // Script for carrying settings data between scenes
 {
-    // Script for carrying settings data between scenes
+    // Instance singleton
     public static SettingsCarrier Instance { get; private set; }
+
+    // Enums
+    public enum SoundType
+    {
+        none,
+        menu,
+        music,
+        sound
+    }
+
+    public enum TextSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    // Events
+    public event Action OnTextSizeChange;
+    public event Action OnButtonLabelVisibilityChange;
+    public event Action<CharacterID> OnCharacterGalleryCharacterStatWindowToShowChange;
+
+    // Settings variables
     public int mainMenuWindowIndex;
 
-    private TextSize _textSize;
+    public float masterVolume;
+    public float menuVolume;
+    public float musicVolume;
+    public float soundVolume;
 
+    private TextSize _textSize;
+    public TextSize Textsize { get => _textSize; }
+    
+    private bool _showButtonLabels;
+    public bool ShowButtonLabels
+    {
+        get
+        {
+            return _showButtonLabels;
+        }
+
+        set
+        {
+            _showButtonLabels = value;
+            OnButtonLabelVisibilityChange?.Invoke();
+        }
+    }
+
+    // Determines which character stat window to load/show from character gallery
+    private CharacterID _characterGalleryCharacterStatWindowToShow = CharacterID.None;
+    public CharacterID CharacterGalleryCharacterStatWindowToShow
+    {
+        get => _characterGalleryCharacterStatWindowToShow;
+        set
+        {
+            if (_characterGalleryCharacterStatWindowToShow != value)
+            {
+                _characterGalleryCharacterStatWindowToShow = value;
+                OnCharacterGalleryCharacterStatWindowToShowChange?.Invoke(_characterGalleryCharacterStatWindowToShow);
+                Debug.Log("CharacterGallery value changed" + _characterGalleryCharacterStatWindowToShow);
+            }
+        }
+    }
+
+    // Functions
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -33,26 +94,6 @@ public class SettingsCarrier : MonoBehaviour
         _showButtonLabels = (PlayerPrefs.GetInt("showButtonLabels", 1) == 1);
     }
 
-    public float masterVolume;
-    public float menuVolume;
-    public float musicVolume;
-    public float soundVolume;
-
-    public enum SoundType
-    {
-        none,
-        menu,
-        music,
-        sound
-    }
-    public enum TextSize
-    {
-        Small,
-        Medium,
-        Large
-    }
-
-
     // SentVolume combines masterVolume and another volume chosen by the sent type
     public float SentVolume(SoundType type)
     {
@@ -71,42 +112,6 @@ public class SettingsCarrier : MonoBehaviour
     {
         _textSize = size;
         PlayerPrefs.SetInt("Textsize", (int)size);
+        OnTextSizeChange?.Invoke();
     }
-
-    // Determines which character stat window to load/show from character gallery
-    private CharacterID _characterGalleryCharacterStatWindowToShow = CharacterID.None;
-    public CharacterID CharacterGalleryCharacterStatWindowToShow
-    {
-        get => _characterGalleryCharacterStatWindowToShow;
-        set
-        {
-            if (_characterGalleryCharacterStatWindowToShow != value)
-            {
-                _characterGalleryCharacterStatWindowToShow = value;
-                OnCharacterGalleryCharacterStatWindowToShowChange?.Invoke(_characterGalleryCharacterStatWindowToShow);
-                Debug.Log("CharacterGallery value changed" + _characterGalleryCharacterStatWindowToShow);
-            }
-        }
-    }
-
-    public TextSize Textsize { get => _textSize; }
-
-    public event Action<CharacterID> OnCharacterGalleryCharacterStatWindowToShowChange;
-
-    private bool _showButtonLabels;
-    public bool ShowButtonLabels
-    {
-        get
-        {
-            return _showButtonLabels;
-        }
-
-        set
-        {
-            _showButtonLabels = value;
-            OnButtonLabelVisibilityChange?.Invoke();
-        }
-    }
-
-    public event Action OnButtonLabelVisibilityChange;
 }
