99ae26d670ce6fb1431fdffdbaa1cd17e532c1ee
Ilona <venusflytrapd.d@gmail.com>
2025-01-14 18:52:10 +0300

    Pop Up progress revert

--- Assets/MenuUi/Scripts/Kauppa/GameFurnitureVisualizer.cs
+++ Assets/MenuUi/Scripts/Kauppa/GameFurnitureVisualizer.cs
@@ -2,23 +2,19 @@ using UnityEngine;
 using TMPro;
 using Altzone.Scripts.Model.Poco.Game;
 using UnityEngine.UI;
+using MenuUI.Scripts;
 
 public class GameFurnitureVisualizer : MonoBehaviour
 {
     [SerializeField] private TMP_Text _productText;
     [SerializeField] private TMP_Text _priceText;
     [SerializeField] private Button _button;
-
-    private GameObject _popUp;
     private GameFurniture _gameFurniture;
 
-    public void Initialize(GameFurniture gameFurniture, GameObject correspondingPopUp)
+    public void Initialize(GameFurniture gameFurniture)
     {
         _gameFurniture = gameFurniture;
         _productText.text = _gameFurniture.Name;
         _priceText.text = _gameFurniture.Value.ToString();
-
-        _popUp = correspondingPopUp;
-        _button.onClick.AddListener(() => _popUp.SetActive(true));
     }
 }
--- Assets/MenuUi/Scripts/Kauppa/PerusKauppaStorage.cs
+++ Assets/MenuUi/Scripts/Kauppa/PerusKauppaStorage.cs
@@ -21,12 +21,6 @@ public class PerusKauppaStorage : ShopPanelStorage
     [SerializeField] private GameFurnitureVisualizer _epicPrefab;
     [SerializeField] private GameFurnitureVisualizer _antiquePrefab;
 
-    [Space(5f)]
-
-    [Header("Parameters for Furniture")]
-    [SerializeField] private GameObject _correspondingPopUp;
-
-
     private Dictionary<FurnitureRarity, Transform> _rarityToParent;
     private Dictionary<FurnitureRarity, GameFurnitureVisualizer> _rarityToPrefab;
 
@@ -73,7 +67,7 @@ public class PerusKauppaStorage : ShopPanelStorage
                 {
                     Debug.Log("Furniture of " + furniture1.Name + "" + furniture1.Value +  " is created");
                     var newItem = Instantiate(_prefab, _parent);
-                    newItem.Initialize(furniture1, _correspondingPopUp);
+                    newItem.Initialize(furniture1);
                     gameFurnituresOnScene.Add(newItem);
                 }
                 else
