fc99831a749b980c19b3c31c1d37d7d6b21f9681
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-01-16 14:21:34 +0200

    StatsWindowHeaderPanel.cs script created
    Used in header panel to set info

---
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterStatsWindow/StatsWindowHeaderPanel.cs
@@ -0,0 +1,39 @@
+using MenuUi.Scripts.DefenceScreen.CharacterStatsWindow;
+using TMPro;
+using UnityEngine;
+
+public class StatsWindowHeaderPanel : MonoBehaviour
+{
+    [SerializeField] private StatsWindowController _controller;
+    [SerializeField] private TMP_Text _characterName;
+    [SerializeField] private TMP_Text _diamondsAmount;
+    [SerializeField] private TMP_Text _eraserAmount;
+
+    private void OnEnable()
+    {
+        SetCharacterName();
+        SetDiamondsAmount();
+        SetEraserAmount();
+    }
+
+
+    private void SetCharacterName()
+    {
+        if (_controller != null)
+        {
+            _characterName.text = _controller.GetCurrentCharacterName();
+        }
+    }
+
+
+    private void SetDiamondsAmount()
+    {
+        _diamondsAmount.text = _controller.GetDiamondAmount().ToString();
+    }
+
+
+    private void SetEraserAmount()
+    {
+        _eraserAmount.text = _controller.GetEraserAmount().ToString();
+    }
+}
