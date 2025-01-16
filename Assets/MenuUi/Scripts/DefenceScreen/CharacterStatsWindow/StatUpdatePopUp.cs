fbc258d0fd3a29899e6f830d5abaf3fa54bd28eb
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-01-16 10:10:10 +0200

    Set stat icon and stat name to StatUpdatePopUp from StatsReference sheet

--- Assets/MenuUi/Scripts/DefenceScreen/CharacterStatsWindow/StatUpdatePopUp.cs
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterStatsWindow/StatUpdatePopUp.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Altzone.Scripts.Model.Poco.Game;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MenuUi.Scripts.DefenceScreen.CharacterStatsWindow
 {
     public class StatUpdatePopUp : MonoBehaviour
     {
+        [SerializeField] private StatsReference _statsReference;
         [SerializeField] private GameObject _contents;
+        [SerializeField] private Image _statIcon;
+        [SerializeField] private TMP_Text _statName;
+        [SerializeField] private TMP_Text _statNumber;
+        [SerializeField] private TMP_Text _diamondCost;
 
 
         private void Awake()
@@ -18,9 +26,45 @@ namespace MenuUi.Scripts.DefenceScreen.CharacterStatsWindow
         /// <summary>
         /// Open StatUpdatePopUp
         /// </summary>
-        public void OpenPopUp()
+        /// <param name="statType">Stat type int which is compared to StatType enum. (is there a better way to do this?)</param>
+        public void OpenPopUp(int statType)
         {
-            _contents.SetActive(true);
+            StatInfo statInfo = null;
+
+            // Getting StatInfo from StatsReference sheet
+            switch ((StatType)statType)
+            {
+                case StatType.None:
+                    ClosePopUp();
+                    break;
+                case StatType.Attack:
+                    statInfo = _statsReference.GetStatInfo(StatType.Attack);
+                    break;
+                case StatType.Defence:
+                    statInfo = _statsReference.GetStatInfo(StatType.Defence);
+                    break;
+                case StatType.Resistance:
+                    statInfo = _statsReference.GetStatInfo(StatType.Resistance);
+                    break;
+                case StatType.Hp:
+                    statInfo = _statsReference.GetStatInfo(StatType.Hp);
+                    break;
+                case StatType.Speed:
+                    statInfo = _statsReference.GetStatInfo(StatType.Speed);
+                    break;
+            }
+
+            // Setting stat icon and stat name
+            if (statInfo != null)
+            {
+                _statIcon.sprite = statInfo.Image;
+                _statName.text = statInfo.Name;
+                _contents.SetActive(true);
+            }
+            else
+            {
+                ClosePopUp();
+            }
         }
 
 
