7855896a9e0d88468248396cdfcb37489172ea31
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-01-20 16:41:03 +0200

    Updated piechart to new one which does not use UI Toolkit

--- Assets/MenuUi/Scripts/DefenceScreen/CharacterStatsWindow/PieChart.cs
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterStatsWindow/PieChart.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Altzone.Scripts.Model.Poco.Game;
-using UnityEngine.UIElements;
-using System.Collections;
+using UnityEngine.UI;
+
 
 namespace MenuUi.Scripts.DefenceScreen.CharacterStatsWindow
 {
     public class PieChartManager : MonoBehaviour
     {
         [SerializeField] private StatsWindowController _controller;
+
+        [SerializeField] private int _sliceAmount;
 
         // Set the color, what kind of piece the piece should turn into according to a certain stat. These can be changed directly inside unity.
         [SerializeField] private Color impactForceColor = new Color(1f, 0.5f, 0f);
@@ -18,24 +20,38 @@ namespace MenuUi.Scripts.DefenceScreen.CharacterStatsWindow
         [SerializeField] private Color speedColor = new Color(0f, 0.5f, 0f);
         [SerializeField] private Color defaultColor = Color.white;
 
-        PieChartVisuals _pieChart;
+        [SerializeField] private Sprite circleSprite;
 
 
-        private void Update() // updatechart has to be called every frame so that it follows swipe
+        private void OnEnable()
         {
             UpdateChart();
+            _controller.OnStatUpdated += UpdateChart;
+        }
+
+        private void OnDisable()
+        {
+            _controller.OnStatUpdated -= UpdateChart;
         }
 
 
-        public void UpdateChart()
+        public void UpdateChart(StatType statType = StatType.None)
         {
+            // Destroy old pie slices
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+
+            // Get stats
             int impactForce = _controller.GetStat(StatType.Attack);
             int healthPoints = _controller.GetStat(StatType.Hp);
             int defence = _controller.GetStat(StatType.Defence);
             int characterSize = _controller.GetStat(StatType.Resistance);
             int speed = _controller.GetStat(StatType.Speed);
 
-            // Arrange stats.
+
+            // Arrange stats
             var stats = new List<(int level, Color color)>
             {
                 (defence, defenceColor),
@@ -45,11 +61,73 @@ namespace MenuUi.Scripts.DefenceScreen.CharacterStatsWindow
                 (impactForce, impactForceColor),
             };
 
-            // create visuals
-            RectTransform rectTransform = GetComponent<RectTransform>();
-            _pieChart = new PieChartVisuals(rectTransform.position, rectTransform.rect.width / 2, 50, stats);
-            GetComponent<UIDocument>().rootVisualElement.Clear();
-            GetComponent<UIDocument>().rootVisualElement.Add(_pieChart);
+            // Create slices
+            float sliceFillAmount = 1.0f / (float)_sliceAmount;
+            float currentSliceFill = 1.0f;
+
+            int remainingSlices = _sliceAmount;
+
+            // Colored slices
+            foreach (var stat in stats)
+            {
+                for (int i = 0; i < stat.level; i++)
+                {
+                    CreateSlice(currentSliceFill, stat.color);
+
+                    currentSliceFill -= sliceFillAmount;
+
+                    remainingSlices--;
+                    if (remainingSlices == 0) // if runs out of slices return
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // White slices
+            for (int i = remainingSlices; i > 0; i--)
+            {
+                CreateSlice(currentSliceFill, defaultColor);
+
+                currentSliceFill -= sliceFillAmount;
+            }
+        }
+
+
+        private void CreateSlice(float fillAmount, Color color)
+        {
+            // Create gameobject and add components
+            GameObject slice = new GameObject();
+            slice.AddComponent<RectTransform>();
+            slice.AddComponent<Image>();
+            slice.AddComponent<Outline>();
+
+            // Modify image properties
+            Image sliceImage = slice.GetComponent<Image>();
+            sliceImage.sprite = circleSprite;
+            sliceImage.color = color;
+            sliceImage.type = Image.Type.Filled;
+            sliceImage.fillClockwise = false;
+            sliceImage.fillOrigin = (int)Image.Origin360.Top;
+            sliceImage.preserveAspect = true;
+            sliceImage.fillAmount = fillAmount;
+
+            // Outline thickness
+            Outline sliceOutline = slice.GetComponent<Outline>();
+            sliceOutline.effectDistance = new Vector2(1, -1);
+
+            // Reparent to this node
+            slice.transform.SetParent(transform);
+
+            // Set scale
+            slice.transform.localScale = Vector3.one;
+
+            // Set anchors
+            RectTransform sliceRect = slice.GetComponent<RectTransform>();
+            sliceRect.offsetMax = Vector2.zero;
+            sliceRect.offsetMin = Vector2.zero;
+            sliceRect.anchorMin = Vector3.zero;
+            sliceRect.anchorMax = Vector3.one;
         }
     }
 }
