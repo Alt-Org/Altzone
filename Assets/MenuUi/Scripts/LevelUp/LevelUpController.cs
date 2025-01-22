b6d81960bb863103f556b7f9056dffd6c2a03487
Eero Pokki <s2200880@edu.bc.fi>
2025-01-22 14:41:44 +0200

    OpenPopup method
    OpenPopup method opens the levelupwindow

--- Assets/MenuUi/Scripts/LevelUp/LevelUpController.cs
+++ Assets/MenuUi/Scripts/LevelUp/LevelUpController.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class LevelUpController : MonoBehaviour
 {
+    public GameObject LevelUpPanel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        OpenPopup();
+    }
+
+    public void OpenPopup()
+    {
+        if (LevelUpPanel != null)
+        {
+            LevelUpPanel.SetActive(true);
+        }
     }
 }
