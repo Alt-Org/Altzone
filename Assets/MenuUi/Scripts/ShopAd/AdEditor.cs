8b301901223c605a06947929a448dad17e1beaf4
LauraMariaK <laurakarjalainen03@gmail.com>
2025-01-24 12:39:55 +0200

    Updated AdEditor
    Created button to change frame. This is for testing the new frames

--- Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
+++ Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
@@ -7,13 +7,32 @@ using UnityEngine.UI;
 public class AdEditor: MonoBehaviour
 {
     [SerializeField] private Image _backgroundImage;
-    [SerializeField] private Image _borderImage;
     [SerializeField] private Image _effectImage;
     [SerializeField] private Image _itemImage;
     [SerializeField] private TMP_Text _titleText;
+    [SerializeField] private Image _borderImage;
+   
+
+    public List<Sprite> spriteChoices;
+    private int counter;
+    private int currentBorder = 0;
 
 
-    
+    public void NextBorder()
+    {
+        counter++;
+        if (counter == 1)
+        {
+
+            currentBorder++;
+            counter = 0;
+            if (currentBorder >= spriteChoices.Count)
+            {
+                currentBorder = 0;
+            }
+            _borderImage.sprite = spriteChoices[currentBorder];
+        }
+    }
 
 
     public void CloseEditor()
