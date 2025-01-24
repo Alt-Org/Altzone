081cde3a1e866cbc42db1db9dea62f2469a4f391
Sebastian Backman <seba.backman@pp.inet.fi>
2025-01-24 14:10:47 +0200

    DT Clan rewards progress bar
    -DailyTask Clan rewards progress bar has now functionality and testing button.
    -ClanTask views player list now starts from first player.

--- Assets/MenuUi/Scripts/DailyQuest/DailyTaskManager.cs
+++ Assets/MenuUi/Scripts/DailyQuest/DailyTaskManager.cs
@@ -46,8 +46,17 @@ public class DailyTaskManager : MonoBehaviour
 
     private List<GameObject> _clanPlayers = new List<GameObject>();
 
+    [Header("ClanTaskProgressBar")]
+    [SerializeField] private Slider _clanProgressBarSlider;
+    [SerializeField] private RectTransform _clanProgressBarMarkersBase;
+    [SerializeField] private GameObject _clanProgressBarMarkerPrefab;
+
+    private List<GameObject> _clanProgressBarMarkers = new List<GameObject>();
+
     //Local Testing
     private int _ownTaksProgress = 0;
+    private int _clanProgressBarGoal = 10000;
+    private int _clanProgressBarCurrentPoints = 0;
 
     public enum SelectedTab
     {
@@ -62,6 +71,8 @@ public class DailyTaskManager : MonoBehaviour
     {
         TaskGenerator();
         StartCoroutine(PopulateClanPlayers());
+        StartCoroutine(SetClanProgressBar());
+        StartCoroutine(CreateClanProgressBar());
 
         //Tab bar
         _dailyTasksTabButton.onClick.AddListener(() => SwitchTab(SelectedTab.Tasks));
@@ -237,6 +248,13 @@ public class DailyTaskManager : MonoBehaviour
             _clanPlayers.Add(player);
             Debug.Log("Created clan player: " + i);
         }
+
+        //Needed to update the instantiated DT cards spacing in HorizontalLayoutGroups.
+        //LayoutRebuilder.ForceRebuildLayoutImmediate(_clanPlayersList);
+
+        //Sets DT cards to left side.
+        _clanPlayersList.anchoredPosition = new Vector2(0f, -500f);
+
         yield return true;
     }
 
@@ -277,6 +295,7 @@ public class DailyTaskManager : MonoBehaviour
         });
     }
 
+    //Handle popup data.
     private void PopupDataHandler(PopupData data)
     {
         switch (data.Type)
@@ -286,10 +305,12 @@ public class DailyTaskManager : MonoBehaviour
         }
     }
 
+    //Set OwnTask page.
     private void HandleOwnTask(PopupData.OwnPageData data)
     {
         //TODO: Add task accept code when server side has functionality.
-        CalculateOwnTaskProgressBar(data.TaskAmount);
+        StartCoroutine(_ownTaskPageHandler.SetTaskProgress(0f));
+
         StartCoroutine(_ownTaskPageHandler.SetDailyTask(data.TaskDescription, data.TaskAmount, data.TaskPoints, data.TaskCoins));
         _ownTaskId = data.TaskId;
         Debug.Log("Task id: " + _ownTaskId + ", has been accepted.");
@@ -305,21 +326,26 @@ public class DailyTaskManager : MonoBehaviour
 
             if (quest.TaskData.Id == _ownTaskId)
             {
-                CalculateOwnTaskProgressBar(quest.TaskData.Amount);
+                UpdateOwnTaskProgress(quest);
                 return;
             }
         }
     }
 
-    private void CalculateOwnTaskProgressBar(int taskAmount)
+    private void UpdateOwnTaskProgress(DailyQuest quest)
     {
-        float progress = (float)_ownTaksProgress / (float)taskAmount;
+        float progress = CalculateProgressBar(quest.TaskData.Amount, _ownTaksProgress);
         StartCoroutine(_ownTaskPageHandler.SetTaskProgress(progress));
         Debug.Log("Task id: " + _ownTaskId + ", current progress: " + progress);
         if (progress >= 1f)
         {
             Debug.Log("Task id:" + _ownTaskId + ", is done");
         }
+    }
+
+    private float CalculateProgressBar(int targetPoints, int currentPoints)
+    {
+        return ((float)currentPoints / (float)targetPoints);
     }
 
     // Calling popup for canceling task.
@@ -338,6 +364,7 @@ public class DailyTaskManager : MonoBehaviour
         _ownTaskId = null;
     }
 
+    //Switch tab.
     public void SwitchTab(SelectedTab tab)
     {
         //Hide old tab
@@ -360,5 +387,109 @@ public class DailyTaskManager : MonoBehaviour
         }
 
         Debug.Log($"Switched to {_selectedTab}.");
+    }
+
+    //Clan progress bar functions.
+    private IEnumerator SetClanProgressBar()
+    {
+        //TODO: Get clan task data and fill the clan progress bar based on that data.
+        string clanId = null;
+        ClanData clan = null;
+        Storefront.Get().GetPlayerData(GameConfig.Get().PlayerSettings.PlayerGuid, p => clanId = p.ClanId);
+
+        if (clanId == null)
+            StartCoroutine(ServerManager.Instance.GetPlayerFromServer(content =>
+            {
+                if (content != null)
+                    clanId = content.clan_id;
+                else
+                {
+                    Debug.LogError("Could not connect to server and receive player data");
+                    return;
+                }
+            }));
+
+        yield return new WaitUntil(() => clanId != null);
+
+        Storefront.Get().GetClanData(clanId, c => clan = c);
+
+        if (clan == null)
+            StartCoroutine(ServerManager.Instance.GetClanFromServer(content =>
+            {
+                if (content != null)
+                    clan = new(content);
+                else
+                {
+                    Debug.LogError("Could not connect to server and receive player data");
+                    return;
+                }
+            }));
+
+        yield return new WaitUntil(() => clan != null);
+
+        _clanProgressBarSlider.value = CalculateProgressBar(_clanProgressBarGoal, clan.Points);
+    }
+
+    private List<DailyTaskClanReward.ClanRewardData> TESTGenerateClanRewardsBar()
+    {
+        var clanRewardDatas = new List<DailyTaskClanReward.ClanRewardData>()
+        {
+            new DailyTaskClanReward.ClanRewardData(true, DailyTaskClanReward.ClanRewardType.Box, 500),
+            new DailyTaskClanReward.ClanRewardData(false, DailyTaskClanReward.ClanRewardType.Box, 1000),
+            new DailyTaskClanReward.ClanRewardData(false, DailyTaskClanReward.ClanRewardType.Box, 5000),
+            new DailyTaskClanReward.ClanRewardData(false, DailyTaskClanReward.ClanRewardType.Chest, 10000)
+        };
+        return clanRewardDatas;
+    }
+
+    public void TESTAddClanRewardBarPoints()
+    {
+        _clanProgressBarCurrentPoints += 100;
+
+        StartCoroutine(CalculateClanRewardBarProgress());
+    }
+
+    private IEnumerator CalculateClanRewardBarProgress()
+    {
+        float sectionLenghts = (1f / (float)_clanProgressBarMarkers.Count);
+
+        for (int i = 0; i < _clanProgressBarMarkers.Count; i++)
+        {
+            int startPoints = (
+                (i) <= 0 ?
+                0 :
+                _clanProgressBarMarkers[i - 1].GetComponent<DailyTaskClanReward>().Data.Threshold
+                );
+
+            int endPoints = _clanProgressBarMarkers[i].GetComponent<DailyTaskClanReward>().Data.Threshold;
+
+            if ((_clanProgressBarCurrentPoints <= endPoints) || (i + 1 >= _clanProgressBarMarkers.Count))
+            {
+                float startPosition = sectionLenghts * i;
+                float endPosition = ((i + 1) >= _clanProgressBarMarkers.Count ? 1f : (sectionLenghts * (float)(i + 1)));
+
+                float chunkProgress = (float)(_clanProgressBarCurrentPoints - startPoints) / (float)(endPoints - startPoints);
+                Debug.Log("ClanRewardsProgressBar: chunk progress: " + chunkProgress + ", start points: " + startPoints + ", end points: " + endPoints);
+
+                _clanProgressBarSlider.value = Mathf.Lerp(startPosition, endPosition, chunkProgress);
+                break;
+            }
+        }
+
+        yield return true;
+    }
+
+    private IEnumerator CreateClanProgressBar()
+    {
+        //TODO: Replace with data from server.
+        var datas = TESTGenerateClanRewardsBar();
+
+        foreach (var data in datas)
+        {
+            GameObject rewardMarker = Instantiate(_clanProgressBarMarkerPrefab, _clanProgressBarMarkersBase);
+            rewardMarker.GetComponent<DailyTaskClanReward>().Set(data);
+            _clanProgressBarMarkers.Add(rewardMarker);
+        }
+        yield return true;
     }
 }
