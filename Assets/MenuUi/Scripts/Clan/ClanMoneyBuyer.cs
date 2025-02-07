1f9e4356ac974be2e07c8cdb052d54a1747c6b3c
Ilona <venusflytrapd.d@gmail.com>
2025-02-07 14:15:13 +0300

    Added simple reactions to "buy" buttons

---
+++ Assets/MenuUi/Scripts/Clan/ClanMoneyBuyer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClanMoneyBuyer : MonoBehaviour
+{
+    public void BuyMoney(int amount)
+    {
+        Debug.Log("Bought! " + amount);
+    }
+}
