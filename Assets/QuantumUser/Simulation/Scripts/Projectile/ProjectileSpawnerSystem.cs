a1336b86bef4e386a48e2f6d893b174097de43b6
Niklas <niklas.tontti603619@gmail.com>
2025-02-11 16:06:19 +0200

    ProjectileSpawnerSystem: Changed comments

--- Assets/QuantumUser/Simulation/Scripts/Projectile/ProjectileSpawnerSystem.cs
+++ Assets/QuantumUser/Simulation/Scripts/Projectile/ProjectileSpawnerSystem.cs
@@ -14,16 +14,16 @@ namespace Quantum
             public ProjectileSpawner* Spawner;
         }
 
-        //Adding the SpawnerSystem.qtn component to ProjectileSpawnerSystem to ensure that the filter works
+        // adding the SpawnerSystem.qtn component to ProjectileSpawnerSystem to ensure that the filter works
         public override void OnInit(Frame f)
         {
-            // Create a new entity
+            // create a new entity
             EntityRef entity = f.Create();
 
-            // Add the ProjectileSpawner component
+            // add the ProjectileSpawner component
             f.Add<ProjectileSpawner>(entity);
 
-            // Initialize the ProjectileSpawner component
+            // initialize the ProjectileSpawner component
             ProjectileSpawner* spawner = f.Unsafe.GetPointer<ProjectileSpawner>(entity);
             spawner->HasSpawned = false;
 
@@ -38,27 +38,27 @@ namespace Quantum
             if (gameSession==null) return;
             if (gameSession->state != GameState.Playing) return;
 
-            // Ensure the projectile is spawned only once
+            // ensure the projectile is spawned only once
             if (!filter.Spawner->HasSpawned)
             {
                 Debug.Log("Projectile should spawn");
                 ProjectileSpec  config = f.FindAsset(f.RuntimeConfig.ProjectileSpec);
                 SpawnProjectile(f, config.ProjectilePrototype);
 
-                // Mark projectile as spawned
+                // mark projectile as spawned
                 filter.Spawner->HasSpawned = true;
             }
         }
 
         private void SpawnProjectile(Frame f, AssetRef<EntityPrototype> childPrototype)
         {
-            // Retrieve the game configuration asset for custom projectile settings.
+            // retrieve the game configuration asset for custom projectile settings.
             ProjectileSpec config = f.FindAsset(f.RuntimeConfig.ProjectileSpec);
 
-            // Create a new entity based on the provided prototype.
+            // create a new entity based on the provided prototype.
             EntityRef projectileEntity = f.Create(childPrototype);
 
-            // Get a pointer to the Transform2D component of the created projectile entity.
+            // get a pointer to the Transform2D component of the created projectile entity.
             Projectile* projectile = f.Unsafe.GetPointer<Projectile>(projectileEntity);
             Transform2D* projectileTransform = f.Unsafe.GetPointer<Transform2D>(projectileEntity);
             PhysicsCollider2D* projectileCollider = f.Unsafe.GetPointer<PhysicsCollider2D>(projectileEntity);
