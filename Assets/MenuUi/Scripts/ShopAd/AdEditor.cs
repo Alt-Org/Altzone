03bb956e92fad5535bf5db7832a7afbbba3c6f13
LauraMariaK <laurakarjalainen03@gmail.com>
2025-02-16 14:59:32 +0200

    Continued AdEditor (in progress)
    -Change background color started (in progress)
    - buttons for changing item, background color and border made (will be changed)

--- Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
+++ Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
@@ -1,23 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AdEditor: MonoBehaviour
+public class AdEditor : MonoBehaviour
 {
     [SerializeField] private Image _backgroundImage;
     [SerializeField] private Image _effectImage;
     [SerializeField] private Image _itemImage;
     [SerializeField] private TMP_Text _titleText;
     [SerializeField] private Image _borderImage;
-   
+
+    [SerializeField] private Color redColor;
+    [SerializeField] private Color blueColor;
+    [SerializeField] private Color greenColor;
+    [SerializeField] private Color yellowColor;
 
     public List<Sprite> spriteChoices;
     private int counter;
     private int currentBorder = 0;
 
+    public void ChangeRedColor()
+    {
+        _backgroundImage.color = redColor;
+    }
+    public void ChangeBlueColor()
+    {
+        _backgroundImage.color = blueColor;
+    }
+    public void ChangeGreenColor()
+    {
+        _backgroundImage.color = greenColor;
+    }
 
+    public void ChangetYellowColor()
+    {
+        _backgroundImage.color = yellowColor;
+    }
     public void NextBorder()
     {
         counter++;
@@ -33,7 +54,7 @@ public class AdEditor: MonoBehaviour
             _borderImage.sprite = spriteChoices[currentBorder];
         }
     }
-
+    
 
     public void CloseEditor()
     {
