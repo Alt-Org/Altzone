093b078fb7ffae846ce41cb5242d7cbea4c8cfb6
LauraMariaK <laurakarjalainen03@gmail.com>
2025-02-16 16:48:06 +0200

    Continued AdEditor
    -changed buttons to change color, border and effect
    -color and border buttons are working
    -effect button doesn't have any function yet
    -changed AdEditors look

--- Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
+++ Assets/MenuUi/Scripts/ShopAd/AdEditor.cs
@@ -13,48 +13,71 @@ public class AdEditor : MonoBehaviour
     [SerializeField] private TMP_Text _titleText;
     [SerializeField] private Image _borderImage;
 
-    [SerializeField] private Color redColor;
     [SerializeField] private Color blueColor;
+    [SerializeField] private Color purpleColor;
     [SerializeField] private Color greenColor;
-    [SerializeField] private Color yellowColor;
+    [SerializeField] private Color orangeColor;
+    [SerializeField] private Color whiteColor;
 
-    public List<Sprite> spriteChoices;
-    private int counter;
+    public List<Sprite> borderChoices;
+    public List<Sprite> itemChoices;
+    private int borderCounter;
+    private int itemCounter;
     private int currentBorder = 0;
+    private int currentItem = 0;
 
-    public void ChangeRedColor()
-    {
-        _backgroundImage.color = redColor;
-    }
     public void ChangeBlueColor()
     {
         _backgroundImage.color = blueColor;
+    }
+    public void ChangePurpleColor()
+    {
+        _backgroundImage.color = purpleColor;
     }
     public void ChangeGreenColor()
     {
         _backgroundImage.color = greenColor;
     }
 
-    public void ChangetYellowColor()
+    public void ChangetOrangeColor()
     {
-        _backgroundImage.color = yellowColor;
+        _backgroundImage.color = orangeColor;
     }
-    public void NextBorder()
+    public void ChangetWhiteColor()
     {
-        counter++;
-        if (counter == 1)
+        _backgroundImage.color = whiteColor;
+    }
+    public void NextItem()
+    {
+        itemCounter++;
+        if (itemCounter == 1)
         {
 
-            currentBorder++;
-            counter = 0;
-            if (currentBorder >= spriteChoices.Count)
+            currentItem++;
+            itemCounter = 0;
+            if (currentItem >= itemChoices.Count)
             {
                 currentBorder = 0;
             }
-            _borderImage.sprite = spriteChoices[currentBorder];
+            _itemImage.sprite = itemChoices[currentItem];
         }
     }
-    
+    public void NextBorder()
+    {
+        borderCounter++;
+        if (borderCounter == 1)
+        {
+
+            currentBorder++;
+            borderCounter = 0;
+            if (currentBorder >= borderChoices.Count)
+            {
+                currentBorder = 0;
+            }
+            _borderImage.sprite = borderChoices[currentBorder];
+        }
+    }
+
 
     public void CloseEditor()
     {
