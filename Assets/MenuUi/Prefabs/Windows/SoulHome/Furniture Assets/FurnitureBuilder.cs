3cd61a4920490ecaa640f13182a882de3046d3d7
Niko Mattila <niko.mattila@pp.inet.fi>
2025-02-24 03:53:33 +0200

    Created a base for the furniture builder
    Compiles the data given to it into FurnitureInfoObjects to be placed into furniture reference.
    Also creates Furniture SetInfo if needed.
    Data doesn't yet go into correct place.
    Also creates the Prefabs that are needed for soulhome.
    These are created into the same folder as the Builder asset.
    Data for them not yet put into place.

---
+++ Assets/MenuUi/Prefabs/Windows/SoulHome/Furniture Assets/FurnitureBuilder.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using Altzone.Scripts.Model.Poco.Game;
+using Altzone.Scripts.ReferenceSheets;
+using MenuUI.Scripts.SoulHome;
+using UnityEditor;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName = "ALT-Zone/FurnitureBuilder", fileName = "FurnitureBuilder")]
+public class FurnitureBuilder : ScriptableObject
+{
+    [SerializeField, Header("Reference sheets")]
+    private StorageFurnitureReference _furnitureReference;
+    [SerializeField]
+    private SoulHomeFurnitureReference _soulHomeReference;
+
+    [SerializeField, Header("Furniture Prefab bases")]
+    private GameObject _furniturePrefabBase;
+    [SerializeField]
+    private GameObject _trayFurniturePrefabBase;
+
+    [SerializeField, Header("Furniture General info")]
+    private string _furnitureName;
+    [SerializeField]
+    private string _setName;
+    [SerializeField]
+    private string _creatorName;
+    [SerializeField]
+    private string _visibleName;
+    [SerializeField]
+    private string _description;
+    [SerializeField]
+    private string _diagnoseNumber;
+
+    [SerializeField, Header("Furniture Sprites")]
+    private Sprite _furnitureFrontSprite;
+    [SerializeField]
+    private Sprite _furnitureLeftSprite;
+    [SerializeField]
+    private Sprite _furnitureRightSprite;
+    [SerializeField]
+    private Sprite _furnitureBackSprite;
+
+
+    [SerializeField, Header("Furniture base data")]
+    private FurnitureRarity _rarity;
+    [SerializeField]
+    private FurnitureSize _size;
+    [SerializeField]
+    private FurnitureSize _rotatedSize;
+    [SerializeField]
+    private FurniturePlacement _placement;
+    [SerializeField]
+    private double _weight;
+    [SerializeField]
+    private float _value;
+    [SerializeField]
+    private string _material;
+    [SerializeField]
+    private string _recycling;
+
+    public void BuildFurniture()
+    {
+        BaseFurniture baseFurniture = new BaseFurniture()
+        {
+            Name = _furnitureName+"_"+_setName,
+            Rarity = _rarity,
+            Size = _size,
+            RotatedSize = _rotatedSize,
+            Placement = _placement,
+            Weight = _weight,
+            Value = _value,
+            Material = _material,
+            Recycling = _recycling
+
+        };
+
+        FurnitureInfoObject furnitureObject = new FurnitureInfoObject()
+        {
+            Name = _furnitureName,
+            Image = _furnitureFrontSprite,
+            VisibleName = _visibleName,
+            ArtisticDescription = _description,
+            DiagnoseNumber = _diagnoseNumber,
+            BaseFurniture = baseFurniture
+        };
+
+        FurnitureSetInfo furnitureSetInfo = new FurnitureSetInfo()
+        {
+            SetName = _setName,
+            ArtistName = _creatorName
+        };
+
+        string folderPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        if (folderPath.Contains("."))
+            folderPath = folderPath.Remove(folderPath.LastIndexOf('/'));
+        Debug.Log(folderPath);
+
+        GameObject newFurniturePrefab = (GameObject)PrefabUtility.InstantiatePrefab(_furniturePrefabBase);
+        newFurniturePrefab.name = _furnitureName+"_"+_setName+"_Furniture";
+
+        GameObject newTrayPrefab = (GameObject)PrefabUtility.InstantiatePrefab(_trayFurniturePrefabBase);
+        newTrayPrefab.name = _furnitureName + "_" + _setName + "_FurnitureImage";
+        newTrayPrefab.GetComponent<TrayFurniture>().FurnitureObject = newFurniturePrefab;
+
+        GameObject obj = PrefabUtility.SaveAsPrefabAsset(newFurniturePrefab, folderPath+"/"+ newFurniturePrefab.name +".prefab");
+        GameObject obj2 = PrefabUtility.SaveAsPrefabAsset(newTrayPrefab, folderPath + "/" + newTrayPrefab.name + ".prefab");
+        obj2.GetComponent<TrayFurniture>().FurnitureObject = obj;
+    }
+
+
+}
+
+#region Editor Code
+#if UNITY_EDITOR
+[CustomEditor(typeof(FurnitureBuilder))]
+public class FurnitureBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        FurnitureBuilder script = (FurnitureBuilder)target;
+
+        if (GUILayout.Button("Build Furniture"))
+        {
+            script.BuildFurniture();
+        }
+    }
+}
+#endif
+#endregion
