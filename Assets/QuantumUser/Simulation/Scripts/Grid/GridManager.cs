a5758a9bd16494ebee62bd9b3719646c73c99737
Niklas <niklas.tontti603619@gmail.com>
2025-03-03 16:48:08 +0200

    GridManager: Fixed conversion math

--- Assets/QuantumUser/Simulation/Scripts/Grid/GridManager.cs
+++ Assets/QuantumUser/Simulation/Scripts/Grid/GridManager.cs
@@ -10,15 +10,18 @@ namespace Quantum
 
         public static void Init(BattleArenaSpec battleArenaSpec)
         {
-            s_battleArenaSpec = battleArenaSpec;
-            GridScaleFactor = s_battleArenaSpec.WorldHeight / s_battleArenaSpec.GridHeight;
+            GridScaleFactor = battleArenaSpec.WorldHeight / battleArenaSpec.GridHeight;
+            s_gridOffset = new FPVector2(
+                battleArenaSpec.GridWidth * GridScaleFactor * FP._0_50,
+                battleArenaSpec.WorldHeight * FP._0_50
+            );
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int WorldXPositionToGridCol(FP x) { return (int)((x + s_battleArenaSpec.WorldWidth  * FP._0_50) / GridScaleFactor); }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int WorldYPositionToGridRow(FP y) { return (int)((y + s_battleArenaSpec.WorldHeight * FP._0_50) / GridScaleFactor); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int WorldXPositionToGridCol(FP x) { return (int)((x + s_gridOffset.X) / GridScaleFactor); }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int WorldYPositionToGridRow(FP y) { return (int)((y + s_gridOffset.Y) / GridScaleFactor); }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static FP GridRowToWorldYPosition(int row) { return (((FP)row) + FP._0_50) * GridScaleFactor - s_battleArenaSpec.WorldHeight * FP._0_50; }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static FP GridColToWorldXPosition(int col) { return (((FP)col) + FP._0_50) * GridScaleFactor - s_battleArenaSpec.WorldWidth  * FP._0_50; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static FP GridRowToWorldYPosition(int row) { return (((FP)row) + FP._0_50) * GridScaleFactor - s_gridOffset.Y; }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static FP GridColToWorldXPosition(int col) { return (((FP)col) + FP._0_50) * GridScaleFactor - s_gridOffset.X; }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static GridPosition WorldPositionToGridPosition(FPVector2 wordPosition)
@@ -39,6 +42,6 @@ namespace Quantum
             );
         }
 
-        private static BattleArenaSpec s_battleArenaSpec;
+        private static FPVector2 s_gridOffset;
     }
 }
