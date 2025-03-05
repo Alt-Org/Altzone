33737423c224384fe9c78b56de3e7672b90a7ed5
Jani Peltonen <62407359+Reldin@users.noreply.github.com>
2025-03-05 15:27:50 +0200

    Renamed various things
    Renamed PlayerSpawnSystem to PlayerJoinSystem
    Renamed BattlePlayerPosition to BattlePlayerSlot     // to better reflect its function and to avoid confusion
    Renamed BattleCharacterBase ClassID to Class  // Because it is not really an id

--- Assets/QuantumUser/Simulation/Scripts/Player/PlayerJoinSystem.cs
+++ Assets/QuantumUser/Simulation/Scripts/Player/PlayerJoinSystem.cs
@@ -1,16 +1,16 @@
 using System.Data.Common;
 using Photon.Deterministic;
 using Quantum.Prototypes;
+using UnityEngine;
 using UnityEngine.Scripting;
 
 namespace Quantum
 {
     [Preserve]
-    public unsafe class PlayerSpawnSystem  : SystemSignalsOnly, ISignalOnPlayerAdded
+    public unsafe class PlayerJoinSystem  : SystemSignalsOnly, ISignalOnPlayerAdded
     {
         public void OnPlayerAdded(Frame f, PlayerRef player, bool firstTime)
         {
-
             FPVector2[] spawnPoints = new FPVector2[]
             {
                 new(-2, -5),
@@ -18,6 +18,10 @@ namespace Quantum
                 new(2, 5),
                 new(-2, 5)
             };
+
+       /*     BattleCharacterBase character;*/
+            // Stat overrides
+            //character.Attack = 10;
 
             //gets spawnpoint for player depending on PlayerRef, will be replaced with playerpositions from lobby
             FPVector2 spawnPos2D = spawnPoints[player];
@@ -35,11 +39,12 @@ namespace Quantum
                 normal = new FPVector2(0,-1);
             }
 
+
             //creates player entity from prefab and gives it playerdata component
             RuntimePlayer data = f.GetPlayerData(player);
             EntityPrototype entityPrototypeAsset = f.FindAsset(data.PlayerAvatar);
             EntityRef playerEntity = f.Create(entityPrototypeAsset);
-            f.Add(playerEntity, new PlayerData{Player = player, Speed = 20, TargetPosition = spawnPos2D, Rotation = 0, Normal = normal, CollisionMinOffset = 1});
+            f.Add(playerEntity, new PlayerData { Player = player, TargetPosition = spawnPos2D, Rotation = 0, Normal = normal, CollisionMinOffset = 1, Speed = 20 });
 
             //set position and rotation
             Transform2D* playerTransform = f.Unsafe.GetPointer<Transform2D>(playerEntity);
