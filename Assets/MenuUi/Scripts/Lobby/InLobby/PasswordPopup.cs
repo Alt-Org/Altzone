75d610a142ab8a756d914438f5c90b4e024c3f4c
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-03-11 14:14:13 +0200

    Added comments to PasswordPopup.cs

--- Assets/MenuUi/Scripts/Lobby/InLobby/PasswordPopup.cs
+++ Assets/MenuUi/Scripts/Lobby/InLobby/PasswordPopup.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 namespace MenuUi.Scripts.Lobby.InLobby
 {
+    /// <summary>
+    /// Handles opening the password popup and asking for a password and closing it.
+    /// </summary>
     public class PasswordPopup : MonoBehaviour
     {
         [SerializeField] TMP_InputField _passwordInput;
         [SerializeField] Button _confirmButton;
 
+        /// <summary>
+        /// Open password popup and ask for password. Return the password player typed as a string parameter to the callback.
+        /// </summary>
+        /// <param name="callback">Callback function which takes string as a parameter.</param>
         public IEnumerator AskForPassword(System.Action<string> callback)
         {
             gameObject.SetActive(true);
@@ -24,6 +31,9 @@ namespace MenuUi.Scripts.Lobby.InLobby
             callback(_passwordInput.text);
         }
 
+        /// <summary>
+        /// Closes the password popup.
+        /// </summary>
         public void ClosePopup()
         {
             StopAllCoroutines();
