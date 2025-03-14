0e0c1d2dc9a3f5d7c504c1a6f1c7415a224ef0a5
Sebastian Backman <seba.backman@pp.inet.fi>
2025-03-14 12:13:24 +0200

    Added AvatarPartsReference methods
    - Added GetAvatarPartById and GetAvatarPartsByCategory.

--- Assets/MenuUi/Scripts/AvatarEditor/AvatarPartsReference.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/AvatarPartsReference.cs
@@ -1,106 +1,44 @@
-using System.Collections;
 using System.Collections.Generic;
-using Altzone.Scripts.Model.Poco.Game;
-using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "ALT-Zone/AvatarPartsData")]
 public class AvatarPartsReference : ScriptableObject
 {
     [SerializeField] private Sprite _head;
-    [SerializeField] private List<AvatarBaseCategoriesInfo> _info;
+    [SerializeField] private List<AvatarPartCategoryInfo> _info;
 
-    public List<AvatarBaseCategoriesInfo> Info => _info; // Public accessor for _info
-
-    //public FurnitureInfo GetFurnitureInfo(string name)
-    //{
-    //    (AvatarCategoriesInfo data, AvatarPartsInfo setData) = GetFurnitureDataSet(name);
-    //    if (data == null) return null;
-    //    return new(data, setData);
-    //}
-
-    public AvatarCategoriesInfo GetFurnitureData(string name)
+    public AvatarPartInfo GetAvatarPartById(string Id)
     {
-        (AvatarCategoriesInfo data, AvatarBaseCategoriesInfo setData) = GetFurnitureDataSet(name);
-        return data;
+        string[] parts = Id.Split("-");
+        try
+        {
+            var data = _info.Find(category => category.Id == parts[0]).
+                AvatarCategories.Find(character => character.Id == parts[1]).
+                Parts.Find(part => part.Id == Id);
+
+            return (data);
+        }
+        catch
+        {
+            Debug.LogError($"Could not find avatar part with ID: {Id}");
+            return (null);
+        }
     }
 
-    public (AvatarCategoriesInfo, AvatarBaseCategoriesInfo) GetFurnitureDataSet(string name)
+    public AvatarPartCategoryInfo GetAvatarPartsByCategory(string Id)
     {
-        //Debug.LogWarning($"Full name: {name}");
-        if (string.IsNullOrWhiteSpace(name))
-        {
-            return (null, null);
-        }
+        string[] parts = Id.Split("-");
 
-        string[] parts = name.Split("_");
+        var data = _info.Find(category => category.Id == parts[0]);
 
-        //Debug.LogWarning($"Set name: {parts[1]}");
-        if (parts.Length != 2) { return (null, null); }
+        if (data == null)
+            Debug.LogError($"Could not find avatar parts category with ID: {Id}");
 
-        foreach (AvatarBaseCategoriesInfo info in _info)
-        {
-            if (info.SetName == parts[1])
-            {
-                //foreach (AvatarCategoriesInfo info2 in info.list)
-                //{
-                //    if (info2.Name == parts[0]) return (info2, info);
-                //}
-            }
-        }
-        return (null, null);
-    }
-
-    public List<GameFurniture> GetGameFurniture()
-    {
-        List<GameFurniture> furnitures = new();
-        int i = 1;
-        foreach (AvatarBaseCategoriesInfo info in _info)
-        {
-            //foreach (AvatarCategoriesInfo info2 in info.list)
-            //{
-
-            //}
-        }
-        return furnitures;
-    }
-
-    public bool AddFurniture(AvatarBaseCategoriesInfo setInfo)
-    {
-        if (Application.isPlaying)
-        {
-            Debug.LogError("Don't try to add furniture while the game is running.");
-            return false;
-        }
-
-        AvatarBaseCategoriesInfo localSet = _info.FirstOrDefault(x => x.SetName == setInfo.SetName);
-        if (localSet == null)
-        {
-            _info.Add(setInfo);
-            return true;
-        }
-        else
-        {
-            bool added = false;
-            //foreach (AvatarCategoriesInfo furnitureInfo in setInfo.list)
-            //{
-            //    AvatarCategoriesInfo localFurniture = localSet.list.FirstOrDefault(x => x.Name == furnitureInfo.Name);
-            //    if (localFurniture == null)
-            //    {
-            //        localSet.list.Add(furnitureInfo);
-            //        added = true;
-            //    }
-            //    else
-            //    {
-            //        Debug.LogError("Furniture already exist.");
-            //    }
-            //}
-            return added;
-        }
+        return (data);
     }
 
     [System.Serializable]
-    public class PartInfo
+    public class AvatarPartInfo
     {
         public string Name;
         public string Id;
@@ -110,18 +48,18 @@ public class AvatarPartsReference : ScriptableObject
     }
 
     [System.Serializable]
-    public class AvatarCategoriesInfo
+    public class AvatarClassCategoryInfo
     {
         public string Name;
         public string Id;
-        public List<PartInfo> Parts;
+        public List<AvatarPartInfo> Parts;
     }
 
     [System.Serializable]
-    public class AvatarBaseCategoriesInfo
+    public class AvatarPartCategoryInfo
     {
         public string SetName;
         public string Id;
-        public List<AvatarCategoriesInfo> AvatarCategories;
+        public List<AvatarClassCategoryInfo> AvatarCategories;
     }
 }
