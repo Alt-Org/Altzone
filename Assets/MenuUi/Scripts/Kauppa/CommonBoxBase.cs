e88af6885c414bc8c61e9a072fc387d531eb18c1
vesime <158283226+vesime@users.noreply.github.com>
2025-03-25 14:08:51 +0200

    .

---
+++ Assets/MenuUi/Scripts/Kauppa/CommonBoxBase.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonBoxBase : MonoBehaviour
+{
+    [SerializeField] BoxCollider2D platformCollider;
+    //[SerializeField] BoxLighning boxLighning;
+
+    //public BoxLighning BoxLighning { get => boxLighning; set => boxLighning = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject boxBaze = new GameObject("CommonBoxBaze");
+
+        BoxCollider BoxCollider = boxBaze.AddComponent<BoxCollider>();
+
+        BoxCollider.size = new Vector3(5, 1, 5);
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.SetParent(boxBaze.transform);
+        cube.transform.localScale = new Vector3(5, 1, 5);
+        cube.transform.position = new Vector3(0, 0, 0);
+
+        boxBaze.transform.position = new Vector3(0, 0, 0);
+
+        GameObject lightObject = new GameObject("CommonBoxLight");
+
+        Light lightComponent = lightObject.AddComponent<Light>();
+        lightComponent.type = LightType.Point;
+        lightComponent.range = 10f;
+        lightComponent.intensity = 1f;
+        lightComponent.color = Color.white;
+
+        lightObject.transform.position = new Vector3(0, 5, 0);
+    }
+}
