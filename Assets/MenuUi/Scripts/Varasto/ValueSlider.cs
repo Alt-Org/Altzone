badbe17ddc07414a84669e47b72a0ed69e481706
Mikael <miksunyman1000@gmail.com>
2025-03-25 16:59:41 +0200

    Use C# events and delegates instead of UnityEvents
    - Value sliders now update the inventory with C# events and delegates instead of UnityEvents.

--- Assets/MenuUi/Scripts/Varasto/ValueSlider.cs
+++ Assets/MenuUi/Scripts/Varasto/ValueSlider.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -8,11 +7,12 @@ namespace MenuUi.Scripts.Storage
 {
     public class ValueSliderUpHandler : MonoBehaviour, IPointerUpHandler
     {
-        public UnityEvent upEvent;
+        public delegate void ValueSliderUp();
+        public event ValueSliderUp OnValueSliderUp;
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            upEvent.Invoke();
+            OnValueSliderUp?.Invoke();
         }
     }
 
@@ -20,15 +20,15 @@ namespace MenuUi.Scripts.Storage
     {
         [SerializeField] private TMP_InputField _inputField;
         [SerializeField] private Slider _slider;
-        [SerializeField] private UnityEvent _sliderUpEvent;
         [SerializeField] private TMP_InputField.SubmitEvent _inputFieldSubmitEvent;
+        [SerializeField] private InvFront _invFront;
 
         private void Start()
         {
             _inputField.onEndEdit.AddListener(delegate { UpdateSliderValue(); });
             _inputField.onSubmit = _inputFieldSubmitEvent;
             _slider.onValueChanged.AddListener(delegate { UpdateInputFieldValue(); });
-            _slider.gameObject.AddComponent<ValueSliderUpHandler>().upEvent = _sliderUpEvent;
+            _slider.gameObject.AddComponent<ValueSliderUpHandler>().OnValueSliderUp += _invFront.UpdateInventory;
         }
 
         private void UpdateSliderValue()
