1fd4cf2d352aacad53a7e9888d0bd81977675862
Jani Peltonen <62407359+Reldin@users.noreply.github.com>
2025-04-02 17:49:46 +0300

    Refactored player horizontal flip
    Refactored player horizontal flip check to work with XOR operator.
    
    Co-Authored-By: Niklas <129056239+Niklas603619@users.noreply.github.com>

--- Assets/QuantumUser/View/PlayerViewController.cs
+++ Assets/QuantumUser/View/PlayerViewController.cs
@@ -26,32 +26,33 @@ public unsafe class PlayerViewController : QuantumEntityViewComponent
         BattleTeamNumber battleTeamNumber = playerData->TeamNumber;
 
         UpdateModelPositionAdjustment(&targetPosition);
-        UpdateAnimator(&targetPosition, &battleTeamNumber);
+        UpdateAnimator(&targetPosition, battleTeamNumber);
     }
 
     private void UpdateModelPositionAdjustment(Vector3* targetPosition)
     {
         const float adjustmentDistance = 0.25f;
         Vector3 distanceToTargetPosition = *targetPosition - transform.position;
-        if(distanceToTargetPosition.sqrMagnitude < adjustmentDistance * adjustmentDistance)
+        if (distanceToTargetPosition.sqrMagnitude < adjustmentDistance * adjustmentDistance)
         {
             transform.position = *targetPosition;
         }
     }
 
-    private void UpdateAnimator(Vector3* targetPosition, BattleTeamNumber* battleTeamNumber)
+    private void UpdateAnimator(Vector3* targetPosition, BattleTeamNumber battleTeamNumber)
     {
         int animationState = 0;
-        bool flipX = *battleTeamNumber == BattleTeamNumber.TeamBeta;
+        bool flipX = false;
         
         if (transform.position != *targetPosition)
         {
             Vector3 movement = *targetPosition - transform.position;
-            if(Mathf.Abs(movement.x) >= Mathf.Abs(movement.z))
+            if (Mathf.Abs(movement.x) >= Mathf.Abs(movement.z))
             {
-                flipX = flipX ? (movement.x > 0) : (movement.x < 0);
+                flipX = (battleTeamNumber == BattleTeamNumber.TeamBeta) ^ (movement.x < 0f);
                 animationState = 1;
-            } else
+            }
+            else
             {
                 animationState = 2;
             }
