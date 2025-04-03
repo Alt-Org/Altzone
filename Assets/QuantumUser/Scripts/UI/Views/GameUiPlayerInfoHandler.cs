b7001e637eb80643bea0f589237631f70c86a5fb
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-04-03 17:26:58 +0300

    Added character image serializefields to GameUiPlayerInfoHandler.cs
    - Setting character image and player name in script, I will probably refactor this tomorrow so that the character slot has individual script.
    - Connected references in BattleUi prefab.

--- Assets/QuantumUser/Scripts/UI/Views/GameUiPlayerInfoHandler.cs
+++ Assets/QuantumUser/Scripts/UI/Views/GameUiPlayerInfoHandler.cs
@@ -1,4 +1,5 @@
 using Altzone.Scripts.BattleUi;
+using Altzone.Scripts.ModelV2;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,22 +16,61 @@ namespace QuantumUser.Scripts.UI.Views
 
         [Header("Character slot 1")]
         [SerializeField] private Button _characterSelectButton1;
+        [SerializeField] private Image _characterImage1;
         [SerializeField] private Image _damageFill1;
         [SerializeField] private Image _shieldFill1;
 
         [Header("Character slot 2")]
         [SerializeField] private Button _characterSelectButton2;
+        [SerializeField] private Image _characterImage2;
         [SerializeField] private Image _damageFill2;
         [SerializeField] private Image _shieldFill2;
 
         [Header("Character slot 3")]
         [SerializeField] private Button _characterSelectButton3;
+        [SerializeField] private Image _characterImage3;
         [SerializeField] private Image _damageFill3;
         [SerializeField] private Image _shieldFill3;
 
         [Header("Movable UI")]
         public BattleUiElement MovableUiElement;
 
+        private void OnDisable()
+        {
+            MovableUiElement.gameObject.SetActive(false);
+        }
+
+        private void SetCharacterIcon(int characterId, int slotIdx)
+        {
+            PlayerCharacterPrototype info = PlayerCharacterPrototypes.GetCharacter(characterId.ToString());
+
+            if (info == null) return;
+
+            switch (slotIdx)
+            {
+                case 0:
+                    _characterImage1.sprite = info.GalleryImage;
+                    break;
+                case 1:
+                    _characterImage2.sprite = info.GalleryImage;
+                    break;
+                case 2:
+                    _characterImage3.sprite = info.GalleryImage;
+                    break;
+            }
+        }
+
+        public void SetInfo(string playerName, int[] characterIds)
+        {
+            _playerName.text = playerName;
+
+            for (int i = 0; i < characterIds.Length; i++)
+            {
+                SetCharacterIcon(characterIds[i], i);
+            }
+
+            MovableUiElement.gameObject.SetActive(true);
+        }
     }
 }
 
