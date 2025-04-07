2348bfabe500a3562b4f034dc9b49a557a1d2ea1
LauraMariaK <laurakarjalainen03@gmail.com>
2025-04-07 11:50:56 +0300

    Made new script for opening statpopup
    -still in progress

---
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterGallery/StatPopUpToShowButton.cs
@@ -0,0 +1,41 @@
+using Altzone.Scripts.Model.Poco.Game;
+using MenuUi.Scripts.DefenceScreen.CharacterStatsWindow;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace MenuUi.Scripts.CharacterGallery
+{
+    [RequireComponent(typeof(Button))]
+    public class StatPopUpToShowButton : MonoBehaviour
+    {
+        private CharacterID CharacterStatWindowToShowValue;
+
+        private StatsWindowController _controller;
+        private Button _button;
+
+        private void OnEnable()
+        {
+            if (_controller == null) _controller = FindObjectOfType<StatsWindowController>();
+            if (_button == null)
+            {
+                _button = GetComponent<Button>();
+                _button.onClick.AddListener(OnButtonClick);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveAllListeners();
+        }
+
+        protected void OnButtonClick()
+        {
+            IGalleryCharacterData data = GetComponent<IGalleryCharacterData>();
+
+            CharacterStatWindowToShowValue = data.Id;
+
+            SettingsCarrier.Instance.CharacterGalleryCharacterStatWindowToShow = CharacterStatWindowToShowValue;
+
+        }
+    }
+}
