8ef0fa4e6425869e448b482bfee567853c0163ba
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-04-07 11:33:13 +0300

    Created script for BattleUiEditor

---
+++ Assets/MenuUi/Scripts/Settings/BattleUiEditor/BattleUiEditor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace MenuUi.Scripts.Settings.BattleUiEditor
+{
+    /// <summary>
+    /// Handles the functionality for BattleUiEditor prefab in Settings.
+    /// </summary>
+    public class BattleUiEditor : MonoBehaviour
+    {
+    }
+}
