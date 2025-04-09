0b0a402afde25a1d5b68dd0d9685624757230d00
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-04-09 18:00:01 +0300

    Updated BattleUiEditor script and prefab
    - Added background image and close button to prefab.
    - Added SerializeField variables to reference close button, Ui elements holder gameobject and Ui prefabs which will be instantiated.
    - Added references in BattleUiEditor prefab.
    - Methods to open and close editor and connecting close button listener.

--- Assets/MenuUi/Scripts/Settings/BattleUiEditor/BattleUiEditor.cs
+++ Assets/MenuUi/Scripts/Settings/BattleUiEditor/BattleUiEditor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MenuUi.Scripts.Settings.BattleUiEditor
 {
@@ -7,5 +8,40 @@ namespace MenuUi.Scripts.Settings.BattleUiEditor
     /// </summary>
     public class BattleUiEditor : MonoBehaviour
     {
+        [Header("GameObject references")]
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private GameObject _uiElementsHolder;
+
+        [Header("BattleUi prefabs")]
+        [SerializeField] private GameObject _timer;
+        [SerializeField] private GameObject _playerInfo;
+        [SerializeField] private GameObject _diamonds;
+        [SerializeField] private GameObject _giveUpButton;
+
+        /// <summary>
+        /// Open and initialize BattleUiEditor
+        /// </summary>
+        public void OpenEditor()
+        {
+            gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// Close BattleUiEditor
+        /// </summary>
+        public void CloseEditor()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(CloseEditor);
+        }
+
+        private void OnDestroy()
+        {
+            _closeButton.onClick.RemoveAllListeners();
+        }
     }
 }
