b4c36a3a342dff165c0eef2db2fc6f81457ffcac
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-04-10 14:16:29 +0300

    Instantiating BattleUi prefabs to the editor
    - Removed arena background from the editor prefab, and added new SerializeField references
    - Still in progress, no default position etc.

--- Assets/MenuUi/Scripts/Settings/BattleUiEditor/BattleUiEditor.cs
+++ Assets/MenuUi/Scripts/Settings/BattleUiEditor/BattleUiEditor.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+using Altzone.Scripts.BattleUiShared;
+
 namespace MenuUi.Scripts.Settings.BattleUiEditor
 {
     /// <summary>
@@ -10,9 +12,10 @@ namespace MenuUi.Scripts.Settings.BattleUiEditor
     {
         [Header("GameObject references")]
         [SerializeField] private Button _closeButton;
-        [SerializeField] private GameObject _uiElementsHolder;
+        [SerializeField] private Transform _uiElementsHolder;
 
         [Header("BattleUi prefabs")]
+        [SerializeField] private GameObject _editingComponent;
         [SerializeField] private GameObject _timer;
         [SerializeField] private GameObject _playerInfo;
         [SerializeField] private GameObject _diamonds;
@@ -24,6 +27,12 @@ namespace MenuUi.Scripts.Settings.BattleUiEditor
         public void OpenEditor()
         {
             gameObject.SetActive(true);
+
+            _instantiatedTimer = InstantiateBattleUiElement(_timer);
+            _instantiatedPlayerInfo = InstantiateBattleUiElement(_playerInfo);
+            _instantiatedTeammateInfo = InstantiateBattleUiElement(_playerInfo);
+            _instantiatedDiamonds = InstantiateBattleUiElement(_diamonds);
+            _instantiatedGiveUpButton = InstantiateBattleUiElement(_giveUpButton);
         }
 
         /// <summary>
@@ -34,6 +43,17 @@ namespace MenuUi.Scripts.Settings.BattleUiEditor
             gameObject.SetActive(false);
         }
 
+        private GameObject _instantiatedTimer;
+        private GameObject _instantiatedPlayerInfo;
+        private GameObject _instantiatedTeammateInfo;
+        private GameObject _instantiatedDiamonds;
+        private GameObject _instantiatedGiveUpButton;
+
+        private void OnEnable()
+        {
+            OpenEditor(); // for testing only
+        }
+
         private void Awake()
         {
             _closeButton.onClick.AddListener(CloseEditor);
@@ -42,6 +62,40 @@ namespace MenuUi.Scripts.Settings.BattleUiEditor
         private void OnDestroy()
         {
             _closeButton.onClick.RemoveAllListeners();
+        }
+
+        private GameObject InstantiateBattleUiElement(GameObject uiElementPrefab)
+        {
+            if (uiElementPrefab == null) return null;
+            if (_editingComponent == null) return null;
+
+            // Instantiating gameobjects for ui element and editing component
+            GameObject uiElementGameObject = Instantiate(uiElementPrefab, _uiElementsHolder);
+            GameObject editingComponentGameObject = Instantiate(_editingComponent, uiElementGameObject.transform);
+
+            // Getting editing component script from the editing component game object
+            BattleUiEditingComponent editingComponent = editingComponentGameObject.GetComponent<BattleUiEditingComponent>();
+            if (editingComponent == null) return null;
+
+            // Getting movable element and multi orientation script from the ui element game object
+            BattleUiMovableElement movableElement = uiElementGameObject.GetComponent<BattleUiMovableElement>();
+            BattleUiMultiOrientationElement multiOrientationElement = uiElementGameObject.GetComponent<BattleUiMultiOrientationElement>();
+
+            // Setting info to the editing component
+            if (movableElement != null && multiOrientationElement == null)
+            {
+                editingComponent.SetInfo(movableElement);
+            }
+            else if (multiOrientationElement != null)
+            {
+                editingComponent.SetInfo(multiOrientationElement);
+            }
+            else
+            {
+                return null;
+            }
+
+            return uiElementGameObject;
         }
     }
 }
