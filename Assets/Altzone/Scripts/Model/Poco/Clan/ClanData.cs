b73108a9b3e243b9f7c27d68506326cf09094a92
Niko Mattila <niko.mattila@pp.inet.fi>
2025-04-10 03:55:02 +0300

    The local ClanData is now fetched properly on startup
    The data from the server is then written over it.

--- Assets/Altzone/Scripts/Model/Poco/Clan/ClanData.cs
+++ Assets/Altzone/Scripts/Model/Poco/Clan/ClanData.cs
@@ -88,6 +88,41 @@ namespace Altzone.Scripts.Model.Poco.Clan
             if (clan.polls != null) Polls = clan.polls;
         }
 
+        public void UpdateClanData(ServerClan clan)
+        {
+            Assert.IsTrue(clan._id.IsPrimaryKey());
+            Assert.IsTrue(clan.name.IsMandatory());
+            Assert.IsTrue(clan.tag.IsNullOEmptyOrNonWhiteSpace());
+            Assert.IsTrue(clan.gameCoins >= 0);
+            Id = clan._id;
+            Name = clan.name;
+            Tag = clan.tag ?? string.Empty;
+            Phrase = clan.phrase ?? string.Empty;
+            _gameCoins = clan.gameCoins;
+            Points = clan.points;
+            foreach (string point in clan.labels)
+            {
+                Values.Add((ClanValues)Enum.Parse(typeof(ClanValues), string.Concat(point[0].ToString().ToUpper(), point.AsSpan(1).ToString()).Replace("ä", "a").Replace("ö", "o").Replace("+", "").Replace(" ", "")));
+            }
+            ClanAge = clan.ageRange;
+            Language = clan.language;
+            Goals = clan.goal;
+            ClanHeartPieces = new();
+            int i = 0;
+            if (clan.clanLogo != null)
+                foreach (var piece in clan.clanLogo.pieceColors)
+                {
+                    if (!ColorUtility.TryParseHtmlString("#" + piece, out Color colour)) colour = Color.white;
+                    ClanHeartPieces.Add(new(i, colour));
+
+                    i++;
+                }
+            IsOpen = clan.isOpen;
+            if (clan.polls != null) Polls = clan.polls;
+            else Polls = new();
+            Rooms = new();
+        }
+
         public void CallDataUpdate()
         {
             OnClanInfoUpdated?.Invoke();
@@ -95,10 +130,14 @@ namespace Altzone.Scripts.Model.Poco.Clan
 
         public override string ToString()
         {
-            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Tag)}: {Tag}, {nameof(GameCoins)}: {GameCoins}" +
+            return $"  {nameof(Id)}: {Id}, " +
+                   $"  {nameof(Name)}: {Name}, " +
+                   $"  {nameof(Tag)}: {Tag}, " +
+                   $"  {nameof(GameCoins)}: {GameCoins}" +
                    $", {nameof(Inventory)}: {Inventory}" +
                    $", {nameof(Polls)}: {Polls.Count}" +
-                   $", {nameof(Members)}: {Members.Count}, {nameof(Rooms)}: {Rooms.Count}";
+                   $", {nameof(Members)}: {Members.Count}, " +
+                   $"  {nameof(Rooms)}: {Rooms.Count}";
         }
     }
 }
