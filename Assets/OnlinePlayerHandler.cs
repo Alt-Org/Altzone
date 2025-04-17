9fc1ab6e682532cd417946e812b50d600e7b52c3
Niko Mattila <niko.mattila@pp.inet.fi>
2025-04-17 17:47:59 +0300

    Created the first version of the way of show online players.
    Players not in the same call as the user don't have their names showing yet, but instead it shows their UID.
    This is because the current server call endpoint only return the UIDs.

---
+++ Assets/OnlinePlayerHandler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Altzone.Scripts.Model.Poco.Clan;
+using UnityEngine;
+
+public class OnlinePlayerHandler : AltMonoBehaviour
+{
+    [SerializeField]
+    private GameObject _textPrefab;
+    [SerializeField]
+    private Transform _content;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServerManager.OnOnlinePlayersChanged += BuildOnlinePlayerList;
+    }
+
+    private void OnEnable()
+    {
+        BuildOnlinePlayerList(ServerManager.Instance.OnlinePlayers);
+    }
+
+    private void OnDestroy()
+    {
+        ServerManager.OnOnlinePlayersChanged -= BuildOnlinePlayerList;
+    }
+
+    private void BuildOnlinePlayerList(List<string> list)
+    {
+        foreach(Transform t in _content)
+        {
+            Destroy(t);
+        }
+        StartCoroutine(GetClanData(data =>
+        {
+            foreach (string name in list)
+            {
+                string newName = null;
+                if (data != null) newName = data.Members.FirstOrDefault(x => x.Id == name)?.Name;
+                if(newName == null) newName = name;
+                GameObject textObject = Instantiate(_textPrefab, _content);
+                textObject.GetComponent<OnlinePlayerTextHandler>().SetInfo(newName);
+            }
+        }));
+    }
+}
