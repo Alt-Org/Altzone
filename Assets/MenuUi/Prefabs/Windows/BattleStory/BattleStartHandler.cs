402e0ea318bf33b3567cf8edf2d2fd00d790ecec
Niko Mattila <niko.mattila@pp.inet.fi>
2025-04-30 14:19:32 +0300

    BattleStart now uses battle table sprite halfway through the countdown

--- Assets/MenuUi/Prefabs/Windows/BattleStory/BattleStartHandler.cs
+++ Assets/MenuUi/Prefabs/Windows/BattleStory/BattleStartHandler.cs
@@ -5,11 +5,18 @@ using Altzone.Scripts.Lobby;
 using MenuUi.Scripts.Audio;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleStartHandler : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI _timerText;
+    [SerializeField]
+    private Image _loadImage;
+    [SerializeField]
+    private Sprite _sittingDownSprite;
+    [SerializeField]
+    private Sprite _tableAboveSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -36,11 +43,26 @@ public class BattleStartHandler : MonoBehaviour
 
     private IEnumerator TimerStart(long startTime)
     {
+        _loadImage.sprite = _sittingDownSprite;
+        _loadImage.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0f);
+        _loadImage.transform.localScale = new Vector2(1f, 1f);
         float timeleft = startTime/1000f;
         do
         {
-            _timerText.text = Mathf.CeilToInt(timeleft).ToString();
             timeleft -= Time.deltaTime;
+            _timerText.text = Mathf.CeilToInt(timeleft).ToString();
+
+            if(timeleft <= 3)
+            {
+                if (_loadImage.sprite.Equals(_sittingDownSprite))
+                {
+                    _loadImage.sprite = _tableAboveSprite;
+                    _loadImage.GetComponent<RectTransform>().pivot = new Vector2(0.5f, 0.5f);
+                }
+                float scale = 1f + 0.4f * (1 - (Mathf.Clamp(timeleft,0,3f) / 3f));
+                _loadImage.transform.localScale = new Vector2(scale, scale);
+            }
+
             yield return null;
         } while (timeleft > 0f);
     }
