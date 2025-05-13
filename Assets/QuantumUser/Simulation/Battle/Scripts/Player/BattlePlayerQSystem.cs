0509b3fc4d98c288a341c034ba157f7ddeb83faa
Niklas <niklas.tontti603619@gmail.com>
2025-05-13 15:59:54 +0300

    Added BattlePlayerQSystem
    BattlePlayerQSystem for handling players
    (currently does nothing)

---
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Player/BattlePlayerQSystem.cs
@@ -0,0 +1,31 @@
+using UnityEngine.Scripting;
+
+using Quantum;
+using Photon.Deterministic;
+
+namespace Battle.QSimulation.Player
+{
+    [Preserve]
+    public unsafe class BattlePlayerQSystem : SystemMainThread
+    {
+        public override void Update(Frame f)
+        {
+            Input* input;
+
+            EntityRef                   playerEntity;
+            BattlePlayerDataQComponent* playerData;
+            Transform2D*                playerTransform;
+
+            foreach (BattlePlayerManager.PlayerHandle playerHandle in BattlePlayerManager.PlayerHandle.GetPlayerHandleArray(f))
+            {
+                if (playerHandle.PlayState != BattlePlayerPlayState.InPlay) continue;
+
+                input = f.GetPlayerInput(playerHandle.PlayerRef);
+
+                playerEntity    = playerHandle.SelectedCharacter;
+                playerData      = f.Unsafe.GetPointer<BattlePlayerDataQComponent>(playerEntity);
+                playerTransform = f.Unsafe.GetPointer<Transform2D>(playerEntity);
+            }
+        }
+    }
+}
