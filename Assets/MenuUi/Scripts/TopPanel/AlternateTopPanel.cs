e712595615da82bfb8b7b8d2a951f9ba95d34c46
Niko Mattila <niko.mattila@pp.inet.fi>
2025-05-14 16:19:36 +0300

    Updated the alternating top bar to remember its status
    Meaning if the clan is visible on the topbar it will also stay visible when changing windows.
    Also the top bar also remembers the timer of when it is supposed to change the values.

--- Assets/MenuUi/Scripts/TopPanel/AlternateTopPanel.cs
+++ Assets/MenuUi/Scripts/TopPanel/AlternateTopPanel.cs
@@ -11,6 +11,8 @@ public class AlternateTopPanel : AltMonoBehaviour
     [Header("Clan/Player info")]
     [SerializeField] private GameObject _playerInfo;
     [SerializeField] private GameObject _clanInfo;
+    [SerializeField] private Button _playerButton;
+    [SerializeField] private Button _clanButton;
 
     [Header("LeaderBoard")]
     [SerializeField] private Image _leaderboardButton;
@@ -21,6 +23,8 @@ public class AlternateTopPanel : AltMonoBehaviour
 
     private float _timerLeaderboard = 5;
     private float _timerInfo = 10;
+    private static float _currenttimerLeaderboard = 0;
+    private static float _currenttimerInfo = 0;
     private string _ownPlayerID;
     private string _ownClanID;
     private int _playerActivityRanking;
@@ -33,12 +37,32 @@ public class AlternateTopPanel : AltMonoBehaviour
         Player,
         Clan
     }
+    private enum TopLeaderboardInfo
+    {
+        Wins,
+        Activity
+    }
 
-    private TopPanelInfo _currentTopPanelInfo = TopPanelInfo.Player;
+    private static TopPanelInfo _currentTopPanelInfo = TopPanelInfo.Player;
+    private static TopLeaderboardInfo _currentTopLeaderboardInfo = TopLeaderboardInfo.Wins;
+    private static Coroutine _timerCoroutine;
+    private static Coroutine _leaderBoardCoroutine;
+
+    private delegate void TopPanelChanged();
+    private static event TopPanelChanged OnTopPanelChanged;
+
+    private delegate void LeaderBoardChange();
+    private static event LeaderBoardChange OnLeaderBoardChange;
+
 
     private void OnEnable()
     {
-        if(ServerManager.Instance.Player != null)
+        OnTopPanelChanged += ChangeInfoData;
+        OnLeaderBoardChange += ChangeLeaderboardType;
+        ChangeInfoData();
+        ChangeLeaderboardType();
+
+        if (ServerManager.Instance.Player != null)
         {
             _ownPlayerID = ServerManager.Instance.Player._id;
             _ownClanID = ServerManager.Instance.Player.clan_id;
@@ -47,15 +71,23 @@ public class AlternateTopPanel : AltMonoBehaviour
             StartCoroutine(ChangeInfoType());
         }
         else
-        StartCoroutine(ServerManager.Instance.GetOwnPlayerFromServer((player) =>
         {
-            if (player == null) { Debug.LogError("Cannot get PlayerData."); return; }
-            _ownPlayerID = player._id;
-            _ownClanID = player.clan_id;
+            StartCoroutine(ServerManager.Instance.GetOwnPlayerFromServer((player) =>
+            {
+                if (player == null) { Debug.LogError("Cannot get PlayerData."); return; }
+                _ownPlayerID = player._id;
+                _ownClanID = player.clan_id;
 
-            FetchRankings();
-            StartCoroutine(ChangeInfoType());
-        }));
+                FetchRankings();
+                StartCoroutine(ChangeInfoType());
+            }));
+        }
+    }
+    private void OnDisable()
+    {
+        OnTopPanelChanged -= ChangeInfoData;
+        OnLeaderBoardChange -= ChangeLeaderboardType;
+        StopCoroutine(ChangeLeaderboardCoroutine());
     }
 
     /// <summary>
@@ -63,40 +95,83 @@ public class AlternateTopPanel : AltMonoBehaviour
     /// </summary>
     private IEnumerator ChangeInfoType()
     {
-        YieldInstruction wait = new WaitForSeconds(_timerInfo);
+        if (_currenttimerInfo <= 0) _currenttimerInfo += _timerInfo;
+        //if (_currenttimerLeaderboard < 0) _currenttimerLeaderboard = 0;
+        if (_timerCoroutine != null)
+        StopCoroutine(_timerCoroutine);
+        _timerCoroutine = StartCoroutine(StartTimer());
+        //YieldInstruction wait = new WaitUntil(() => _currenttimerInfo <= 0);
 
         while (enabled)
         {
-            _playerInfo.SetActive(_currentTopPanelInfo == TopPanelInfo.Player);
-            _clanInfo.SetActive(_currentTopPanelInfo == TopPanelInfo.Clan);
-            StartCoroutine(ChangeLeaderboardType());
+            StartCoroutine(ChangeLeaderboardCoroutine());
 
-            yield return wait;
-
-            switch (_currentTopPanelInfo)
+            yield return new WaitUntil(() => _currenttimerInfo <= 0);
+            switch (AlternateTopPanel._currentTopPanelInfo)
             {
                 case TopPanelInfo.Player:
-                    _currentTopPanelInfo = TopPanelInfo.Clan;
+                    AlternateTopPanel._currentTopPanelInfo = TopPanelInfo.Clan;
                     break;
                 case TopPanelInfo.Clan:
-                    _currentTopPanelInfo = TopPanelInfo.Player;
+                    AlternateTopPanel._currentTopPanelInfo = TopPanelInfo.Player;
                     break;
             }
+            OnTopPanelChanged.Invoke();
+            _currenttimerInfo += _timerInfo;
+        }
+    }
+
+    private IEnumerator StartTimer()
+    {
+        while (true)
+        {
+            yield return null;
+            _currenttimerInfo -= Time.deltaTime;
+            _currenttimerLeaderboard -= Time.deltaTime;
         }
     }
 
     /// <summary>
     /// Alternates between the activity and wins leaderboards.
     /// </summary>
-    private IEnumerator ChangeLeaderboardType()
+    private IEnumerator ChangeLeaderboardCoroutine()
     {
         YieldInstruction wait = new WaitForSeconds(_timerLeaderboard);
 
-        LoadWins();
+        if (AlternateTopPanel._currentTopLeaderboardInfo != TopLeaderboardInfo.Wins)
+        {
+            if (_currenttimerLeaderboard <= 0) _currenttimerLeaderboard += _timerLeaderboard;
+            yield return new WaitUntil(() => _currenttimerLeaderboard <= 0);
+            AlternateTopPanel._currentTopLeaderboardInfo = TopLeaderboardInfo.Wins;
+            OnLeaderBoardChange.Invoke();
+        }
+        if (_currenttimerLeaderboard <= 0) _currenttimerLeaderboard += _timerLeaderboard;
+        yield return new WaitUntil(() => _currenttimerLeaderboard <= 0);
+        AlternateTopPanel._currentTopLeaderboardInfo = TopLeaderboardInfo.Activity;
+        OnLeaderBoardChange.Invoke();
 
-        yield return wait;
+    }
 
-        LoadActivity();
+    private void ChangeInfoData()
+    {
+        _playerInfo.SetActive(_currentTopPanelInfo == TopPanelInfo.Player);
+        _playerButton.enabled = _currentTopPanelInfo == TopPanelInfo.Player;
+        _clanInfo.SetActive(_currentTopPanelInfo == TopPanelInfo.Clan);
+        _clanButton.enabled = _currentTopPanelInfo == TopPanelInfo.Clan;
+        _clanButton.GetComponent<Image>().raycastTarget = _currentTopPanelInfo == TopPanelInfo.Clan;
+    }
+
+    private void ChangeLeaderboardType()
+    {
+        switch (AlternateTopPanel._currentTopLeaderboardInfo)
+        {
+            case TopLeaderboardInfo.Wins:
+                LoadWins();
+                break;
+            case TopLeaderboardInfo.Activity:
+                LoadActivity();
+                break;
+        }
     }
 
     private void LoadWins()
