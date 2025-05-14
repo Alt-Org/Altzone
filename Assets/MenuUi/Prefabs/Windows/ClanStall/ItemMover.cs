0967c312cb51784c28407b55987c04910179d780
Jere Kari <jepu116@gmail.com>
2025-05-14 12:56:24 +0300

    Koju Update
    -Created KojuItemSlot script for the purpose of assigning empty card slots
    -Updated ItemMover to work with the KojuItemSlot script
    -Replaced the cards in the Koju panel with the KojuEmptyObject prefabs

--- Assets/MenuUi/Prefabs/Windows/ClanStall/ItemMover.cs
+++ Assets/MenuUi/Prefabs/Windows/ClanStall/ItemMover.cs
@@ -1,43 +1,48 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemMover : MonoBehaviour
 {
-    private Transform gridParent;
     private Transform trayParent;
+    private Transform gridParent;
+    private KojuItemSlot assignedSlot;
 
     void Start()
     {
+        //Find containers in the start and add listeners to the buttons in the cards
 
-        //Searching Grid and Bar references by name in the koju
-        GameObject gridItems = GameObject.Find("PanelContent");
-        GameObject trayItems = GameObject.Find("TrayContent");
+        trayParent = GameObject.Find("TrayContent").transform;
+        gridParent = GameObject.Find("PanelContent").transform;
 
-        if (gridItems != null && trayItems != null)
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        //When clicking the card, move it into an available slot in the Koju panel. If the object is already in the Koju panel, move it back to the tray.
+
+        if (assignedSlot == null)
         {
-            gridParent = gridItems.transform;
-            trayParent = trayItems.transform;
+           
+            KojuItemSlot[] slots = GameObject.FindObjectsOfType<KojuItemSlot>();
+            foreach (var slot in slots)
+            {
+                if (!slot.IsOccupied)
+                {
+                    slot.AssignCard(gameObject, gridParent);
+                    assignedSlot = slot;
+                    return;
+                }
+            }
+
+            Debug.Log("No more space available for furniture");
         }
         else
         {
-            Debug.LogError("Error finding Koju grid or tray");
-            return;
-        }
-
-       GetComponent<Button>().onClick.AddListener(MoveItem);
-    }
-
-    void MoveItem()
-    {
-
-        //If an item is in the grid, move it to bar. If an item is in the bar, move it to the grid.
-        if (transform.parent == gridParent)
-        {
+            
             transform.SetParent(trayParent, false);
-        }
-        else if (transform.parent == trayParent)
-        {
-            transform.SetParent(gridParent, false);
+            assignedSlot.ClearSlot();
+            assignedSlot = null;
         }
     }
 }
---
+++ Assets/MenuUi/Prefabs/Windows/ClanStall/KojuItemSlot.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class KojuItemSlot : MonoBehaviour
+{
+    public bool IsOccupied => currentCard != null;
+    private GameObject currentCard;
+
+    public void AssignCard(GameObject card, Transform PanelContent)
+    {
+        //Moves the card to the Koju Panel, disables the available card slot element and replaces that with the assigned card by utilizing the same index.
+
+        currentCard = card;
+
+        
+        card.transform.SetParent(PanelContent, false);
+        card.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        gameObject.SetActive(false); 
+    }
+
+    public void ClearSlot()
+    {
+        currentCard = null;
+        gameObject.SetActive(true); 
+    }
+}
