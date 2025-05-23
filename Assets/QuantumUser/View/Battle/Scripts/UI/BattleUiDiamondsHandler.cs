e547f1e657e3a0fe01b3ad2e02cbc195f9f07e7a
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-05-23 15:15:20 +0300

    BattleDoc: Added documentation comments to BattleUiDiamondsHandler.cs

--- Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiDiamondsHandler.cs
+++ Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiDiamondsHandler.cs
@@ -1,3 +1,11 @@
+/// @file BattleUiGameOverHandler.cs
+/// <summary>
+/// Has a class BattleUiDiamondsHandler which handles setting collected diamond amount text.
+/// </summary>
+///
+/// This script:<br/>
+/// Handles setting collected diamond amount text. Part of @ref UIHandlers.
+
 using UnityEngine;
 using TMPro;
 
@@ -6,21 +14,35 @@ using Altzone.Scripts.BattleUiShared;
 namespace Battle.View.UI
 {
     /// <summary>
-    /// Handles setting collected diamond amount text.
+    /// Handles setting collected diamond amount text. Part of @ref UIHandlers.
     /// </summary>
     public class BattleUiDiamondsHandler : MonoBehaviour
     {
+        /// <value>[SerializeField] Reference to the BattleUiMovableElement script which is attached to a BattleUiDiamonds prefab.</value>
         [SerializeField] private BattleUiMovableElement _movableUiElement;
+
+        /// <value>[SerializeField] Reference to the TMP_Text component which the diamond amount text is set to.</value>
         [SerializeField] private TMP_Text _diamondText;
 
+        /// <value>Is the %UI element visible or not.</value>
         public bool IsVisible => MovableUiElement.gameObject.activeSelf;
+
+        /// <value>Public getter for #_movableUiElement.</value>
         public BattleUiMovableElement MovableUiElement => _movableUiElement;
 
+        /// <summary>
+        /// Sets the %UI element visibility.
+        /// </summary>
+        /// <param name="show">If the %UI element should be visible or not.</param>
         public void SetShow(bool show)
         {
             MovableUiElement.gameObject.SetActive(show);
         }
 
+        /// <summary>
+        /// Sets the diamonds amount to the #_diamondText.
+        /// </summary>
+        /// <param name="diamondAmount">The current diamond amount as integer.</param>
         public void SetDiamondsText(int diamondAmount)
         {
             _diamondText.text = diamondAmount.ToString();
