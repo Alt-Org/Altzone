1dd172e9e48a9e759c3f665411bd77f39cf66a83
Niklas <niklas.tontti603619@gmail.com>
2025-05-26 18:05:23 +0300

    BattlePlayerManager: Refactored InitPlayer
    BattlePlayerManager:
    
    Refactored InitPlayer by splitting it to two methods RegisterPlayer and CreatePlayers
    RegisterPlayer uses BattleParameters to check that the player has joined correctly and saves/updates relevant player data
    CreatePlayers has most of the code from InitPlayer but it creates all players at once using the data saved by RegisterPlayer
    
    Added IsAllPlayersRegistered for checking if all players has been joined and registered (compares how many players there are to how many there should be)
    
    BattlePlayerJoinQSystem:
    
    Disabled OnPlayerAdded code because it uses the old BattlePlayerManager.InitPlayer

--- Assets/QuantumUser/Simulation/Battle/Scripts/Player/BattlePlayerJoinQSystem.cs
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Player/BattlePlayerJoinQSystem.cs
@@ -8,12 +8,14 @@ namespace Battle.QSimulation.Player
     {
         public void OnPlayerAdded(Frame f, PlayerRef player, bool firstTime)
         {
+            /*
             RuntimePlayer data = f.GetPlayerData(player);
 
             BattlePlayerSlot playerSlot = BattlePlayerManager.InitPlayer(f, player);
             BattlePlayerManager.SpawnPlayer(f, playerSlot, 0);
 
             f.Events.BattleDebugUpdateStatsOverlay(data.Characters[0]);
+            */
         }
     }
 }
