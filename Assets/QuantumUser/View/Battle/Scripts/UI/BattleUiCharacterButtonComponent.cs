adcc5d9ad2a9fcb19d19690561d1d806efe676dc
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-06-03 13:17:34 +0300

    BattleDoc: Added documentation comments to UI Components
    - Added "UI Component" link to Doxyfile

--- Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiCharacterButtonComponent.cs
+++ Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiCharacterButtonComponent.cs
@@ -1,3 +1,11 @@
+/// @file BattleUiCharacterButtonComponent.cs
+/// <summary>
+/// Has a class BattleUiCharacterButtonComponent which handles character button visuals and component references.
+/// </summary>
+///
+/// This script:<br/>
+/// Handles a BattleUiPlayerInfo prefab's character button visuals and component references.
+
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,18 +14,37 @@ using Altzone.Scripts.ModelV2;
 namespace Battle.View.UI
 {
     /// <summary>
-    /// Handles Battle Ui character button visuals and component references.
+    /// <span class="brief-h">CharacterButton @uicomponentlink (<a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.html">Unity MonoBehaviour script@u-exlink</a>).</span><br/>
+    /// Handles a BattleUiPlayerInfo prefab's character button visuals and component references. Attached to each of BattleUiPlayerInfo prefab's character button <a href="https://docs.unity3d.com/ScriptReference/GameObject.html">GameObjects@u-exlink</a>.
     /// </summary>
     [RequireComponent(typeof(Button))]
     public class BattleUiCharacterButtonComponent : MonoBehaviour
     {
+        /// @name SerializeField variables
+        /// <a href="https://docs.unity3d.com/6000.1/Documentation/ScriptReference/SerializeField.html">SerializeFields@u-exlink</a> are serialized variables exposed to the Unity editor.
+        /// @{
+
+        /// <value>[SerializeField] Reference to the button component of the character button.</value>
         [SerializeField] private Button _button;
+
+        /// <value>[SerializeField] Reference to the character image of the character button.</value>
         [SerializeField] private Image _characterImage;
+
+        /// <value>[SerializeField] Reference to the damage fill image of the character button. It is used to display the character's current Hp.</value>
         [SerializeField] private Image _damageFill;
+
+        /// <value>[SerializeField] Reference to the shield fill image of the character button. It is used to display the character's current shield health.</value>
         [SerializeField] private Image _shieldFill;
 
+        /// @}
+
+        /// <value>Public getter for #_button.</value>
         public Button ButtonComponent => _button;
 
+        /// <summary>
+        /// Sets the character image from PlayerCharacterPrototype to #_characterImage.
+        /// </summary>
+        /// <param name="characterId">The CharacterId of the character as a int.</param>
         public void SetCharacterIcon(int characterId)
         {
             PlayerCharacterPrototype info = PlayerCharacterPrototypes.GetCharacter(characterId.ToString());
@@ -27,6 +54,9 @@ namespace Battle.View.UI
             _characterImage.sprite = info.GalleryImage;
         }
 
+        /// <summary>
+        /// Private <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/MonoBehaviour.OnDisable.html">OnDisable@u-exlink</a> function. Removed all listeners from #_button's <a href="https://docs.unity3d.com/530/Documentation/ScriptReference/UI.Button-onClick.html">onClick@u-exlink</a> event.
+        /// </summary>
         private void OnDisable()
         {
             ButtonComponent.onClick.RemoveAllListeners();
