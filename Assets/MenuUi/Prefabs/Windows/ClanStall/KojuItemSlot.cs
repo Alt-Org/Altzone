1a07a01ab8762a897ef3d3617fbda758246d4e90
Jere Kari <jepu116@gmail.com>
2025-06-03 12:52:58 +0300

    Koju Update
    -fixed the issue with the card position being wrong when moving it to the panel
    -Fixed an issue with the fonts being too big on cards

--- Assets/MenuUi/Prefabs/Windows/ClanStall/KojuItemSlot.cs
+++ Assets/MenuUi/Prefabs/Windows/ClanStall/KojuItemSlot.cs
@@ -24,6 +24,22 @@ public class KojuItemSlot : MonoBehaviour
     public void AssignCard(GameObject card)
     {
         card.transform.SetParent(transform, false);
+
+        var rectTransform = card.GetComponent<RectTransform>();
+        if (rectTransform != null)
+        {
+            // Set anchors to stretch
+            rectTransform.anchorMin = new Vector2(0f, 0f);
+            rectTransform.anchorMax = new Vector2(1f, 1f);
+
+            // Reset position and size
+            rectTransform.anchoredPosition = Vector2.zero;
+            rectTransform.sizeDelta = Vector2.zero;
+
+            // Set the pivot just in case
+            rectTransform.pivot = new Vector2(0.5f, 0.5f);
+        }
+
         if (kojuEmptyVisual != null)
         {
             kojuEmptyVisual.SetActive(false);
