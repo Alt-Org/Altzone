829943fe29b6bb47d71d3a733701c718fd997b3d
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-06-06 16:43:56 +0300

    Implemented the pencil button to open EditingPanel

--- Assets/MenuUi/Scripts/DefenceScreen/CharacterGallery/SelectedCharSlot.cs
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterGallery/SelectedCharSlot.cs
@@ -7,7 +7,7 @@ using MenuUi.Scripts.DefenceScreen.CharacterGallery;
 
 namespace MenuUi.Scripts.CharacterGallery
 {
-    public class SelectedCharSlot : SlotBase
+    public class SelectedCharSlot : SlotBase, IGalleryCharacterData
     {
         [SerializeField] private Image _characterImage;
         [SerializeField] private TMP_Text _className;
@@ -18,10 +18,21 @@ namespace MenuUi.Scripts.CharacterGallery
 
         [SerializeField] private PieChartPreview _pieChartPreview;
 
+        [SerializeField] private Button _editingPopupButton;
+
         [SerializeField] private GameObject _characterCard;
+
+        private CharacterID _id;
+        public CharacterID Id => _id;
+
         private void Awake()
         {
-            if (_slotButton != null) _slotButton.onClick.AddListener(SignalBus.OnDefenceGalleryEditPanelRequestedSignal);
+            if (_editingPopupButton != null) _editingPopupButton.onClick.AddListener(SignalBus.OnDefenceGalleryEditPanelRequestedSignal);
+        }
+
+        private void OnDestroy()
+        {
+            _editingPopupButton.onClick.RemoveAllListeners();
         }
 
         public void SetInfo(Sprite sprite, Color bgColor, Color bgAltColor, string name, string className, CharacterID id)
@@ -32,11 +43,13 @@ namespace MenuUi.Scripts.CharacterGallery
             _backgroundLowerImage.color = bgColor;
             _backgroundUpperImage.color = bgAltColor;
             _pieChartPreview.UpdateChart(id);
+            _id = id;
         }
 
         public void SetCharacterVisibility(bool visible)
         {
            _characterCard.SetActive(visible);
+           _slotButton.enabled = visible;
         }
     }
 }
