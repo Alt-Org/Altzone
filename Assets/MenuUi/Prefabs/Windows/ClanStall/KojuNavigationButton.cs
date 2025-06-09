e3f408957061f549504c42f9971b6eb331f0b256
Jere Kari <jepu116@gmail.com>
2025-06-09 14:29:54 +0300

    Added navigation to koju from store
    -Adcard now properly changes the window to koju view instead of varasto view

---
+++ Assets/MenuUi/Prefabs/Windows/ClanStall/KojuNavigationButton.cs
@@ -0,0 +1,12 @@
+using MenuUi.Scripts.Window;
+using UnityEngine;
+
+public class KojuNavigationButton : MonoBehaviour
+{
+    private int windowIdToOpen = 1; // This represents the ID for Koju, change this if the ID for koju is changed
+
+    public void OnClickNavigateToWindow()
+    {
+        DataCarrier.AddData(DataCarrier.RequestedWindow, windowIdToOpen);
+    }
+}
