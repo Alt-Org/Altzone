22ead26af33bc192f2de2aa98051a5ae5abd9e6b
ENABLEDsam <114464225+ENABLEDsam@users.noreply.github.com>
2025-07-04 14:20:48 +0300

    Update CharacterPopupHandler.cs

--- Assets/MenuUi/Scripts/Loader/CharacterPopupHandler.cs
+++ Assets/MenuUi/Scripts/Loader/CharacterPopupHandler.cs
@@ -17,7 +17,7 @@ public class CharacterPopupHandler : MonoBehaviour
     TextMeshProUGUI classChoiseText;
     [SerializeField]
     Image charaterImage;
-
+    
     // Start is called before the first frame update
     //void Start()
     //{
