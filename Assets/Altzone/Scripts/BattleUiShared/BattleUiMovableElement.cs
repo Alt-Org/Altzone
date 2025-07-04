15de52ab2100344fc9064679908c978792ffb30f
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-07-04 12:27:55 +0300

    Fixed bug that the editing component buttons would also become transparent
    - Getting the components for setting transparency in awake

--- Assets/Altzone/Scripts/BattleUiShared/BattleUiMovableElement.cs
+++ Assets/Altzone/Scripts/BattleUiShared/BattleUiMovableElement.cs
@@ -68,14 +68,12 @@ namespace Altzone.Scripts.BattleUiShared
         protected void Awake()
         {
             if (_rectTransform == null) _rectTransform = GetComponent<RectTransform>();
+            if (_images == null) _images = GetComponentsInChildren<Image>().ToList();
+            if (_texts == null) _texts = GetComponentsInChildren<TMP_Text>().ToList();
         }
 
         protected void SetTransparency()
         {
-            // If lists are null getting the components
-            if (_images == null) _images = GetComponentsInChildren<Image>().ToList();
-            if (_texts == null) _texts = GetComponentsInChildren<TMP_Text>().ToList();
-
             // Converting transparency to opacity
             float newOpacity = 1f - _currentTransparency / 100f;
 
