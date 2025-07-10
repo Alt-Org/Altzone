3db3d3ab4cec5161d656fde79c47e5a99b91d45f
Niko Mattila <niko.mattila@pp.inet.fi>
2025-07-10 19:23:47 +0300

    Changed VarastoTabChangeHandler to inherit TabChangeHandler

--- Assets/MenuUi/Scripts/PlayerProfile/TabChangeHandler.cs
+++ Assets/MenuUi/Scripts/PlayerProfile/TabChangeHandler.cs
@@ -17,12 +17,11 @@ public class TabChangeHandler : MonoBehaviour
     [Header("TablineScript reference")]
     public TabLine _tablineScript;
 
-    [SerializeField] List<ButtonWindowBind> _buttons = new List<ButtonWindowBind>();
-
-    [SerializeField] private int _defaultTab = 1;
+    [SerializeField] protected List<ButtonWindowBind> _buttons = new List<ButtonWindowBind>();
+    [SerializeField] protected int _defaultTab = 1;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         for (int i = 0; i < _buttons.Count; i++)
         {
@@ -31,7 +30,7 @@ public class TabChangeHandler : MonoBehaviour
         }
     }
 
-    private void OnEnable()
+    protected virtual void OnEnable()
     {
         int? value;
         if (_tablineScript.Swipe == null)
@@ -42,7 +41,7 @@ public class TabChangeHandler : MonoBehaviour
         else SetVisible(_defaultTab);
     }
 
-    void SetVisible(int activeIndex)
+    protected virtual void SetVisible(int activeIndex)
     {
         // If the window uses a swipe scroll then send the message to it to change the tab, otherwise switch panels the old way.
         if (_tablineScript.Swipe == null)
--- Assets/MenuUi/Scripts/Varasto/VarastoTabChangeHandler.cs
+++ Assets/MenuUi/Scripts/Varasto/VarastoTabChangeHandler.cs
@@ -5,46 +5,19 @@ using Altzone.Scripts.Window;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VarastoTabChangeHandler : MonoBehaviour
+public class VarastoTabChangeHandler : TabChangeHandler
 {
-    [System.Serializable]
-    public class ButtonWindowBind
-    {
-        public Button Button;
-        public GameObject Window;
-        public Image Image;
-    }
-
-    [Header("TablineScript reference")]
-    public TabLine _tablineScript;
-
-    [SerializeField] List<ButtonWindowBind> _buttons = new List<ButtonWindowBind>();
-
-    [SerializeField] private int _defaultTab = 1;
 
     private bool _canClick = true;
-
     private int _currentTabIndex = -1; // Integer to track tabs
 
-    void Start()
+    protected override void Start()
     {
         for (int i = 0; i < _buttons.Count; i++)
         {
             int j = i;
             _buttons[i].Button.onClick.AddListener(() => OnTabButtonClicked(j));
         }
-    }
-
-    private void OnEnable()
-    {
-        int? value;
-        if (_tablineScript.Swipe == null)
-            value = DataCarrier.GetData<int?>(DataCarrier.RequestedWindow, true, suppressWarning: true);
-        else
-            value = DataCarrier.GetData<int?>(DataCarrier.RequestedWindow, false, suppressWarning: true);
-
-        if (value != null) SetVisible((int)value);
-        else SetVisible(_defaultTab);
     }
 
     private void OnTabButtonClicked(int index)
@@ -69,14 +42,9 @@ public class VarastoTabChangeHandler : MonoBehaviour
         _canClick = true;
     }
 
-    void SetVisible(int activeIndex)
+    protected override void SetVisible(int activeIndex)
     {
-        // If the window uses a swipe scroll then send the message to it to change the tab, otherwise switch panels the old way.
-        if (_tablineScript.Swipe == null)
-            for (int i = 0; i < _buttons.Count; i++)
-                _buttons[i].Window.SetActive(i == activeIndex);
-        else _tablineScript.Swipe.CurrentPage = activeIndex;
-        _tablineScript.ActivateTabButton(activeIndex);
+        base.SetVisible(activeIndex);
 
         _currentTabIndex = activeIndex; // Update the currently active tab
     }
