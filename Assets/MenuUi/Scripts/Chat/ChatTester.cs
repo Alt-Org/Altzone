053069832de0831760ca966e649beaae8baac932
ENABLEDsam <114464225+ENABLEDsam@users.noreply.github.com>
2025-07-18 11:13:58 +0300

    chatti test script and buttons
    i got the chat test to function, finally, big messages borks the message box

--- Assets/MenuUi/Scripts/Chat/ChatTester.cs
+++ Assets/MenuUi/Scripts/Chat/ChatTester.cs
@@ -11,15 +11,17 @@ public class ChatTester : MonoBehaviour
 
     [SerializeField]
     Chat _chat;
+    [SerializeField]
+    GameObject _messagePrefab;
 
 
     public void DoShortMessage()
     {
-        _chat.SendChatMessage(_chat.giveJoyPref());
+        _chat.DisplayMessage("test", _messagePrefab);
     }
 
     public void DoLongMessage()
     {
-
+        _chat.DisplayMessage("\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consequat velit non purus auctor pharetra. Maecenas sit amet porttitor lectus, et pellentesque neque. Aenean quis lectus accumsan, imperdiet sem in, lobortis massa. Ut eu efficitur lectus. In hac habitasse platea dictumst. Morbi sit amet vehicula augue.", _messagePrefab);
     }
 }
