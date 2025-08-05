78fdac39e0ac8b65468042fb001eb84c921ad6dc
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-08-05 10:11:25 +0300

    BattleDoc: Created OnPointerDownButton.cs documentation

--- Assets/Altzone/Scripts/BattleUiShared/OnPointerDownButton.cs
+++ Assets/Altzone/Scripts/BattleUiShared/OnPointerDownButton.cs
@@ -1,14 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+/// @file OnPointerDownButton.cs
+/// <summary>
+/// Has a helper class OnPointerDownButton for invoking a UnityEvent on pointer down.
+/// </summary>
+///
+/// This script:<br/>
+/// Handles invoking a UnityEvent on pointer down.
+
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
+/// <summary>
+/// Handles invoking a UnityEvent on pointer down.<br/>
+/// Implements IPointerDownHandler interface to detect pointer down.
+/// </summary>
 public class OnPointerDownButton : MonoBehaviour, IPointerDownHandler
 {
+    /// <value>Public field for the UnityEvent.</value>
     public UnityEvent onClick;
 
+    /// <summary>
+    /// Handler method required by IPointerDownHandler interface. Invokes #onClick UnityEvent.
+    /// </summary>
+    /// <param name="eventData">The event data.</param>
     public void OnPointerDown(PointerEventData eventData)
     {
         onClick.Invoke();
