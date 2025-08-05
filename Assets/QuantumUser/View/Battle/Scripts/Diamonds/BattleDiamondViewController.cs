d9a8b852432623a132aab6031a96c3418b0db324
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-08-05 12:01:44 +0300

    BattleDoc: Updated BattleDiamondViewController.cs

--- Assets/QuantumUser/View/Battle/Scripts/Diamonds/BattleDiamondViewController.cs
+++ Assets/QuantumUser/View/Battle/Scripts/Diamonds/BattleDiamondViewController.cs
@@ -1,11 +1,28 @@
+/// @file BattleDiamondViewController.cs
+/// <summary>
+/// Has a class BattleDiamondViewController which handles diamonds visual functionality.
+/// </summary>
+///
+/// This script:<br/>
+/// Handles diamonds visual functionality.
+
 using UnityEngine;
 using Quantum;
 using Battle.View.Game;
 
 namespace Battle.View.Diamond
 {
+    /// <summary>
+    /// Diamond's entityview script.<br/>
+    /// Handles diamonds visual functionality.
+    /// </summary>
     public class BattleDiamondViewController : QuantumEntityViewComponent
     {
+        /// <summary>
+        /// Public method that is called when entity is activated upon its creation.<br/>
+        /// Rotates the diamond visuals 180 degrees for the beta team.
+        /// </summary>
+        /// <param name="_">Current simulation frame.</param>
         public override void OnActivate(Frame _)
         {
             if (BattleGameViewController.LocalPlayerTeam == BattleTeamNumber.TeamBeta)
