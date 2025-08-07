159664fc91e09514bd00e395a43e91793cfeedb7
Anna-Sofia Hiltunen <100351136+annasohva@users.noreply.github.com>
2025-08-07 16:25:27 +0300

    BattleDoc: Created BattleUiMultiOrientationElement.cs documentation

--- Assets/Altzone/Scripts/BattleUiShared/BattleUiMultiOrientationElement.cs
+++ Assets/Altzone/Scripts/BattleUiShared/BattleUiMultiOrientationElement.cs
@@ -1,18 +1,43 @@
+/// @file BattleUiMultiOrientationElement.cs
+/// <summary>
+/// Has a class BattleUiMultiOrientationElement which sets the %Battle Ui multi orientation element's data.
+/// </summary>
+///
+/// This script:<br/>
+/// Handles setting the %Battle Ui multi orientation element position and orientation and flipping it.
+
 using UnityEngine;
 
 namespace Altzone.Scripts.BattleUiShared
 {
     /// <summary>
-    /// Handles setting and getting the Battle Ui multi orientation element position and orientation.
+    /// <span class="brief-h">Multi orientation BattleUiMovableElement.</span><br/>
+    /// Handles setting the %Battle Ui multi orientation element position and orientation and flipping it.
     /// </summary>
     [RequireComponent(typeof(RectTransform))]
     public class BattleUiMultiOrientationElement : BattleUiMovableElement
     {
+        /// @name SerializeField variables
+        /// <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/SerializeField.html">SerializeFields@u-exlink</a> are serialized variables exposed to the Unity editor.
+        /// @{
+
+        /// <value>[SerializeField] Reference to the horizontal orientation's parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.</value>
         [SerializeField] private GameObject _horizontalConfiguration;
+
+        /// <value>[SerializeField] Multi orientation element's aspect ratio for horizontal orientation.</value>
         [SerializeField] private float _horizontalAspectRatio;
+
+        /// <value>[SerializeField] Reference to the vertical orientation's parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.</value>
         [SerializeField] private GameObject _verticalConfiguration;
+
+        /// <value>[SerializeField] Multi orientation element's aspect ratio for vertical orientation.</value>
         [SerializeField] private float _verticalAspectRatio;
 
+        /// @}
+
+        /// <summary>
+        /// Multi orientation element's possible orientations.
+        /// </summary>
         public enum OrientationType
         {
             None = -1,
@@ -20,14 +45,35 @@ namespace Altzone.Scripts.BattleUiShared
             Vertical = 1,
         }
 
+        /// <summary>Public getter for #_orientation.</summary>
+        /// <value>Multi orientation element's current orientation.</value>
         public OrientationType Orientation => _orientation;
+
+        /// <summary>Public getter for #_horizontalConfiguration.</summary>
+        /// <value>Reference to the horizontal orientation's parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.</value>
         public GameObject HorizontalConfiguration => _horizontalConfiguration;
+
+        /// <summary>Public getter for #_verticalConfiguration.</summary>
+        /// <value>Reference to the vertical orientation's parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.</value>
         public GameObject VerticalConfiguration => _verticalConfiguration;
+
+        /// <summary>Public getter for #_isFlippedHorizontally.</summary>
+        /// <value>Is multi orientation element flipped horizontally.</value>
         public bool IsFlippedHorizontally => _isFlippedHorizontally;
+
+        /// <summary>Public getter for #_isFlippedVertically.</summary>
+        /// <value>Is multi orientation element flipped vertically.</value>
         public bool IsFlippedVertically => _isFlippedVertically;
+
+        /// <summary>Public getter for #_horizontalAspectRatio.</summary>
+        /// <value>Multi orientation element's aspect ratio for horizontal orientation.</value>
         public float HorizontalAspectRatio => _horizontalAspectRatio;
+
+        /// <summary>Public getter for #_verticalAspectRatio.</summary>
+        /// <value>Multi orientation element's aspect ratio for vertical orientation.</value>
         public float VerticalAspectRatio => _verticalAspectRatio;
 
+        /// <value>Is the multi orientation element currently horizontal.</value>
         public bool IsHorizontal
         {
             get
@@ -37,9 +83,10 @@ namespace Altzone.Scripts.BattleUiShared
         }
 
         /// <summary>
-        /// Get the currently active gameobject.
+        /// Get the currently active orientation <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.
         /// </summary>
-        /// <returns>GameObject which is either the horizontal or vertical configuration.</returns>
+        ///
+        /// <returns>Horizontal or vertical orientation <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/GameObject.html">GameObject@u-exlink</a>.</returns>
         public GameObject GetActiveGameObject()
         {
             if (IsHorizontal)
@@ -55,6 +102,7 @@ namespace Altzone.Scripts.BattleUiShared
         /// <summary>
         /// Set BattleUiMovableElementData to this Ui multi orientation element.
         /// </summary>
+        ///
         /// <param name="data">The data which to set to this Ui element.</param>
         public override void SetData(BattleUiMovableElementData data)
         {
@@ -65,6 +113,7 @@ namespace Altzone.Scripts.BattleUiShared
         /// <summary>
         /// Get the data from this Ui multi orientation element.
         /// </summary>
+        ///
         /// <returns>Returns BattleUiMovableElementData serializable object. Null if couldn't get valid data.</returns>
         public override BattleUiMovableElementData GetData()
         {
@@ -86,10 +135,20 @@ namespace Altzone.Scripts.BattleUiShared
             }
         }
 
+        /// <value>Multi orientation element's current orientation.</value>
         private OrientationType _orientation = OrientationType.Horizontal;
+
+        /// <value>Is multi orientation element flipped horizontally.</value>
         private bool _isFlippedHorizontally = false;
+
+        /// <value>Is multi orientation element flipped vertically.</value>
         private bool _isFlippedVertically = false;
 
+        /// <summary>
+        /// Set orientation from BattleUiMovableData.
+        /// </summary>
+        ///
+        /// <param name="data">The BattleUiMovableData which contains the new orientation.</param>
         private void SetOrientation(BattleUiMovableElementData data)
         {
             // If orientation and flip is same we don't have to do anything
@@ -122,6 +181,11 @@ namespace Altzone.Scripts.BattleUiShared
             _isFlippedVertically = data.IsFlippedVertically;
         }
 
+        /// <summary>
+        /// Flip children's anchors horizontally from side to the other side, in the x axis.
+        /// </summary>
+        ///
+        /// <param name="parent">The parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/Transform.html">Transform@u-exlink</a>.</param>
         private void FlipChildrenHorizontally(Transform parent)
         {
             for (int i = 0; i < parent.childCount; i++)
@@ -147,6 +211,11 @@ namespace Altzone.Scripts.BattleUiShared
             }
         }
 
+        /// <summary>
+        /// Flip children's anchors vertically from side to the other side, in the y axis.
+        /// </summary>
+        ///
+        /// <param name="parent">The parent <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/Transform.html">Transform@u-exlink</a>.</param>
         private void FlipChildrenVertically(Transform parent)
         {
             for (int i = 0; i < parent.childCount; i++)
@@ -172,6 +241,13 @@ namespace Altzone.Scripts.BattleUiShared
             }
         }
 
+        /// <summary>
+        /// Calculates a flipped anchor value for the given anchor value.
+        /// </summary>
+        ///
+        /// <param name="anchorValue">The anchor value to be flipped.</param>
+        ///
+        /// <returns>Flipped anchor as float.</returns>
         private float GetFlippedAnchor(float anchorValue)
         {
             return -anchorValue + 1;
