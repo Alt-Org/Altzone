467047dfda7502e17677514a23b8594df42247e5
JoonasFrog <joonas.cheesus@gmail.com>
2025-08-27 17:54:01 +0300

    BattleUiLoadScreenHandler: Added namespace

--- Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiLoadScreenHandler.cs
+++ Assets/QuantumUser/View/Battle/Scripts/UI/BattleUiLoadScreenHandler.cs
@@ -5,45 +5,48 @@ using TMPro;
 using MenuUi.Scripts.Lobby.SelectedCharacters;
 using Battle.QSimulation.Game;
 
-public class BattleUiLoadScreenHandler : MonoBehaviour
+namespace Battle.View.UI
 {
-    [SerializeField] private GameObject _loadingScreen;
-    [SerializeField] private BattlePopupCharacterSlotController[] _characterSlotControllers;
-    [SerializeField] private TextMeshProUGUI[] _playerNames;
-
-    public void PlayerConnected(BattlePlayerSlot playerSlot, int[] characterIds)
+    public class BattleUiLoadScreenHandler : MonoBehaviour
     {
-        int slotIndex = playerSlot switch
+        [SerializeField] private GameObject _loadingScreen;
+        [SerializeField] private BattlePopupCharacterSlotController[] _characterSlotControllers;
+        [SerializeField] private TextMeshProUGUI[] _playerNames;
+
+        public void PlayerConnected(BattlePlayerSlot playerSlot, int[] characterIds)
         {
-            BattlePlayerSlot.Slot1 => 0,
-            BattlePlayerSlot.Slot2 => 1,
-            BattlePlayerSlot.Slot3 => 2,
-            BattlePlayerSlot.Slot4 => 3,
-            _ => -1,
-        };
-
-        if (slotIndex == -1) return;
-
-        _characterSlotControllers[slotIndex].gameObject.SetActive(true);
-        _characterSlotControllers[slotIndex].SetCharacters(characterIds);
-
-        _playerNames[(int)playerSlot - 1].alpha = 1;
-    }
-
-    public void Show(BattleParameters.PlayerType[] playerSlotTypes, FixedArray<QString64> playerNames)
-    {
-        for (int i = 0; i < playerSlotTypes.Length; i++)
-        {
-            if (playerSlotTypes[i] == BattleParameters.PlayerType.Player)
+            int slotIndex = playerSlot switch
             {
-                _playerNames[i].text = playerNames[i];
-                _playerNames[i].gameObject.SetActive(true);
+                BattlePlayerSlot.Slot1 => 0,
+                BattlePlayerSlot.Slot2 => 1,
+                BattlePlayerSlot.Slot3 => 2,
+                BattlePlayerSlot.Slot4 => 3,
+                _ => -1,
+            };
+
+            if (slotIndex == -1) return;
+
+            _characterSlotControllers[slotIndex].gameObject.SetActive(true);
+            _characterSlotControllers[slotIndex].SetCharacters(characterIds);
+
+            _playerNames[(int)playerSlot - 1].alpha = 1;
+        }
+
+        public void Show(BattleParameters.PlayerType[] playerSlotTypes, FixedArray<QString64> playerNames)
+        {
+            for (int i = 0; i < playerSlotTypes.Length; i++)
+            {
+                if (playerSlotTypes[i] == BattleParameters.PlayerType.Player)
+                {
+                    _playerNames[i].text = playerNames[i];
+                    _playerNames[i].gameObject.SetActive(true);
+                }
             }
         }
-    }
 
-    public void Hide()
-    {
-        _loadingScreen.SetActive(false);
+        public void Hide()
+        {
+            _loadingScreen.SetActive(false);
+        }
     }
 }
