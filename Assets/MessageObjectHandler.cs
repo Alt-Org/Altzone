8c042d0e0f46481ae47b3d8cdec1789a3b84c791
Niko Mattila <niko.mattila@pp.inet.fi>
2025-09-01 04:16:41 +0300

    Added the start of the messagehandler.
    You can set the text and the avatar image for it.
    You can also set it active.
    Deactivation event isn't yet in Chat script.

---
+++ Assets/MessageObjectHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using MenuUi.Scripts.AvatarEditor;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageObjectHandler : MonoBehaviour
+{
+    [SerializeField] private AvatarFaceLoader _avatar;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private Button _button;
+    private Image _image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _button.onClick.AddListener(SetMessageActive);
+        _image = _button.GetComponent<Image>();
+    }
+
+    public void SetMessageInfo(string messageText, AvatarVisualData avatarData)
+    {
+        if (avatarData != null) _avatar.UpdateVisuals(avatarData);
+        _text.text = messageText;
+    }
+
+    private void SetMessageActive()
+    {
+        if (_image != null)
+        {
+            _image.color = Color.gray;
+        }
+    }
+
+    private void SetMessageInactive(MessageObjectHandler handler)
+    {
+        if (handler == this) return;
+        if (_image != null)
+        {
+            _image.color = Color.white;
+        }
+    }
+
+}
