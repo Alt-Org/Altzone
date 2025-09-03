63986eea45b34c6f9024a54d97e97439ba176529
KimKiva <kim.kiva@hotmail.fi>
2025-09-03 14:48:39 +0300

    Add SwipeBlockOverlay to block swipe when popups are open
    - Added SwipeBlockOverlay (invisible UI element) with SwipeBlocker script
      to intercept drag gestures and prevent SwipeUI navigation.
    - Updated SettingsPopup script to activate the overlay when opening
      a popup and deactivate it when all popups are closed.
    - Ensures swipe navigation is disabled while popups are active,
      preventing accidental page swipes behind dialogs.

--- Assets/MenuUi/Scripts/Settings/SettingsPopup.cs
+++ Assets/MenuUi/Scripts/Settings/SettingsPopup.cs
@@ -5,24 +5,51 @@ using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour
 {
-    [SerializeField] private Button[] _closeButtons;        // background button is huge invisible button behind the popup to close it if clicked outside
-
+    [SerializeField] private Button[] _closeButtons;   // esim. se näkymätön taustabuttoni
+    [SerializeField] private GameObject _swipeBlockOverlay; // se SwipeBlockOverlay-objekti
 
     private void Start()
     {
-        foreach (Button _button in _closeButtons)
+        foreach (Button button in _closeButtons)
         {
-            _button.onClick.AddListener(() => ClosePopup());
+            button.onClick.AddListener(ClosePopup);
         }
     }
 
     public void OpenPopup()
     {
         gameObject.SetActive(true);
+
+        if (_swipeBlockOverlay != null)
+        {
+            _swipeBlockOverlay.SetActive(true);
+        }
     }
 
     public void ClosePopup()
     {
         gameObject.SetActive(false);
+
+        if (_swipeBlockOverlay != null)
+        {
+            // tarkista onko vielä muita popuppeja aktiivisena
+            bool anyPopupOpen = false;
+
+            // käy läpi saman parentin kaikki SettingsPopupit
+            var siblings = GetComponentsInParent<Transform>(true)[0].GetComponentsInChildren<SettingsPopup>(true);
+            foreach (var popup in siblings)
+            {
+                if (popup.gameObject.activeSelf)
+                {
+                    anyPopupOpen = true;
+                    break;
+                }
+            }
+
+            if (!anyPopupOpen)
+            {
+                _swipeBlockOverlay.SetActive(false);
+            }
+        }
     }
 }
