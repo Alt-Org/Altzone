2b49a3eeed54ac9003718bcb74f712e7e637d9ea
jdjok <juuso.jokinen8@gmail.com>
2025-09-22 16:15:18 +0300

    Added BattlePlayerClassNoneViewController
    Added default none/empty character class view controller.

---
+++ Assets/QuantumUser/View/Battle/Scripts/Player/PlayerClasses/BattlePlayerClassNoneViewController.cs
@@ -0,0 +1,9 @@
+using Quantum;
+
+namespace Battle.View.Player
+{
+    public class BattlePlayerClassNoneViewController : BattlePlayerClassBaseViewController
+    {
+        public override BattlePlayerCharacterClass Class => BattlePlayerCharacterClass.None;
+    }
+}
