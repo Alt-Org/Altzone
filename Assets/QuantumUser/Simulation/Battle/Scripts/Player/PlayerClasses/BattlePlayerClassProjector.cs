8bbf07284a208558cea89fbed95d47d22134c483
jdjok <juuso.jokinen8@gmail.com>
2025-09-30 14:14:13 +0300

    BattlePlayerClassProjector: Added exception handling

--- Assets/QuantumUser/Simulation/Battle/Scripts/Player/PlayerClasses/BattlePlayerClassProjector.cs
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Player/PlayerClasses/BattlePlayerClassProjector.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Battle.QSimulation.Projectile;
 using Photon.Deterministic;
 using Quantum;
-using UnityEngine;
 using static Battle.QSimulation.Player.BattlePlayerManager;
+using static Battle.QSimulation.Projectile.BattleProjectileQSystem;
 
 namespace Battle.QSimulation.Player
 {
@@ -15,25 +12,39 @@ namespace Battle.QSimulation.Player
 
         public override unsafe void OnCreate(Frame f, BattlePlayerManager.PlayerHandle playerHandle, BattlePlayerDataQComponent* playerData, EntityRef playerEntity)
         {
-            Debug.LogWarning("Projector Class is being used");
             BattlePlayerClassProjectorDataQComponent* data = GetClassData(f, playerEntity);
         }
 
         public override unsafe void OnProjectileHitPlayerShield(Frame f, BattleProjectileQComponent* projectile, EntityRef projectileEntity, BattlePlayerHitboxQComponent* playerHitbox, EntityRef playerHitboxEntity)
         {
             BattlePlayerClassProjectorDataQComponent* data = GetClassData(f, playerHitbox->PlayerEntity);
+            bool grab = !projectile->IsPassed;
 
-            Transform2D* transformPlayer = f.Unsafe.GetPointer<Transform2D>(playerHitbox->PlayerEntity);
-            Transform2D* transformProjectile = f.Unsafe.GetPointer<Transform2D>(projectileEntity);
+            if (grab)
+            {
+                BattlePlayerManager.PlayerHandle teammateHandle = BattlePlayerManager.PlayerHandle.GetTeammateHandle(f, f.Unsafe.GetPointer<BattlePlayerDataQComponent>(playerHitbox->PlayerEntity)->Slot);
+                if (!teammateHandle.PlayState.IsInPlay()) grab = false;
+            }
 
-            FPVector2 toProjectile = transformProjectile->Position - transformPlayer->Position;
+            if (grab)
+            {
+                Transform2D* transformPlayer = f.Unsafe.GetPointer<Transform2D>(playerHitbox->PlayerEntity);
+                Transform2D* transformProjectile = f.Unsafe.GetPointer<Transform2D>(projectileEntity);
 
-            BattleProjectileQSystem.SetHeld(f, projectile, true);
-            data->IsHoldingProjectile = true;
-            data->HeldProjectileEntity = projectileEntity;
-            data->HeldProjectileAngleRadians = FPVector2.RadiansSigned(FPVector2.Up, toProjectile);
-            data->HeldProjectileDistance = toProjectile.Magnitude;
-            data->HoldStartFrame = f.Number;
+                FPVector2 toProjectile = transformProjectile->Position - transformPlayer->Position;
+
+                BattleProjectileQSystem.SetHeld(f, projectile, true);
+                data->IsHoldingProjectile = true;
+                data->HeldProjectileEntity = projectileEntity;
+                data->HeldProjectileAngleRadians = FPVector2.RadiansSigned(FPVector2.Up, toProjectile);
+                data->HeldProjectileDistance = toProjectile.Magnitude;
+                data->HoldStartFrame = f.Number;
+            }
+            else
+            {
+                BattleProjectileQSystem.HandleIntersection(f, projectile, projectileEntity, playerHitboxEntity, playerHitbox->Normal, playerHitbox->CollisionMinOffset);
+                BattleProjectileQSystem.UpdateVelocity(f, projectile, playerHitbox->Normal, SpeedChange.Increment);
+            }
         }
 
         public override unsafe void OnUpdate(Frame f, PlayerHandle playerHandle, BattlePlayerDataQComponent* playerData, EntityRef playerEntity)
@@ -71,7 +82,7 @@ namespace Battle.QSimulation.Player
 
                 if (newAngle == angleTeammate)
                 {
-                    BattleProjectileQSystem.UpdateVelocity(f, projectile, newDirection);
+                    BattleProjectileQSystem.UpdateVelocity(f, projectile, newDirection, BattleProjectileQSystem.SpeedChange.Increment, passed: true);
                     BattleProjectileQSystem.SetHeld(f, projectile, false);
                     data->IsHoldingProjectile = false;
                 }
