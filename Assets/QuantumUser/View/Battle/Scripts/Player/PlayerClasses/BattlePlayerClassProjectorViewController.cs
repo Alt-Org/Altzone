ece34b15c0af0c6d8d190f553ef08ace8997067e
jdjok <juuso.jokinen8@gmail.com>
2025-09-30 17:51:54 +0300

    BattlePlayerClassProjectorViewController: Added documentation

--- Assets/QuantumUser/View/Battle/Scripts/Player/PlayerClasses/BattlePlayerClassProjectorViewController.cs
+++ Assets/QuantumUser/View/Battle/Scripts/Player/PlayerClasses/BattlePlayerClassProjectorViewController.cs
@@ -1,3 +1,9 @@
+/// @file BattlePlayerClassProjectorViewController.cs
+/// <summary>
+/// Contains @cref{Battle.View.Player,BattlePlayerClassProjectorViewController} class,
+/// which is a <see cref="Battle.View.Player.BattlePlayerClassProjectorViewController">class %view controller</see> for the Projector character class.
+/// </summary>
+
 using Battle.View.Game;
 using Photon.Deterministic;
 using Quantum;
@@ -5,12 +11,41 @@ using UnityEngine;
 
 namespace Battle.View.Player
 {
+    /// <summary>
+    /// <span class="brief-h">Projector <see cref="Battle.View.Player.BattlePlayerClassBaseViewController">class %view controller</see>.</span><br/>
+    /// Handles view logic for the Projector character class
+    /// </summary>
     public class BattlePlayerClassProjectorViewController : BattlePlayerClassBaseViewController
     {
+        /// @anchor BattlePlayerClassProjectorViewController-SerializeFields
+        /// @name SerializeField variables
+        /// <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/SerializeField.html">SerializeFields@u-exlink</a> are serialized variables exposed to the Unity editor.
+        /// @{
+
+        /// <summary>Reference to the moving shield object.</summary>
+        /// @ref BattlePlayerClassProjectorViewController-SerializeFields
+        [Tooltip("Reference to the moving shield object.")]
         [SerializeField] private GameObject _movingShield;
+
+        /// <summary>Indicates if the moving shield should be flipped.</summary>
+        /// @ref BattlePlayerClassProjectorViewController-SerializeFields
+        [Tooltip("Indicates if the moving shield should be flipped.")]
         [SerializeField] private bool _movingShieldFlipped;
 
+        /// @}
+
+        /// <summary>
+        /// Gets the character class associated with this Class.<br/>
+        /// Always returns <see cref="Quantum.BattlePlayerCharacterClass.Projector">BattlePlayerCharacterClass.Projector</see>.
+        /// </summary>
         public override BattlePlayerCharacterClass Class => BattlePlayerCharacterClass.Projector;
+
+        /// <summary>
+        /// Called when the shield takes damage.<br/>
+        /// Deactivating moving shield when defence value reaches 0.
+        /// </summary>
+        ///
+        /// <param name="e">The shield damage event data.</param>
         public override void OnShieldTakeDamage(EventBattleShieldTakeDamage e)
         {
             if (e.DefenceValue <= FP._0)
@@ -19,6 +54,10 @@ namespace Battle.View.Player
             }
         }
 
+        /// <summary>
+        /// Updates the shield view.<br/>
+        /// Customized view logic where the shield tracks projectile in an arc.
+        /// </summary>
         public override void OnUpdateView()
         {
             if (_movingShield == null) return;
