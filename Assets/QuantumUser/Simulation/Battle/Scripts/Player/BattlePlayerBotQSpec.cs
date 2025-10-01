cdca7a23e99cb63dce52f7a2d70930ca740b8c30
JoonasFrog <joonas.cheesus@gmail.com>
2025-10-01 18:13:12 +0300

    BattlePlayerBotQSpec: Created player bot spec #589

---
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Player/BattlePlayerBotQSpec.cs
@@ -0,0 +1,12 @@
+using Photon.Deterministic;
+using Quantum;
+
+namespace Battle.QSimulation.Player
+{
+    public class BattlePlayerBotQSpec : AssetObject
+    {
+        public FP MovementCooldownSecMin;
+        public FP MovementCooldownSecMax;
+        public FP LookAheadTime;
+    }
+}
