33dde62fd919659d111d52ba2fdf2b2e52f039f2
KimKiva <kim.kiva@hotmail.fi>
2025-10-08 21:45:45 +0300

    Refactor: TopbarToggleDrag

--- Assets/MenuUi/Scripts/TopPanel/TopBarToggleDrag.cs
+++ Assets/MenuUi/Scripts/TopPanel/TopBarToggleDrag.cs
@@ -1,23 +1,19 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class TopBarToggleDrag : MonoBehaviour,
-    IBeginDragHandler, IDragHandler, IEndDragHandler
+public class TopBarToggleDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [Header("Bounds (optional)")]
     [SerializeField] private RectTransform _clampArea;
+    public Action OnDropped;
 
-    public System.Action OnDropped;
-
-    // Cached
     private RectTransform _rectTransform, _listContainer, _boundsTransform;
     private LayoutElement _layoutElement;
     private CanvasGroup _canvasGroup;
     private Canvas _rootCanvas;
     private Transform _originalParent;
-
-    // Drag state
     private GameObject _placeholderObject;
     private Vector2 _pointerOffsetInRootCanvas;
     private float _lockedXInRootCanvas;
@@ -30,113 +26,117 @@ public class TopBarToggleDrag : MonoBehaviour,
         _rootCanvas = GetComponentInParent<Canvas>();
     }
 
-    public void OnBeginDrag(PointerEventData eventData)
+   public void OnBeginDrag(PointerEventData e)
+   {
+    if (_rootCanvas == null) return; // turvatarkistus
+
+    // V‰likeston konteksti
+    _originalParent = transform.parent;
+    _listContainer = (RectTransform)_originalParent;
+    _boundsTransform = (_clampArea != null) ? _clampArea : _listContainer;
+
+    // Placeholder alkuper‰iseen paikkaan
+    _placeholderObject = new GameObject("Placeholder", typeof(RectTransform), typeof(LayoutElement));
+    _placeholderObject.transform.SetParent(_originalParent, false);
+    LayoutElement ple = _placeholderObject.GetComponent<LayoutElement>();
+    ple.preferredHeight = (_layoutElement != null && _layoutElement.preferredHeight > 1f)
+        ? _layoutElement.preferredHeight
+        : _rectTransform.rect.height;
+    _placeholderObject.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+    // Vedon aikaiset asetukset
+    if (_layoutElement != null) _layoutElement.ignoreLayout = true;
+    if (_canvasGroup != null) _canvasGroup.blocksRaycasts = false;
+
+    // Siirr‰ vedett‰v‰ root-canvakseen
+    transform.SetParent(_rootCanvas.transform, true);
+
+    // Laske osoitinsiirto, jotta elementti ei "hypp‰‰" tarttuessa
+    RectTransform rootRT = (RectTransform)_rootCanvas.transform;
+    _lockedXInRootCanvas = _rectTransform.localPosition.x;
+
+    Vector2 lp;
+    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        rootRT, e.position, e.pressEventCamera, out lp);
+
+    _pointerOffsetInRootCanvas = (Vector2)_rectTransform.localPosition - lp;
+
+     FollowPointer(e);
+   }
+
+    public void OnDrag(PointerEventData e)
     {
-        if (_rootCanvas == null) return;
-
-        _originalParent = transform.parent;
-        _listContainer = (RectTransform)_originalParent;
-        _boundsTransform = _clampArea ? _clampArea : _listContainer;
-
-        CreatePlaceholder();
-        SetDragging(true);
-
-        var rootRT = (RectTransform)_rootCanvas.transform;
-        _lockedXInRootCanvas = _rectTransform.localPosition.x;
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(rootRT, eventData.position, eventData.pressEventCamera, out var p);
-        _pointerOffsetInRootCanvas = (Vector2)_rectTransform.localPosition - p;
-
-        FollowPointer(eventData);
+      FollowPointer(e);
+      UpdatePlaceholderIndex(e);
     }
 
-    public void OnDrag(PointerEventData eventData)
+    public void OnEndDrag(PointerEventData e)
     {
-        FollowPointer(eventData);
-        UpdatePlaceholderIndex(eventData);
-    }
+       
+        transform.SetParent(_originalParent, false); // Palaa listaan ennen indeksin asettamista
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        transform.SetParent(_originalParent, false);
-        if (_placeholderObject) transform.SetSiblingIndex(_placeholderObject.transform.GetSiblingIndex());
-        SetDragging(false);
-        DestroyPlaceholder();
+        if (_placeholderObject != null)
+        {
+            transform.SetSiblingIndex(_placeholderObject.transform.GetSiblingIndex());  // Aseta lopullinen indeksi ja siivoa placeholder ()
+            Destroy(_placeholderObject);
+            _placeholderObject = null;
+        }
+
+        // Palauta layout & raycastit
+        if (_layoutElement != null) _layoutElement.ignoreLayout = false;
+
+        if (_canvasGroup != null)
+        {
+            _canvasGroup.blocksRaycasts = true;
+            _canvasGroup.alpha = 1f;
+        }
+
         OnDropped?.Invoke();
     }
 
-    private void SetDragging(bool isDragging)
+    private void FollowPointer(PointerEventData e)
     {
-        if (_layoutElement) _layoutElement.ignoreLayout = isDragging;
-        _canvasGroup.blocksRaycasts = !isDragging;
-        _canvasGroup.alpha = isDragging ? 0.75f : 1f;
-        transform.SetParent(isDragging ? _rootCanvas.transform : _originalParent, true);
-    }
-
-    private void CreatePlaceholder()
-    {
-        _placeholderObject = new GameObject("Placeholder", typeof(RectTransform), typeof(LayoutElement));
-        _placeholderObject.transform.SetParent(_originalParent, false);
-        var ple = _placeholderObject.GetComponent<LayoutElement>();
-        ple.preferredHeight = (_layoutElement && _layoutElement.preferredHeight > 1f)
-            ? _layoutElement.preferredHeight
-            : _rectTransform.rect.height;
-        _placeholderObject.transform.SetSiblingIndex(transform.GetSiblingIndex());
-    }
-
-    private void DestroyPlaceholder()
-    {
-        if (!_placeholderObject) return;
-        Destroy(_placeholderObject);
-        _placeholderObject = null;
-    }
-
-    private void FollowPointer(PointerEventData eventData)
-    {
-        var rootRT = (RectTransform)_rootCanvas.transform;
-
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(rootRT, eventData.position, eventData.pressEventCamera, out var p);
-        Vector2 desired = p + _pointerOffsetInRootCanvas; desired.x = _lockedXInRootCanvas;
-
-        if (_boundsTransform)
+        RectTransform rootRT = (RectTransform)_rootCanvas.transform;
+        Vector2 lp; RectTransformUtility.ScreenPointToLocalPointInRectangle(rootRT, e.position, e.pressEventCamera, out lp);
+        Vector2 desired = lp + _pointerOffsetInRootCanvas; desired.x = _lockedXInRootCanvas;
+        if (_boundsTransform != null)
         {
-            Vector2 inBounds = (Vector2)_boundsTransform.InverseTransformPoint(rootRT.TransformPoint(desired));
-            Rect br = _boundsTransform.rect;
-            float hw = _rectTransform.rect.width * 0.5f, hh = _rectTransform.rect.height * 0.5f;
-            inBounds.x = Mathf.Clamp(inBounds.x, br.xMin + hw, br.xMax - hw);
-            inBounds.y = Mathf.Clamp(inBounds.y, br.yMin + hh, br.yMax - hh);
-            desired = (Vector2)rootRT.InverseTransformPoint(_boundsTransform.TransformPoint(inBounds));
+            Vector2 inB = (Vector2)_boundsTransform.InverseTransformPoint(rootRT.TransformPoint(desired));
+            Rect r = _boundsTransform.rect; float hw = _rectTransform.rect.width * 0.5f, hh = _rectTransform.rect.height * 0.5f;
+            inB.x = Mathf.Clamp(inB.x, r.xMin + hw, r.xMax - hw); inB.y = Mathf.Clamp(inB.y, r.yMin + hh, r.yMax - hh);
+            desired = (Vector2)rootRT.InverseTransformPoint(_boundsTransform.TransformPoint(inB));
         }
-
         _rectTransform.localPosition = new Vector2(_lockedXInRootCanvas, desired.y);
     }
 
-    private void UpdatePlaceholderIndex(PointerEventData eventData)
+    private void UpdatePlaceholderIndex(PointerEventData e)
     {
-        if (!_placeholderObject || !_listContainer) return;
+        if (_placeholderObject == null || _listContainer == null) return;
+        List<RaycastResult> hits = new List<RaycastResult>(); EventSystem es = EventSystem.current; if (es != null) es.RaycastAll(e, hits);
+        TopBarToggleDrag row = null;
 
-        // Find the row by walking up via Unity's internal parent search
-        var hit = eventData.pointerCurrentRaycast.gameObject;
-        var hitRow = hit ? hit.GetComponentInParent<TopBarToggleDrag>() : null;
-        var rowTransform = hitRow ? hitRow.transform : null;
-
-        if (rowTransform && rowTransform != transform && rowTransform.gameObject != _placeholderObject)
+        foreach (RaycastResult hit in hits)
         {
-            int idx = rowTransform.GetSiblingIndex()
-                    + (_rectTransform.position.y < ((RectTransform)rowTransform).position.y ? 1 : 0);
+            if (hit.gameObject == null) continue;
 
-            int ph = _placeholderObject.transform.GetSiblingIndex();
-            _placeholderObject.transform.SetSiblingIndex(Mathf.Clamp(ph < idx ? idx - 1 : idx, 0, _listContainer.childCount));
-            return;
+            TopBarToggleDrag rowitem = hit.gameObject.GetComponentInParent<TopBarToggleDrag>();
+            if (rowitem != null)
+            {
+                row = rowitem;
+                break;
+            }
         }
 
-        // No valid row: snap to edges
-        Vector2 local = _listContainer.InverseTransformPoint(_rectTransform.position);
-        Rect lr = _listContainer.rect;
-        int edgeIdx = (local.y > lr.yMax) ? 0
-                   : (local.y < lr.yMin) ? _listContainer.childCount
-                   : _placeholderObject.transform.GetSiblingIndex();
+        Transform rt = row != null ? row.transform : null;
+        if (rt != null)
+        {
+            int target = rt.GetSiblingIndex()
+                + (_rectTransform.position.y < ((RectTransform)rt).position.y ? 1 : 0);
 
-        _placeholderObject.transform.SetSiblingIndex(edgeIdx);
+            int ph = _placeholderObject.transform.GetSiblingIndex();
+            _placeholderObject.transform.SetSiblingIndex(
+                Mathf.Clamp(ph < target ? target - 1 : target, 0, _listContainer.childCount));
+            return;
+        }
     }
 }
