dad05b2a1db2d51d1593c095c0ee2ab4410f37dd
Niklas <niklas.tontti603619@gmail.com>
2025-10-13 16:04:35 +0300

    RuntimeConfig: Added AzGameType

--- Assets/QuantumUser/Simulation/Altzone/RuntimeConfig.User.cs
+++ Assets/QuantumUser/Simulation/Altzone/RuntimeConfig.User.cs
@@ -2,9 +2,15 @@
 
 namespace Quantum
 {
+    public enum AzGameType
+    {
+        Battle
+    }
 
     public partial class RuntimeConfig
     {
+        public AzGameType AzGameType;
+
         public AssetRef<BattleQConfig> BattleConfig;
         public BattleParameters BattleParameters;
     }
