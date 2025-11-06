094c800df01e7d8a7380c74b0135ed57418e9d58
Niko Mattila <niko.mattila@pp.inet.fi>
2025-11-06 12:48:04 +0200

    Update to the tutorial system.
    It currently contains a messy update polling position refresh.
    It should now place the cutouts more or less properly.
    Restructured the UICanvas and MainMenuSwipe to allow both of them having tutorials added to them.

--- Assets/MenuUi/Scripts/Tutorial/TutorialPanelHandler.cs
+++ Assets/MenuUi/Scripts/Tutorial/TutorialPanelHandler.cs
@@ -7,11 +7,29 @@ using UnityEngine.UI;
 public class TutorialPanelHandler : MonoBehaviour
 {
     [SerializeField] private Button _tutorialAdvanceButton;
+    [SerializeField] private Image _imageToCutOut;
+    [SerializeField] private GameObject _cutOut;
 
     public void SetData(Action advanceAction)
     {
         if(_tutorialAdvanceButton != null)
             _tutorialAdvanceButton.onClick.AddListener(advanceAction.Invoke);
+
+        if(_cutOut != null)
+        {
+            _cutOut.GetComponent<RectTransform>().anchorMin = new(0.5f, 0.5f);
+            _cutOut.GetComponent<RectTransform>().anchorMax = new(0.5f, 0.5f);
+            _cutOut.transform.position = _imageToCutOut.transform.position;
+            _cutOut.GetComponent<RectTransform>().sizeDelta = new(_imageToCutOut.GetComponent<RectTransform>().rect.width, _imageToCutOut.GetComponent<RectTransform>().rect.height);
+            _cutOut.GetComponent<Image>().sprite = _imageToCutOut.sprite;
+            _cutOut.GetComponent<Image>().preserveAspect = _imageToCutOut.preserveAspect;
+        }
+    }
+
+    private void Update()
+    {
+        _cutOut.transform.position = _imageToCutOut.transform.position;
+        _cutOut.GetComponent<RectTransform>().sizeDelta = new(_imageToCutOut.GetComponent<RectTransform>().rect.width, _imageToCutOut.GetComponent<RectTransform>().rect.height);
     }
 
 }
