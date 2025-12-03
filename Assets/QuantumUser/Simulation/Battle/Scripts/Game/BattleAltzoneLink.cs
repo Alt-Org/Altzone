06cacebfe6cd8016f1aa1783fbb9ba76bea83ea3
JoonasFrog <joonas.cheesus@gmail.com>
2025-12-03 17:13:01 +0200

    AltzoneLink: Added GetShieldPrototypes method #1110

--- Assets/QuantumUser/Simulation/Battle/Scripts/Game/BattleAltzoneLink.cs
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Game/BattleAltzoneLink.cs
@@ -10,15 +10,20 @@ namespace Battle.QSimulation.Game
     public static class BattleAltzoneLink
     {
         public static void InitLink(
-            Func<int, AssetRef<EntityPrototype>> getCharacterPrototypeFnRef
+            Func<int, AssetRef<EntityPrototype>> getCharacterPrototypeFnRef, Func<int, AssetRef<EntityPrototype>[]> getShieldPrototypesFnRef
         )
         {
             s_getCharacterPrototypeFnRef = getCharacterPrototypeFnRef;
+            s_getShieldPrototypesFnRef = getShieldPrototypesFnRef;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static AssetRef<EntityPrototype> GetCharacterPrototype(int id) => s_getCharacterPrototypeFnRef(id);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static AssetRef<EntityPrototype>[] GetShieldPrototypes(int id) => s_getShieldPrototypesFnRef(id);
+
         private static Func<int, AssetRef<EntityPrototype>> s_getCharacterPrototypeFnRef;
+        private static Func<int, AssetRef<EntityPrototype>[]> s_getShieldPrototypesFnRef;
     }
 }
