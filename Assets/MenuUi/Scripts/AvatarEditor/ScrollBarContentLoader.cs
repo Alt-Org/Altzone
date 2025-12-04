9029131d0c3ef48424077d21392bba16e7a6d2bc
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-04 16:50:50 +0200

    Made grid get real avatarparts
    - Hardcoded to hair atm

--- Assets/MenuUi/Scripts/AvatarEditor/ScrollBarContentLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ScrollBarContentLoader.cs
@@ -1,34 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Altzone.Scripts.AvatarPartsInfo;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScrollBarContentLoader : MonoBehaviour
 {
-    private AvatarPartsReference _avatarPartsReference;
+    [SerializeField] private AvatarPartsReference _avatarPartsReference;
     [SerializeField] private Transform _content;
     [SerializeField] private GameObject _gridCellPrefab;
     [SerializeField] private Sprite _testSprite;
+    private List<AvatarPartInfo> _avatarPartInfo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _avatarPartInfo = _avatarPartsReference.GetAvatarPartsByCategory("10");
+        foreach (var part in _avatarPartInfo)
+        {
+            AddCell(part.IconImage);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnEnable()
     {
-        for (int i = 0; i < 14; i++)
-        {
-            AddCell(_testSprite);
-        }
+
     }
 
     private void AddCell(Sprite sprite)
