3e7d5aff50cc813abd2d4b2f2b4aa2b7a6b7f320
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-04 19:21:40 +0200

    Modified script to check if scrollbar works
    - Also removed placeholder images from verticalscroll

--- Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
@@ -11,15 +11,30 @@ public class ScrollBarCategoryLoader : MonoBehaviour
     [SerializeField] private Transform _content;
     [SerializeField] private GameObject _avatarPartCategoryGridCellPrefab;
     [SerializeField] private AvatarDefaultReference _avatarDefaultReference;
+    private List<AvatarPartInfo> _avatarPartInfo;
+    private List<string> _testlist;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _testlist = new List<string> { "10", "21", "22", "23", "31", "32", "33" };
+        foreach (var categoryID in _testlist)
+        {
+            _avatarPartInfo = _avatarPartsReference.GetAvatarPartsByCategory(categoryID);
+            AddCell(_avatarPartInfo[0].IconImage);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AddCell(Sprite sprite)
+    {
+        GameObject _gridCell = Instantiate(_avatarPartCategoryGridCellPrefab, _content);
+        Image _avatarPart = _gridCell.transform.Find("FeatureImage").GetComponent<Image>();
+        _avatarPart.sprite = sprite;
+
     }
 }
