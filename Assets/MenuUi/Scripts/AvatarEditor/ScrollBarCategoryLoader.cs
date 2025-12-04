9dbc78260f5e992558ccca1d3115d3fa547e8d8d
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-04 18:24:54 +0200

    added script and components to verticallscrollview-grid

--- Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using Altzone.Scripts.AvatarPartsInfo;
 
 public class ScrollBarCategoryLoader : MonoBehaviour
 {
+    [SerializeField] private AvatarPartsReference _avatarPartsReference;
+    [SerializeField] private Transform _content;
+    [SerializeField] private GameObject _avatarPartCategoryGridCellPrefab;
+    [SerializeField] private AvatarDefaultReference _avatarDefaultReference;
     // Start is called before the first frame update
     void Start()
     {
