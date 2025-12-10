5558a0b9c161c753602d1f1ae969458f88b3ce29
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-10 18:31:30 +0200

    Refactoring

---
+++ Assets/MenuUi/Scripts/AvatarEditor/NewAvatarEditorController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using MenuUi.Scripts.AvatarEditor;
+using UnityEngine;
+
+public class NewAvatarEditorController : MonoBehaviour
+{
+    [SerializeField] ScrollBarCategoryLoader _categoryLoader;
+    [SerializeField] ScrollBarFeatureLoader _featureLoader;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _categoryLoader.SetCategoryCells((categoryId) => _featureLoader.RefreshFeatureListItems(categoryId));
+        _categoryLoader.UpdateCellSize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ScrollBarCategoryLoader.cs
@@ -7,6 +7,8 @@ using Altzone.Scripts.AvatarPartsInfo;
 using NUnit.Framework.Constraints;
 using System.Runtime.CompilerServices;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Composites;
+using System;
 
 namespace MenuUi.Scripts.AvatarEditor
 {
@@ -14,35 +16,27 @@ namespace MenuUi.Scripts.AvatarEditor
     public class ScrollBarCategoryLoader : MonoBehaviour, IEndDragHandler
     {
         [SerializeField] private AvatarPartsReference _avatarPartsReference;
-        [SerializeField] private ScrollBarFeatureLoader _scrollBarFeatureLoader;
-        [SerializeField] private RectTransform _content;
+        [SerializeField] private RectTransform _categoryGridContent;
         [SerializeField] private GameObject _avatarPartCategoryGridCellPrefab;
-        private GameObject _emptyCell;
-        [SerializeField] private AvatarDefaultReference _avatarDefaultReference;
+        [SerializeField] private RectTransform _categoryGrid;
+        [SerializeField] private VerticalLayoutGroup _verticalLayoutGroup;
+        [SerializeField] private ScrollRect _categoryGridScrollRect;
+        [SerializeField, Range(0f, 0.3f)] private float _horizontalPadding = 0.1f;
+        [SerializeField, Range(0f, 0.3f)] private float _spacing = 0.05f;
+        [SerializeField, Range(0f, 0.2f)] private float _verticalPadding = 0.05f;
+
         private List<AvatarPartInfo> _avatarPartInfo;
         private List<string> _allCategoryIds;
         private float _cellHeight;
-        [SerializeField] private RectTransform _categoryGrid;
-        [SerializeField] private VerticalLayoutGroup _verticalLayoutGroup;
-        [SerializeField] private ScrollRect _scrollRect;
-        private float _spacing = 0;
+        private float _actualSpacing;
+        private float _actualVerticalPadding;
         private float _viewPortHeight;
+        private readonly int _cellsShownAtATime = 3;
         // Start is called before the first frame update
         void Start()
         {
-            
-            _allCategoryIds = _avatarPartsReference.GetAllCategoryIds();
-            for (int i = 0; i < 3; i++)
-            {
-                foreach (var categoryID in _allCategoryIds)
-                {
-                    _avatarPartInfo = _avatarPartsReference.GetAvatarPartsByCategory(categoryID);
-                    AddCategoryCell(_avatarPartInfo[0].IconImage, categoryID);
-                }
-            }
-
-            UpdateCellSize();
-            _content.anchoredPosition = new Vector2(_categoryGrid.anchoredPosition.x, _categoryGrid.anchoredPosition.y + (_cellHeight + _spacing) * 3);
+            // Used to "start the scroll position not at the top"
+            //_categoryGridContent.anchoredPosition = new Vector2(_categoryGrid.anchoredPosition.x, _categoryGrid.anchoredPosition.y + (_cellHeight + _actualSpacing) * 3);
         }
 
         public void OnEndDrag(PointerEventData eventData)
@@ -50,35 +44,20 @@ namespace MenuUi.Scripts.AvatarEditor
             ClampToCenter();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         private void AddCategoryCell(Sprite sprite, string categoryId)
-        {
-            GameObject _gridCell = Instantiate(_avatarPartCategoryGridCellPrefab, _content);
-            Image _avatarPart = _gridCell.transform.Find("FeatureImage").GetComponent<Image>();
-            _avatarPart.sprite = sprite;
-
-            Button _button = _gridCell.GetComponent<Button>();
-            if (_scrollBarFeatureLoader == null)
-            {
-                Debug.LogError("_scrollbarfeatureloader is null");
-            }
-            _button.onClick.AddListener(() => _scrollBarFeatureLoader.RefreshFeatureListItems(categoryId));
-        }
-        private void UpdateCellSize()
+        public void UpdateCellSize()
         {
             _viewPortHeight = _categoryGrid.rect.height;
-            _spacing = 0.05f * _viewPortHeight;
-            _verticalLayoutGroup.spacing = _spacing;
-            _verticalLayoutGroup.padding.left = Mathf.CeilToInt(0.1f * _categoryGrid.rect.width);
-            _verticalLayoutGroup.padding.right = Mathf.CeilToInt(0.1f * _categoryGrid.rect.width);
+            _actualSpacing = _spacing * _viewPortHeight;
+            _actualVerticalPadding = _verticalPadding * _categoryGrid.rect.height;
+            _verticalLayoutGroup.spacing = _actualSpacing;
+            _verticalLayoutGroup.padding.left = Mathf.CeilToInt(_horizontalPadding * _categoryGrid.rect.width);
+            _verticalLayoutGroup.padding.right = Mathf.CeilToInt(_horizontalPadding * _categoryGrid.rect.width);
+            _verticalLayoutGroup.padding.top = Mathf.CeilToInt(_actualVerticalPadding);
+            _verticalLayoutGroup.padding.bottom = Mathf.CeilToInt(_actualVerticalPadding);
 
-            _cellHeight = (_viewPortHeight - _spacing * 2) / 3;
-            foreach (RectTransform child in _content)
+            _cellHeight = (_viewPortHeight - _actualSpacing * (_cellsShownAtATime -1) - _verticalLayoutGroup.padding.top - _verticalLayoutGroup.padding.bottom) / _cellsShownAtATime;
+            foreach (RectTransform child in _categoryGridContent)
             {
                 LayoutElement le = child.GetComponent<LayoutElement>();
                 if (le != null)
@@ -94,9 +73,9 @@ namespace MenuUi.Scripts.AvatarEditor
 
         private void ClampToCenter()
         {
-            float totalCellSize = _cellHeight + _spacing;
+            float totalCellSize = _cellHeight + _actualSpacing;
 
-            float contentYPos = _scrollRect.content.anchoredPosition.y;
+            float contentYPos = _categoryGridScrollRect.content.anchoredPosition.y;
 
             int nearestIndex = Mathf.RoundToInt(contentYPos / totalCellSize);
 
@@ -107,7 +86,7 @@ namespace MenuUi.Scripts.AvatarEditor
 
         IEnumerator Clamp(float targetYPos)
         {
-            Vector2 pos = _scrollRect.content.anchoredPosition;
+            Vector2 pos = _categoryGridScrollRect.content.anchoredPosition;
             Vector2 target = new(pos.x, targetYPos);
 
             float t = 0f;
@@ -115,24 +94,56 @@ namespace MenuUi.Scripts.AvatarEditor
             while (t < 1)
             {
                 t += Time.deltaTime * 5f;
-                _scrollRect.content.anchoredPosition = Vector2.Lerp(pos, target, t);
+                _categoryGridScrollRect.content.anchoredPosition = Vector2.Lerp(pos, target, t);
                 yield return null;
             }
 
-            _scrollRect.content.anchoredPosition = target;
+            _categoryGridScrollRect.content.anchoredPosition = target;
 
             OnClamp(targetYPos);
         }
 
         private void OnClamp(float targetYPos)
         {
-            float totalCellSize = _cellHeight + _spacing;
-            int topIndex = Mathf.RoundToInt(targetYPos /totalCellSize);
+            float totalCellSize = _cellHeight + _actualSpacing;
+            int topIndex = Mathf.RoundToInt(targetYPos / totalCellSize);
             int centerIndex = topIndex + 1;
 
-            var centerCell = _scrollRect.content.GetChild(centerIndex);
+            var centerCell = _categoryGridScrollRect.content.GetChild(centerIndex);
 
             centerCell.GetComponent<Button>().onClick.Invoke();
+        }
+
+        //Planning to add better way to add the category images later
+        public void SetCategoryCells(Action<string> buttonFunction)
+        {
+            DestroyCategoryCells();
+            _allCategoryIds = _avatarPartsReference.GetAllCategoryIds();
+
+            foreach (string categoryId in _allCategoryIds)
+            {
+                _avatarPartInfo = _avatarPartsReference.GetAvatarPartsByCategory(categoryId);
+                AddCategoryCell(categoryId, _avatarPartInfo[0].IconImage, buttonFunction);
+            }
+        }
+
+        private void AddCategoryCell(string FeatureCategoryId, Sprite CellImage, Action<string> buttonFunction)
+        {
+            GameObject gridCell = Instantiate(_avatarPartCategoryGridCellPrefab, _categoryGridContent);
+            Image avatarpart = gridCell.transform.Find("FeatureImage").GetComponent<Image>();
+            Button button = gridCell.GetComponent<Button>();
+
+            avatarpart.sprite = CellImage;
+            button.interactable = false;
+            button.onClick.AddListener(() => buttonFunction.Invoke(FeatureCategoryId)); 
+        }
+
+        private void DestroyCategoryCells()
+        {
+            foreach (Transform child in _categoryGridContent.transform)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
