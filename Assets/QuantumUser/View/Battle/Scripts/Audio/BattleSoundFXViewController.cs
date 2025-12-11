bdb14380e8b38932419fd6945162c94a5af9f1f3
JoonasFrog <joonas.cheesus@gmail.com>
2025-12-11 18:33:28 +0200

    BattleSoundFXViewController: Refactored to utilize updated Audio system
    All SerializeField variables removed.
    
    Effect Serializable class removed.
    
    PlaySound casts the given effect to BattleSFXNameTypes enum and passes it to AudioManager.PlaySfxAudio.

--- Assets/QuantumUser/View/Battle/Scripts/Audio/BattleSoundFXViewController.cs
+++ Assets/QuantumUser/View/Battle/Scripts/Audio/BattleSoundFXViewController.cs
@@ -2,12 +2,6 @@
 /// <summary>
 /// Contains @cref{Battle.View.Audio,BattleSoundFXViewController} class which handles playing sound effects.
 /// </summary>
-///
-/// This script:<br/>
-/// Handles playing sound effects.
-
-// System usings
-using System;
 
 // Unity usings
 using UnityEngine;
@@ -17,6 +11,7 @@ using Quantum;
 
 // Altzone usings
 using Altzone.Scripts.Audio;
+using Assets.Altzone.Scripts.Reference_Sheets;
 
 namespace Battle.View.Audio
 {
@@ -26,25 +21,6 @@ namespace Battle.View.Audio
     /// </summary>
     public class BattleSoundFXViewController : MonoBehaviour
     {
-        /// @anchor BattleSoundFXViewController-SerializeFields
-        /// @name SerializeField variables
-        /// <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/SerializeField.html">SerializeFields@u-exlink</a> are serialized variables exposed to the Unity editor.
-        /// @{
-
-        /// <summary>[SerializeField] GoalHit sound effect data.</summary>
-        /// @ref BattleSoundFXViewController-SerializeFields
-        [SerializeField] private Effect _goalHit;
-
-        /// <summary>[SerializeField] SideWallHit sound effect data.</summary>
-        /// @ref BattleSoundFXViewController-SerializeFields
-        [SerializeField] private Effect _sideWallHit;
-
-        /// <summary>[SerializeField] WallBroken sound effect data.</summary>
-        /// @ref BattleSoundFXViewController-SerializeFields
-        [SerializeField] private Effect _wallBroken;
-
-        /// @}
-
         /// <summary>
         /// Plays a sound effect.
         /// </summary>
@@ -52,28 +28,9 @@ namespace Battle.View.Audio
         /// <param name="effect">The BattleSoundFX which to play.</param>
         public void PlaySound(BattleSoundFX effect)
         {
-            // Map SoundEffect enum to the correct effect
-            Effect effectRef = effect switch
-            {
-                BattleSoundFX.GoalHit     => _goalHit,
-                BattleSoundFX.SideWallHit => _sideWallHit,
-                BattleSoundFX.WallBroken  => _wallBroken,
+            BattleSFXNameTypes effectName = (BattleSFXNameTypes)effect;
 
-                _ => null,
-            };
-
-            if (!effectRef.Active) return;
-            AudioManager.Instance.PlaySfxAudio("Battle_Common", effectRef.Name);
-        }
-
-        /// <summary>
-        /// Private Serializable class for sound effect data.
-        /// </summary>
-        [Serializable]
-        private class Effect
-        {
-            public bool Active = false;
-            public string Name;
+            AudioManager.Instance.PlayBattleSfxAudio(effectName);
         }
     }
 }
