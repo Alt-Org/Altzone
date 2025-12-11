9dc6bb8b10c97ecfae8c3f228d27c7b97764eada
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-11 15:02:00 +0200

    Updated Color Selection
    - Added the color selection objects, haven't added listeners yet

--- Assets/MenuUi/Scripts/AvatarEditor/ColorGridLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ColorGridLoader.cs
@@ -1,21 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ColorGridLoader : MonoBehaviour
+namespace MenuUi.Scripts.AvatarEditor
 {
-    [SerializeField] private Transform _ColorGridContent;
-    [SerializeField] private GameObject _gridCellPrefab;
-    //Took the colors directly from the screenshon on github
-    [SerializeField]
-    private List<Color> _colors = new List<Color>
+    public class ColorGridLoader : MonoBehaviour
     {
-        new Color(1, 0, 0),
-        new Color(0.996078431372549f, 0.5529411764705883f, 0),
-        new Color(1, 1, 0),
-        new Color(0, 0.5568627450980392f, 0),
-        new Color(0, 0.7529411764705882f, 0.7529411764705882f),
-        new Color(0.25098039215686274f, 0, 0.596078431372549f),
-        new Color(0.5568627450980392f, 0, 0.5568627450980392f)
-    };
+        [SerializeField] private RectTransform _colorGridContent;
+        [SerializeField] private RectTransform _colorSelection;
+        [SerializeField] private GameObject _gridCellPrefab;
+        [SerializeField] private HorizontalLayoutGroup _colorGrid;
+        [SerializeField] private Color _backgroundColor = new(0, 0, 0);
+        //Took the colors directly from the screenshot on github
+        [SerializeField]
+        private List<Color> _colors = new List<Color>
+        {
+            new Color(1, 0, 0),
+            new Color(0.996078431372549f, 0.5529411764705883f, 0),
+            new Color(1, 1, 0),
+            new Color(0, 0.5568627450980392f, 0),
+            new Color(0, 0.7529411764705882f, 0.7529411764705882f),
+            new Color(0.25098039215686274f, 0, 0.596078431372549f),
+            new Color(0.5568627450980392f, 0, 0.5568627450980392f)
+        };
+
+        private float _viewPortHeight;
+        private float _cellHeight;
+        private float _colorGridPadding;
+
+        private void AddColorCell(Color color)
+        {
+            GameObject colorGridCell = Instantiate(_gridCellPrefab, _colorGridContent);
+            Image colorImage = colorGridCell.transform.Find("FeatureImage").GetComponent<Image>();
+            Image backgroundImage = colorGridCell.transform.Find("BackgroundImage").GetComponent<Image>();
+            RectTransform colorImageRect = colorGridCell.transform.Find("FeatureImage").GetComponent<RectTransform>();
+
+            colorImage.color = color;
+            colorImageRect.anchorMin = new Vector2(0.05f, 0.05f);
+            colorImageRect.anchorMax = new Vector2(0.95f, 0.95f);
+            backgroundImage.color = _backgroundColor;
+            AddListener();
+        }
+
+        public void SetColorCells()
+        {
+            foreach (Color color in _colors)
+            {
+                AddColorCell(color);
+            }
+        }
+
+        public void UpdateCellSize()
+        {
+            _viewPortHeight = _colorSelection.rect.height;
+            _cellHeight = _viewPortHeight * 0.9f;
+            _colorGridPadding = _viewPortHeight * 0.05f;
+
+            _colorGrid.padding.left = Mathf.CeilToInt(_colorGridPadding);
+            _colorGrid.padding.right = Mathf.CeilToInt(_colorGridPadding);
+            _colorGrid.padding.top = Mathf.CeilToInt(_colorGridPadding);
+            _colorGrid.padding.bottom = Mathf.CeilToInt(_colorGridPadding);
+            _colorGrid.spacing = 0.05f * _cellHeight;
+
+            foreach (RectTransform child in _colorGridContent)
+            {
+                LayoutElement le = child.GetComponent<LayoutElement>();
+                if (le != null)
+                {
+                    le.preferredHeight = _cellHeight;
+                    le.preferredWidth = _cellHeight;
+                }
+            }
+        }
+
+        private void AddListener()
+        {
+
+        }
+    }
 }
--- Assets/MenuUi/Scripts/AvatarEditor/NewAvatarEditorController.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/NewAvatarEditorController.cs
@@ -10,6 +10,7 @@ namespace MenuUi.scripts.AvatarEditor
         //Planning to add this to the original AvatarEditorController, but find it easier to do in it's own script while making it
         [SerializeField] ScrollBarCategoryLoader _categoryLoader;
         [SerializeField] ScrollBarFeatureLoader _featureLoader;
+        [SerializeField] ColorGridLoader _colorLoader;
 
         //disabling for now
         [SerializeField] private GameObject _editorMenu;
@@ -22,16 +23,24 @@ namespace MenuUi.scripts.AvatarEditor
         {
             _categoryLoader.SetCategoryCells((categoryId) => _featureLoader.RefreshFeatureListItems(categoryId));
             _categoryLoader.UpdateCellSize();
+            _colorLoader.SetColorCells();
+            _colorLoader.UpdateCellSize();
 
+            StartCoroutine(Wait());
+        }
+
+        //This is stupid but i don't want to remove the old objects yet so it works for now to clean up the view
+        private IEnumerator Wait()
+        {
+            yield return new WaitForSeconds(1);
             _switchModeButtons.SetActive(false);
             _editorMenu.SetActive(false);
             _revertButton.SetActive(false);
         }
-
         // Update is called once per frame
         void Update()
         {
-        
+
         }
     }
 }
