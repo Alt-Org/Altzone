19766d0f4acab152a08ba099ca79e504a1ef8eda
JoonasFrog <joonas.cheesus@gmail.com>
2025-12-12 11:28:19 +0200

    BattleSoundFXController: Marked PlaySound with AgressiveInlining

--- Assets/QuantumUser/View/Battle/Scripts/Audio/BattleSoundFXController.cs
+++ Assets/QuantumUser/View/Battle/Scripts/Audio/BattleSoundFXController.cs
@@ -3,6 +3,9 @@
 /// Contains @cref{Battle.View.Audio,BattleSoundFXController} class which handles playing sound effects.
 /// </summary>
 
+// System usings
+using System.Runtime.CompilerServices;
+
 // Quantum usings
 using Quantum;
 
@@ -22,6 +25,7 @@ namespace Battle.View.Audio
         /// </summary>
         ///
         /// <param name="effect">The BattleSoundFX which to play.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void PlaySound(BattleSoundFX effect)
         {
             AudioManager.Instance.PlayBattleSfxAudio((BattleSFXNameTypes)effect);
