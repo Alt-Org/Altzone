794bad79e12b10e2d4a80e0f040b809abf5ebce9
jdjok <juuso.jokinen8@gmail.com>
2025-12-15 17:32:40 +0200

    BattleUiDebugConsoleHandler.cs: Added documentation

--- Assets/QuantumUser/View/Battle/Scripts/UI/Handler/BattleUiDebugConsoleHandler.cs
+++ Assets/QuantumUser/View/Battle/Scripts/UI/Handler/BattleUiDebugConsoleHandler.cs
@@ -1,4 +1,9 @@
-﻿// System usings
+﻿/// @file BattleUiDebugConsoleHandler.cs
+/// <summary>
+/// Contains @cref{Battle.View.UI,BattleUiDebugConsoleHandler} class which displays custom on-screen debug console.
+/// </summary>
+
+// System usings
 using System;
 using System.Text;
 
@@ -13,53 +18,162 @@ using LogType = Battle.QSimulation.BattleDebugLogger.LogType;
 
 namespace Battle.View.UI
 {
+    /// <summary>
+    /// <span class="brief-h">DebugConsole @uihandlerlink (<a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.html">Unity MonoBehaviour script@u-exlink</a>).</span><br/>
+    /// Handles displaying the custom on-screen debug console.
+    /// </summary>
+    ///
+    /// @anchor BattleUiDebugConsoleHandler-DetailedDescription
+    ///
+    /// Controlled by @cref{Battle.QSimulation,BattleDebugLogger}
+    ///
+    /// Shows only certain amount of log entries. First line shows the type of log and it's source and second line shows the message of the log.<br/>
+    /// Logs appear on console when @cref{Battle.QSimulation,BattleDebugLogger} requests log to be shown.<br/>
+    /// Each log entry has a lifetime after which they disappear.<br/>
+    /// See @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields" for all settings.
+    ///
+    /// When visible, automatically shows entry logs if there are any, otherwise debug console will be hidden.<br/>
+    /// When not visible, console is never shown.<br/>
+    /// See @cref{SetShow}
     public class BattleUiDebugConsoleHandler : MonoBehaviour
     {
+        /// @anchor BattleUiDebugConsoleHandler-SerializeFields
+        /// @name SerializeField variables
+        /// <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/SerializeField.html">SerializeFields@u-exlink</a> are serialized variables exposed to the Unity editor.
+        /// @{
+
+        /// <summary>[SerializeField] <see cref="UnityReferences"/> struct.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("UnityReferences struct")]
         [SerializeField] private UnityReferences _unity;
 
         [Header("Settings")]
+
+        /// <summary>[SerializeField] The max amount of entries.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("The max amount of entries")]
         [SerializeField] private int _entriesMax;
+
+        /// <summary>[SerializeField] The font size.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("The font size")]
         [SerializeField] private int _entryFontSize;
+
+        /// <summary>[SerializeField] The size of one character including padding.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("The size of one character including padding")]
         [SerializeField] private Vector2 _entryCharacterSize;
+
+        /// <summary>[SerializeField] The max message length.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("The max message length")]
         [SerializeField] private int _entryMessageLengthMax;
+
+        /// <summary>[SerializeField] The lifetime of individual log entries in seconds.</summary>
+        /// @ref BattleUiDebugConsoleHandler-SerializeFields "SerializeFields"
+        [Tooltip("The lifetime of individual log entries in seconds")]
         [SerializeField] private float _entryLifetimeSec;
 
+        /// @}
+
+        /// <summary>
+        /// Sets the %UI element visibility.
+        /// </summary>
+        ///
+        /// <param name="show">If the %UI element should be visible or not.</param>
         public void SetShow(bool show)
         {
             _isVisible = show;
 
-            UpdateTextField();
+            UpdateUI();
         }
 
+        /// <summary>Private struct used internally to hold references to Unity components.</summary>
         [Serializable]
         private struct UnityReferences
         {
+            /// <summary>Root RectTransform component of the debug console.</summary>
+            [Tooltip("Root RectTransform component of the debug console")]
             public RectTransform RectTransform;
+
+            /// <summary>TMP_Text component of the debug console textfield.</summary>
+            [Tooltip("TMP_Text component of the debug console textfield")]
             public TMP_Text TextField;
         }
 
+        /// <summary>Private struct used internally to hold the data of an entry.</summary>
         private struct Entry
         {
+            /// <summary>Type of the log entry.</summary>
             public LogType Type;
-            public string  Source;
-            public string  Message;
-            public float   Timestamp;
+
+            /// <summary>Source of the log entry.</summary>
+            public string Source;
+
+            /// <summary>Message of the log entry.</summary>
+            public string Message;
+
+            /// <summary>Lifetime of the log entry.</summary>
+            public float Timestamp;
         }
 
-        private const string PrefixTypeLog     = "[ <color=#FFFFFF>Log</color> ]";
-        private const string PrefixTypeWarning = "[<color=#FFBF00>Warn</color> ]";
-        private const string PrefixTypeError   = "[<color=#E34234>Error</color>]";
-        private const string PrefixEmpty       = "       ";
+        //{ Prefixes
 
+        /// <summary>Prefix for log entry of type Log.</summary>
+        private const string PrefixTypeLog = "[ <color=#FFFFFF>Log</color> ]";
+
+        /// <summary>Prefix for log entry of type Warning.</summary>
+        private const string PrefixTypeWarning = "[<color=#FFBF00>Warn</color> ]";
+
+        /// <summary>Prefix for log entry of type Error.</summary>
+        private const string PrefixTypeError = "[<color=#E34234>Error</color>]";
+
+        /// <summary>Prefix used for formatting and fallback.</summary>
+        private const string PrefixEmpty = "       ";
+
+        /// <summary>Length of the prefix.</summary>
         private const int PrefixLength = 8;
 
+        //} Prefixes
+
+        //{ Entries array
+        // Entries array is allocated with maximum amount of entries.
+        // Entries will be added at the end of the array (_entriesStartingIndex + _entriesCount).
+        // Entries will be removed from the start of the array (_entriesStartingIndex).
+        // To achieve this without unnecessarily moving the entries around in memory, we keep track of _entriesStartingIndex and _entriesCount.
+        // Expanding the array can be achieved by incrementing the _entriesCount.
+        // Shrinking the array can be achieved by incrementing _entriesStartingIndex and decrementing the _entriesCount.
+        // To make this work the array wraps around from the end to the beginning.
+        // Example: (Array size = 8, _entriesStartingIndex = 5, _entriesCount = 6)
+        // _entries[0] entry 4
+        // _entries[1] entry 5
+        // _entries[2] entry 6 <- _entriesStartingIndex + _entriesCount -1
+        // _entries[3] unused
+        // _entries[4] unused
+        // _entries[5] entry 1 <- _entriesStartingIndex
+        // _entries[6] entry 2
+        // _entries[7] entry 3
+
+        /// <summary>An array of log entries.</summary>
         private Entry[] _entries;
-        private int     _entriesStartingIndex = 0;
-        private int     _entriesCount = 0;
 
-        private bool _isVisible       = false;
-        private bool _updateTextField = false;
+        /// <summary>Starting index for the log entries.</summary>
+        private int _entriesStartingIndex = 0;
 
+        /// <summary>Amount of log entries.</summary>
+        private int _entriesCount = 0;
+
+        //} Entries array
+
+        /// <summary>Is the %UI element visible or not.</summary>
+        private bool _isVisible = false;
+
+        /// <summary>Should %UI be updated or not.</summary>
+        private bool _updateUI = false;
+
+        /// <summary>
+        /// Initializes the link between <see cref="BattleDebugLogger"/>, the size of the debug console, font size and an entry array.
+        /// </summary>
         private void Awake()
         {
             BattleDebugLogger.InitOnScreenConsoleLink(AddLog);
@@ -83,6 +197,16 @@ namespace Battle.View.UI
             }
         }
 
+        /// <summary>
+        /// Used by <see cref="BattleDebugLogger"/> to add logs to debug console.
+        /// </summary>
+        ///
+        /// Formats log entry and adds it to @cref{_entries} array after which @cref{_updateUI} will be set to true to signal that %UI needs to be updated.<br/>
+        /// See @cref{UpdateUI}
+        ///
+        /// <param name="type">Type of the log entry.</param>
+        /// <param name="source">Source of the log entry.</param>
+        /// <param name="message">Message of the log entry.</param>
         private void AddLog(LogType type, string source, string message)
         {
             if (_entriesCount == _entriesMax)
@@ -120,9 +244,15 @@ namespace Battle.View.UI
             _entries[newIndex].Message   = message;
             _entries[newIndex].Timestamp = Time.time;
 
-            _updateTextField = true;
+            _updateUI = true;
         }
 
+        /// <summary>
+        /// Private <a href="https://docs.unity3d.com/2022.3/Documentation/ScriptReference/MonoBehaviour.Update.html">Update@u-exlink</a> method.
+        /// Checks if a log has ran out of lifetime and if %UI should be updated.
+        /// </summary>
+        ///
+        /// See @cref{UpdateUI}
         private void Update()
         {
             while (_entriesCount > 0 && Time.time - _entries[_entriesStartingIndex].Timestamp >= _entryLifetimeSec)
@@ -130,21 +260,29 @@ namespace Battle.View.UI
                 _entriesStartingIndex = (_entriesStartingIndex + 1) % _entriesMax;
                 _entriesCount--;
 
-                _updateTextField = true;
+                _updateUI = true;
             }
 
-            if (_updateTextField)
+            if (_updateUI)
             {
-                UpdateTextField();
+                UpdateUI();
             }
         }
 
-        private void UpdateTextField()
+        /// <summary>
+        /// Handles visibility and updating the debug console text.
+        /// </summary>
+        ///
+        /// Shows and hides the debug console automatically.<br/>
+        /// Builds and sets debug console text based on the current state of the @cref{_entries} array.
+        ///
+        /// See @ref BattleUiDebugConsoleHandler-DetailedDescription "Detailed Description" for more info.
+        private void UpdateUI()
         {
             if (!_isVisible || _entriesCount < 1)
             {
                 _unity.RectTransform.gameObject.SetActive(false);
-                _updateTextField = false;
+                _updateUI = false;
                 return;
             }
 
@@ -164,7 +302,7 @@ namespace Battle.View.UI
 
             _unity.TextField.text = stringBuilder.ToString();
 
-            _updateTextField = false;
+            _updateUI = false;
         }
     }
 }
