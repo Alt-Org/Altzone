6115eeae761d8553a0e1ab73fb7ae6595982022e
tomipir <121516298+tomipir@users.noreply.github.com>
2025-12-15 18:07:14 +0200

    Removed unnecessary color list

--- Assets/MenuUi/Scripts/AvatarEditor/ColorGridLoader.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ColorGridLoader.cs
@@ -12,18 +12,7 @@ namespace MenuUi.Scripts.AvatarEditor
         [SerializeField] private GameObject _gridCellPrefab;
         [SerializeField] private HorizontalLayoutGroup _colorGrid;
         [SerializeField] private Color _backgroundColor = new(0, 0, 0);
-        //Took the colors directly from the screenshot on github
-        [SerializeField]
-        private List<Color> _colors = new List<Color>
-        {
-            new Color(1, 0, 0),
-            new Color(0.996078431372549f, 0.5529411764705883f, 0),
-            new Color(1, 1, 0),
-            new Color(0, 0.5568627450980392f, 0),
-            new Color(0, 0.7529411764705882f, 0.7529411764705882f),
-            new Color(0.25098039215686274f, 0, 0.596078431372549f),
-            new Color(0.5568627450980392f, 0, 0.5568627450980392f)
-        };
+        [SerializeField] private List<Color> _colors;
 
         private float _viewPortHeight;
         private float _cellHeight;
