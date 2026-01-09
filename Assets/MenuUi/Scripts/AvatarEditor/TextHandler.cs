184ae4e96e1a2a156a1623b917a5b8d2742e4cde
tomipir <121516298+tomipir@users.noreply.github.com>
2026-01-09 16:37:54 +0200

    Localised speechbubbletext
    -Localised speech bubble text in the main editor window

--- Assets/MenuUi/Scripts/AvatarEditor/TextHandler.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/TextHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using TMPro;
 using UnityEngine;
 
@@ -8,10 +9,28 @@ namespace MenuUi.Scripts.AvatarEditor
     public class TextHandler : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _text;
-        [SerializeField] private List<string> _phraseList;
+        [SerializeField] private List<string> _finnishPhraseList;
+        [SerializeField] private List<string> _englishPhraseList;
+        private List<string> _phraseList;
 
         public void SetRandomSpeechBubbleText()
         {
+            SettingsCarrier.LanguageType language = SettingsCarrier.Instance.Language;
+
+            if (language == SettingsCarrier.LanguageType.Finnish)
+            {
+                _phraseList = _finnishPhraseList;
+            }
+            else if (language == SettingsCarrier.LanguageType.English)
+            {
+                _phraseList = _englishPhraseList;
+            }
+            else
+            {
+                Debug.LogWarning($"No phraselist for language: {language}");
+                return;
+            }
+
             if (_phraseList.Count == 0)
             {
                 Debug.LogError("Phrase List is empty");
