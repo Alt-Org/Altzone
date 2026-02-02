db44713903e8f624b60324420ffa6e26d766023e
tomipir <121516298+tomipir@users.noreply.github.com>
2026-02-02 18:24:24 +0200

    Added tag script to prefab parts

---
+++ Assets/MenuUi/Scripts/SoulHome/AvatarPart.cs
@@ -0,0 +1,16 @@
+public enum AvatarPart
+{
+    R_Hand,
+    L_Hand,
+    L_Eyebrow,
+    L_Eye,
+    R_Eyebrow,
+    R_Eye,
+    Nose,
+    Mouth,
+    Head,
+    Body,
+    R_Leg,
+    L_Leg,
+    Shadow
+}
---
+++ Assets/MenuUi/Scripts/SoulHome/AvatarPartRenderer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarPartRenderer : MonoBehaviour
+{
+    public AvatarPart Part;
+    public SpriteRenderer spriteRenderer;
+}
