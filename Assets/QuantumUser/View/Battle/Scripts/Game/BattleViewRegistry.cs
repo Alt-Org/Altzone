93c595b7100ed1e0445e88a781b74f525b4943a1
Topi3 <dtoku004@edu.xamk.fi>
2026-02-04 16:58:45 +0200

    BattleViewRegistry: added missing documentation #1110

--- Assets/QuantumUser/View/Battle/Scripts/Game/BattleViewRegistry.cs
+++ Assets/QuantumUser/View/Battle/Scripts/Game/BattleViewRegistry.cs
@@ -1,8 +1,8 @@
-///@file BattleViewRegistry.cs
-///<summary>
-///Contains @cref {Battle.View,BattleViewRegistry} class which handles mapping quantum entities
-///to various objects they are related to.
-///</summary>
+/// @file BattleViewRegistry.cs
+/// <summary>
+/// Contains @cref {Battle.View,BattleViewRegistry} class which handles mapping quantum entities
+/// to various objects they are related to.
+/// </summary>
 
 //system usings
 using System;
@@ -14,14 +14,76 @@ using Quantum;
 namespace Battle.View
 {
     /// <summary>
-    /// Handles mapping quantum entities to various objects they are related to.
+    /// Provides a way to link Quantum entities on the view side of Quantum
     /// </summary>
+    ///
+    /// @anchor BattleViewRegistry-MethodsDoc
+    /// @bigtext{**Methods Overview**}
+    ///
+    /// **Register**<br/>
+    /// The Register method handles mapping an object to the Quantum entity that owns it, and then calls all callbacks it may have mapped beforehand.<br/>
+    ///
+    /// **Example:**<br/>
+    /// ```cs
+    /// public unsafe class BattlePlayerCharacterViewController : QuantumEntityViewComponent
+    /// {
+    /// ...
+    ///     public override void OnActivate(Frame _)
+    ///     {
+    ///         ...
+    ///         BattleViewRegistry.Register(this.EntityRef, this);
+    ///         ...
+    ///     }
+    ///     ...
+    /// }
+    /// ```
+    ///
+    /// **WhenRegistered**<br/>
+    /// The WhenRegistered method handles adding callbacks for a Quantum entity to a list that gets called when the entity is registered.
+    ///
+    /// **Example:**<br/>
+    /// ```cs
+    /// public unsafe class BattlePlayerShieldViewController : QuantumEntityViewComponent
+    /// {
+    /// ...
+    ///     public override void OnActivate(Frame _)
+    ///     {
+    ///         ...
+    ///         BattleViewRegistry.WhenRegistered(characterRef, ExampleCallback);
+    ///         ...
+    ///     }
+    ///     ...
+    ///     private void ExampleCallback()
+    ///     {
+    ///         ...
+    ///     }
+    ///     ...
+    /// }
+    /// ```
+    ///
+    /// **Get methods**<br/>
+    /// The get methods are getter methods that get either a single object or a list of objects registered to a Quantum entity.
+    ///
+    /// **Example:**<br/>
+    /// ```cs
+    /// public unsafe class BattlePlayerShieldViewController : QuantumEntityViewComponent
+    /// {
+    ///     private EntityRef entityRef;
+    ///     private void ExampleMethod()
+    ///     {
+    ///         BattlePlayerCharacterViewController example = BattleViewRegistry.GetObject<BattlePlayerCharacterViewController>(entityRef);
+    ///         List<BattlePlayerCharacterViewController> exampleList = BattleViewRegistry.GetObjects<BattlePlayerCharacterViewController(entityRef);
+    ///     }
+    ///     ...
+    /// }
+    /// ```
     public static class BattleViewRegistry
     {
         /// <summary>
-        /// Public method that handles mapping an object to a quantum entity.<br/>
+        /// Registers an object to a quantum entity.<br/>
         /// If the quantum entity has other objects registered to it, does callbacks to them.
         /// </summary>
+        ///
         /// <param name="entityRef">Quantum entity the object is being mapped to.</param>
         /// <param name="object">object that is being mapped to the quantum entity.</param>
         public static void Register(EntityRef entityRef, object @object)
@@ -41,8 +103,10 @@ namespace Battle.View
         /// <summary>
         /// Helper method for getting an object mapped to a quantum entity.
         /// </summary>
+        ///
         /// <typeparam name="T">type of object trying to be Got.</typeparam>
         /// <param name="entityRef">Quantum Entity being Get from.</param>
+        ///
         /// <returns>Object mapped to entity if it exists.</returns>
         public static T GetObject<T>(EntityRef entityRef) where T : class
         {
@@ -58,8 +122,10 @@ namespace Battle.View
         /// <summary>
         /// Helper method for getting a list of objects mapped to a quantum entity.
         /// </summary>
+        ///
         /// <typeparam name="T">type of objects trying to be Got.</typeparam>
         /// <param name="entityRef">Quantum Entity being Get from.</param>
+        ///
         /// <returns>a list of objects if it exists.</returns>
         public static List<T> GetObjects<T>(EntityRef entityRef) where T : class
         {
@@ -78,6 +144,7 @@ namespace Battle.View
         /// <summary>
         /// Public method that handles adding callbacks for a quantum entity to a list.
         /// </summary>
+        ///
         /// <param name="entityRef">Quantum entity callback needs to be mapped to.</param>
         /// <param name="callback">Callback that needs to be mapped to the quantum entity.</param>
         public static void WhenRegistered(EntityRef entityRef, Func<bool> callback)
@@ -143,11 +210,31 @@ namespace Battle.View
         }
 
         /// <summary>
-        /// Dictionary that holds the object that is mapped to each quantum entity.
+        /// Helper method for getting an entry mapped to a Quantum entity if it exists.
         /// </summary>
+        ///
+        /// <param name="entityRef">Quantum entity trying to be Get from.</param>
+        /// <param name="entry">Entry that is returned.</param>
+        ///
+        /// <returns>An entry mapped to the Quantum entity if it exists.</returns>
+        private static bool TryGetEntry(EntityRef entityRef, out RegistryEntry entry)
+        {
+            return _registryMap.TryGetValue(entityRef, out entry);
+        }
 
         /// <summary>
-        /// Dictionary that holds a list of callbacks being mapped to each quantum entity.
+        /// Helper method for handling callbacks.
         /// </summary>
+        /// <param name="callbacks">List of callbacks that need to be handled.</param>
+        private static void HandleCallbacks(List<RegistryCallback> callbacks)
+        {
+            for (int i = 0; i < callbacks.Count; i++)
+            {
+                RegistryCallback callback = callbacks[i];
+                if (callback.Handled) continue;
+                callback.Handled = callback.Callback();
+                callbacks[i] = callback;
+            }
+        }
     }
 }
--- Doc/Doxygen/Battle/additional-documentation/pages/1-concepts-view-registry.md
+++ Doc/Doxygen/Battle/additional-documentation/pages/1-concepts-view-registry.md
@@ -5,6 +5,7 @@
 ## View Registry {#page-concepts-view-registry-view-registry}
 
 The @cref{Battle.View,BattleViewRegistry} handles mapping **%Quantum entities** to any **Objects** related to the entities.<br/>
-**Objects** are mapped to a given **%Quantum entity** using @cref{Battle.View.BattleViewRegistry,Register(EntityRef\, object)}.<br/>
-**Objects** are retrieved using @cref{Battle.View.BattleViewRegistry,GetObject(EntityRef)} or @cref{Battle.View.BattleViewRegistry,GetObjects(EntityRef)} depending on if multiple objects need to be retrieved or not.<br/>
-If an **Object** needs another **Object** related to a different **%Quantum entity** to function and it isnt known if the object has been registered to this system yet or not, the @cref{Battle.View.BattleViewRegistry,WhenRegistered(EntityRef\, Action)} method needs to be used. <br/>
+The point of this class is to link **%Quantum entities** to each other on the view side of Quantum.<br/>
+**Objects** are mapped to a given **%Quantum entity** using @cref{Battle.View.BattleViewRegistry,Register}.<br/>
+**Objects** are retrieved using @cref{Battle.View.BattleViewRegistry,GetObject<T>} or @cref{Battle.View.BattleViewRegistry,GetObjects<T>} depending on if multiple objects need to be retrieved or not.<br/>
+If an **Object** needs another **Object** related to a different **%Quantum entity** to function and it isnt known if the object has been registered to this system yet or not, the @cref{Battle.View.BattleViewRegistry,WhenRegistered} method needs to be used. <br/>
