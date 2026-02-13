ca076042aad40d9ef4f193b8d140b9cec17d586a
tomipir <121516298+tomipir@users.noreply.github.com>
2026-02-13 16:47:04 +0200

    Moved resolverdictionary to its own script

--- Assets/MenuUi/Scripts/SoulHome/AvatarRig.cs
+++ Assets/MenuUi/Scripts/SoulHome/AvatarRig.cs
@@ -11,109 +11,6 @@ public class AvatarRig : MonoBehaviour
 
     public Dictionary<AvatarPart, SpriteResolver> Resolvers { get { return _resolvers; } }
 
-    private readonly Dictionary<AvatarPart, AvatarPartSetter.AvatarResolverStruct> _resolverDictionary = new()
-        {
-            {
-                AvatarPart.Body,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Clothes,
-                    _category = "Body",
-                    _suffix = "",
-                }
-            },
-            {
-                AvatarPart.R_Hand,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Hands,
-                    _category = "Hands",
-                    _suffix = "R"
-                }
-            },
-            {
-                AvatarPart.L_Hand,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Hands,
-                    _category = "Hands",
-                    _suffix = "L"
-                }
-            },
-            {
-                AvatarPart.L_Eyebrow,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Eyes,
-                    _category = "Eyebrows",
-                    _suffix = "L"
-                }
-            },
-            {
-                AvatarPart.L_Eye,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Eyes,
-                    _category ="Eyes",
-                    _suffix = "L"
-                }
-            },
-            {
-                AvatarPart.R_Eyebrow,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Eyes,
-                    _category = "Eyebrows",
-                    _suffix = "R"
-                }
-            },
-            {
-                AvatarPart.R_Eye,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Eyes,
-                    _category = "Eyes",
-                    _suffix = "R"
-                }
-            },
-            {
-                AvatarPart.Nose,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Nose,
-                    _category = "Nose",
-                    _suffix = ""
-                }
-            },
-            {
-                AvatarPart.Mouth,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Mouth,
-                    _category = "Mouth",
-                    _suffix = ""
-                }
-            },
-            {
-                AvatarPart.R_Leg,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Feet,
-                    _category = "Legs",
-                    _suffix= "R"
-                }
-            },
-            {
-                AvatarPart.L_Leg,
-                new AvatarPartSetter.AvatarResolverStruct
-                {
-                    _dataGetter = data => data.Feet,
-                    _category = "Legs",
-                    _suffix = "L"
-                }
-            },
-        };
-
     private void Awake()
     {
         _resolvers = new Dictionary<AvatarPart, SpriteResolver>();
@@ -133,7 +30,7 @@ public class AvatarRig : MonoBehaviour
     {
         foreach ((AvatarPart part, SpriteResolver resolver) in _resolvers)
         {
-            if (!_resolverDictionary.TryGetValue(part, out AvatarPartSetter.AvatarResolverStruct resolverStruct))
+            if (!AvatarResolverDictionary.Instance.TryGetValue(part, out AvatarPartSetter.AvatarResolverStruct resolverStruct))
             {
                 continue;
             }
