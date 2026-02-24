cd06ad101bf2aa0bb94f4fa22eb5658b2930a262
tomipir <121516298+tomipir@users.noreply.github.com>
2026-02-24 14:07:55 +0200

    Multiple changes
    - Added a trasnparent texture
    - modified script
    - modified gameobjects

--- Assets/MenuUi/Scripts/AvatarEditor/ColorPicker.cs
+++ Assets/MenuUi/Scripts/AvatarEditor/ColorPicker.cs
@@ -12,10 +12,33 @@ public class ColorPicker : MonoBehaviour
     [SerializeField] private ScrollBarFeatureLoader _featureLoader;
     [SerializeField] private AvatarEditorCharacterHandle _characterHandle;
     [SerializeField] private AvatarEditorController _controller;
+    [SerializeField] private Button _clickAreaButton;
+    [SerializeField] private Button _popupConfirmButton;
+    [SerializeField] private GameObject _colorPickerPopup;
+    [SerializeField] private Image _previewColor;
+    [SerializeField] private Image _colorPickerColorButton;
+    [SerializeField] private Image _sliderBackground;
+
 
     private void Start()
     {
         _colorApplyButton.onClick.AddListener(() => AddListener(Color.red));
+        //_clickArea.SetActive(false);
+        _popupConfirmButton.onClick.AddListener(() => ClosePopup());
+        _clickAreaButton.onClick.AddListener(() => ClosePopup());
+    }
+
+    private void ClosePopup()
+    {
+        _clickAreaButton.gameObject.SetActive(false);
+        _colorPickerPopup.SetActive(false);
+    }
+
+    private void UpdateColor(Color color)
+    {
+        _previewColor.color = color;
+        _colorPickerColorButton.color = color;
+        _sliderBackground.color = color;
     }
 
     private void AddListener(Color color)
