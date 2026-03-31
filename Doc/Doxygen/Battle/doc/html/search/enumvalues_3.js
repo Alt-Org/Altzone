eaf591bf63a5b65c51d21047650203fddaeee77e
Niklas <niklas.tontti603619@gmail.com>
2026-03-31 11:43:19 +0300

    Battle: Updated generated documentation

--- Doc/Doxygen/Battle/doc/html/search/enumvalues_3.js
+++ Doc/Doxygen/Battle/doc/html/search/enumvalues_3.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['dead_0',['Dead',['../namespace_quantum.html#ac833707607fdef72ea1658c65f5bda73a183b62c7f067711f9c5a54913c054617',1,'Quantum']]],
-  ['desensitizer_1',['Desensitizer',['../namespace_quantum.html#abe9d5ee206f2ff73cc7fee5c92b6bc2eae4cee4a275173aeb30e9a993221a9333',1,'Quantum']]],
-  ['diamondpickup_2',['DiamondPickUp',['../namespace_quantum.html#ab1787f1ce82c5d51193c9f7ca86a8b4ba7469e4c681361b31a7beb09a35552a7a',1,'Quantum']]],
-  ['direction_3',['Direction',['../namespace_quantum.html#a640ca4a7203626a46d94c4fa1d63db1da02674a4ef33e11c879283629996c8ff8',1,'Quantum']]]
+  ['death_1',['Death',['../class_battle_1_1_q_simulation_1_1_player_1_1_battle_player_q_system.html#a6b78991c2d62a075bf74bb863046fca1a6097f89e3092d4ccd249c9d479785c1f',1,'Battle::QSimulation::Player::BattlePlayerQSystem']]],
+  ['desensitizer_2',['Desensitizer',['../namespace_quantum.html#abe9d5ee206f2ff73cc7fee5c92b6bc2eae4cee4a275173aeb30e9a993221a9333',1,'Quantum']]],
+  ['diamondpickup_3',['DiamondPickUp',['../namespace_quantum.html#ab1787f1ce82c5d51193c9f7ca86a8b4ba7469e4c681361b31a7beb09a35552a7a',1,'Quantum']]],
+  ['direction_4',['Direction',['../namespace_quantum.html#a640ca4a7203626a46d94c4fa1d63db1da02674a4ef33e11c879283629996c8ff8',1,'Quantum']]]
 ];
