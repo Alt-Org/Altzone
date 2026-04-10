4c1426eb85c050ccd8a28bbcc8ee8d2ddedb348b
Niko Mattila <niko.mattila@pp.inet.fi>
2026-04-10 14:46:32 +0300

    Cleaned the LanguageSelector code.
    Also re-added the old language selector since it is used in that form in the initial language selection.

--- Assets/MenuUi/Scripts/Loader/LanguageSelectorHandler.cs
+++ Assets/MenuUi/Scripts/Loader/LanguageSelectorHandler.cs
@@ -14,38 +14,27 @@ public class LanguageSelectorHandler : MonoBehaviour
     [SerializeField] private Button _increase;
 
     [SerializeField] private WindowNavigation _navigation;
-    [SerializeField] private int _buttonValue;
     [SerializeField] private Image _flagHolder;
     [SerializeField] private Sprite[] _flagsImages;
 
     // Start is called before the first frame update
     void Start()
     {
-        _decrease.onClick.AddListener(() => SetLanguage());
-        _increase.onClick.AddListener(() => SetLanguage());
+        _decrease.onClick.AddListener(() => SetLanguage(-1));
+        _increase.onClick.AddListener(() => SetLanguage(1));
 
 
         _flagHolder.sprite = _flagsImages[(int)SettingsCarrier.Instance.Language];
 
     }
 
-    private void OnDestroy()
-    {
-    }
-
-    public void ChangeValue(int amount)
-    {
-        _buttonValue = amount;
-    }
-
-
-    private void SetLanguage()
+    private void SetLanguage(int value)
     {
 
         ///RN the code is like this because other languages do not work as is now but when they do just modify the <see cref="int max"/>.Length -2 into -1 and the index = 1 into 0 instead
         int index = (int)carrier.Language;
         int max = (int)(SettingsCarrier.LanguageType)Enum.GetValues(typeof(SettingsCarrier.LanguageType)).Length - 2;
-        index += _buttonValue;
+        index += value;
 
         if (index > max)
         {
