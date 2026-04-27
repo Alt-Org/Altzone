d82fde21696b28d7be4caede2b262805866e5c62
tomipir <121516298+tomipir@users.noreply.github.com>
2026-04-27 18:45:37 +0300

    CommandActivateAbility.cs: Added documentation #1469

--- Assets/QuantumUser/Simulation/Battle/Scripts/Player/Commands/CommandActivateAbility.cs
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Player/Commands/CommandActivateAbility.cs
@@ -1,13 +1,30 @@
+/// @file CommandActivateAbility.cs
+/// <summary>
+/// A deterministic command that triggers the ability activation for a specific player.
+/// </summary>
+
 // Quantum usings
 using Quantum;
 using Photon.Deterministic;
 
 namespace Battle.QSimulation.Player
 {
+    /// <summary>
+    /// A deterministic command that triggers the ability activation for a specific player.
+    /// </summary>
     public unsafe class CommandActivateAbility : DeterministicCommand
     {
+        /// <summary>
+        /// Method that is required to be implemented but not needed here.
+        /// </summary>
         public override void Serialize(BitStream stream) { }
 
+        /// <summary>
+        /// Executes the ability activation logic by setting an input buffer for the specified player.
+        /// </summary>
+        ///
+        /// <param name="f">The current simulation frame.</param>
+        /// <param name="playerHandle">The player handle of the player initiating the command.</param>
         public void Execute(Frame f, BattlePlayerManager.PlayerHandle playerHandle)
         {
             BattlePlayerDataQComponent* playerData = f.Unsafe.GetPointer<BattlePlayerDataQComponent>(playerHandle.SelectedCharacterEntity);
