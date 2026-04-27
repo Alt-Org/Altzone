28fb143e4bf8cb19bdfb6a418e61cdae384693c7
tomipir <121516298+tomipir@users.noreply.github.com>
2026-04-27 18:45:08 +0300

    CommandSetup.User.cs: Added documentation #1469

--- Assets/QuantumUser/Simulation/CommandSetup.User.cs
+++ Assets/QuantumUser/Simulation/CommandSetup.User.cs
@@ -1,4 +1,9 @@
-﻿// System usings
+﻿/// @file CommandSetup.User.cs
+/// <summary>
+/// Registers the game's custom deterministic commands with the %Quantum simulation.
+/// </summary>
+
+// System usings
 using System.Collections.Generic;
 using Battle.QSimulation.Player;
 
@@ -7,8 +12,20 @@ using Photon.Deterministic;
 
 namespace Quantum
 {
+    /// <summary>
+    /// Contains all project-specific commands. %Quantum invokes the generated side of this partial class at startup.
+    /// </summary>
     public static partial class DeterministicCommandSetup
     {
+        /// <summary>
+        /// Registers the game's custom <see cref="IDeterministicCommandFactory"/> instances with
+        /// the %Quantum command system. Called once by the framework during simulation
+        /// initialization. Should not be called manually.
+        /// </summary>
+        ///
+        /// <param name="factories">The command factory collection to register commands into</param>
+        /// <param name="gameConfig">The runtime game configuration for the current session</param>
+        /// <param name="simulationConfig">The simulation configuration for the current session</param>
         static partial void AddCommandFactoriesUser(ICollection<IDeterministicCommandFactory> factories, RuntimeConfig gameConfig, SimulationConfig simulationConfig)
         {
             // Add or remove commands to the collection.
