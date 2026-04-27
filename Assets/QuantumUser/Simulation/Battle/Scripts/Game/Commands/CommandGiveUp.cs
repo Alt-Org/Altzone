e3985f631f9d124b06ceb9d6581334123f79940f
tomipir <121516298+tomipir@users.noreply.github.com>
2026-04-27 18:45:53 +0300

    CommandGiveUp.cs: Added documentation #1469

--- Assets/QuantumUser/Simulation/Battle/Scripts/Game/Commands/CommandGiveUp.cs
+++ Assets/QuantumUser/Simulation/Battle/Scripts/Game/Commands/CommandGiveUp.cs
@@ -1,13 +1,30 @@
+/// @file CommandGiveUp.cs
+/// <summary>
+/// A deterministic command that triggers the give up logic for a specific player.
+/// </summary>
+
 // Quantum usings
 using Quantum;
 using Photon.Deterministic;
 
 namespace Battle.QSimulation.Player
 {
+    /// <summary>
+    /// A deterministic command that triggers the give up logic for a specific player.
+    /// </summary>
     public class CommandGiveUp : DeterministicCommand
     {
+        /// <summary>
+        /// Method that is required to be implemented but not needed here.
+        /// </summary>
         public override void Serialize(BitStream stream) { }
 
+        /// <summary>
+        /// Executes the give up logic for the specified player.
+        /// </summary>
+        ///
+        /// <param name="f">The current simulation frame</param>
+        /// <param name="playerHandle">The player handle of the player initiating the command.</param>
         public void Execute(Frame f, BattlePlayerManager.PlayerHandle playerHandle)
         {
             BattlePlayerQSystem.HandleGiveUp(f,  playerHandle);
