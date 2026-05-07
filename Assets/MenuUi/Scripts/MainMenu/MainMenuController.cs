e29020ad013075e766918301d61f04ae7cf0846d
Niko Mattila <niko.mattila@pp.inet.fi>
2026-05-07 13:48:02 +0300

    Fixed the initial state of the selected button.

--- Assets/MenuUi/Scripts/MainMenu/MainMenuController.cs
+++ Assets/MenuUi/Scripts/MainMenu/MainMenuController.cs
@@ -81,6 +81,7 @@ namespace MenuUi.Scripts.MainMenu
                 SetMainMenuLayoutDimensions();
             }
             AudioManager.Instance.UpdateMaxVolume();
+            OverlayPanelCheck.Instance?.UpdateButtonContent();
         }
 
         private void OnDestroy()
