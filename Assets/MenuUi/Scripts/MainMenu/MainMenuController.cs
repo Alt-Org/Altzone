051537bf242f7e54a729e53e396fb6cbca5333a6
Niko Mattila <niko.mattila@pp.inet.fi>
2026-05-10 23:21:54 +0300

    Removed some old code.

--- Assets/MenuUi/Scripts/MainMenu/MainMenuController.cs
+++ Assets/MenuUi/Scripts/MainMenu/MainMenuController.cs
@@ -23,7 +23,6 @@ namespace MenuUi.Scripts.MainMenu
         private int lastWidth;
         private int lastHeight;
 
-        private SetVolume[] audioSources;
         private SettingsCarrier carrier = SettingsCarrier.Instance;
 
         [Header("TurboEducation")]
