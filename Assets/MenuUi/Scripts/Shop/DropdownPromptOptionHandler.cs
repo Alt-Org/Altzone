530224bd9954c82e2a6dcf94f8a5a8b51f80d054
Davetsa <taavi.alasalmi@gmail.com>
2026-07-14 17:19:26 +0300

    Refine dropdown prompt options and add outline styling

--- Assets/MenuUi/Scripts/Shop/DropdownPromptOptionHandler.cs
+++ Assets/MenuUi/Scripts/Shop/DropdownPromptOptionHandler.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DropdownPromptOptionHandler : MonoBehaviour
 {
@@ -8,40 +7,8 @@ public class DropdownPromptOptionHandler : MonoBehaviour
     [SerializeField] private TMP_Text _captionLabel;
     [SerializeField] private string _promptText;
 
-    private bool _promptOptionHidden;
-
-    private void Update()
-    {
-        if (!_dropdown.IsExpanded)
-        {
-            _promptOptionHidden = false;
-            return;
-        }
-
-        if (_promptOptionHidden)
-            return;
-
-        Transform dropdownList = _dropdown.transform.Find("Dropdown List");
-        if (dropdownList == null)
-            return;
-
-        Toggle[] items = dropdownList.GetComponentsInChildren<Toggle>(false);
-        if (items.Length == 0 || items[0].transform is not RectTransform promptItem)
-            return;
-
-        RectTransform content = promptItem.parent as RectTransform;
-        float promptItemHeight = promptItem.rect.height;
-        promptItem.gameObject.SetActive(false);
-
-        if (content != null)
-            content.sizeDelta = new Vector2(content.sizeDelta.x, Mathf.Max(0f, content.sizeDelta.y - promptItemHeight));
-
-        _promptOptionHidden = true;
-    }
-
     public void ResetPrompt()
     {
-        _promptOptionHidden = false;
         _dropdown.SetValueWithoutNotify(0);
         _dropdown.RefreshShownValue();
 
