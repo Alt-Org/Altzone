084d0e6ae99e78a347b7e6657dbe41fe068d8064
Emilia Pesonen <fempe004@edu.xamk.fi>
2026-08-04 16:50:19 +0300

    Defence gallery tabline buttons
    Added the filter and loadout buttons to tab line, linked the loadout button and modified the layout of the loadout prefab.

--- Assets/MenuUi/Scripts/DefenceScreen/CharacterGallery/LoadoutPopUpController.cs
+++ Assets/MenuUi/Scripts/DefenceScreen/CharacterGallery/LoadoutPopUpController.cs
@@ -2,20 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class LoadoutPopUpController : MonoBehaviour
 {
 
+
+    [SerializeField] private GameObject _loadoutPopUp;
+    [SerializeField] private Button _openLoadoutPopUpButton1;
+    [SerializeField] private Button _openLoadoutPopUpButton2;
+    [SerializeField] private Button _openLoadoutPopUpButton3;
+
+
+    private void Start()
+    {
+        _openLoadoutPopUpButton1.onClick.AddListener(TabOpen);
+        _openLoadoutPopUpButton2.onClick.AddListener(TabOpen);
+        _openLoadoutPopUpButton3.onClick.AddListener(TabOpen);
+    }
+
     public void Open()
     {
-        gameObject.SetActive(true);
-       
+        _loadoutPopUp.SetActive(true);    
     }
 
     public void Close()
     {
-        gameObject.SetActive(false);
-        
+        _loadoutPopUp.SetActive(false);        
+    }
+
+    private void TabOpen()
+    {
+        if (!_loadoutPopUp.activeSelf)
+        {
+            Open();
+        }
+        else if (_loadoutPopUp.activeSelf)
+        {
+            Close();
+        }
     }
 }
 
