1f7b7bc3342a3ff54fbc9be0b1f21fba6c0a12ce
PerttuR <54833434+PerttRuo@users.noreply.github.com>
2026-09-07 12:34:54 +0300

    AdPoster size fix and KojuContent slot fix

--- Assets/MenuUi/Prefabs/Windows/ClanStall/ItemMover.cs
+++ Assets/MenuUi/Prefabs/Windows/ClanStall/ItemMover.cs
@@ -88,7 +88,7 @@ public class ItemMover : MonoBehaviour
             // Move from tray to panel
             foreach (var slot in panelSlots)
             {
-                if (slot.transform.GetSiblingIndex() == 0) continue; // Skips the slot meant for the poster card
+                //if (slot.transform.GetSiblingIndex() == 0) continue; // Skips the slot meant for the poster card
 
                 if (!slot.IsOccupied)
                 {
