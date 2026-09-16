4c9347a3f1902bdf6f2c4d317f79c6201560fb84
Niko Mattila <niko.mattila@pp.inet.fi>
2026-09-16 18:29:02 +0300

    Moved the IntialSelection panel data settings into proper script.
    Previous structure was pretty questionable.

--- Assets/MenuUi/Prefabs/Introduction/IntroductionCharacterChoice/IntroCharacters.cs
+++ Assets/MenuUi/Prefabs/Introduction/IntroductionCharacterChoice/IntroCharacters.cs
@@ -10,7 +10,7 @@ using UnityEngine.UI;
 public class IntroCharacters : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> characterCards;
+    private List<CharacterThumbnailHandler> characterCards;
 
     private List<int> characterIDs = new List<int>();
 
@@ -27,31 +27,19 @@ public class IntroCharacters : MonoBehaviour
 
         int j = 0;                     //for going through the classtype list
         AvatarReference avatarreference= AvatarReference.Instance;
-        foreach (GameObject characterCard in characterCards)
+        foreach (CharacterThumbnailHandler characterCard in characterCards)
         {
             j++;
 
             CharacterClassType classType = (CharacterClassType)characterIDs[j]; //get classtype from list
 
-            characterCard.GetComponent<Image>().color = avatarreference.GetColour(classType); //change card color
-
-            CharacterThumbnailHandler characterThumbnailHandler = characterCard.GetComponent<CharacterThumbnailHandler>(); // get correct thumbnailhandler 
-
-            Image characterSprite = characterThumbnailHandler._characterSprite;
-            characterSprite.sprite = avatarreference.GetCharacterSprite(classType); //set character sprite
-
-            AvatarData data = new(avatarreference.GetDefaultAvatar(classType));
-            AvatarVisualData visualData = AvatarDesignLoader.Instance.CreateAvatarVisualData(data);
-            characterThumbnailHandler._avatarLoader.UpdateVisuals(visualData); //set avatar design
-
-
-            TextMeshProUGUI nameSprite = characterThumbnailHandler._nameText; //find name-child
-            nameSprite.text = avatarreference.GetName(classType); //set name sprite
-            nameSprite.color = avatarreference.GetColour(classType);
-
-            characterThumbnailHandler._selectButton.onClick.AddListener(() => { OnInitialAvatarSelection?.Invoke(classType); });
+            characterCard.SetData(classType, SelectInitialAvatar);
         }
     }
 
+    private void SelectInitialAvatar(CharacterClassType classType)
+    {
+        OnInitialAvatarSelection?.Invoke(classType);
+    }
 
 }
